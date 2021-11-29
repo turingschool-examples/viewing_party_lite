32e4483c48d6cf1804b56b7619844eac5ec9eb9e
Philip Medlin <phmedlin@gmail.com>
2021-11-29 16:37:12 -0700

    Create UserParty,ViewingParty: model,rspec,migration

---
+++ db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_11_29_232548) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "user_parties", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "viewing_party_id"
+    t.index ["user_id"], name: "index_user_parties_on_user_id"
+    t.index ["viewing_party_id"], name: "index_user_parties_on_viewing_party_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+  end
+
+  create_table "viewing_parties", force: :cascade do |t|
+    t.string "movie_name"
+    t.integer "duration"
+    t.time "start_time"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_viewing_parties_on_user_id"
+  end
+
+  add_foreign_key "user_parties", "users"
+  add_foreign_key "user_parties", "viewing_parties"
+  add_foreign_key "viewing_parties", "users"
+end
