0d64bdd78ef2fe321a900a18333427a515ecdc65
Haewon Jeon <haewonito@gmail.com>
2021-11-29 23:06:10 -0700

    email uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :parties
   has_many :user_parties
   has_many :parties, through: :user_parties
+
+  validates :email, uniqueness: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe User, type: :model do
     it { should have_many(:parties).through(:user_parties)}
   end
 
+  describe "validations" do
+    it { should validate_uniqueness_of :email }
+  end
+
   describe "class methods" do
     xit "" do
 
