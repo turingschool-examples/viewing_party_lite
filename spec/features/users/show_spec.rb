389c50554b42231f78c486b1f2ceea29c44d1695
matthewjholmes <ilookup@gmail.com>
2021-11-30 14:41:37 -0700

    Dashboard spec

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'user dashboard page' do
+  before do
+    visit registration_path
+    fill_in 'Name', with: 'User 1'
+    fill_in 'Email', with: 'user@email.com'
+    click_button 'Create User'
+
+    @user = User.last
+  end
+
+ it 'displays dashboard sections' do
+   expect(page).to have_content("#{@user.name}'s Dashboard")
+   expect(page).to have_button("Discover Movies")
+ end
+end
