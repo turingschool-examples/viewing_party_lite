a7dbc17b82bf2ba15f3d24674eff06fb9b438d65
Gregory Fischer <gwhoisj@gmail.com>
2021-11-30 15:40:30 -0500

    Feat: add controller set-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
   end
 
   def create
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to user_path(user)
+    else
+      flash[:alert] = "Could not create user"
+      redirect_to new_user_path
+    end
   end
 
-  def movie
+  private
+
+  def user_params
+    params.permit(:name, :email)
   end
 end
