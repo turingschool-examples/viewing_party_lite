8baf29b1a50ad1d82b4b217ed58850cb0caa2ae7
Gregory Fischer <gwhoisj@gmail.com>
2021-11-30 15:33:50 -0500

    Feat: add routes for paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :parties
-  end
+  get '/register', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create', as: 'create_user'
+
+  get 'users/:id', to: 'users#show', as: 'user'
 
 end
