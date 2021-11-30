2feae00a1bb428878126d74ec067cdfd2a44804b
matthewjholmes <ilookup@gmail.com>
2021-11-30 15:58:10 -0700

    Discover movies link

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :users, only: :create
+resources :users, only: :create do
+  resources :discover, only: :index
+end
 
 get '/users/:user_id', to: 'users#show', as: 'dashboard'
 get '/registration', to: 'users#new', as: 'registration'
 
-resources :discover, only: :index
 
 root to: "landing#index"
 end
