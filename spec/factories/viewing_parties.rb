b750e4aab1db2389dcdc31a5f6056016adbaf81d
Alex McConnell <alexmmcconnell@gmail.com>
2021-11-30 14:06:33 -0700

    Fix - Rename viewing party factory

--- spec/factories/viewing_parties.rb
+++ spec/factories/viewing_parties.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :welcome_party do
+  factory :viewing_party do
     movie_name { Faker::Ancient.hero }
     duration { Faker::Number.number(digits: 2) }
     start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+    user
   end
 end
