a4849998fc85150750f4d59a7487c9768a85e2e1
Stephanie Helm <stephaniemhelm@gmail.com>
2021-11-30 20:55:29 -0700

    user dashboard

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MovieService do
-  it "can find popular movies" do
-    require "pry"; binding.pry
+  xit "can find popular movies" do
+    #require "pry"; binding.pry
   expect(MovieService.popular_movies).to eq(Hash)
 
   end
