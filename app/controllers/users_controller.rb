62a5cc8dec627de34446c259ad46effed710ff9d
Alex McConnell <alexmmcconnell@gmail.com>
2021-12-01 10:07:04 -0700

    Feat - Add controller logic for discover and movies pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'faraday'
 
 class UsersController < ApplicationController
   def new; end
@@ -14,6 +15,23 @@ class UsersController < ApplicationController
     else
       flash[:alert] = 'User could not be created'
       redirect_to '/register'
+    end
+  end
+
+  def discover
+    @user = User.find(params[:id])
+  end
+
+  def movies
+    @user = User.find(params[:id])
+    if params[:q] == "top_40_movies"
+      @movies = MovieService.top_40
+    elsif !params[:find_movie].nil?
+      @movies = MovieService.find(params[:find_movie].downcase)
+    else
+      redirect_to "/users/#{params[:id]}/discover"
+      binding.pry
+      # flash "please select a valid input"
     end
   end
 
