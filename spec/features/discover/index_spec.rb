cbd73890eda11f15424ff66582da1959ba0253e5
Haewon Jeon <haewonito@gmail.com>
2021-12-01 17:06:13 -0700

    test: user discover page

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -1,12 +1,21 @@
 require "rails_helper"
 
-RSpec.describe "", type: :feature do
+RSpec.describe "User's Discover Page", type: :feature do
   describe "" do
     before(:each) do
-
+      @user = User.create!(name: "Sean Morris", email: "seanmorris@gmail.com")
+      visit user_discover_index_path(@user)
     end
 
-    it "" do
+    xit "has a button to find top rated movies" do
+      click_button "Find Top Rated Movies"
+      expect(current_path).to eq("/users/#{@user.id}/movies?q=top%20rated")
+    end
+
+    xit "has a text field to enter keyword(s) to search by movie title and a search button" do
+      fill_in "Search by movie title", with: "Pulp"
+      click_button "Find Movies"
+      expect(current_path).to eq("/users/#{@user.id}/movies?q=Pulp")
     end
   end
 end
