85b389cb7d4e86c6e840e84fa34385b15398e379
Stephanie Helm <stephaniemhelm@gmail.com>
2021-12-03 09:43:56 -0700

    fix vcr bugs

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,5 @@
 class MoviesController < ApplicationController
 
-  def show
-    @user = User.find(params[:user_id])
-  end
-
   def new
     @user = User.find(params[:id])
   end
@@ -14,9 +10,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    # movie_id = 58089 #params[:movie_id]
-    # @movie_data = MovieFacade.get_movie_details(params[:movie_id])
+    @user = User.find(params[:user_id])
     @movie = MovieFacade.get_movie_details(params[:id])
-
   end
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'movie show page' do
-  it "has a has a link to take you to the Discover Page" do
-      VCR.use_cassette('popular_movies', re_record_interval: 30.days) do
+  it "has a has a link to take you to the Discover Page", :vcr do
+
       user = User.create!(name: "Stephanie", email: "steph127@hotmail.com")
-      movie = Movie.new(original_title: "Red Notice", vote_average: 6.8, id: 512195)
+      movie = Movie.new(movie_data)
 
       visit "/users/#{user.id}/movies/#{movie.id}"
 
       click_on('Discover Page')
 
       expect(current_path).to eq("/users/#{user.id}/movies")
-    end
   end
 end
