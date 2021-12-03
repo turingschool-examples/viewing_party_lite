12b2710776d898f94cd7435cefe6e5bd96bd16a9
Haewon Jeon <haewonito@gmail.com>
2021-12-03 11:42:02 -0700

    us2 still working

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -6,17 +6,18 @@ class PartiesController < ApplicationController
   end
 
   def create
+
     user = User.find(params[:user_id])
     movie = MovieFacade.movie_details(params[:movie_id])
     start_time = ((params[:start_time] + " " + params[:day]).to_datetime).to_s
     @users = User.all
+        require "pry"; binding.pry
     if params[:party_duration].to_i >= movie.runtime
       @party = Party.create(user_id: user.id, movie_id: movie.id, start_time: start_time, duration: params[:party_duration])
       redirect_to dashboard_path(user)
-    else flash[:not_enough_time] = "That's not enough time for the movie. Give it more time!"
-      redirect_to new_user_movie_party_path
+    else flash[:alert] = "That's not enough time for the movie. Give it more time!"
+      redirect_to new_user_movie_party_path(user, movie.id)
     end
-require "pry"; binding.pry
     if (@party and params[:party_invitees])
 
       params[:party_invitees].each do |invitee|
