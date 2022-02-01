25e01ef698c33dea66115e1cd202e4043c880a48
Eldridge Turambi <eturambi95@gmail.com>
2022-01-31 20:09:01 -0700

    Add status attribute for party_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_014321) do
+ActiveRecord::Schema.define(version: 2022_02_01_030752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_014321) do
     t.bigint "party_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["party_id"], name: "index_party_users_on_party_id"
     t.index ["user_id"], name: "index_party_users_on_user_id"
   end
