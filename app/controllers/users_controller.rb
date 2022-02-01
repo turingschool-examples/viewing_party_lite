1ea49d0d492cccdc52dce47b465d087dbd8102ae
Robin Dittrich <robinodittrich@gmail.com>
2022-02-01 11:46:16 -0800

    Add index, new, create, and user_params actions to user controller.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to user_path(user.id)
+  end
+
+private
+  def user_params
+    params.permit(:name, :email)
+  end
+end
