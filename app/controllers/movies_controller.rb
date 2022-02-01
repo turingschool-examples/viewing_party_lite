929af48a23b424d3c29e30c4508eac437cf5168a
Robin Dittrich <robinodittrich@gmail.com>
2022-02-01 14:00:43 -0800

    Set up test API call.

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,10 @@
+class MoviesController < ApplicationController
+  def index
+    # movie = params[:movie]
+    # conn = Faraday.get("https://api.themoviedb.org")
+    # binding.pry
+    # response = conn.get("/3/movie/550?#{movie}")
+    # data = JSON.parse(response.body, symbolize_names: true)
+    # @movie = Movie.new(data)
+  end
+end
