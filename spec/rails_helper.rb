0947ed4d32d6f61d1ca5b25641c2336079f6c955
Kevin Gloss <kgloss89@gmail.com>
2022-02-01 11:06:27 -0700

    Add vcr configure block to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,3 +69,10 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('movies_api_key') { ENV['govt_api_key']}
+  config.configure_rspec_metadata!
+end
