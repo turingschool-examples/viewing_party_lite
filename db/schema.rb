739be700823e26a7a6dfb6fe16aa54b9d6837138
Robin Dittrich <robinodittrich@gmail.com>
2022-01-31 16:56:33 -0800

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_003439) do
+ActiveRecord::Schema.define(version: 2022_02_01_004204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_02_01_003439) do
     t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_parties_on_user_id"
   end
 
   create_table "user_parties", force: :cascade do |t|
@@ -40,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_003439) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "parties", "users"
   add_foreign_key "user_parties", "parties"
   add_foreign_key "user_parties", "users"
 end
