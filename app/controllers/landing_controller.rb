5265bc1f25b3c59e01ebc6889b5b6f2dd401a443
Kevin Gloss <kgloss89@gmail.com>
2022-02-01 13:00:32 -0700

    Remove landing page and combine with user index

--- app/controllers/landing_controller.rb
+++
@@ -1,5 +0,0 @@
-class LandingController < ApplicationController
-    def index
-        @users = User.all
-    end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,30 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+    
     def new
         @user = User.new
     end
 
+    def show 
+        @user = User.find(params[:id])
+    end
+
     def create
-        binding.pry
+        # binding.pry
+        user = User.new(user_params)
+
+        if user.save 
+            redirect_to model: user 
+        else
+            flash[:error] = user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
+    private 
+    def user_params 
+        params.require(:user).permit(:name, :email)
     end
 end
