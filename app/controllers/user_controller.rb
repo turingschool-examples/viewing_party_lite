2bfc2fcd688f2f75f83ff96bef84ba36dd2a7c1b
Karan Mehta <karanm645@gmail.com>
2022-02-01 17:42:48 -0600

    Create new and create action

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,19 @@
 class UserController < ApplicationController 
   def show 
-
+    @user = User.find(params[:id])
   end
 
   def new 
-    
+
+  end
+
+  def create
+    user = User.create!(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+  def user_params
+    params.permit(:name, :email)    
   end
 end
