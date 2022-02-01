4dfd92488cf71a24b061fa5fdaf6218377d015c8
Eric Chon <echon92@gmail.com>
2022-02-01 13:48:23 -0700

    partial for user dashboard ticket

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe 'User show page' do
     user_1 = User.create!(name: 'User 1', email: 'email1@gmail.com')
     visit user_path(user_1)
     expect(page).to have_content("#{user_1.name}'s Dashboard")
+  end
 
+  it "checks for a button to discover movies " do
+    user_1 = User.create!(name: 'User 1', email: 'email1@gmail.com')
+    visit user_path(user_1)
+
+    click_button "Discover Movies"
   end
 end
