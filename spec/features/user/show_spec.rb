f9dd956e141362f95137240fbbc4ee215afe53bd
Eric Chon <echon92@gmail.com>
2022-02-01 13:31:11 -0700

    created test for user dashboard to show users name

---
+++ spec/features/user/show_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'User show page' do
+
+  it "checks that the users name is on the page " do
+    user_1 = User.create!(name: 'User 1', email: 'email1@gmail.com')
+    visit user_path(user_1)
+
+    expect(page).to have_content("#{user_1.name}'s Dashboard'")
+  end
+end
