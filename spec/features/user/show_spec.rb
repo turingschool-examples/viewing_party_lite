5419ba19240a07f16afd808eabad33106fded746
Eric Chon <echon92@gmail.com>
2022-02-01 13:32:45 -0700

    Test to check for users name passing

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'User show page' do
   it "checks that the users name is on the page " do
     user_1 = User.create!(name: 'User 1', email: 'email1@gmail.com')
     visit user_path(user_1)
+    expect(page).to have_content("#{user_1.name}'s Dashboard")
 
-    expect(page).to have_content("#{user_1.name}'s Dashboard'")
   end
 end
