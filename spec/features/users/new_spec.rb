8b22cdeba633e2ab334a8a4f144386a068ae6649
Kevin Gloss <kgloss89@gmail.com>
2022-02-01 12:10:39 -0700

    Test for new user registration

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 describe 'New User' do
-    let!(user) {User.create!(name: '', email: '')}
-    let!(user) {User.create!(name: '', email: '')}
-    let!(user) {User.create!(name: '', email: '')}
-  
+    before :each do 
+        @eldridge = User.create!(name: 'Eldridge', email: 'eldridge@gmail.com')
+        @kevin = User.create!(name: 'Kevin', email: 'kevin@gmail.com')
+        @suzie = User.create!(name: 'Suzie', email: 'suzieq@gmail.com')
+    end
+
     it 'should be have a button from the landing page' do
         visit root_path
 
-        click_button
+        click_link 'Create New User'
 
         expect(current_path).to eq(new_user_path)
     end
 
     it "can create a new user with valid input" do
+        visit new_user_path 
+
+        fill_in(:user_name, with: 'John')
+        fill_in(:user_email, with: 'john@gmail.com')
+        click_button 'Create User'
         
+        expect(current_path).to eq(user_path()) 
     end
 
     it "has a sad path for invalid data" do
