e61e6212412b01126ab137546c9130e1a37c9e1f
Eric Chon <echon92@gmail.com>
2022-02-02 10:37:50 -0700

    created test to check for button to find top rated movies and that main page has a title

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Discover index page' do
+  it "checks for description of page" do
+    user_1 = User.create!(name: 'User 1', email: 'email1@gmail.com')
+    visit user_discover_index_path(user_1)
+    expect(page).to have_content("Discover Movies")
+  end
 
+  it "check for top rated movies button" do
+    user_1 = User.create!(name: 'User 1', email: 'email1@gmail.com')
+    visit user_discover_index_path(user_1)
+    expect(page).to have_button("Find Top Rated Movies")
+    click_button "Find Top Rated Movies"
+    expect(current_path).to eq("users/#{user_1.id}/movies?q=top%40rated")
+  end
 
+  # it "text" do
+  #
+  # end
 end
