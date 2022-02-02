bd1d207c3fa6357ab835de36be1c9c64226d6b26
Robin Dittrich <robinodittrich@gmail.com>
2022-02-02 14:29:11 -0800

    Move code from controller to movie service.

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -3,29 +3,14 @@ class UserMoviesController < ApplicationController
   def index
     @query = params[:query]
     @user = User.find(params[:id])
-    conn = Faraday.new("https://api.themoviedb.org") do |faraday|
-      faraday.params['api_key'] = ENV['movie_api_key']
-    end
-    response_page_one = conn.get("/3/movie/top_rated?api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
-    response_page_two = conn.get("/3/movie/top_rated?page=2&api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
-    data_page_one = JSON.parse(response_page_one.body, symbolize_names: true)
-    data_page_two = JSON.parse(response_page_two.body, symbolize_names: true)
-    a = data_page_one[:results]
-    b = data_page_two[:results]
-    @top_movies = (a << b).flatten
-
+    @top_movies = MovieService.top_movies
   end
 
   def search
       @user = User.find(params[:id])
-      conn = Faraday.new("https://api.themoviedb.org") do |faraday|
-        faraday.params['api_key'] = ENV['movie_api_key']
-      end
-      response = conn.get("/3/search/movie?query=#{params[:search]}&api_key=#{ENV['movie_api_key']}")
-      data = JSON.parse(response.body, symbolize_names: true)
-      @movies = data[:results]
+      @movies = MovieService.search_for_movie(params[:search])
       @search = params[:search]
+      
       render :index
     end
-
   end
