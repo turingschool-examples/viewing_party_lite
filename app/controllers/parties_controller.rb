e07b9e83c0b0b55e4e546313416ede2fa5fa1017
Robin Dittrich <robinodittrich@gmail.com>
2022-02-03 14:20:23 -0800

    Create parties controller.

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -7,11 +7,15 @@ class PartiesController < ApplicationController
   def create
     user = User.find(params[:user_id])
     movie = MovieService.movie_info(params[:id])
-    party = Party.new()
+    party = Party.create(party_params.merge(title: movie.title, poster_path: movie.poster_path, day: params[:day], start_time: params[:start_time]))
+
+    UserParty.create(user_id: user.id, party_id: party.id)
+
+    redirect_to "/users/#{user.id}"
   end
 
   private
   def party_params
-    params.permit(:host_id, :duration, :day, :start_time, :movie_id, :user_ids)
+    params.permit(:host_id, :duration, :day, :start_time, :movie_id, :title, :poster_path, :user_ids)
   end
 end
