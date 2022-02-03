0c6f4943469721f0381abd50cc80a3cc5543bf13
Karan Mehta <karanm645@gmail.com>
2022-02-02 23:54:34 -0600

    Create keyword method in facade
    Co-authored-by: John Napier <jcee.napier@gmail.com>

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -17,4 +17,16 @@ class TmdbService
     json_pages << json_2[:results]
     json_pages.flatten
   end
+
+  def self.keyword(search_term)
+    json_pages = []
+    conn = get_url("https://api.themoviedb.org")
+    response = conn.get("/3/search/movie?api_key=#{ENV['tmdb_key']}&language=en-US&query=#{search_term}&page=1&include_adult=false")
+    response_2 = conn.get("/3/search/movie?api_key=#{ENV['tmdb_key']}&language=en-US&query=#{search_term}&page=2&include_adult=false")
+    json = JSON.parse(response.body, symbolize_names: true)
+    json_2 = JSON.parse(response_2.body, symbolize_names: true)
+    json_pages << json[:results] 
+    json_pages << json_2[:results]
+    json_pages.flatten
+  end
 end
