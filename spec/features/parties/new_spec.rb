888e145bc43f311a546767a8e5adca48b83c85ee
Robin Dittrich <robinodittrich@gmail.com>
2022-02-03 14:19:20 -0800

    Format time and date correctly.

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Party New Page' do
   describe 'view' do
     before(:each) do
       @user_1 = User.create!(name: "David", email: "david@email.com")
-      @party_1 = @user_1.parties.create!(duration: 180, day: "December 12, 2021", start_time: "7:00 pm", movie_id: 1, user_id: @user_1.id)
     end
 
     it 'includes all information about the viewing party' do
@@ -13,15 +12,15 @@ RSpec.describe 'Party New Page' do
       visit "/users/#{@user_1.id}/movies/#{top_movie.id}/viewing-party/new"
 
       fill_in :duration, with: '180'
-      fill_in :day, with: 'February 5th'
-      fill_in :start_time, with: '120'
+      fill_in :day, with: '02/03/21'
+      fill_in :start_time, with: '7:00'
 
       click_button 'Submit'
-
       expect(current_path).to eq("/users/#{@user_1.id}")
 
+
       expect(page).to have_content(top_movie.title)
-      expect(page).to have_content(top_movie.start_time)
+      expect(page).to have_content("Start Time: 2000-01-01 07:00:00")
       # Movie Title
       # Vote Average of the movie
       # Runtime in hours & minutes
