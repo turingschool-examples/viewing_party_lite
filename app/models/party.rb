9704c9ab68d4c64abb9a8d86d5133c62d417891b
Robin Dittrich <robinodittrich@gmail.com>
2022-02-04 09:45:40 -0800

    Create sad path.

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,9 +2,12 @@ class Party < ApplicationRecord
   has_many :user_parties
   has_many :users, through: :user_parties
 
-  validates_presence_of :duration
-  validates_presence_of :day
-  validates_presence_of :start_time
+  validates :duration,
+            :presence => {message: "can't be blank"}
+  validates :day,
+            :presence => {message: "can't be blank"}
+  validates :start_time,
+            :presence => {message: "can't be blank"}
 
   def find_invitee(params)
     all_users = User.all
