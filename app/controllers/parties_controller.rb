5c6a6756e85ee67a7bdc5127d13087a05f0361e8
Robin Dittrich <robinodittrich@gmail.com>
2022-02-04 10:00:35 -0800

    Complete new party sad path testing.

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -8,17 +8,17 @@ class PartiesController < ApplicationController
 
   def create
     all_users = User.all
-    user = User.find(params[:user_id])
-    movie = MovieFacade.movie_info(params[:id])
-    @party = Party.new(party_params.merge(host: user.id, title: movie.title, poster_path: movie.poster_path, day: params[:day], start_time: params[:start_time]))
+    @user = User.find(params[:user_id])
+    @movie = MovieFacade.movie_info(params[:id])
+    @party = Party.new(party_params.merge(host: @user.id, title: @movie.title, poster_path: @movie.poster_path, day: params[:day], start_time: params[:start_time]))
     if @party.save
-      UserParty.create(user_id: user.id, party_id: @party.id)
+      UserParty.create(user_id: @user.id, party_id: @party.id)
 
       @party.find_invitee(params).each do |guest|
         UserParty.create(user_id: guest.id, party_id: @party.id)
       end
 
-      redirect_to "/users/#{user.id}"
+      redirect_to "/users/#{@user.id}"
 
     else
       render :new
