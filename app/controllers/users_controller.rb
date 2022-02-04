1efb00f64205ea380a2b6c5580c8fb98a8a49838
David Kassin <david.j.kassin@gmail.com>
2022-02-04 09:30:10 -0700

    removed merge conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,17 +10,7 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create
-    user = User.create(user_params)
-    redirect_to user_path(user.id)
-  end
-
   def discover
     @user = User.find(params[:id])
-  end
-
-private
-  def user_params
-    params.permit(:name, :email)
   end
 end
