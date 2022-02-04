bb72ee86f6e0e759d86af42b8a52be9b34b753f0
Robin Dittrich <robinodittrich@gmail.com>
2022-02-03 23:17:01 -0800

    Add movie info test.

--- spec/services/movie_services_spec.rb
+++ spec/services/movie_services_spec.rb
@@ -26,5 +26,52 @@ RSpec.describe MovieService do
         expect(top_movie_data[:overview]).to be_a String
       end
     end
+
+    context '.search_for_movie' do
+      it "returns movie search" do
+        search = MovieService.search_for_movie('Dune')
+        expect(search).to be_a Hash
+        expect(search[:results]).to be_a Array
+
+        search_data = search[:results].first
+
+        expect(search_data).to have_key :title
+        expect(search_data[:title]).to be_a String
+
+        expect(search_data).to have_key :id
+        expect(search_data[:id]).to be_a Integer
+
+        expect(search_data).to have_key :vote_average
+        expect(search_data[:vote_average]).to be_a Float
+
+        expect(search_data).to have_key :poster_path
+        expect(search_data[:poster_path]).to be_a String
+
+        expect(search_data).to have_key :overview
+        expect(search_data[:overview]).to be_a String
+      end
+    end
+
+    context '.movie_info' do
+      it "returns movie_info" do
+        info = MovieService.movie_info(100)
+        expect(info).to be_a Hash
+
+        expect(info).to have_key :title
+        expect(info[:title]).to be_a String
+
+        expect(info).to have_key :id
+        expect(info[:id]).to be_a Integer
+
+        expect(info).to have_key :vote_average
+        expect(info[:vote_average]).to be_a Float
+
+        expect(info).to have_key :poster_path
+        expect(info[:poster_path]).to be_a String
+
+        expect(info).to have_key :overview
+        expect(info[:overview]).to be_a String
+      end
+    end
   end
 end
