a0da70e4e4ce950de0c848bc7d28b90f431d3e79
John Napier <jcee.napier@gmail.com>
2022-02-04 11:49:12 -0700

    Fix migrations

--- db/migrate/20220204092057_create_user_parties.rb
+++ db/migrate/20220204092057_create_user_parties.rb
@@ -2,7 +2,7 @@ class CreateUserParties < ActiveRecord::Migration[5.2]
   def change
     create_table :user_parties do |t|
       t.references :user, foreign_key: true
-      t.references :viewing_parties, foreign_key: true
+      t.references :viewing_party, foreign_key: true
 
       t.timestamps
     end
