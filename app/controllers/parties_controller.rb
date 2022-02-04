72a7facccd8b2ebbcf5ff5bffdcdbc3c68677784
David Kassin <david.j.kassin@gmail.com>
2022-02-04 09:29:02 -0700

    Add model method to parties_controller to clean up controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -12,21 +12,16 @@ class PartiesController < ApplicationController
     party = Party.create(party_params.merge(host: user.id, title: movie.title, poster_path: movie.poster_path, day: params[:day], start_time: params[:start_time]))
 
     UserParty.create(user_id: user.id, party_id: party.id)
-    @params = params
-      x = all_users.select do |user|
-      @params.keys.include?(user.email)
-    end
 
-    x.each do |y|
-      party[:host] = 2
-      UserParty.create(user_id: y.id, party_id: party.id)
+    party.find_invitee(params).each do |guest|
+      UserParty.create(user_id: guest.id, party_id: party.id)
     end
 
     redirect_to "/users/#{user.id}"
   end
 
   private
-  
+
   def party_params
     params.permit(:host, :duration, :day, :start_time, :movie_id, :title, :poster_path, :user_ids)
   end
