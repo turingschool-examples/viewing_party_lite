bb954908e7d1667b0d4fdcbf8f09e60579695b74
John Napier <jcee.napier@gmail.com>
2022-02-04 10:24:05 -0700

    Update viewing party model, and add viewing party controller

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -7,8 +7,12 @@ class ViewingPartyController < ApplicationController
 
   def create 
     viewing_party = ViewingParty.new(party_params)
+    movie = TmdbFacade.details(params[:movie_id])
     
-    redirect_to user_path(params[:user_id])
+    if (viewing_party.length >= movie.total_min) & viewing_party.save
+      
+      redirect_to user_path(params[:user_id])
+    end 
   end
 
   private 
