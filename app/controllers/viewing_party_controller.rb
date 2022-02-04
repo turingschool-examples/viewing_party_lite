3b437379a9622cba4cb63a6e9f7c7210dd2ba596
John Napier <jcee.napier@gmail.com>
2022-02-04 01:29:54 -0700

    Complete form for new party

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,5 +1,11 @@
 class ViewingPartyController < ApplicationController 
   def new 
-    
+    @user = User.find(params[:user_id])
+    @uninvited = User.where.not(id: @user.id)
+    @movie = TmdbFacade.details(params[:movie_id])
+  end
+
+  def create 
+    redirect_to user_path(params[:user_id])
   end
 end
