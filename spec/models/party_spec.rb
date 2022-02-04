ce8aa784223233a7364d15bbd24a617d0caaf99a
David Kassin <david.j.kassin@gmail.com>
2022-02-04 09:28:21 -0700

    Test: Party model test

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Party, type: :model do
     it { should validate_presence_of :day}
     it { should validate_presence_of :start_time}
   end
+
+   describe 'instance methods' do
+     it 'should return users that are in a hash key' do
+      user_1 = User.create!(name: "Jim", email: "Jim@email.com")
+      user_2 = User.create!(name: "Jeff", email: "Jeff@email.com")
+      user_3 = User.create!(name: "Jill", email: "Jill@email.com")
+      party_1 = user_1.parties.create!(duration: 180, day: "December 12, 2021", start_time: "7:00 pm", movie_id: 1)
+      params_hash = {
+        "Jeff@email.com" => 1,
+        "Jill@email.com" => 1
+      }
+
+      expect(party_1.find_invitee(params_hash)).to eq([user_2, user_3])
+    end
+  end
 end
