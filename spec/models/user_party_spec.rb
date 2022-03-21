99c338487486f17a8104be94570f73f0938f9021
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-21 18:54:09 -0500

    Add UserParty model spec

---
+++ spec/models/user_party_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'relationships' do 
+    it { should belong_to :user }
+    it { should belong_to :viewing_party }
+  end
+  
+  describe 'validations' do 
+    it { should validate_presence_of :host }
+  end
+end
