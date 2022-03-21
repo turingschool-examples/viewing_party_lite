a8befe7662b1fc4346d9a126b76293077de73b7b
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-21 16:11:28 -0600

    uncomment relationship tests now that all models actually exist (lol)

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ViewingParty, type: :model do
   describe "relationships" do
-    it { should have_many :user_viewing_parties }
-    it { should have_many(:users).through(:user_viewing_parties) }
+     it { should have_many :user_viewing_parties }
+     it { should have_many(:users).through(:user_viewing_parties) }
   end
 
   describe "attributes" do
