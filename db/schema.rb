23ed213607b49e4cb09797909806c37803dd94bd
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-21 17:43:14 -0500

    Add Viewing Party to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_21_224140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "viewing_parties", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "duration"
+    t.date "date"
+    t.time "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
