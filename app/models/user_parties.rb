81211a039489fe12480b55d9c65ed3f31e3afa22
Joseph W Galvin <jwgalvin@gmail.com>
2022-03-21 16:25:23 -0600

    Initial commit Database schema

---
+++ app/models/user_parties.rb
@@ -0,0 +1,2 @@
+class UserParties < ApplicationRecord
+end
---
+++ db/migrate/20220321222014_create_parties.rb
@@ -0,0 +1,12 @@
+class CreateParties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parties do |t|
+      t.integer :duration
+      t.date :date
+      t.time :start
+      t.integer :movie_id
+
+      t.timestamps
+    end
+  end
+end
