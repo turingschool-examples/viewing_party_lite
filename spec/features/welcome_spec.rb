b508c1ca50c4d8ace5701c87e8b609728f4e2e9d
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-21 16:07:52 -0600

    Add: first passing test

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome Page' do
+  it 'exists' do
+    visit "/"
+    expect(current_path).to eq("/")
+    expect(page).to have_content("Viewing Party Lite!")
+  end
+end
