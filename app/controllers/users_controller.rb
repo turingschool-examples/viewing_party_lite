f54fe44b1a7801c532493e0fa0db00dd015e7766
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-22 15:39:05 -0600

    User Model Created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+    
+  end
+
   def create
     user = user_params
     user[:email] = user[:email].downcase
@@ -11,7 +15,7 @@ class UsersController < ApplicationController
     if new_user.save
       flash[:success] = 'Account Successfully Created!'
       session[:user_id] = new_user.id
-      redirect_to dashboard_path
+      redirect_to user_path(new_user.id)
     else
       flash[:error] = 'Email address is blank/already in use.'
       redirect_to new_user_path
