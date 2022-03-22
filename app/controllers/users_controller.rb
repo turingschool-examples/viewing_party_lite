587ed53115b40bca3a45dcae49da8a307881fa24
Sam Smith <samlsmith424@gmail.com>
2022-03-22 13:11:37 -0700

    Test: all fields must be properly filled in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,22 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
-    user = User.create!(user_params)
-    redirect_to "/users/#{user.id}"
+    user = User.new(user_params)
+    if user.save
+      redirect_to "/users/#{user.id}"
+    else
+      redirect_to '/register', notice: "Please kindly fill in all fields"
+      
+    end
   end
 
   def show
 
   end
-  
+
   private
 
   def user_params
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe 'register form' do
 
     expect(current_path).to eq("/users/#{User.last.id}")
   end
+
+  it 'returns user to same page if all fields are not properly filled' do
+    visit '/register'
+
+    fill_in "Name", with: "Asil"
+    click_button "Create New User"
+
+    expect(current_path).to eq('/register')
+    expect(page).to have_content("Please kindly fill in all fields")
+  end
+
+  it 'requires a unique email' do
+    visit '/register'
+
+    
+  end
 end
