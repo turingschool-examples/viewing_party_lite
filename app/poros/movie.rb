6f12d9370032436602f1ed9cc31fca848eccad0c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-22 15:14:37 -0500

    Test: Movie exists
    Co-authored-by: Becky Nisttahus <92293363+benistta@users.noreply.github.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,5 +1,10 @@
 class Movie 
-  def initialize(attributes)
+  attr_reader :id, :title, :runtime, :vote_average 
 
+  def initialize(attributes)
+    @id = attributes[:id]
+    @title = attributes[:title]
+    @runtime = attributes[:runtime]
+    @vote_average = attributes[:vote_average]
   end
 end
