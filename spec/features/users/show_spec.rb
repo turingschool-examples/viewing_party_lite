eeba7bc88ae1eef6d874f7df548fd4bb542d2aee
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-21 22:54:50 -0500

    Test: User Dashboard has User name

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'User Dashboard Page' do
+  before(:each) do 
+    @vp1 = ViewingParty.create!(movie_id: 111, duration: 151, date: Time.new(2022, 04, 12, 21, 00), start_time: Time.new(2022, 04, 12, 21, 00))
+    @vp2 = ViewingParty.create!(movie_id: 112, duration: 152, date: Time.new(2022, 04, 11, 20, 30), start_time: Time.new(2022, 04, 11, 20, 30))
+    @user1 = User.create!(name: "Becky", email: "becky@example.com")
+    @user2 = User.create!(name: "Steven", email: "steven@example.com")
+    @user5 = User.create!(name: "Bruce", email: "Bruce@example.com")
+    @user6 = User.create!(name: "Tony", email: "Tony@example.com")
+    @up1 = UserParty.create!(viewing_party: @vp1, user: @user1, host: true)
+    @up2 = UserParty.create!(viewing_party: @vp1, user: @user2, host: false)
+    @up5 = UserParty.create!(viewing_party: @vp2, user: @user5, host: true)
+    @up6 = UserParty.create!(viewing_party: @vp2, user: @user6, host: false)
+
+    visit user_path(@user1) 
+  end
+
+  context 'data is displayed on page' do 
+    it 'shows user name on dashboard header' do 
+      expect(page).to have_content("#{@user1.name}'s Dashboard")
+      expect(page).to_not have_content("#{@user2.name}'s Dashboard")
+    end
+    xit 'has a button to Discover Movies' do 
+      expect(page).to have_button("Discover Movies")
+      click_button("Discover Movies")
+      expect(current_path).to eq()
+    end
+    xit 'has a section that lists viewing parties' do 
+      expect(page).to have_content
+    end
+  end
+
+end
