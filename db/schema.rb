9129e1a00d6dbbb871af8f2f4f87fe941698eb52
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-22 18:40:15 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_220056) do
+ActiveRecord::Schema.define(version: 2022_03_22_223813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2022_03_22_220056) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "status"
   end
 
   add_foreign_key "user_parties", "parties"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe User, type: :model do
      it { should have_many(:user_parties) }
      it { should have_many(:parties).through(:user_parties) }
    end
+
+   describe "validations" do
+     it { should validate_presence_of(:name) }
+     it { should validate_presence_of(:email) }
+   end
 end
