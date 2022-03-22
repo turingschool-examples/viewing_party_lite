e49f2b4c4e995a368a0efbd71b0335b6cc3d7604
Blake Dempsey <bdempsey864@gmail.com>
2022-03-22 17:20:53 -0400

    Feat: Created a section for a viewing party

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe 'User Show Page' do
 
     expect(page).to have_button "Discover Movies"
   end
+
+  it "has a section to list viewing parties" do
+    user1 = User.create(name: "Asil Rolyat", email: "asil.rolyat@yourmom.com")
+    party1 = Party.create(duration: 400, date: Date.today, start_time: Time.now.utc)
+    party_user1 = PartyUser.create(user_id: user1.id, party_id: party1.id)
+
+    visit "/users/#{user1.id}"
+
+    expect(page).to have_content("Viewing Parties")
+    expect(page).to have_content("#{party1.date}")
+  end
 end
