65af6cceb3871c775fdd1fe0388faf6d115feec4
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-22 15:20:36 -0600

    Add create method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,21 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    user = user_params
+    user[:email] = user[:email].downcase
+    new_user = User.new(user)
+    if new_user.save
+      flash[:success] = 'Account Successfully Created!'
+      session[:user_id] = new_user.id
+      redirect_to dashboard_path
+    else
+      flash[:error] = 'Email address is blank/already in use.'
+      redirect_to new_user_path
+    end
   end
+
+  private
+   def user_params
+     params.permit(:email, :name)
+   end
 end
