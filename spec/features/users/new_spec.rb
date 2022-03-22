c3150e5863deff765bfe706b679650d9c556a173
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-22 16:52:54 -0600

    test: write sad path test for new user form will not register an invalid user

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -10,7 +10,18 @@ RSpec.describe 'The new user view/form' do
     click_button('Register')
 
     expect(current_path).to eq(user_path(User.last))
-    expect(User.first.name).to eq('Plain Name')
-    expect(User.first.email).to eq('User@gmail.com')
+    expect(User.last.name).to eq('Plain Name')
+    expect(User.last.email).to eq('User@gmail.com')
+  end 
+
+  it 'does not register an invalid user' do 
+    visit register_path 
+
+    fill_in 'Name', with: 'Plain Name'
+    fill_in 'Email', with: ''
+
+    click_button('Register')
+    
+    expect(page).to have_content("Email can't be blank")
   end 
 end 
