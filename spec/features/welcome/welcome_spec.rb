0784e5a511d8bd87fca7cc2c1c43d842f70076c1
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-21 22:04:26 -0400

    Test/Feat: Welcome Page has button to create user

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper' 
 
 RSpec.describe "Welcome Page" do 
-    before(:each) do
-      user = User.create!(name: "Benson" ,email: "benson@example.com")
-      user2 = User.create!(name: "Mordecai" ,email: "mc@gmail.com")
-      user3 = User.create!(name: "Rigby" ,email: "rigzbee@aol.com")
-    end 
+  before(:each) do
+    user = User.create!(name: "Benson" ,email: "benson@example.com")
+    user2 = User.create!(name: "Mordecai" ,email: "mc@gmail.com")
+    user3 = User.create!(name: "Rigby" ,email: "rigzbee@aol.com")
 
-    it "should title of application" do 
-      visit '/'
-      expect(page).to have_content("Viewing Party Light")      
-      save_and_open_page
-    end 
+    visit '/'
+  end 
+
+  it "should title of application" do 
+    expect(page).to have_content("Viewing Party Light")      
+  end 
+
+  it "should have button to create a new user" do 
+    expect(page).to have_button("Create a New User")    
+  end 
 end 
