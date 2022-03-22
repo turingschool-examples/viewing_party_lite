28c465f8aecc920117d5ea2260b9258946617442
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-22 15:54:10 -0600

    update to handrolled routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get '/', to: 'landings#index'
+    get '/register', to: 'users#new'
+    post '/register', to: 'users#create'
+    post '/users', to: 'users#create'
+    get '/users/:id', to: 'users#show'
+  end
