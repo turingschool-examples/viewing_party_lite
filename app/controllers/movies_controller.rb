58f424c87c59757cb645687923c4e2de03d67404
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-22 16:50:03 -0400

    Add: Api consumption to Movies Controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,14 @@
 class MoviesController < ApplicationController 
 
   def index 
-    
+    movie = params[:movie]
+
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["x-api-key"] = ENV['api_key']
+    end
+
+    response = conn.get("/3/search/movie?api_key=#{ENV['api_key']}&query=#{movie}&page=1")
+
+    data = JSON.parse(response.body, symbolize_names: true)
   end 
 end 
