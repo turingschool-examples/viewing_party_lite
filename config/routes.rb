5b0302d9a98b2a971fbe50bb2235844f39de42d3
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-22 15:07:08 -0600

    Feat: US9 Model, controller, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "landing#index"
+  resources :users
+
+  post '/users/new', to: 'users#create'
 end
