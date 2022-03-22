2d3f81988d10e0af7e1b6c996cc5cdedaba1ac41
Joseph W Galvin <jwgalvin@gmail.com>
2022-03-21 19:51:01 -0600

    Test: Test for registering User, Happy and sad path

---
+++ spec/features/user/create_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Creating a new user:', type: :feature do
+
+  it "has stuff on the page" do
+    visit '/register'
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Email")
+    expect(page).to have_button("Save")
+  end
+
+  it 'happy path' do
+    visit '/register'
+    fill_in("Name", with: "Bliffert's Blankship")
+    fill_in("Email", with: "Bliff@aol.com")
+    click_button("Save")
+
+    expect(current_path).to eq("/users/#{User.last.id}")
+  end
+
+  it ' sad- does not create user if Name empty' do
+    visit '/register'
+
+    fill_in("Email", with: "Bliff@aol.com")
+    click_button("Save")
+    expect(current_path).to eq('/register')
+    expect(page).to have_content("Error: Name can't be blank")
+  end
+
+  it ' sad- does not create user if Name empty' do
+    visit '/register'
+
+    fill_in("Name", with: "Bliffert's Blankship")
+    click_button("Save")
+    expect(current_path).to eq('/register')
+    expect(page).to have_content("Error: Email can't be blank")
+  end
+end
