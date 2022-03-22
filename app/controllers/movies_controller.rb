c70a9b014fc2250a7b8b4e9a29508ca52709c806
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-22 14:51:51 -0500

    Add API to Movies controller
    Co-authored-by: Becky Nisttahus <92293363+benistta@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
   def index 
+    conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
+      faraday.params['api_key'] = ENV["movie_api_key"]
+    end
+    response = conn.get("movie/top_rated")
+    json = JSON.parse(response.body, symbolize_names: true)
+    @top_rated = json[:results].map do |movie_data|
+      Movie.new(movie_data)
+    end
     
   end
 end
