163a8d749c49e407363edac0ca46807f91353401
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-22 10:02:19 -0400

    Test: Discover Page has top rated movies button

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe 'Discover things:', type: :feature do
   before(:each) do
     @user1 = User.create!(name: "Bliffert Blankship", email: "Bliff@aol.com")
     @user2 = User.create!(name: "Blankert Bliffship", email: "Blank@aol.com")
-  end
-  it "has stuff on the page" do
 
     visit "/users/#{@user1.id}/discover"
-
-    expect(page).to have_content("Discover movies and things about yourself #{@user1.name}!")
-
   end
+
+  it "has stuff on the page" do
+    expect(page).to have_content("Discover movies and things about yourself #{@user1.name}!")
+  end
+
+  it "has button to discover Top Rated Movies" do 
+    click_on("Find Top Rated Movies")
+    expect(current_path).to eq("/users/#{@user1.id}/movies")
+  end 
+
 end
