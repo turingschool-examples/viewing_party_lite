5832766f24eab795546e3180e21ebba5a86b8423
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-22 17:59:06 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
    get '/', to: 'landings#index'
    get '/register', to: 'users#new'
    post '/register', to: 'users#create'
+   
    post '/users', to: 'users#create'
    get '/users/:id', to: 'users#show'
 end
-
