9d67f93370f912169203945e46454834b3bc0112
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-22 16:35:39 -0600

    feature: add user_params private method to UsersController. Define create action in users controller. Render erb form to register new user. Pass tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,18 @@ class UsersController < ApplicationController
   end 
 
   def create 
+    user = User.create!(user_params)
+    if user.save
+      redirect_to user_path(user.id)
+    else
+      flash.now[:user_errors] = 'Error, User Not Registered'
+      render 'new'
+    end 
   end 
+
+private 
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 
 end
