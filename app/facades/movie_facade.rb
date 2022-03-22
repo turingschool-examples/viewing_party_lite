60ad66eeb249cc6e470b21f0a7860844eece0a9a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-22 16:01:06 -0500

    Refactor: Move API call to MovieService
    Co-authored-by: Becky Nisttahus <92293363+benistta@users.noreply.github.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,12 +1,7 @@
 class MovieFacade
-  def self.top_rated 
-    conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
-      faraday.params['api_key'] = ENV["movie_api_key"]
-      faraday.adapter Faraday.default_adapter
-    end
-    
-    response = conn.get("movie/top_rated")
-    json = JSON.parse(response.body, symbolize_names: true)
+  def self.top_rated
+    json = MovieService.top_rated
+
     @top_rated = json[:results].map do |movie_data|
       Movie.new(movie_data)
     end
