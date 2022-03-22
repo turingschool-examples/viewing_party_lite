7539b9c03f3c69a80a4d215413664d91b86dca01
Richard LaBrecque <richardlabreque@gmail.com>
2022-03-22 17:38:03 -0400

    feat: new user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
 end
