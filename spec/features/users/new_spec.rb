10572bf0972d5f589995250aa64863845df724df
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-22 17:08:55 -0600

    test: write sad path test for email must be unique. Test passes

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -18,4 +18,13 @@ RSpec.describe 'The new user view/form' do
     click_button('Register')
     expect(page).to have_content("Email can't be blank")
   end 
+
+  it 'does not register the same email twice' do 
+    user = User.create!(name: 'user_3', email: 'email3@gmail.com')
+    visit register_path 
+    fill_in 'Name', with: 'Plain Name'
+    fill_in 'Email', with: 'email3@gmail.com'
+    click_button('Register')
+    expect(page).to have_content("Email has already been taken")
+  end 
 end 
