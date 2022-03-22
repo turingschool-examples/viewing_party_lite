c9c68402d3bb883b99e3b21a4739070f702c3708
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-22 17:13:43 -0600

    fix: problems as declared by RuboCop. Trailing whitespaces, unnecessary test variables

--- spec/features/root/root_spec.rb
+++ spec/features/root/root_spec.rb
@@ -1,27 +1,26 @@
-require 'rails_helper' 
+require 'rails_helper'
 
-RSpec.describe 'The landing page' do 
-  it 'displays the title of application' do 
-    user_1 = User.create!(name: 'user_1', email: 'email@gmail.com')
-    user_2 = User.create!(name: 'user_2', email: 'email2@gmail.com')
-    user_3 = User.create!(name: 'user_3', email: 'email3@gmail.com')
+RSpec.describe 'The landing page' do
+  it 'displays the title of application' do
+    user = User.create!(name: 'user_1', email: 'email@gmail.com')
+
     visit root_path
 
-    within '#title' do 
+    within '#title' do
       expect(page).to have_content('Welcome to Viewing Party')
-    end 
+    end
 
-    within '#new-user' do 
+    within '#new-user' do
       expect(page).to have_button 'Register' 
       click_button 'Register'
       expect(current_path).to eq('/register')
-    end 
+    end
 
     visit root_path
-    within '#users' do 
+    within '#users' do
       expect(page).to have_link(user_1.name)
-      click_link(user_1.name)
-      expect(current_path).to eq(user_path(user_1.id))
-    end 
-  end 
+      click_link(user.name)
+      expect(current_path).to eq(user_path(user.id))
+    end
+  end
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'The new user view/form' do 
+RSpec.describe 'The new user view/form' do
   it 'has a form a user can fill out to register' do
     visit register_path
     fill_in 'Name', with: 'Plain Name'
@@ -9,22 +9,22 @@ RSpec.describe 'The new user view/form' do
     expect(current_path).to eq(user_path(User.last))
     expect(User.last.name).to eq('Plain Name')
     expect(User.last.email).to eq('User@gmail.com')
-  end 
+  end
 
-  it 'does not register an invalid user' do 
-    visit register_path 
+  it 'does not register an invalid user' do
+    visit register_path
     fill_in 'Name', with: 'Plain Name'
     fill_in 'Email', with: ''
     click_button('Register')
     expect(page).to have_content("Email can't be blank")
-  end 
+  end
 
-  it 'does not register the same email twice' do 
+  it 'does not register the same email twice' do
     user = User.create!(name: 'user_3', email: 'email3@gmail.com')
-    visit register_path 
+    visit register_path
     fill_in 'Name', with: 'Plain Name'
     fill_in 'Email', with: 'email3@gmail.com'
     click_button('Register')
     expect(page).to have_content("Email has already been taken")
-  end 
-end 
+  end
+end
