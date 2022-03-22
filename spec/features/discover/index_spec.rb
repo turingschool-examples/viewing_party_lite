d2467a6ffff812f51f77c3161547498d8c78381c
Blake Dempsey <bdempsey864@gmail.com>
2022-03-22 18:36:14 -0400

    Feat: Add Search Movie Title button and update Gemfile

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Discover Index Page' do
-  it 'has a button for Top Rated Movies' do
+  it 'has a button for Top Rated Movies and Search by Movie Title' do
     user1 = User.create(name: "Asil Rolyat", email: "asil.rolyat@yourmom.com")
 
     visit user_discover_index_path(user1)
 
     expect(page).to have_button('Top Rated Movies')
+    expect(page).to have_button('Search Movie Title')
   end
+
+  # it 'has a text field for key words' do
+  #   user1 = User.create(name: "Asil Rolyat", email: "asil.rolyat@yourmom.com")
+
+  #   visit user_discover_index_path(user1)
+
+  #   expect(page).to have_field('Keywords')
+  # end
 end
