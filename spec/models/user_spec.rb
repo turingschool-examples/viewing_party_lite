991e5c4aef910b5a0cb61b9c77a95ded30fd6e4a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-21 19:04:51 -0500

    Add User model. Validations and relationships tested at model level

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'relationships' do 
     it { should have_many :user_parties }
-    it { should have_many :viewing_parties, through: :user_parties }
+    it { should have_many(:viewing_parties).through(:user_parties) }
   end
   
   describe 'validations' do 
