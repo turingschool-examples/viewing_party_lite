1be3ac287a67cdac4ceef8bbf8877934a41fbdb1
Sully Birashk <sully.birashk@gmail.com>
2022-03-22 15:01:00 -0700

    Add appropriate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'landings#index'
+  post '/register', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
 end
