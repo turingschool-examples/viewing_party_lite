18a1e9ec819b8b7a01ae0e57892c5b684759585c
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-22 18:20:26 -0600

    Feat: Routes for all users stories through 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "landing#index"
-  resources :users
+  resources :users do
+    resources :discover, only: [:index]
+    resources :movies, only: [:index, :show]
+  end
 
   post '/users/new', to: 'users#create'
 end
