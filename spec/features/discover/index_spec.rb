3dcfbdd5f0707de23c4edd2411b8fc555209756a
Sam Smith <samlsmith424@gmail.com>
2022-03-23 13:31:13 -0700

    Test: user can search by multiple keywords

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -22,16 +22,16 @@ RSpec.describe 'Discover Index Page' do
     user1 = User.create(name: "Asil Rolyat", email: "asil.rolyat@yourmom.com")
 
     visit user_discover_index_path(user1)
-  
+
     click_button 'Top Rated Movies'
-  
+
     expect(current_path).to eq(user_movies_path(user1))
 
     expect(page.status_code).to eq 200
     expect(page).to have_content("Shawshank Redemption")
   end
 
-  it 'allows user to search for keywords' do
+  it 'allows user to search by keyword' do
     user1 = User.create(name: "Asil Rolyat", email: "asil.rolyat@yourmom.com")
 
     visit user_discover_index_path(user1)
@@ -41,5 +41,29 @@ RSpec.describe 'Discover Index Page' do
 
     expect(page.status_code).to eq 200
     expect(page).to have_content("Shawshank Redemption")
+  end
+
+  it 'allows user to search by multiple keywords' do
+    user1 = User.create(name: "Asil Rolyat", email: "asil.rolyat@yourmom.com")
+
+    visit user_discover_index_path(user1)
+
+    fill_in :search, with: 'Batman Beg'
+    click_button 'Search'
+
+    expect(page.status_code).to eq 200
+    expect(page).to have_content("Batman Begins")
+  end
+
+  it 'returns nothing if no keywords match search' do
+    user1 = User.create(name: "Asil Rolyat", email: "asil.rolyat@yourmom.com")
+
+    visit user_discover_index_path(user1)
+
+    fill_in :search, with: 'Batman Begggg'
+    click_button 'Search'
+
+    expect(page.status_code).to eq 200
+    expect(page).to_not have_content("Batman Begins")
   end
 end
