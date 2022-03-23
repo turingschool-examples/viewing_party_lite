337e207ad0d46700fee683327f0bdad8bdd9349e
Becky Nisttahuz <benistta@hotmail.com>
2022-03-22 20:04:44 -0600

    Feat: Create method show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
-class UsersController < ActionController::Base
+class UsersController < ApplicationController
   def new
     @user = User.new
-    # user = User.find(params[:id])
+  end
+
+  def show
+  end
+
+  def create
+    user = User.create!(name: params[:name], email: params[:email])
+    redirect_to "/users/#{user.id}"
   end
 end
