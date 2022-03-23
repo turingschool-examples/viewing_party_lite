1796b915604da67cbc62d8cf9735f285d5e5ec6c
Eldridge Turambi <eturambi95@gmail.com>
2022-03-23 12:05:25 -0600

    Add: Spec for discover index page for coverage
    Co-Authored-By: Glsorensen <81265307+glsorensen@users.noreply.github.com>

---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover Index Page' do
+
+  it 'has movie information' do
+    eldridge = User.create!(name: 'Eldridge', email: 'eldridge@gmail.com')
+
+    visit user_discover_index_path(eldridge)
+
+    expect(page).to have_content('Test')
+  end
+end
