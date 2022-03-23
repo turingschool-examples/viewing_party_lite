9cc894483d54068233e6ec30928a222a6bba8172
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-23 15:16:28 -0500

    Test: movie search form routes to Movie#index

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -32,7 +32,14 @@ RSpec.describe 'Discover Index Page' do
   end
   context 'top rated movies button' do 
     it 'routes to movies index' do 
-      click_button("Find Top Rated Movies")
+      click_button 'Find Top Rated Movies'
+      expect(current_path).to eq(user_movies_path(@user1))
+    end
+  end
+  context 'find movies form' do 
+    it 'routes to movies index' do 
+      fill_in 'search', with: 'Marvel'
+      click_button 'Find Movies'
       expect(current_path).to eq(user_movies_path(@user1))
     end
   end
