c2bee924e0e5bfcece69868440e34816fe5891b2
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-22 21:29:27 -0400

    Write spec for User Create view

---
+++ spec/features/users/create_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'user create page' do
+  before(:each) do
+    visit "/users/new"
+  end
+  describe "when a user visits the user create page" do
+    scenario "they see a form to create a new user, when filled out it redirects to landing page with user listed" do
+      fill_in :name, with: "Paul"
+      fill_in :email, with: "paul@fake.com"
+      click_button "Submit"
+
+      expect(current_path).to eq("/")
+      expect(page).to have_content("Paul")
+      expect(User.last.email).to eq("paul@fake.com")
+    end
+  end
+end
