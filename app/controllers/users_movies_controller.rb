d40746a7a77409a9459192577c479a5e975342f1
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-23 15:50:33 -0600

    define movie search api consumption in usersmovies controller. Add erb for button to top rated movies and search bar for movies into discover view. Tests pass

--- app/controllers/users_movies_controller.rb
+++ app/controllers/users_movies_controller.rb
@@ -1,22 +1,29 @@
 class UsersMoviesController < ApplicationController
-  def index 
-  
+  def index
     @user = User.find(params[:user_id])
-    conn = Faraday.new(url: "https://api.themoviedb.org/") do |faraday|
+    conn = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
       faraday.params[:api_key] = ENV['movie_api_key']
-      # faraday.params['q'] = params[:search]
+      faraday.params['query'] = params[:search]
     end
-    
-    top_rated_response = conn.get("3/movie/top_rated")
-    
-    data = JSON.parse(top_rated_response.body, symbolize_names: true)
-    
-    @movies = data[:results].map do |result|
-      Movies.new(result)
+
+    if params[:top_rated] == "top_rated"
+      top_rated_response = conn.get("movie/top_rated")
+      data = JSON.parse(top_rated_response.body, symbolize_names: true)
+
+      @movies = data[:results].map do |result|
+          Movies.new(result)
+      end.take(20)
+
+    elsif params[:search]
+      response = conn.get("search/movie")
+      data = JSON.parse(response.body, symbolize_names: true)
+      
+      @movies = data[:results].map do |result|
+        Movies.new(result)
+      end.take(40)
     end
-  end 
+  end
 
   def show 
-
   end 
 end 
