824519781ee4d3ed867dce97728c8214aa002b1c
Richard LaBrecque <richardlabreque@gmail.com>
2022-03-22 20:31:32 -0400

    feat: discover page exists "

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,14 @@ class UsersController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def discover
+    @user = User.find(params[:id])
+  end
   private
 
   def user_params
     params.permit(:name, :email)
   end
+
 
 end
