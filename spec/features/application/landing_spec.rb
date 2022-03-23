c657261d1b7efa08fa299d7683cebc2e68b08266
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-22 19:59:15 -0500

    Test: Landing page has button to create a new user

--- spec/features/application/landing_spec.rb
+++ spec/features/application/landing_spec.rb
@@ -16,7 +16,21 @@ RSpec.describe 'Landing Page' do
     visit "/"
   end
 
-  context 'it has the title of the application' do 
+  it 'has the title of the application' do 
     expect(page).to have_content("Viewing Party Light")
+  end
+
+  it 'has a button to create a new user' do 
+    expect(page).to have_button("Create a New User")
+    click_button("Create a New User")
+    expect(current_path).to eq(new_user_path)
+  end
+
+  it 'has a link to the landing page' do 
+    visit user_path(@user1)
+
+    expect(page).to have_link("Home")
+    click_link("Home")
+    expect(current_path).to eq("/")
   end
 end 
