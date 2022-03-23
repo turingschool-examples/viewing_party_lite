28c9d06b87653c2104bf4ad6e3e787eabee44850
Michael Brandt <msbrandt00@gmail.com>
2022-03-22 18:03:27 -0600

    Add callback for adding host to user_viewing_party

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -7,10 +7,16 @@ class ViewingParty < ApplicationRecord
   validates_presence_of :movie_id
 
   after_validation :assign_duration
+  after_save :add_host_to_user_viewing_parties
 
   private
 
   def assign_duration
     self.duration = MovieFacade.new(movie_id).movie.minutes
+  end
+
+  def add_host_to_user_viewing_parties
+    binding.pry
+    UserViewingParty.create!(viewing_party: self, user: user)
   end
 end
