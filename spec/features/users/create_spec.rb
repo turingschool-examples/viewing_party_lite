591ebec3d9883f83e6a1b3625d42d915c15a73c7
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-23 15:35:39 -0600

    Add sad path test for unique email

--- spec/features/users/create_spec.rb
+++ spec/features/users/create_spec.rb
@@ -8,11 +8,24 @@ RSpec.describe 'user create page' do
     scenario "they see a form to create a new user, when filled out it redirects to landing page with user listed" do
       fill_in :name, with: "Paul"
       fill_in :email, with: "paul@fake.com"
-      click_button "Submit"
+      click_button "Register"
 
       expect(current_path).to eq("/")
       expect(page).to have_content("Paul")
       expect(User.last.email).to eq("paul@fake.com")
+    end
+
+    scenario "they see a form to create a new user, gives error when not unique email" do
+      fill_in :name, with: "Paul"
+      fill_in :email, with: "paul@fake.com"
+      click_button "Register"
+
+      visit '/register'
+      fill_in :name, with: "Paul"
+      fill_in :email, with: "paul@fake.com"
+      click_button "Register"
+      expect(page).to have_content("Error: Email already registered")
+      expect(current_path).to eq('/register')
     end
   end
 end
