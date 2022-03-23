a2c17f0518707e7cd509dc41eacf29533d4a03cf
Sam Iavri <sammi80134@gmail.com>
2022-03-23 12:24:29 -0600

    Deleted API, redo to come

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,2 @@
 class MoviesController < ApplicationController
-  def index
-    @movies = MovieService::TopRated.call
-  end
 end
--- app/poros/movie_rep.rb
+++
@@ -1,8 +0,0 @@
-class MovieRep
-  attr_reader :title, :vote_average
-
-  def initialize(result)
-    @title = result['title']
-    @vote_average = result['vote_average']
-  end
-end
