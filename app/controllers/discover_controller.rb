20c39aeb72e03bc36d946c307e23aee0136b73f8
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-23 12:47:07 -0500

    Add DiscoverController and route to Discover#index

---
+++ app/controllers/discover_controller.rb
@@ -0,0 +1,11 @@
+class DiscoverController < ApplicationController
+  before_action :find_user, only: [:index]
+
+  def index; end 
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+end
