6bc7bae75ddd07602aa20b0cf928f13dd3bff892
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-23 12:54:47 -0500

    Test: User Movie Dashboard has button to top rated movies

---
+++ spec/features/movies/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover Movie Page' do
+  before(:each) do 
+    @vp1 = ViewingParty.create!(movie_id: 111, duration: 151, date: Time.new(2022, 04, 12, 21, 00), start_time: Time.new(2022, 04, 12, 21, 00))
+    @vp2 = ViewingParty.create!(movie_id: 112, duration: 152, date: Time.new(2022, 04, 11, 20, 30), start_time: Time.new(2022, 04, 11, 20, 30))
+    @user1 = User.create!(name: "Becky", email: "becky@example.com")
+    @user2 = User.create!(name: "Steven", email: "steven@example.com")
+    @user5 = User.create!(name: "Bruce", email: "Bruce@example.com")
+    @user6 = User.create!(name: "Tony", email: "Tony@example.com")
+    @up1 = UserParty.create!(viewing_party: @vp1, user: @user1, host: true)
+    @up2 = UserParty.create!(viewing_party: @vp1, user: @user2, host: false)
+    @up5 = UserParty.create!(viewing_party: @vp2, user: @user5, host: true)
+    @up6 = UserParty.create!(viewing_party: @vp2, user: @user6, host: false)
+
+    visit user_discover_index_path(@user1) 
+  end
+  context 'it displays correctly' do 
+    it 'has button to top rated movies' do 
+      expect(page).to have_button("Find Top Rated Movies")
+    end
+    
+  end
+  
+end 
