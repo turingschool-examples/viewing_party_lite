c163c033480f7b5a7f02980de6b3d099cbb5b820
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-24 15:04:05 -0500

    Refactor: Update to use callback

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,7 +1,15 @@
 class ViewingPartyController < ApplicationController
+  before_action :find_user_and_movie, only: [:new]
+
   def new
-    @user = User.find(params[:user_id])
     @movie = Movie.new(id: 22, title: 'Pirates of the Caribbean: The Curse of the Black Pearl', runtime: 102,
-                       vote_average: 10)
+      vote_average: 10)
+  end
+
+  private
+  
+  def find_user_and_movie
+    @user = User.find(params[:user_id])
+    # @movie = Movie.find(params[:movie_id])
   end
 end
