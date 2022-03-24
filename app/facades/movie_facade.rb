b00c53b853b13d98df5bc59dc4a37362828381ce
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-23 21:32:10 -0500

    Add methods for movie_search(query) to hit api and return data

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -6,4 +6,12 @@ class MovieFacade
       Movie.new(movie_data)
     end
   end
+
+  def self.movie_search(query)
+    json = MovieService.movie_search(query)
+
+    @movie_search = json[:results].map do |movie_data|
+      Movie.new(movie_data)
+    end
+  end
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,9 +1,4 @@
 class MovieService 
-  def self.top_rated 
-    response = conn.get("movie/top_rated")
-    json = JSON.parse(response.body, symbolize_names: true)
-  end
-
   def self.conn 
     Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
       faraday.params['api_key'] = ENV["movie_api_key"]
@@ -11,4 +6,13 @@ class MovieService
     end
   end
 
+  def self.top_rated 
+    response = conn.get("movie/top_rated")
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.movie_search(query)
+    response = conn.get("search/keyword?query=#{query}")
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
 end
