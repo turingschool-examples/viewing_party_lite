8a122883baa4a1d38dcda0026615610eb8af2ec3
Becky Nisttahuz <benistta@hotmail.com>
2022-03-23 21:08:05 -0600

    Refactor: controller for viewing party

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,6 +1,9 @@
 class ViewingPartyController < ApplicationController
   def new
+    # require "pry"; binding.pry
     @user = User.find(params[:user_id])
     @movie = Movie.new(id: 22, title: "Pirates of the Caribbean: The Curse of the Black Pearl", runtime: 102, vote_average: 10)
+    # # @movie = Movie.find(params[:movie_id])
+    # @movie = Movie.new.search(params[:movie_id])
   end
 end
