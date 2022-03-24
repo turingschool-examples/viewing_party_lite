58f7970a54a4bb9ebd5cb26cb75550334a8cba88
Conor Barthel <conor.barthel@gmail.com>
2022-03-24 08:18:49 -0600

    fix:database issues

--- db/migrate/20220321221525_create_movies.rb
+++ db/migrate/20220321221525_create_movies.rb
@@ -1,13 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.string :title
-      t.integer :rating
-      t.string :runtime
-      t.text :summary
-      t.integer :review_count
-      t.string :image
-      t.references :genre, foreign_key: true
+      t.integer :api_key
 
       t.timestamps
     end
--- db/migrate/20220324120128_drop_genres.rb
+++
@@ -1,5 +0,0 @@
-class DropGenres < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :genres
-  end
-end
