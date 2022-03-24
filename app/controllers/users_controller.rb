4a7cd8ae2a27ca2d8402ee5398ec7bc47f7532c9
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-23 20:58:19 -0400

    Feat: add users and parties to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  before_action :set_parties, only: [:show]
+
   def new
     @user = User.new
   end
@@ -8,10 +11,20 @@ class UsersController < ApplicationController
     redirect_to user_path(user)
   end
 
-  def show;end
+  def show
+    
+  end
 
   private
     def user_params
       params.require(:user).permit(:name, :email)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def set_parties
+      @parties = @user.parties
     end
 end
