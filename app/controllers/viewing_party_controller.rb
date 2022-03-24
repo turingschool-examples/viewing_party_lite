c53be55ad031c5301f7bce3c47561c637d1894c7
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-24 18:08:09 -0500

    Refactor: Update controller to use new API endpoint

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,15 +1,12 @@
 class ViewingPartyController < ApplicationController
   before_action :find_user_and_movie, only: [:new]
 
-  def new
-    @movie = Movie.new(id: 22, title: 'Pirates of the Caribbean: The Curse of the Black Pearl', runtime: 102,
-      vote_average: 10)
-  end
+  def new ; end
 
   private
   
   def find_user_and_movie
     @user = User.find(params[:user_id])
-    # @movie = Movie.find(params[:movie_id])
+    @movie = MovieFacade.movie_id_search(params[:movie_id])
   end
 end
