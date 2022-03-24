06d76ac0e3fd158cbc23236989277911ab2d758a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-24 11:35:29 -0500

    Rubocop auto-correct offenses

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper'
-# When I visit the new viewing party page (/users/:user_id/movies/:movid_id/viewing-party/new, where :user_id is a valid user's id),
-# I should see the name of the movie title rendered above a form with the following fields:
-#
-#  Duration of Party with a default value of movie runtime in minutes; a viewing party should NOT be created if set to a value less than the duration of the movie
-#  When: field to select date
-#  Start Time: field to select time
-#  Checkboxes next to each existing user in the system
-#  Button to create a party
-# Details When the party is created, the user should be redirected back to the dashboard where the new event is shown. The event should also be listed on any other user's dashbaords that were also invited to the party.
 
 RSpec.describe 'New party page' do
   it 'When i visit the new party page has title and form', :vcr do
