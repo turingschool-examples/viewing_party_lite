12b8b8e1ea5ac736afb8edbdf7591e1bace3d32b
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-03-24 17:46:32 -0400

    test: review poro

--- app/facades/top_facade.rb
+++ app/facades/top_facade.rb
@@ -1,13 +1,13 @@
 require './app/poros/movie'
 
 class TopFacade
-  attr_reader :top_rated
-  def initialize
+
+  def self.top_rated
     @top_rated = []
     MovieService.top_rated[:results].each do |data|
       top_movie = MovieService.movie(data[:id])
       @top_rated << Movie.new(top_movie)
-
     end
+      @top_rated
   end
 end
