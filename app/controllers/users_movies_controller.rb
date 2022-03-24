10a27687f1d2e0d6db81787b6841e4ab8457df60
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-24 07:53:04 -0600

    refactor: move functional API call from usersmoviescontroller into encapsulated movie_service to make api call, and top_rated/movie_search facades. Tests pass

--- app/controllers/users_movies_controller.rb
+++ app/controllers/users_movies_controller.rb
@@ -1,27 +1,12 @@
 class UsersMoviesController < ApplicationController
+
   def index
     @user = User.find(params[:user_id])
-    conn = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
-      faraday.params[:api_key] = ENV['movie_api_key']
-      faraday.params['query'] = params[:search]
-    end
-
     if params[:top_rated] == "top_rated"
-      top_rated_response = conn.get("movie/top_rated")
-      data = JSON.parse(top_rated_response.body, symbolize_names: true)
-
-      @movies = data[:results].map do |result|
-          Movies.new(result)
-      end.take(20)
-
+      @top_movies = TopRated.new.movies
     elsif params[:search]
-      response = conn.get("search/movie")
-      data = JSON.parse(response.body, symbolize_names: true)
-      
-      @movies = data[:results].map do |result|
-        Movies.new(result)
-      end.take(40)
-    end
+      @movies = MovieSearch.new.search(params[:search])
+    end 
   end
 
   def show 
--- app/poros/movies.rb
+++ app/poros/movies.rb
@@ -1,7 +1,7 @@
-class Movies 
+class Movies
   attr_reader :id, :title
   def initialize(data)
     @id = data[:id]
     @title = data[:title]
-  end 
-end 
+  end
+end
