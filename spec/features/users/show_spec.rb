7690de0d0cd39628ca7b0823893063b778fcbf4f
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-24 15:30:56 -0500

    Test: Viewing party appears on user dashboard

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'User Dashboard Page' do
   before(:each) do
-    @vp1 = ViewingParty.create!(movie_id: 111, duration: 151, date: Time.new(2022, 0o4, 12, 21, 0o0),
-                                start_time: Time.new(2022, 0o4, 12, 21, 0o0))
-    @vp2 = ViewingParty.create!(movie_id: 112, duration: 152, date: Time.new(2022, 0o4, 11, 20, 30),
-                                start_time: Time.new(2022, 0o4, 11, 20, 30))
+    @vp1 = ViewingParty.create!(movie_id: 111, duration: 151, date: Time.new(2022, 04, 12, 21, 00),
+                                start_time: Time.new(2022, 04, 12, 21, 0o0))
+    @vp2 = ViewingParty.create!(movie_id: 550, duration: 152, date: Time.new(2022, 04, 11, 20, 30),
+                                start_time: Time.new(2022, 04, 11, 20, 30))
+    @vp3 = ViewingParty.create!(movie_id: 314, duration: 154, date: Time.new(2022, 04, 14, 18, 30),
+                                start_time: Time.new(2022, 04, 14, 18, 30))
     @user1 = User.create!(name: 'Becky', email: 'becky@example.com')
     @user2 = User.create!(name: 'Steven', email: 'steven@example.com')
     @user5 = User.create!(name: 'Bruce', email: 'Bruce@example.com')
-    @user6 = User.create!(name: 'Tony', email: 'Tony@example.com')
     @up1 = UserParty.create!(viewing_party: @vp1, user: @user1, host: true)
     @up2 = UserParty.create!(viewing_party: @vp1, user: @user2, host: false)
     @up5 = UserParty.create!(viewing_party: @vp2, user: @user5, host: true)
-    @up6 = UserParty.create!(viewing_party: @vp2, user: @user6, host: false)
+    @up6 = UserParty.create!(viewing_party: @vp2, user: @user1, host: false)
 
     visit user_path(@user1)
   end
@@ -33,5 +34,15 @@ RSpec.describe 'User Dashboard Page' do
         expect(page).to have_content('Viewing Parties')
       end
     end
+  end
+
+  context 'Viewing Parties' do 
+    it '' do 
+      within '#viewing_parties' do 
+        # expect(page).to have_content(@vp1.)
+
+      end
+    end
+
   end
 end
