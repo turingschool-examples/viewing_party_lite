69c69f60da5794d44b1a578678665550c9e1805a
Blake Dempsey <bdempsey864@gmail.com>
2022-03-24 16:25:54 -0400

    Feat: Add Poro/Facade/Service for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,37 +1,9 @@
 class MoviesController < ApplicationController
   def index
-    # @jam = MovieService.discover_movies(params[:search])
-    if params[:search] == "top_rated"
-      conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-        faraday.params["api_key"] = ENV['movie_api_key']
-      end
-      response = conn.get("/3/movie/top_rated")
-
-      data = JSON.parse(response.body, symbolize_names: true)
-
-      @movie_titles = data[:results].map do |result|
-        result[:title]
-      end
-
-      @movie_vote_average = data[:results].map do |result|
-        result[:vote_average]
-      end
+    if params[:top_rated] == "top_rated"
+      @movies = MovieFacade.top_movies
     elsif params[:search] != nil
-      conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-        faraday.params["api_key"] = ENV['movie_api_key']
-      end
-
-      response = conn.get("/3/search/movie?query=#{params[:search]}")
-
-      data = JSON.parse(response.body, symbolize_names: true)
-
-      @movie_titles = data[:results].map do |result|
-        result[:title]
-      end
-
-      @movie_vote_average = data[:results].map do |result|
-        result[:vote_average]
-      end
+      @movies = MovieFacade.keywords(params[:search])
     end
   end
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,37 +1,17 @@
-# class MovieService
-#   def self.discover_movies(search_keyword)
-#     if search_keyword == "top_rated"
-#       conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-#         faraday.params["api_key"] = ENV['movie_api_key']
-#       end
-#       response = conn.get("/3/movie/top_rated")
-#
-#       data = JSON.parse(response.body, symbolize_names: true)
-#
-#       @movie_titles = data[:results].map do |result|
-#         result[:title]
-#       end
-#
-#       @movie_vote_average = data[:results].map do |result|
-#         result[:vote_average]
-#       end
-#     elsif search_keyword != nil
-#       conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-#         faraday.params["api_key"] = ENV['movie_api_key']
-#       end
-#
-#       response = conn.get("/3/search/movie?query=#{search_keyword}")
-#
-#       data = JSON.parse(response.body, symbolize_names: true)
-#
-#       @movie_titles = data[:results].map do |result|
-#         result[:title]
-#       end
-#
-#       @movie_vote_average = data[:results].map do |result|
-#         result[:vote_average]
-#       end
-#     end
-#   end
-#
-# end
+class MovieService
+  def self.top_rated_movies
+    response = conn.get("/3/movie/top_rated")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.movie_keywords(keyword)
+    response = conn.get("/3/search/movie?query=#{keyword}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = ENV['movie_api_key']
+    end
+  end
+end
