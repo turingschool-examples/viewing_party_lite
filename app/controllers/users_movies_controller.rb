d100adce7afbe7640dfcddd7ffdd9cf815550c06
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-24 08:20:13 -0600

    add render 'index' to conditiional statements in index action of UsersMoviesController in order to properly render instance variables in view.Tests pass

--- app/controllers/users_movies_controller.rb
+++ app/controllers/users_movies_controller.rb
@@ -3,9 +3,11 @@ class UsersMoviesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     if params[:top_rated] == "top_rated"
-      @top_movies = TopRated.new.movies
+      @top_rated = TopRated.new.movies
+      render 'index'
     elsif params[:search]
-      @movies = MovieSearch.new.search(params[:search])
+      @found_movies = MovieSearch.new.search(params[:search])
+      render 'index'
     end 
   end
 
