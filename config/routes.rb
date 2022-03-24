cbe6a098f78b899b99afb73d8f7a09a9097954b5
Becky Nisttahuz <benistta@hotmail.com>
2022-03-23 20:32:04 -0600

    Feat:Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   get '/', to: 'application#landing'
   get '/register', to: 'users#new', as: "new_user"
-  resources :users, only: [:show, :create] do 
+  resources :users, only: [:show, :create] do
     resources :discover, only: [:index]
-    resources :movies, only: [:index, :show]
+    resources :movies, only: [:index, :show] do
+      resources :viewing_party, only: [:new, :create]
+    end
   end
 end
