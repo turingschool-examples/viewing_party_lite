7b75efa2c9245703f1f881d5595d34bbed76f61b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-24 17:28:19 -0500

    Create method to return movie data for a viewing party

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -3,4 +3,8 @@ class ViewingParty < ApplicationRecord
   has_many :users, through: :user_parties
 
   validates_presence_of :movie_id, :duration, :date, :start_time
+
+  def movie_data
+    MovieFacade.movie_id_search(movie_id)
+  end
 end
