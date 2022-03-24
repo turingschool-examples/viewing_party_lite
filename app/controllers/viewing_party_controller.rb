7af02a2926ddb194df45a9e718a3bc2b801d60f4
Becky Nisttahuz <benistta@hotmail.com>
2022-03-23 20:33:51 -0600

    Feat: Create viewing party controller and new method

---
+++ app/controllers/viewing_party_controller.rb
@@ -0,0 +1,6 @@
+class ViewingPartyController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @movie = Movie.new(id: 22, title: "Pirates of the Caribbean: The Curse of the Black Pearl", runtime: 102, vote_average: 10)
+  end
+end
