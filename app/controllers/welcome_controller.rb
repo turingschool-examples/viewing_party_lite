527afbf3ac26343d2c4f425198ab633562136bb0
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-24 16:04:12 -0600

    delete API lines of code not working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    top_rated_movies = MoviesFacade.find_top_rated
     @users = User.all
   end
 
---
+++ app/facades/movie_facade.rb
@@ -0,0 +1,3 @@
+class MovieFacade
+
+end
