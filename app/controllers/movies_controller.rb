70fa65a24cea320c1377b8644e3b91cc1276939d
Sam Iavri <sammi80134@gmail.com>
2022-03-24 16:51:50 -0600

    clean up

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,10 +2,8 @@ class MoviesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @movies = if params[:keyword]
-                # DO Search
                 MovieFacade.search(params[:keyword])
               else
-                # Top Rated
                 MovieFacade.top_movie
               end
   end
