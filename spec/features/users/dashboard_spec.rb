8724778165df96b3329bf16c1cef429439b24222
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-24 08:31:17 -0400

    Refactor: refactor tests for new schema

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe 'User Dashboard' do
     before(:each) do
       @jax = User.create!(name: "Jackson", email: "j@jmail.com")
 
-      @movie_1 = Movie.create!(title: "Fight Club")
-      @movie_2 = Movie.create!(title: "Inception")
+      @movie_1 = Movie.create!(api_id: 550)
+      @movie_2 = Movie.create!(api_id: 600)
 
-      @party_1 = Party.create!(movie_id: @movie_1.id, duration: 126, date: "3/22/22", start: '7:00 pm')
-      @party_2 = Party.create!(movie_id: @movie_2.id, duration: 108, date: "3/23/22", start: '8:00 pm')
+
+      @party_1 = Party.create!(movie_id: @movie_1.id, duration: 126, date: Date.new(2022, 3, 21), start: '7:00 pm')
+      @party_2 = Party.create!(movie_id: @movie_2.id, duration: 108, date: Date.new(2022, 3, 22), start: '8:00 pm')
 
       @party_user_1 = PartyUser.create!(is_host: true, party_id: @party_1.id, user_id: @jax.id)
       @party_user_2 = PartyUser.create!(is_host: false, party_id: @party_2.id, user_id: @jax.id)
@@ -19,12 +20,12 @@ RSpec.describe 'User Dashboard' do
       visit user_path(@jax)
       expect(current_path).to eq(user_path(@jax))
 
-      within "div.user_name" do
-        expect(page).to have_content(("#{@jax.name}'s Dashboard'"))
+      within "div.name" do
+        expect(page).to have_content("#{@jax.name}'s Dashboard")
       end
     end
 
-    it 'has users name' do
+    it 'has discover movie button' do
       visit user_path(@jax)
       expect(current_path).to eq(user_path(@jax))
 
@@ -41,14 +42,12 @@ RSpec.describe 'User Dashboard' do
 
       expect(page).to have_content("Viewing Parties")
 
-      within "div.parties-#{@party_user_1.id}" do
-        expect(page).to have_content("March 22, 2022")
-        expect(page).to have_content("Hosting")
+      within "div.parties-#{@party_1.id}" do
+        expect(page).to have_content("March 21, 2022")
       end
 
-      within "div.parties-#{@party_user_1.id}" do
-        expect(page).to have_content("March 23, 2022")
-        expect(page).to have_content("Invited")
+      within "div.parties-#{@party_2.id}" do
+        expect(page).to have_content("March 22, 2022")
       end
     end
   end
