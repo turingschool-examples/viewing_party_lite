bbaa4bf9d9b002bbc77eaf71f144a7ef6dd8fe62
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-23 20:58:05 -0400

    Feat: add movie poro

---
+++ app/poros/movie.rb
@@ -0,0 +1,23 @@
+class Movie
+  attr_reader :api_id,
+             :title,
+             :summary,
+             :runtime,
+             :vote_average,
+             :genres,
+             :hours,
+             :minutes,
+             :poster
+
+ def initialize(data)
+   @api_id = data[:id]
+   @title = data[:title]
+   @summary = data[:overview]
+   @hours = (data[:runtime] / 60)
+   @minutes = (data[:runtime] % 60)
+   @runtime = data[:runtime]
+   @vote_average = data[:vote_average]
+   @genres = data[:genres]
+   @poster = data[:poster_path]
+ end
+end 
