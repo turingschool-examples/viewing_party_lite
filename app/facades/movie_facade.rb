03f095942d8fe2e94049554c708378e28031397e
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-03-24 17:48:22 -0400

    refactor: remove top facade into movie facade

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -21,4 +21,13 @@ class MovieFacade
     end
     @reviews
   end
+
+  def self.top_rated
+    @top_rated = []
+    MovieService.top_rated[:results].each do |data|
+      top_movie = MovieService.movie(data[:id])
+      @top_rated << Movie.new(top_movie)
+    end
+      @top_rated
+  end
 end
