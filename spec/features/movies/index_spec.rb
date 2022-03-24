0ddb12bce186fb350c43c40308fad70f38211cda
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-23 21:35:57 -0400

    Test/Feat: Movie Results and discover page uses vcr gem

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -8,22 +8,28 @@ RSpec.describe 'Movie Results Page', type: :feature do
   end
 
   it "displays Top Rated Movies" do 
-    click_on("Find Top Rated Movies")
-    expect(current_path).to eq("/users/#{@user1.id}/movies")
-    expect(page).to have_content("The Shawshank Redemption")
-    expect(page).to have_content(8.7)
+    VCR.use_cassette('top_movies_results', re_record_interval: 7.days) do 
+      click_on("Find Top Rated Movies")
+      expect(current_path).to eq("/users/#{@user1.id}/movies")
+      expect(page.status_code).to eq 200
+      expect(page).to have_content("The Shawshank Redemption")
+      expect(page).to have_content(8.7)
+    end
   end 
 
-  it "has button to return to Discover Page" do 
+  it "has button to return to Discover Page", :vcr do 
     click_on("Find Top Rated Movies")
     click_on("Discover Page")
     expect(current_path).to eq("/users/#{@user1.id}/discover")
   end 
 
   it "displays movie search results" do 
-    expect(page).to have_button("Find Movies")
-    fill_in("movie", with: "Aliens")
-    click_on("Find Movies")
-    expect(current_path).to eq("/users/#{@user1.id}/movies")
+    VCR.use_cassette('movie_search_results', re_record_interval: 7.days) do
+      expect(page).to have_button("Find Movies")
+      fill_in("movie", with: "Aliens")
+      click_on("Find Movies")
+      expect(current_path).to eq("/users/#{@user1.id}/movies")
+      expect(page).to have_content("Aliens")
+    end 
   end 
 end
