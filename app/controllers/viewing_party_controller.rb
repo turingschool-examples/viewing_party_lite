89fdaf0ac0d8c62771e66d2932c3d6455fb1722d
Becky Nisttahuz <benistta@hotmail.com>
2022-03-23 23:08:05 -0600

    Refactor: Viewing party new method

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -2,6 +2,5 @@ class ViewingPartyController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @movie = Movie.new(id: 22, title: "Pirates of the Caribbean: The Curse of the Black Pearl", runtime: 102, vote_average: 10)
-
   end
 end
