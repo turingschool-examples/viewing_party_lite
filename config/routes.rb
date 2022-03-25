dc5732a8951a0d82e709df704b831c87fde96b02
Sully Birashk <sully.birashk@gmail.com>
2022-03-25 08:08:12 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/users', to: 'users#create'
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :movies, only: [:index, :show]
+    resources :discover, only: [:index]
+  end
 
   namespace :user, only: [:index, :show], module: :user do
     resources :parties, only: [:index, :show, :new, :create]
