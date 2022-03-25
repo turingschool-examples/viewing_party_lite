ca92c4a577fde8fc74c7fd2d395e6e4331b280b5
Maximilian Wagner <maw3350@gmail.com>
2022-03-25 08:53:04 -0400

    fix: update poro to include id

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,8 +1,10 @@
 class Movie
-  attr_reader :title,
+  attr_reader :id,
+              :title,
               :vote_average
 
   def initialize(result)
+    @id = result[:id]
     @title = result[:title]
     @vote_average = result[:vote_average]
   end
