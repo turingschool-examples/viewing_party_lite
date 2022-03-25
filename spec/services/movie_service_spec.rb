8a0f89da5d4e6524d2ce325918ee30bf0ea5f0be
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-24 21:01:48 -0600

    Add movie service tests

---
+++ spec/services/movie_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe MovieService do
+  describe 'class methods' do
+    it 'returns top rated movies' do
+      movies = MovieService.top_rated
+      
+      expect(movies).to be_an(Array)
+      expect(movies[0][:title]).to eq('The Shawshank Redemption')
+    end
+
+    xit 'searches for a movie given a keyword' do
+      search = MovieService.search_for_movie("shawshank")
+      expect(search).to be('The Shawshank Redemption')
+    end
+
+    it 'creates cast member poros' do
+      cast_members = MovieService.cast_members(278)
+
+      expect(cast_members).to be_an(Hash)
+    end
+  end
+end
