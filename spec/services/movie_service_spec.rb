e6f778f1f8a2a8af28dc75dcdda60a181e65c2f4
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-24 19:14:45 -0600

    test: movie service #get_top_movies #get_movie_credits #get_movie_details

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,37 +2,35 @@ require 'rails_helper'
 RSpec.describe MovieService do
   context 'class methods' do
     context '#top_rated_movies' do
-      xit 'returns top_rated_movies' do
-        search = MovieService.top_rated_movies
+      it 'returns top_rated_movies' do
+        search = MovieService.get_top_rated_movies
         expect(search).to be_a Hash
         expect(search[:results]).to be_a Array
-        # expect(search[:results][:original_title]).to be_a String
-        # expect(search[:results][:vote_average]).to be_a Integer
       end
     end
+
     context '#movie_details' do
-      xit 'returns movie_details' do
+      it 'returns movie_details' do
         search = MovieService.get_movie_details(19404)
         expect(search).to be_a Hash
         expect(search[:genres]).to be_a Array
         expect(search[:runtime]).to be_a Integer
-
       end
     end
+
     context '#movie_reviews' do
       it 'returns movie reviews' do
         search = MovieService.get_movie_reviews(19404)
         expect(search).to be_a Hash
         expect(search[:results]).to be_a Array
-
       end
     end
+
     context '#movie_credits' do
-      xit 'returns movie_credits' do
+      it 'returns movie_credits' do
         search = MovieService.get_movie_credits(19404)
         expect(search).to be_a Hash
-        expect(search[:results]).to be_a Array
-
+        expect(search[:cast]).to be_a Array
       end
     end
   end
