bcd3439390f055e73d2a09c6153eef02dd3ae9c8
Blake Dempsey <bdempsey864@gmail.com>
2022-03-25 14:15:16 -0400

    Feat: Viewing Party New Page

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,4 +1,12 @@
 class PartiesController < ApplicationController
   def new
+    @users = User.all
+    @user = User.find(params[:user_id])
+    @movie = MovieFacade.get_movie(params[:movie_id])
   end 
+
+  def create
+    @user = User.find(params[:user_id])
+    redirect_to "/users/#{@user.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
-
+  # get '/users/new', to: 'users#new'
   get '/register', to: 'users#new'
   post '/search', to: 'movies#search'
   # post '/users', to: 'users#create'
   resources :users, only: [:create, :show] do
     resources :discover, only: [:index]
     resources :movies, only: [:index, :show] do
-      resources :parties, only: [:new]
+      resources :parties, only: [:new, :create]
     end
   end
   
