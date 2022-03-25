9e1b10c2177d53b3b1b3949d12832358eaa12935
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-25 12:17:07 -0400

    Test: add model test for get info

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -7,10 +7,23 @@ RSpec.describe Movie, type: :model do
   end
 
   describe '.instance methods' do
+    before(:each) do
+      @movie_1 = Movie.create!(api_id: 550)
+    end
+
     describe '.show_poster' do
       it 'shows poster image of movie' do
-        movie_1 = Movie.create!(api_id: 550)
-        expect(movie_1.get_poster).to eq("https://image.tmdb.org/t/p/w200/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg")
+
+        expect(@movie_1.get_poster).to eq("https://image.tmdb.org/t/p/w200/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg")
+      end
+    end
+
+    describe '.get_info' do
+      it 'gets all api attributes' do
+        VCR.use_cassette('fight_club_api') do
+          expect(@movie_1.get_info).to be_an_instance_of(MovieCall)
+          expect(@movie_1.get_info.title).to eq("Fight Club")
+        end
       end
     end
   end
