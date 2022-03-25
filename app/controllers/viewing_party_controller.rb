c30a804a8c04938bd9a712c55a29b396b1d44fed
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-25 13:52:17 -0600

    Final Push:
    Co-Authored-By: Eldridge Turambi <Eldridge-Turambi@users.noreply.github.com>

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -5,4 +5,10 @@ class ViewingPartyController < ApplicationController
     @user = User.find(params[:user_id])
     @users = User.all
   end
+
+  def create
+    binding.pry
+    @user = User.find(params[:user_id])
+    redirect_to user_path(@user.id)
+  end
 end
