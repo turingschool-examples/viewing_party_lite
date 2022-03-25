d14358c224288a3e57b6e62f6f79900dc1a4ce8e
Conor Barthel <conor.barthel@gmail.com>
2022-03-25 09:52:26 -0600

    fix: changed test file to have correct div

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'movie results page' do
       fill_in "Search", with: "fight"
       click_on 'Find Movies'
 
-      expect(page).to have_content("Movie results for: fight")
+      expect(page).to have_content("Movie Results for: fight")
       expect(page).to_not have_content("Top Rated Movies")
     end
     visit user_discover_index_path(@user)
@@ -32,6 +32,16 @@ RSpec.describe 'movie results page' do
     end
   end
 
+  it "lists the top rated movies" do
+    visit user_discover_index_path(@user)
+    VCR.use_cassette('top_20_api') do
+      click_on 'Find Top Rated Movies'
+
+      expect(page).to have_content('The Shawshank Redemption')
+      expect(page).to have_content('Vote Average: 8.7')
+    end
+  end
+
   it "has links to movies in search results and vote averages" do
     visit user_discover_index_path(@user)
     VCR.use_cassette('fight_results_api') do
@@ -40,8 +50,7 @@ RSpec.describe 'movie results page' do
 
       expect(page).to have_content('Fight Club')
       expect(page).to_not have_content('Godfather')
-      expect(page).to have_content('Vote Average: 5')
-      expect(page).to_not have_content('Vote Average: 8')
+      expect(page).to have_content('Vote Average: 8.4')
     end
   end
 end
