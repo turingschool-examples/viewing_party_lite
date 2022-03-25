c356613375fe1ae0194c3b97afeb4f69103d2336
Conor Barthel <conor.barthel@gmail.com>
2022-03-25 07:38:42 -0600

    Refactor: tests to use vcr cassettes

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 RSpec.describe 'movie results page' do
   before(:each) do
     @user = User.create!(name: 'Trevor', email: 'trev@yahoo.com')
-    @user2 = User.create!(name: 'Kat', email: 'kat@yahoo.com', search: 'fight')
-    @movie = Movie.create!(title: 'Fight Club', vote_average: 5)
-    @movie2 = Movie.create!(title: 'Godfather', vote_average: 8)
+    @user2 = User.create!(name: 'Kat', email: 'kat@yahoo.com')
   end
-  #might ping API consider refactoring
-  it "has links to movies in search results" do
-    visit user_movies_path(@user2)
 
-    expect(page).to have_content('Fight Club')
-    expect(page).to_not have_content('Godfather')
-    expect(page).to have_content('Vote Average: 5')
-    expect(page).to_not have_content('Vote Average: 8')
+  it "has links to movies in search results" do
+    visit user_discover_index_path(@user)
+    VCR.use_cassette('fight_results_api') do
+      fill_in "Search", with: "fight"
+      click_on 'Find Movies'
+      expect(page).to have_content('Fight Club')
+      expect(page).to_not have_content('Godfather')
+      expect(page).to have_content('Vote Average: 5')
+      expect(page).to_not have_content('Vote Average: 8')
+    end
   end
 end
--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -5,20 +5,22 @@ RSpec.describe 'Discover Page' do
     before(:each) do
       @user = User.create!(name: 'Trevor', email: 'trev@yahoo.com')
     end
-    #these two tests might ping the API, not sure
+
     it 'should have a button to discover top 20 rated movies' do
       visit user_discover_index_path(@user)
-
-      click_on 'Find Top Rated Movies'
-      expect(current_path).to eq(user_movies_path(@user))
+      VCR.use_cassette('top_20_api') do
+        click_on 'Find Top Rated Movies'
+        expect(current_path).to eq(user_movies_path(@user))
+      end
     end
 
     it 'has a search bar to find up to 40 movies' do
       visit user_discover_index_path(@user)
-      vcr.use_cassette
-      fill_in "Search", with: "fight"
-      click_on 'Find Movies'
-      expect(current_path).to eq(user_movies_path(@user))
+      VCR.use_cassette('fight_results_api') do
+        fill_in "Search", with: "fight"
+        click_on 'Find Movies'
+        expect(current_path).to eq(user_movies_path(@user))
+      end
     end
   end
 end
