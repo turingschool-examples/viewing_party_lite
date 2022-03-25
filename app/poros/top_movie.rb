68aad1e16de7b454fdce064b0819aa407ce99c08
Sully Birashk <sully.birashk@gmail.com>
2022-03-25 09:14:56 -0700

    Change Poros

--- app/poros/top_movie.rb
+++ app/poros/top_movie.rb
@@ -1,10 +1,11 @@
 
 class TopMovie
-  attr_reader :title, :vote
+  attr_reader :title, :vote, :id
 
   def initialize(attributes)
     @title = attributes[:original_title]
     @vote = attributes[:vote_average]
+    @id = attributes[:id]
   end
 
-end 
+end
