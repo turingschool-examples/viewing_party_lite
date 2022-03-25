d12b73c0e5d1e3a53d4f5494d53a990666f956b9
Sully Birashk <sully.birashk@gmail.com>
2022-03-25 10:21:11 -0700

    Update Controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,15 @@ class MoviesController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @top_movies = MovieFacade.top_rated_movies
+    if params[:search] == nil
+      @movies = MovieFacade.top_rated_movies
+    else
+      @movies = MovieFacade.movie_search(params[:search])
+    end
   end
 
   def show
-    
+
   end
 
 end
