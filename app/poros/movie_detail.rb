35a276a41bf5bec72d4b23c0f2701a97cfc3a01d
Sam Iavri <sammi80134@gmail.com>
2022-03-25 10:46:04 -0600

    Movie Details page, half way complete

--- app/poros/movie_detail.rb
+++ app/poros/movie_detail.rb
@@ -13,6 +13,13 @@ class MovieDetail
   def initialize(movie_details, _movie_cast, _movie_reviews)
     @id = movie_details[:id]
     @title = movie_details[:title]
-    runtime = movie_details[:runtime]
+    @runtime = (movie_details[:runtime].to_f / 60).round(2)
+    @vote_average = movie_details[:vote_average]
+    @genres = all_genre(movie_details)
+    @summary = movie_details[:overview]
+  end
+
+  def all_genre(movie_details)
+    movie_details[:genres].map { |genre| genre[:name] }
   end
 end
