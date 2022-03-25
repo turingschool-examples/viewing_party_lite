b32fe9d88287fbb0830c5f30397fb784aee23a0d
Maximilian Wagner <maw3350@gmail.com>
2022-03-25 10:35:14 -0400

    test: add working tests to service file

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,24 +2,41 @@ require 'rails_helper'
 
 RSpec.describe MovieService do
   context 'class methods' do
-    context '#top_rated' do
+    context '#get_top_movie' do
 
       it 'returns top rated movies' do
 
 
-        movie = MovieService.new
-        binding.pry
-        movies = movie.top_rated
+        response = MovieService.get_top_movie
 
-        movie_data = movies.first
+        expect(response).to be_an(Array)
 
-        expect(movies).to be_a(Array)
-        expect(movie_data).to have_key(:title)
-        expect(movie_data[:title]).to be_a(string)
-        expect(movie_data).to have_key(:id)
-        expect(movie_data[:id]).to be_a(string)
-        expect(movie_data).to have_key(:vote_average)
-        expect(movie_data[:vote_average]).to be_a(string)
+        first_movie_data = response.first
+
+        expect(first_movie_data).to have_key(:title)
+        expect(first_movie_data[:title]).to be_a(String)
+
+        expect(first_movie_data).to have_key :id
+        expect(first_movie_data[:id]).to be_a(Integer)
+      end
+
+    end
+
+    context '#get_search_movie' do
+
+      it "returns a list to a search query" do
+
+        response = MovieService.get_search_movie('the')
+
+        expect(response).to be_an(Array)
+
+        first_movie_data = response.first
+
+        expect(first_movie_data).to have_key(:title)
+        expect(first_movie_data[:title]).to be_a(String)
+
+        expect(first_movie_data).to have_key :id
+        expect(first_movie_data[:id]).to be_a(Integer)
       end
     end
   end
