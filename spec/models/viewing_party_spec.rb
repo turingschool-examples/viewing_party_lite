cb82e76c5099a3bb97599965b1689d83510dbbc0
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-03-25 12:50:08 -0400

    feat: poro, facasde, services testing for streaming

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -7,9 +7,18 @@ RSpec.describe ViewingParty do
     it { should have_many(:user_viewing_parties) }
     it { should have_many(:users).through(:user_viewing_parties) }
   end
-  it 'should assign the duration of a movie after validations', :vcr do
-    user = User.create(name: 'test name', email: 'test@yahoo.com')
-    vp = ViewingParty.create(user: user, date_time: '2022-03-21 22:02:34', movie_id: 143)
-    expect(vp.duration).to eq(133)
+  describe 'self check'do
+    it 'should assign the duration of a movie after validations', :vcr do
+      user = User.create(name: 'test name', email: 'test@yahoo.com')
+      vp = ViewingParty.create(user: user, date_time: '2022-03-21 22:02:34', movie_id: 143)
+      expect(vp.duration).to eq(133)
+    end
+  end
+  describe 'instance method', :vcr do
+    it 'knows its endtime' do
+      user = User.create(name: 'test name', email: 'test@yahoo.com')
+      vp = ViewingParty.create(user: user, date_time: '2022-03-21 22:02:34', movie_id: 143)
+      expect(vp.end_time).to eq("12:15 AM")
+    end
   end
 end
