b9bb5cfcf14473f382fb6eb9a28cfdef9edaae41
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-25 08:09:35 -0600

    Feat: Add route for Viewing Party

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "landing#index"
   resources :users do
     resources :discover, only: [:index]
-    resources :movies, only: [:index, :show]
+    resources :movies, only: [:index, :show] do
+      resources :viewing_party, only: [:new, :create]
+    end
   end
 
   post '/users/new', to: 'users#create'
