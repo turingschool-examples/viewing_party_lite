0b7bdfafb09c38bbc2d9f9de2ef22961fa1b5188
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-25 12:23:43 -0600

    Faet: Viewing party

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,5 +1,8 @@
 class ViewingPartyController < ApplicationController
 
   def new
+    @movie = MovieFacade.movie_details(params[:movie_id])
+    @user = User.find(params[:user_id])
+    @users = User.all
   end
 end
