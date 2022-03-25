65a40cc2d3db310b37c3be87435d1277547145c6
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-25 12:30:14 -0500

    Add instance methods to model spec

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe ViewingParty, type: :model do
     it { should validate_presence_of :date }
     it { should validate_presence_of :start_time }
   end
+
+  describe 'instance_methods' do 
+    describe '.movie_data' do 
+      it 'returns movie data by id from api', :vcr do 
+        vp1 = ViewingParty.create!(movie_id: 111, duration: 151, date: Time.new(2022, 04, 12, 21, 00),
+                            start_time: Time.new(2022, 04, 12, 21, 00, 00, "-06:00"))            
+        scarface = vp1.movie_data
+        expect(scarface.title).to eq("Scarface")
+        expect(scarface.runtime).to eq(170)
+        expect(scarface.id).to eq(111)
+        expect(scarface.vote_average).to eq(8.2)
+
+      end
+    end
+  end
 end
