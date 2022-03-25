06c90761c7bd6d957218f203537235e7e44c3af0
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-25 10:13:30 -0600

    test: write test for party model instance method find_host. Define method in model. Pass test

--- app/models/party.rb
+++ app/models/party.rb
@@ -5,4 +5,8 @@ class Party < ApplicationRecord
   validates :start_date, :start_time, :duration, :movie_id, :host_id, presence: true
 
   validates :duration, :movie_id, :host_id, numericality: { greater_than: 0 }
+
+  def find_host
+    users.find(host_id).name
+  end
 end
