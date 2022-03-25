5b9613883b0fa1e5212565d8c3c85c1b93ba7d7e
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-24 18:58:25 -0600

    feat: movieFacade movie_review

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,3 +1,8 @@
 class MovieFacade
-
+  def self.movie_reviews(movie_id)
+    reviews_data = MovieService.get_movie_reviews(movie_id)
+    reviews_data[:results].map do |review|
+      MovieReview.new(review)
+    end
+  end
 end
