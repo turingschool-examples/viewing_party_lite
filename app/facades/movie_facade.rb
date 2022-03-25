202824ba337ee92e521a9fd2ed69166cdea7aba1
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-24 21:00:42 -0600

    Add to movie facade

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,5 +1,26 @@
 class MovieFacade
-  def service
-    MovieService.new
+
+  def self.search(keyword)
+    response = MovieService.search_for_movie(keyword)
+    response.map do |data|
+      Movie.new(data)
+    end.take(40)
+  end
+  
+  def self.find_top_rated_movies
+    response = MovieService.top_rated
+    
+    response.map do |data|
+      Movie.new(data)
+    end
+  end
+
+  def self.cast_members(movie_id)
+    response = MovieService.cast_members(movie_id)
+    require 'pry'; binding.pry
+
+    response.map do |data|
+      Movie.new(data)
+    end
   end
 end
