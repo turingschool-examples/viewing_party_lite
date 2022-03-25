3176037a2c1afe47a2726c8bae04f94205658928
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-25 14:12:40 -0500

    Viewing party create and form working

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class ViewingPartyController < ApplicationController
-  before_action :find_user_and_movie, only: [:new]
+  before_action :find_user_and_movie, only: [:new, :create]
 
   def new; end
+
+  def create 
+    @vp = ViewingParty.create!(movie_id: params[:movie_id], duration: params[:duration], date: params[:date], start_time: params[:start_time])
+    redirect_to user_path(@user)
+  end
 
   private
 
