4ab28dfaf800d09fa9f94d7f83db220d228f1245
Sam Iavri <sammi80134@gmail.com>
2022-03-25 11:36:26 -0600

    Movie details page complete

--- app/poros/movie_detail.rb
+++ app/poros/movie_detail.rb
@@ -10,7 +10,7 @@ class MovieDetail
               :review_count,
               :poster_path
 
-  def initialize(movie_details, movie_cast, _movie_reviews)
+  def initialize(movie_details, movie_cast, movie_reviews)
     @id = movie_details[:id]
     @title = movie_details[:title]
     @runtime = (movie_details[:runtime].to_f / 60).round(2)
@@ -18,6 +18,8 @@ class MovieDetail
     @genres = all_genre(movie_details)
     @summary = movie_details[:overview]
     @cast = cast_details(movie_cast)
+    @reviews = review_details(movie_reviews)
+    @review_count = movie_reviews.count
   end
 
   def all_genre(movie_details)
@@ -30,5 +32,14 @@ class MovieDetail
       hash[actor[:name]] = actor[:character]
     end
     hash.first(10)
+  end
+
+  def review_details(movie_reviews)
+    hash = {}
+
+    movie_reviews[:results].each do |review|
+      hash[review[:author]] = review[:content]
+    end
+    hash
   end
 end
