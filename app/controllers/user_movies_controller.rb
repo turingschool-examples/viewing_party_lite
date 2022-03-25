8d7a1bef91ef1381cf1ee3662a6a8926c7bc17dc
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-25 12:26:46 -0600

    Add results and details methods

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -2,4 +2,23 @@ class UserMoviesController < ApplicationController
   def discover
     @user = User.find(params[:id])
   end
+
+  def results
+    @user = User.find(params[:id])
+    if params[:top_rated_movies] == "top_rated"
+      @top_rated = MovieFacade.find_top_rated_movies
+      render 'results'
+    elsif params[:search]
+      @found_movies = MovieFacade.new.search(params[:search])
+      render 'results'
+    end
+    require 'pry'; binding.pry
+  end
+  
+  def details
+    @user = User.find(params[:id])
+    @review_info = MovieFacade.new.reviews_info(params[:movie_id])
+    @review_count = MovieFacade.new.review_count(params[:movie_id])
+    @cast = MovieFacade.new.cast_members(params[:id])
+  end
 end
