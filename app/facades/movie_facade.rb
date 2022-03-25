3de3b2002e5b0c5d09dbf86a93f8015c0fc20b77
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-25 09:24:55 -0400

    Feat: build get cast and get reviews in facade

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -2,4 +2,12 @@ class MovieFacade
   def self.get_movie(api_id)
     MovieService.get_movie(api_id)
   end
+
+  def self.get_10_cast(api_id)
+    MovieService.get_cast(api_id)[0..9]
+  end
+
+  def self.get_reviews (api_id)
+    MovieService.get_reviews(api_id)
+  end
 end
