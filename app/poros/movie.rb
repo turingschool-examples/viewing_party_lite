d0fa816be401589cc53a35c0f7721ad27d46fd58
Sam Iavri <sammi80134@gmail.com>
2022-03-24 19:09:43 -0600

    add id to movie poro attributes

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,9 +1,11 @@
 class Movie
   attr_reader :title,
-              :vote_average
+              :vote_average,
+              :id
 
   def initialize(result)
     @title = result[:title]
     @vote_average = result[:vote_average]
+    @id = result[:id]
   end
 end
