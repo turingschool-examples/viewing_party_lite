b7ec77ec0ef820154d3efe0e641ca22b0d2de725
Austin Moore <austinchristianmoore@gmail.com>
2022-03-25 14:37:42 -0400

    Fix: False test assertion

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -44,9 +44,9 @@ RSpec.describe 'the user dashboard' do
       host_id: user.id)
     party_2 = Party.create!(start_date: '2022-03-25', duration: 100, start_time: '6:00', movie_id: 278,
       host_id: user_2.id)
-    user.user_parties.create!(party_id: party_1.id, user_id: user.id)
-    user.user_parties.create!(party_id: party_2.id, user_id: user.id)
-    user_2.user_parties.create!(party_id: party_2.id, user_id: user_2.id)
+    UserParty.create!(party_id: party_1.id, user_id: user.id)
+    UserParty.create!(party_id: party_2.id, user_id: user.id)
+    UserParty.create!(party_id: party_2.id, user_id: user_2.id)
 
     VCR.use_cassette('viewing_party_invites') do
       visit user_path(user.id)
@@ -60,13 +60,13 @@ RSpec.describe 'the user dashboard' do
           expect(page).to have_content("Invited People")
           expect(page).to have_link("The Shawshank Redemption")
           click_link "The Shawshank Redemption"
-          expect(current_path).to eq(user_movie_path(user.id, 278))
+          expect(current_path).to eq("/users/#{user.id}/movies/278")
         end
       end
     end
   end
 
-  it "has a section that shows information about viewing parties where the user is not the host" do
+  it "has a section that shows information about viewing parties where the user is the host" do
     UserParty.destroy_all
     User.destroy_all
     Party.destroy_all
@@ -74,25 +74,26 @@ RSpec.describe 'the user dashboard' do
     user_1 = User.create!(name: 'not host', email: 'nohost@mail.com')
     user_2 = User.create!(name: 'also not host', email: 'neitherhost@mail.com')
     party_1 = Party.create!(start_date: '2022-03-25', duration: 90, start_time: '5:00', movie_id: 24126,
-      host_id: user.id)
+      host_id: user_1.id)
     party_2 = Party.create!(start_date: '2022-03-25', duration: 100, start_time: '6:00', movie_id: 278,
-      host_id: user_2.id)
-    user.user_parties.create!(party_id: party_1.id, user_id: user.id)
-    user.user_parties.create!(party_id: party_2.id, user_id: user.id)
-    user_2.user_parties.create!(party_id: party_2.id, user_id: user_2.id)
+      host_id: user.id)
+    UserParty.create!(party_id: party_1.id, user_id: user.id)
+    UserParty.create!(party_id: party_1.id, user_id: user_1.id)
+    UserParty.create!(party_id: party_2.id, user_id: user.id)
+    UserParty.create!(party_id: party_2.id, user_id: user_2.id)
 
-    VCR.use_cassette('viewing_party_invites') do
+    VCR.use_cassette('viewing_party_invites_1') do
       visit user_path(user.id)
       within '#parties' do
         within '#hosting' do
           expect(page).to have_content("Hosted Parties")
           expect(page).to have_content("Date: 2022-03-25")
-          expect(page).to have_content("Time: 2000-01-01 05:00:00 UTC")
+          expect(page).to have_content("Time: 2000-01-01 06:00:00 UTC")
           expect(page).to have_content("Invited People")
           expect(page).to have_content("You are the host of this party! Host")
           expect(page).to have_link("The Shawshank Redemption")
           click_link "The Shawshank Redemption"
-          expect(current_path).to eq(user_movie_path(user.id, 278))
+          expect(current_path).to eq("/users/#{user.id}/movies/278")
         end
       end
     end
