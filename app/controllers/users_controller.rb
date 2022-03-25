aea9606bc64dd844b68def1079474778a3d9851b
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-25 12:54:07 -0400

    Add: If statement to user controller to change hosting status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,25 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    if params.include?("party_id") 
+     @user = User.find(params[:id])
+     change_status(params[:party_id])
+    else
+      @user = User.find(params[:id])
+    end
   end
 
 
-private
-  def user_params
-    params.permit(:name, :email)
-  end
+  private
+    def user_params
+      params.permit(:name, :email)
+    end
+
+    def change_status(party_id) 
+     view_party = UserParty.find_by(user_id: @user.id, party_id: params[:party_id])
+     view_party.status = "1"
+     view_party.save  
+    end  
 end
+
+
