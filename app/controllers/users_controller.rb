2ae83912058b304321fde00e15e8762640aad2db
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-06 15:46:46 -0600

    add login page and logout functionality. Add helper methods to ApplicationController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  # before_action :authorized, only: [:show]
 
   def new
     @user = User.new
@@ -28,6 +29,26 @@ class UsersController < ApplicationController
   def discover
     @user = User.find(params[:id])
   end
+
+  def login
+  end 
+
+  def logout
+    session[:user_id] = nil 
+    redirect_to login_path 
+  end
+
+  def authenticate_user
+    user = User.find_by(email: params[:email])
+    if user != nil && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else 
+      flash[:error] = 'Invalid email/password'
+      render 'login'
+    end 
+  end 
+
 
 private
   def user_params
