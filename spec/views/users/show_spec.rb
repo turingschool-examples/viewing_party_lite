41e069e7b7cf79b56ccf022cf667b80718f7bdf0
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-09 23:11:38 -0600

    Update spec/rails_helper to use capybara

--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -4,6 +4,7 @@ describe "User dashboard/show" do
   before do
     @user1 = User.create(name: "User One", email: "user1@test.com")
     @user2 = User.create(name: "User Two", email: "user2@test.com")
+
     visit "/users/#{@user1.id}"
   end
 
