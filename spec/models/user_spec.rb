02d29c7f36220be27dd8450a26cfdf97b6bae2b4
James Harkins <jh.harkins@protonmail.com>
2022-05-10 12:36:33 -0600

    ModelTest: create validation test for User model :email attribute uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,15 @@ describe User, type: :model do
     it { should have_many(:invitations) }
     it { should have_many(:parties).through(:invitations) }
   end
+
+  describe "validations" do
+    describe "email" do
+      before do
+        @user_1 = User.create!(name: "Tony Soprano", email: "wokeupthismorning@gmail.com")
+      end
+
+      it { should allow_value("cleavermovie@gmail.com").for(:email) }
+      it { should_not allow_value("wokeupthismorning@gmail.com").for(:email) }
+    end
+  end
 end
