246cc7bddb7d735ce5d941589243d6297d76be6f
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-09 22:06:46 -0600

    Feat: landing index has button to create new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'landing#index'
+
+  get "/users/new", to: "users#new"
+  post "/users/create", to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
