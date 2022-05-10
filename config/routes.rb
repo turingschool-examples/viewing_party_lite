7ded1fcc35ff1d74d376cb8f901b66f21d6de154
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-10 00:11:40 -0600

    Feat: register page requires unique email

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get "/users/new", to: "users#new"
   get "/users/:id", to: "users#show"
   post "/users/create", to: "users#create"
+
+  get "/register", to: "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
