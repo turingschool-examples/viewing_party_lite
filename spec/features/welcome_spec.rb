0dff383cfe22a5407d6c2c85f01f47dc0204327c
Kim G <xiaole.guo@yahoo.com>
2022-05-10 16:49:51 -0600

    refactor with rubocop

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Welcomes', type: :feature do
   describe 'GET /' do
     it 'displays welcome to Viewing Party Lite' do
-      visit root_path 
+      visit root_path
 
       expect(page).to have_content('Welcome to Viewing Party Lite!')
       expect(page).to have_link 'New User'
@@ -11,20 +11,19 @@ RSpec.describe 'Welcomes', type: :feature do
       expect(page).to have_link 'Welcome Page'
     end
 
-    it 'has links to user index page' do 
-       visit root_path 
-       click_link 'Existing Users'
+    it 'has links to user index page' do
+      visit root_path
+      click_link 'Existing Users'
 
-       expect(current_path).to eq users_path
+      expect(current_path).to eq users_path
     end
 
-    it 'has button to new user page' do 
-       visit root_path 
+    it 'has button to new user page' do
+      visit root_path
 
-       click_on 'New User'
-      
-       expect(current_path).to eq new_user_path
+      click_on 'New User'
 
+      expect(current_path).to eq new_user_path
     end
   end
 end
