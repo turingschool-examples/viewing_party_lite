cba6a6f196b696b77e88eac87e457b7043e242b4
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-10 14:50:45 -0600

    Refactor: remove testing of button for creating a new user from title of app and list of existing users welcome feature test, will make it its own test

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Landing/Welcome Page' do
 
   describe 'As a Visitor' do
 
-    it 'has the title of the application, button to create a new user, and a list of existing users' do
+    it 'has the title of the application, and a list of existing users' do
       skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
       lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
       hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
@@ -12,7 +12,7 @@ RSpec.describe 'Landing/Welcome Page' do
       visit "/"
 
       expect(page).to have_content 'Viewing Party Lite'
-      expect(page).to have_button 'Create a New User'
+      
       within "#existing_users" do
         expect(page).to have_content("skeeter@example.com")
         expect(page).to have_content("fatdog@corgi.com")
