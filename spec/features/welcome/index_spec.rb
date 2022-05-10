9fe0765e1b5a7c49518b37466b7dfa552047f3f4
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-10 14:53:09 -0600

    Test: add test for welcome/landing page having a link to reutrn back to the page, link will be present on every page of the application

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -26,20 +26,32 @@ RSpec.describe 'Landing/Welcome Page' do
       hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
       
       visit "/"
-      save_and_open_page
       within "#user_id-#{skeeter.id}" do 
         expect(page).to have_link("skeeter@example.com's dashboard")
       end 
-
+      
       within "#user_id-#{lugnut.id}" do 
         expect(page).to have_link("fatdog@corgi.com's dashboard")
       end 
-
+      
       within "#user_id-#{hazel.id}" do 
         expect(page).to have_link("hazelthehut@food.com's dashboard")
         # click_link "hazelthehut@food.com's dashboard"
         # expect(current_path).to eq("/users/#{hazel.id}")
       end 
+    end 
+    
+    it 'has a link to return back to the landing/welcome page' do #link will be present on every page of application
+      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
+      lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
+      hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
+      
+      visit "/"
+      save_and_open_page
+
+      click_link "Home"
+      expect(current_path).to eq('/')
+
 
     end 
   end 
