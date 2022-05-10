70c6b4feb59fe36915298c63759049dae478acc4
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-10 14:43:52 -0600

    Test: add test for each existing user to be a link to their respective dashboard/show page, refactor first test to expect users emails to be displayed instead of their name

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -10,15 +10,37 @@ RSpec.describe 'Landing/Welcome Page' do
       hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
       
       visit "/"
-      save_and_open_page
 
       expect(page).to have_content 'Viewing Party Lite'
       expect(page).to have_button 'Create a New User'
-      within("#existing_users") do
-        expect(page).to have_content("Skeeter")
-        expect(page).to have_content("LugNut")
-        expect(page).to have_content("Hazel")
+      within "#existing_users" do
+        expect(page).to have_content("skeeter@example.com")
+        expect(page).to have_content("fatdog@corgi.com")
+        expect(page).to have_content("hazelthehut@food.com")
       end
+    end 
+
+    it 'each existing user links to their respective user dashboard' do
+      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
+      lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
+      hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
+      
+      visit "/"
+
+      within "#user-#{skeeter.id}" do 
+        expect(page).to have_link("skeeter@example.com's dashboard")
+      end 
+
+      within "#user-#{lugnut.id}" do 
+        expect(page).to have_link("fatdog@corgi.com's dashboard")
+      end 
+
+      within "#user-#{hazel.id}" do 
+        expect(page).to have_link("hazelthehut@food.com's dashboard")
+        # click_link "hazelthehut@food.com's dashboard"
+        # expect(current_path).to eq("/users/#{hazel.id}")
+      end 
+
     end 
   end 
 end 
