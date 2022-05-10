fdb9d307d7902271d283b7c011bcd51b5222ce00
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-10 00:44:52 -0600

    Test: User dash has section for viewing parties

--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 describe "User dashboard/show" do
   before do
-    @user1 = User.create(name: "User One", email: "user1@test.com")
-    @user2 = User.create(name: "User Two", email: "user2@test.com")
+    @user1 = User.create!(name: "User One", email: "user1@test.com")
+    @user2 = User.create!(name: "User Two", email: "user2@test.com")
+    @user3 = User.create!(name: "User Three", email: "user3@test.com")
 
+    @u1_vp = Party.create!(event_date: Time.new(2002, 04, 26, 1, 0, 0), duration: "230 mins", start_time: Time.new(2002, 04, 26, 1, 0, 0), user_id: @user1.id)
+    @u1_vp_inv = Invitation.create!(user_id: @user3.id, party_id: @u1_vp.id)
+
+    @u2_vp = Party.create!(event_date: Time.new(2002, 02, 24, 2, 0, 0), duration: "230 mins", start_time: Time.new(2002, 02, 24, 2, 0, 0), user_id: @user2.id)
+    @u2_vp_inv = Invitation.create!(user_id: @user3.id, party_id: @u2_vp.id)
     visit "/users/#{@user1.id}"
   end
 
@@ -18,5 +24,14 @@ describe "User dashboard/show" do
 
     expect(current_path).to eq("/users/#{@user1.id}/discover")
     expect(current_path).not_to eq("/users/#{@user2.id}/discover")
+  end
+
+  it 'has a section that lists viewing parties' do
+    within "#viewing_parties" do
+      expcet(page).to have_content("Host: User One")
+      expcet(page).to have_content("Invited: User Three")
+
+      expcet(page).not_to have_content("Host: User Two")
+    end
   end
 end
