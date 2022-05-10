1610229b0411dae5ab98a0c9881af8ff1a16beb7
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-10 14:27:16 -0600

    Add search bar and filter button to movie discover page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
   def index
+    binding.pry
+    @user = User.find(params[:user_id])
   end
 end
--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -8,11 +8,18 @@ describe "Movies discover page" do
 
   it "has a button to discover top rated movies" do
     expect(page).to have_button("Discover top rated movies")
+
+    click_button("Discover top rated movies")
+
+    expect(current_path).to eq("/users/#{@user1.id}/discover")
   end
 
   it "has a search bar/button" do
-    expect(page).to have_field("Search")
+    expect(page).to have_field("keyword")
+    expect(page).to have_button("Search by Title")
 
-    expect(page).to have_button("Search")
+    fill_in "keyword", with: "Nothing"
+    click_button("Search by Title")
+    expect(current_path).to eq("/users/#{@user1.id}/discover")
   end
 end
