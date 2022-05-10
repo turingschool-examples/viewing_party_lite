d8219e42412274b8c744aedeabf9d86215102308
Chris Romero <chrisleeromero1@gmail.com>
2022-05-10 11:15:09 -0600

    feat: update schema with many to many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_171206) do
+ActiveRecord::Schema.define(version: 2022_05_10_171401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2022_05_10_171206) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "party_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "party_id"
+    t.index ["party_id"], name: "index_party_users_on_party_id"
+    t.index ["user_id"], name: "index_party_users_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,4 +39,6 @@ ActiveRecord::Schema.define(version: 2022_05_10_171206) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "party_users", "parties"
+  add_foreign_key "party_users", "users"
 end
