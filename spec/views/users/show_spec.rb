ee9954d4561ba63d5ec0873dc2b42779e83b2d76
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-09 22:57:35 -0600

    Test: user dash displays user name

---
+++ spec/views/users/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "User dashboard/show" do
+  before do
+    @user1 = User.create(name: "User One", email: "user1@test.com")
+    @user2 = User.create(name: "User Two", email: "user2@test.com")
+    visit "/users/#{@user1.id}"
+  end
+
+  it 'displays the users name' do
+    expect(page).to have_content("User One's page")
+    expect(page).to have_content("User Two's page")
+  end
+end
