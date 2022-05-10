8e942acad6cbdd5a9fc965f7df6a2fa49c3131c7
James Harkins <jh.harkins@protonmail.com>
2022-05-10 12:57:20 -0600

    Feat: pass feature tests for user registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/", to: "welcome#index"
+  get "/register", to: "users#new"
 
   resources :users, only: [:show, :new, :create] do
     resources :discover, only: [:index], controller: "movies"
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -3,24 +3,29 @@ require "rails_helper"
 describe "user new page" do
   describe "as a user" do
     describe "when i visit the /register path" do
+      before do
+        @user_1 = User.create!(name: "Tony Soprano", email: "wokeupthismorning@gmail.com")
+      end
+
       it "i see a form to register, including name and email" do
         visit "/register"
 
-        fill_in "Name", with: "Tony Soprano"
-        fill_in "Email", with: "wokeupthismorning@gmail.com"
+        fill_in "Name", with: "Junior Soprano"
+        fill_in "Email", with: "varsityathlete@gmail.com"
 
         click_button "Register"
 
-        expect(current_path).to eq("/users/#{params[:id]}")
+        expect(page).to have_content("Junior Soprano's page")
+        expect(page).not_to have_content("Tony Soprano's page")
       end
 
       it "the form will not accept an email that already exists for some other user in the database" do
-        @user_1 = User.create!(name: "Tony Soprano", email: "wokeupthismorning@gmail.com")
-
         visit "/register"
 
         fill_in "Name", with: "Junior Soprano"
         fill_in "Email", with: "wokeupthismorning@gmail.com"
+
+        click_button "Register"
 
         expect(page).to have_content("There is already an account associated with this e-mail address.")
       end
