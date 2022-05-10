e26333db112fbe7b6ab176cff1204d2eb6820958
Michael Hicks <hicks.michael04@gmail.com>
2022-05-09 20:00:31 -0600

    create feature tests for landing page

---
+++ spec/features/landing/index_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'landing page' do
+
+  describe 'display' do
+    it 'shows the title of the application' do
+      visit root_path
+      expect(page).to have_content('Viewing Party Lite')
+    end
+
+    it 'shows a button to create a new user' do
+      visit root_path
+      click_link 'Create New User'
+      expect(current_path).to eq new_user_path
+    end
+
+    it 'shows existing user list that is linked to respective user dashboard' do
+      user = User.create(name: 'Michael', email: 'michael@example.net')
+      visit root_path
+      click_on "#{user.email}"
+      expect(current_path).to eq(user_path(user.id))
+    end
+
+    it 'shows a header link redirecting to landing page' do
+      visit new_user_path
+      click_on 'Home'
+      expect(current_path).to eq('/')
+    end
+
+  end
+
+end
