ec2e448ab8ed8643804dbd3a46a6a837a3814d61
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-09 22:07:34 -0600

    Feat: /users/new has form to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    # binding.pry
+    user = User.create!(user_params)
+
+    if user.save
+      redirect_to root_path
+    else
+      redirect_to "/users/new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
   end
 end
