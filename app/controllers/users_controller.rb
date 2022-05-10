27d57de4cef468bacb202609ed416913d5165af7
Joseph Downs <jos.w.downs@gmail.com>
2022-05-10 14:13:01 -0600

    Feat: Register new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    user = User.create(user_params)
+  end
+
+  private
+    def user_params
+      params.permit(:name, :email)
+    end
 end
