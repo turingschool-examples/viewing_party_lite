b1ff7837c58abccab2cb0693f376c6953ab502b2
Ian Ross <ianrossdrums@gmail.com>
2022-05-10 10:40:23 -0600

    feat: set up database

---
+++ db/migrate/20220510022555_create_parties.rb
@@ -0,0 +1,11 @@
+class CreateParties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parties do |t|
+      t.integer :start_time
+      t.integer :date
+      t.integer :length
+
+      t.timestamps
+    end
+  end
+end
