1281bae8e19f6960a4c34836c6d9dc1e054be26c
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-10 15:25:04 -0600

    Test: user show page has button leading to discover page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,11 +3,17 @@ require "rails_helper"
 RSpec.describe "user dashboard" do
   before :each do
     @user_1 = User.create!(name: "Unreal Ursa", email: "thisaintreal@gotcha.org")
+
+    visit "/users/#{@user_1.id}"
   end
 
   it "displays user name" do
-    visit "/users/#{@user_1.id}"
-
     expect(page).to have_content("Unreal Ursa's Dashboard")
+  end
+
+  it "has button that directs to discover page" do
+    click_button("Discover Movies")
+
+    expect(current_path).to eq("/users/#{@user_1.id}/discover")
   end
 end
