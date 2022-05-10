c1b7fa967e6c35891d289ee6428e969fef04d766
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-10 13:40:52 -0600

    Feat: add relationships to user and attendee models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :name, :email
   validates_uniqueness_of :email
 
-
+  has_many :attendees
+  has_many :parties
+  has_many :parties, through: :attendees
 end 
