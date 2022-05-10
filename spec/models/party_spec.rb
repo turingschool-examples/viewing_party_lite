ed52f0eb6a5aa3feede12c724861f1dbe5bd6894
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-10 13:35:32 -0600

    Test: add validations test for Party model

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Party, type: :model do
   describe 'validations' do
 
+    it { should validate_presence_of :duration }
+    it { should validate_presence_of :when }
+    it { should validate_presence_of :start_time} 
 
   end 
 end 
