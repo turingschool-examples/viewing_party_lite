d829eaaea3df59ec3dcd8da374ff512b84356b1b
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-10 15:03:17 -0600

    Refactor: add before block to features/welcome/index_spec to DRY up variables used

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Landing/Welcome Page' do 
 
   describe 'As a Visitor' do
+    before do 
+      @skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
+      @lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
+      @hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
+    end 
 
     it 'has the title of the application, and a list of existing users' do
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
-      lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
-      hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
       
       visit "/"
 
@@ -21,20 +23,17 @@ RSpec.describe 'Landing/Welcome Page' do
     end 
 
     it 'each existing user links to their respective user dashboard' do
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
-      lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
-      hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
       
       visit "/"
-      within "#user_id-#{skeeter.id}" do 
+      within "#user_id-#{@skeeter.id}" do 
         expect(page).to have_link("skeeter@example.com's dashboard")
       end 
       
-      within "#user_id-#{lugnut.id}" do 
+      within "#user_id-#{@lugnut.id}" do 
         expect(page).to have_link("fatdog@corgi.com's dashboard")
       end 
       
-      within "#user_id-#{hazel.id}" do 
+      within "#user_id-#{@hazel.id}" do 
         expect(page).to have_link("hazelthehut@food.com's dashboard")
         # click_link "hazelthehut@food.com's dashboard"
         # expect(current_path).to eq("/users/#{hazel.id}")
@@ -42,9 +41,6 @@ RSpec.describe 'Landing/Welcome Page' do
     end 
     
     it 'has a link to return back to the landing/welcome page' do #link will be present on every page of application
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
-      lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
-      hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
       
       visit "/"
 
@@ -53,9 +49,6 @@ RSpec.describe 'Landing/Welcome Page' do
     end 
 
     it 'has a button to create a new user' do 
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
-      lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
-      hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
       
       visit "/"
       
