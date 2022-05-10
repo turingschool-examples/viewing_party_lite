02e6ad430a28e4f9930946776906599dea222252
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-10 16:11:35 -0600

    add controller actions new and create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+   end
+
+  def create
+    user = User.create(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      redirect_to "/register"
+      flash[:alert] = 'Error: please enter a name and unique email to register.'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
