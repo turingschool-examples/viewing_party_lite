e546a45e9fcbf0633f06a587294871715c69f994
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-09 23:18:26 -0600

    Create user dash and associated controller

--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -10,6 +10,6 @@ describe "User dashboard/show" do
 
   it 'displays the users name' do
     expect(page).to have_content("User One's page")
-    expect(page).to have_content("User Two's page")
+    expect(page).not_to have_content("User Two's page")
   end
 end
