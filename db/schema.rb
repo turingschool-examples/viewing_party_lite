98c81ba6b804ebefe50d1918cd08147250487502
Will Medders <wmedders@gmail.com>
2022-05-10 19:40:39 -0400

    Remove password column from users and re-run migration

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2022_05_10_215818) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
