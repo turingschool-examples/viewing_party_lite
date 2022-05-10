8b1bbd8bee117b2a1bc359262a14e17f1759adb0
James Harkins <jh.harkins@protonmail.com>
2022-05-10 13:33:19 -0600

    Feat: pass model tests for User model name and email presence validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :invitations
   has_many :parties, through: :invitations
+  validates_presence_of :name
+  validates_presence_of :email
   validates :email, uniqueness: true
 end
