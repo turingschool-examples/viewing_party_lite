40356806987df381f706754c7108263937b93285
Tyler Ross <tross020890@gmail.com>
2022-05-10 17:22:56 -0500

    Feat: can create new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def error_message(errors)
+      errors.full_messages.join(', ')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
-  resources :users, only: [:new, :show]
+  resources :users, only: [:new, :show, :create]
 end
