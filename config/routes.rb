013fc078134960316241bae198cc50ac97841a9b
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-10 20:43:21 -0600

    Feat: Discover page has button and search bar leading to movies results page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   get "/register", to: "users#new"
 
+  get "/users/:id/movies", to: "movies#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
