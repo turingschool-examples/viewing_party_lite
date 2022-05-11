0ed79a6d39c3fb3116d6913ef97eb95e520cf926
John Hennerich <john.hennerich@gmail.com>
2022-05-10 20:36:20 -0600

    Test: user dashboard show spec

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'User dashboard', type: :feature do
+  before do
+    @user1 = User.create!(name: "John", email: "john.hennerich@gmail.com")
+    @user2 = User.create!(name: "Brylan", email: "brylan.gannon112@gmail.com")
+
+    visit user_path(@user1)
+  end
+  it "Displays user dashboard attributes on the page" do
+    expect(page).to have_content("#{user1.name}'s dashboard")
+    expect(page).to_not have_content("#{user2.name}'s dashboard")
+    expect(page).to have_button("Discover Movies")
+    expect(page).to have_content("Viewing Parties")
+  end
+end
