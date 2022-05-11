728e128400190f9d7a977b6ba15fd3e85df50e78
Chris Romero <chrisleeromero1@gmail.com>
2022-05-10 21:53:51 -0600

    refactor: remove white space and comments

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -9,11 +9,6 @@ RSpec.describe "user show page" do
         expect(page).to have_button("Discover Movies")
         expect(page).to have_content("Viewing Parties")
     end
-    # As a user,
-    # When I go to a user dashbaord,
-    # and click "Discover Movies" button,
-    # I am redirected to a discover page '/users/:id/discover', where :id is the user id of the user who's dashboard I was just on.
-
     it 'has a button that takes the user to a discover page' do
       frank = User.create!(name: "Frank", email: "frank@yahoo.com")
       visit "/users/#{frank.id}"
