5fa9a400d8dff52b06a561e43e89f0fbbd829f25
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-11 09:18:49 -0600

    add model associations and test

--- spec/factories/parties.rb
+++ spec/factories/parties.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :party do
     movie_id { 1 }
     duration { 1 }
-    date { "2022-05-10" }
-    time { "2022-05-10 21:25:36" }
+    date { '2022-05-10' }
+    time { '2022-05-10 21:25:36' }
   end
 end
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Party, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:party_users) }
+    it { should have_many(:users).through(:party_users) }
+  end
 end
