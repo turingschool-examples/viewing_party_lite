0ef2774ac1d698d458864c79b5e7909f17d322c3
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-11 14:55:18 -0600

    Refactor Test: the discover page spec only tests button functionality, no longer tests contents of movie results page

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -15,18 +15,13 @@ describe 'Discover Movies' do
   it 'displays a top 20 movies button that routes to the discover page' do
     click_button 'Top 20 Movies'
 
-    expect(current_path).to eq("/users/#{user_1.id}/movies")
-    expect(page).to have_css('#movie', count: 20)
+    expect(current_path).to eq("/users/#{@user1.id}/movies")
   end 
 
   it 'displays a search bar to find movies by keyword' do
     fill_in :query, with: 'Mad'
     click_button 'Search by Title'
 
-    expect(current_path).to eq("/users/#{user_1.id}/movies")
-    expect(page).to have_css('#movie', count: 20)
-    expect(page).to have_content("Mad Max")
-    expect(page).to have_content("The Mad Hatter")
-    expect(page).to_not have_content("Star Wars")
+    expect(current_path).to eq("/users/#{@user1.id}/movies")
   end
 end
