6fc779b2f44c3846889dd45d3547e204bd623098
Will Medders <wmedders@gmail.com>
2022-05-11 13:26:34 -0400

    FeatureTest: user dashboard has a discover movies button

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe 'a user dashboard' do
     visit "/users/#{user_1.id}"
 
     expect(page).to have_content("Buggs's Dashboard")
-  end    
+  end
+
+  it 'has a Discover Movies button' do
+      user1 = User.create!(name: 'Will', email: 'abc@mail.com')
+
+      visit "/users/#{user1.id}"
+      click_button "Discover Movies"
+
+      expect(page).to have_current_path("/users/#{user1.id}/discover")
+    end
 end
