d994ff7ee69953fe72084186b3c339c5e5ff7663
Joseph Senecal <jasenecal2@gmail.com>
2022-05-11 16:25:44 -0600

    replace empty string with variable call

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe "discover movies page" do
        	  'Accept'=>'*/*',
        	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'User-Agent'=>'Faraday v2.3.0'
-           }).
-         to_return(status: 200, body: "", headers: {})
+           })
+         .to_return(status: 200, body: json_response, headers: {})
     user_1 = User.create!(name: "Twitch", email: "twitch@dogmail.com")
     visit "/users/#{user_1.id}/discover"
 
     click_link "Discover Top Rated Movies"
     expect(current_path).to eq("/users/#{user_1.id}/movies")
   end
-  
+
   it "has a section to search by movie title" do
     json_response = File.read("./spec/fixtures/search_for_gabriel.json")
     stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=131d23d3e9d511ff6fce6fdc6799d9be&include_adult=false&language=en-US&page=1&query=Gabriel").
@@ -27,11 +27,11 @@ RSpec.describe "discover movies page" do
        	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'User-Agent'=>'Faraday v2.3.0'
         })
-        .to_return(status: 200, body: "", headers: {})
-    
+        .to_return(status: 200, body: json_response, headers: {})
+
     user_1 = User.create!(name: "Twitch", email: "twitch@dogmail.com")
     visit "/users/#{user_1.id}/discover"
-  
+
     expect(page).to have_field("Movie Title")
     fill_in "Movie Title", with: "Gabriel"
     click_button "Search by Movie Title"
