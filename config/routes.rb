e38a3b86a4c518c05c59c933b390c118b2fcd28c
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-10 20:31:29 -0600

    Feat: add route for discover movies button, add route and method to button_to on user show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
-
+  
+  get '/users/:id/discover', to: 'movies#index'
+  
   resources :users, only: %i[show new create]
 end
