1eeeaea59e94ca2d2ad1ab7c986a96a703807762
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-11 11:40:59 -0600

    Add data to seeds file Co-Authored by: John Hennerich <91695954+jhennerich>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PartyUser.destroy_all
+ViewingParty.destroy_all
+User.destroy_all
+
+
+@user1 = User.create!(name: 'John', email: 'john.hennerich@gmail.com')
+@user2 = User.create!(name: 'Brylan', email: 'brylan.gannon112@gmail.com')
+
+@vp1 = ViewingParty.create!(date: '2022/05/11', start_time: '12:00', duration: 120, movie_title: 'Star Wars')
+@vp1a = ViewingParty.create!(date: '2022/05/11', start_time: '15:00', duration: 120, movie_title: 'Star Wars Empire Strikes Back')
+
+@vp2 = ViewingParty.create!(date: '2022/05/11', start_time: '12:00', duration: 120, movie_title: 'The Godfather')
+
+@user1.party_users.create!(viewing_party_id: @vp1.id, host: true )
+@user1.party_users.create!(viewing_party_id: @vp1a.id, host: true )
+
+@user2.party_users.create!(viewing_party_id: @vp2.id, host: true )
+@user2.party_users.create!(viewing_party_id: @vp1.id, host: false )
+
