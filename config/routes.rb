d02198d3f96b16f938fbf177d3892539aa4448f6
Andrew Speth <speth.andrew@yahoo.com>
2022-05-10 20:15:52 -0600

    features for landing page user story

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'landing_page#index'
+
+  post '/users/new', to: 'users#new'
+  post 'users', to: 'users#create'
+
+  get 'users/:id', to: 'users#show'
 end
