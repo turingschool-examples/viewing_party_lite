5e034d99664e6312571f6ece1089f599a819f9bb
John Hennerich <john.hennerich@gmail.com>
2022-05-10 21:00:22 -0600

    Feat: implement user dashboard show page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -8,8 +8,8 @@ describe 'User dashboard', type: :feature do
     visit user_path(@user1)
   end
   it "Displays user dashboard attributes on the page" do
-    expect(page).to have_content("#{user1.name}'s dashboard")
-    expect(page).to_not have_content("#{user2.name}'s dashboard")
+    expect(page).to have_content("#{@user1.name}'s dashboard")
+    expect(page).to_not have_content("#{@user2.name}'s dashboard")
     expect(page).to have_button("Discover Movies")
     expect(page).to have_content("Viewing Parties")
   end
