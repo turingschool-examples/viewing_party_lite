e7462f031f486ac185a88358ef8387ff76d38849
John Hennerich <john.hennerich@gmail.com>
2022-05-10 21:18:09 -0600

    Feat: discover movies index page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   get '/register', to: 'users#new'
 
-  resources :users, only: [:show, :create]
+  resources :users, only: [:show, :create] do
+    resources :discover, only: [:index]
+  end
 end
