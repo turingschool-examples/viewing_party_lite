4722e7a26fb224013636caa6e31e1b1c538aa87a
James Harkins <jh.harkins@protonmail.com>
2022-05-11 10:51:00 -0600

    Feat: pass additional tests for Movie poro

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,9 +1,17 @@
 class Movie
-  attr_reader :id, :title, :vote_average
+  attr_reader :id,
+    :title,
+    :vote_average,
+    :genres,
+    :runtime,
+    :summary
 
   def initialize(attributes)
     @id = attributes[:id]
     @title = attributes[:title]
     @vote_average = attributes[:vote_average]
+    @genres = attributes[:genres]
+    @runtime = attributes[:runtime]
+    @summary = attributes[:summary]
   end
 end
--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -22,7 +22,10 @@ describe Movie do
       id: 0,
       title: "Movie One",
       vote_average: 7.0,
-      genres: ["drama", "comedy"],
+      genres: [
+        {id: 35, name: "Drama"},
+        {id: 42, name: "Comedy"}
+      ],
       runtime: 190,
       summary: "this is a movie"
     }
@@ -32,7 +35,10 @@ describe Movie do
     expect(movie.id).to eq(0)
     expect(movie.title).to eq("Movie One")
     expect(movie.vote_average).to eq(7.0)
-    expect(movie.genres).to eq(["drama", "comedy"])
+    expect(movie.genres).to be_a Array
+    expect(movie.genres[0]).to be_a Hash
+    expect(movie.genres[0][:name]).to eq("Drama")
+    expect(movie.genres[1][:name]).to eq("Comedy")
     expect(movie.runtime).to eq(190)
     expect(movie.summary).to eq("this is a movie")
   end
