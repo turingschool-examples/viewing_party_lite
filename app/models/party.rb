5adb9cbcfebc971c8973ff7d9e4322647fe590d3
Will Medders <wmedders@gmail.com>
2022-05-10 20:57:18 -0400

    Model: Party relationships

--- app/models/party.rb
+++ app/models/party.rb
@@ -4,4 +4,7 @@ class Party < ApplicationRecord
   validates_presence_of :start_time
   validates_presence_of :name
   validates_presence_of :host_id
+
+  has_many :party_users
+  has_many :users, through: :party_users
 end
