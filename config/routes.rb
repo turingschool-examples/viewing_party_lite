1613c86fe588d3de594055bb2629597ffbc21331
Ian Ross <ianrossdrums@gmail.com>
2022-05-11 08:27:35 -0600

    spacing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get "/register", to: "users#new"
   post "/register", to: "users#create"
-  
+
   get "/users/:id", to: "users#dashboard"
   get "/users/:id/discover", to: "users#discover"
 end
