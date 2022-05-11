e4207271c455893f5c7b16070fc29f76dfd70e60
John Hennerich <john.hennerich@gmail.com>
2022-05-11 09:20:01 -0600

    Test: refactor spec file per rubocop
    Co-authored-by: Brylan Gannon <94757433+B-gann21@users.noreply.github.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-describe 'User dashboard', type: :feature do
+describe 'User dashboard' do
   before do
-    @user1 = User.create!(name: "John", email: "john.hennerich@gmail.com")
-    @user2 = User.create!(name: "Brylan", email: "brylan.gannon112@gmail.com")
+    @user1 = User.create!(name: 'John', email: 'john.hennerich@gmail.com')
+    @user2 = User.create!(name: 'Brylan', email: 'brylan.gannon112@gmail.com')
 
     visit user_path(@user1)
   end
-  it "Displays user dashboard attributes on the page" do
-    expect(page).to have_content("#{@user1.name}'s dashboard")
-    expect(page).to_not have_content("#{@user2.name}'s dashboard")
-    expect(page).to have_button("Discover Movies")
-    expect(page).to have_content("Viewing Parties")
+
+  it 'Displays user dashboard attributes on the page' do
+    expect(page).to have_content("John's dashboard")
+    expect(page).to_not have_content("Brylan's dashboard")
+    expect(page).to have_button('Discover Movies')
+    expect(page).to have_content('Viewing Parties')
   end
 end
