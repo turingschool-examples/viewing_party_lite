3bafe7bcd442ddfa079aa6895e607fb6828f0478
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-11 17:07:19 -0600

    Feat: add faraday get moviesdb endpoint to user_movies_controller, add logic to view to display top rated movies from api consumtion

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,7 +1,12 @@
 class UserMoviesController < ApplicationController
 
   def index
-    
+    if params[:top_rated]
+      conn = Faraday.new(url: "https://api.themoviedb.org") 
+      response = conn.get("/3/movie/top_rated?api_key=#{ENV['movie_db_key']}")
+      data = JSON.parse(response.body, symbolize_names: true)
+      @movies = data[:results]
+    end 
   end
   
 
