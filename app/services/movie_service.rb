2aaba8f36101e9a2869b4fa8991db939df0e8c57
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-10 18:52:08 -0600

    Create movie service and top_20 method

---
+++ app/services/movie_service.rb
@@ -0,0 +1,16 @@
+class MovieService
+  def self.conn
+    Faraday.new(url: "https://api.themoviedb.org") do |f|
+      f.headers["api_key"] = ENV['movie_api_key']
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def self.top_20
+    response = conn.get("/3/movie/top_rated") do |c|
+      c.options.params_encoder = Faraday::FlatParamsEncoder
+      c.params = {api_key: ENV['movie_api_key']}
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
