3315722ceab5d2d030bcaabef73865863f3e48a8
Susan Boyd <sueboyd922@gmail.com>
2022-05-12 16:26:45 -0400

    Test: for button on movie details return to discover

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -4,9 +4,19 @@ require 'rails_helper'
 RSpec.describe "New User", type: :feature do
   it "has button to create viewing party" do
     user = User.create!(name: "Tim", email: "Tim@mail.com")
-    visit "/users/#{user.id}/movies/278"
+    movie = Movie.new(id: 1, title: "Movie", vote_average: "8.2")
+    visit "/users/#{user.id}/movies/#{movie.id}"
     click_button "Create new viewing party"
 
-    expect(current_path).to eq("/users/#{user.id}/movies/278/viewing_party/new")
+    expect(current_path).to eq("/users/#{user.id}/movies/#{movie.id}/viewing_party/new")
+  end
+
+  it 'has a button to return to the discover page' do
+    user = User.create!(name: "Tim", email: "Tim@mail.com")
+    movie = Movie.new(id: 1, title: "Movie", vote_average: "8.2")
+    visit "/users/#{user.id}/movies/#{movie.id}"
+
+    click_on "Return to Discover"
+    expect(current_path).to eq("/users/#{user.id}/discover")
   end
 end
