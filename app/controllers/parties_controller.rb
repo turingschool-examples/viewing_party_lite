7a4b1b0e069073c3f15b02692963d85cddad7ae8
James Harkins <jh.harkins@protonmail.com>
2022-05-12 15:31:17 -0600

    Test: create additional test for parties#new page

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,4 +1,20 @@
 class PartiesController < ApplicationController
   def new
+    @movie = MovieFacade.details(params[:movie_id])
+    @user = User.find(params[:user_id])
+    @users = User.all
+  end
+
+  def create
+    party = Party.new(
+      event_date: Date.parse("#{params["event_date(1i)"]}/#{params["event_date(2i)"]}/#{params["event_date(3i)"]}"),
+      duration: params[:duration],
+      start_time: Time.parse("#{params["start_time(4i)"]}:#{params["start_time(5i)"]}"),
+      user_id: params[:user_id],
+      movie_id: params[:movie_id]
+    )
+    if party.save
+      redirect_to "/users/#{party.user_id}"
+    end
   end
 end
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -18,8 +18,11 @@ describe "new party page" do
 
       it "and i see a form with fields for duration, date, start_time, and checkboxes for all existing users to be invited", :vcr do
         expect(page).to have_field("Duration", with: 117)
-        expect(page).to have_field("Event Date")
-        expect(page).to have_field("Start Time")
+        expect(page).to have_field("_event_date_2i")
+        expect(page).to have_field("_event_date_3i")
+        expect(page).to have_field("_event_date_1i")
+        expect(page).to have_field("_start_time_4i")
+        expect(page).to have_field("_start_time_5i")
         expect(page).to have_field("Tony Soprano", checked: false)
         expect(page).to have_field("Junior Soprano", checked: false)
         expect(page).to have_field("Christopher Moltisanti", checked: false)
@@ -30,6 +33,15 @@ describe "new party page" do
 
       it "i should not be able to create a party with a duration less than the runtime of the movie", :vcr do
         fill_in "Duration", with: 110
+        select "April", from: "_event_date_2i"
+        select "26", from: "_event_date_3i"
+        select "2026", from: "_event_date_1i"
+        select "20", from: "_start_time_4i"
+        select "00", from: "_start_time_5i"
+        check "Tony Soprano"
+        check "Junior Soprano"
+        check "Bobby Baccalieri"
+        click_button "Create Party"
 
         expect(current_path).to eq("/users/#{@user2.id}/movies/290/parties/new")
         expect(page).to have_content("Invalid Data: Duration must be greater than or equal to the movie's runtime.")
@@ -37,8 +49,11 @@ describe "new party page" do
 
       it "should redirect the user to their dashboard after the party is created and the party is listed", :vcr do
         fill_in "Duration", with: 137
-        fill_in "Event Date", with: Date.parse("26/04/26")
-        fill_in "Start Time", with: Time.parse("08:00 PM")
+        select "April", from: "_event_date_2i"
+        select "26", from: "_event_date_3i"
+        select "2026", from: "_event_date_1i"
+        select "20", from: "_start_time_4i"
+        select "00", from: "_start_time_5i"
         check "Tony Soprano"
         check "Junior Soprano"
         check "Bobby Baccalieri"
@@ -46,15 +61,41 @@ describe "new party page" do
 
         expect(current_path).to eq("/users/#{@user2.id}")
 
+        party = Party.where(movie_id: 290).first
+
         within "#viewing_parties" do
-          expect(page).to have_content("Tony Soprano")
-          expect(page).to have_content("Bobby Baccalieri")
-          expect(page).not_to have_content("Christopher Moltisanti")
-          expect(page).to have_content("Paulie Gualtieri")
+          expect(page).to have_content("Party ##{party.id}")
         end
       end
 
       it "the new party should also be listed on other user dashboards if they were invited" do
+        fill_in "Duration", with: 137
+        select "April", from: "_event_date_2i"
+        select "26", from: "_event_date_3i"
+        select "2026", from: "_event_date_1i"
+        select "20", from: "_start_time_4i"
+        select "00", from: "_start_time_5i"
+        check "Tony Soprano"
+        check "Junior Soprano"
+        check "Bobby Baccalieri"
+        click_button "Create Party"
+
+        party = Party.where(movie_id: 290).first
+
+        visit "/users/#{@user1.id}"
+        expect(page).to have_content("Party ##{party.id}")
+
+        visit "/users/#{@user2.id}"
+        expect(page).to have_content("Party ##{party.id}")
+
+        visit "/users/#{@user5.id}"
+        expect(page).to have_content("Party ##{party.id}")
+
+        visit "/users/#{@user3.id}"
+        expect(page).not_to have_content("Party ##{party.id}")
+
+        visit "/users/#{@user4.id}"
+        expect(page).not_to have_content("Party ##{party.id}")
       end
     end
   end
