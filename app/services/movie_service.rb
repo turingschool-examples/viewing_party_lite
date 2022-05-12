00329b5f5c82910b303e11441fc3597dd919c60f
Susan Boyd <sueboyd922@gmail.com>
2022-05-12 15:56:14 -0400

    Delete Base Service/commented out code

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,12 +1,10 @@
-class MovieService 
+class MovieService
   def self.get_top_rated
-    # response = conn("http://api.themoviedb.org").get("/3/movie/top_rated?api_key=#{ENV['movie_api_key']}")
     response = conn.get('/3/movie/top_rated?')
     get_json(response)
   end
 
   def self.movie_details(id)
-    # response = conn("http://api.themoviedb.org").get("/3/movie/#{id}?api_key=#{ENV['movie_api_key']}&append_to_response=credits,reviews")
     response = conn.get("/3/movie/#{id}?")
     get_json(response)
   end
@@ -20,5 +18,4 @@ class MovieService
   def self.get_json(response)
    JSON.parse(response.body, symbolize_names: true)
   end
-
 end
