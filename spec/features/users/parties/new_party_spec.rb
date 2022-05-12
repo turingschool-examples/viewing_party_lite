de7f388ff40647161c8f51d2a67e616ed93f825a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-12 13:42:13 -0600

    Test: add test for new party page

---
+++ spec/features/users/parties/new_party_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "New Party Page" do 
+
+  describe "As a Visitor" do 
+
+    it 'it has the movie title and a form to create a new party' do
+      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
+      jaws_id = 578
+      date = DateTime.now.strftime("%m/%d/%y")
+      visit "/users/#{skeeter.id}/movies/#{jaws_id}/party/new"
+
+      expect(page).to have_content("Create a Movie Party for Jaws")
+      expect(page).to have_field('Duration of Party', with: '124')
+      expect(page).to have_field('Day', with: "#{date}")
+      expect(page).to have_field('Start Time', with: '7:00')
+
+    end 
+
+  end 
+
+end 
+
