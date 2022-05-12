cbabed74723d58127aeeb103ccbe76abca49eb09
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-11 18:44:48 -0600

    Feat: MovieService can make API calls and return ruby hashes

---
+++ app/services/movie_service.rb
@@ -0,0 +1,18 @@
+class MovieService
+  def get_url(url)
+    response = Faraday.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def top_movies
+    page1 = get_url("https://api.themoviedb.org/3/movie/popular?api_key=#{ENV['tmdb_key']}")[:results]
+    page2 = get_url("https://api.themoviedb.org/3/movie/popular?api_key=#{ENV['tmdb_key']}&page=2")[:results]
+    page1 + page2
+  end
+
+  def movies_by_query(query)
+    page1 = get_url("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['tmdb_key']}&query=#{query}")[:results]
+    page2 = get_url("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['tmdb_key']}&query=#{query}&page=2")[:results]
+    page1 + page2
+  end
+end
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -6,15 +6,15 @@ describe MovieService do
       service = MovieService.new
       response = service.get_url('https://api.coinbase.com/v2/currencies')
 
-      expect(response).to be_a(Array)
-      expect(response[0]).to be_a(Hash)
+      expect(response[:data]).to be_a(Array)
+      expect(response[:data][0]).to be_a(Hash)
     end
 
     it '.top_movies returns an array with 40 movie hashes' do
       service = MovieService.new
       response = service.top_movies
       
-      expect(response.length).to_eq(40)
+      expect(response.length).to eq(40)
       expect(response).to be_all(Hash)
       expect(response[0][:original_title]).to be_a(String)
     end
