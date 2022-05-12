c2b505b7a80e1f11fd0404e1e896b79d889cf9bc
Kim G <xiaole.guo@yahoo.com>
2022-05-12 10:18:07 -0600

    refactor service to user faraday.params

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -1,18 +1,23 @@
 class TmdbService
   def initialize
-    @conn = Faraday.new
+    # @conn = Faraday.new
+    @conn = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
+      faraday.params['api_key'] = ENV['tmdb_api_key']
+    end
   end
 
   def top20
-    get_url("/movie/top_rated?api_key=#{ENV['tmdb_api_key']}")[:results]
+    # get_url("/movie/top_rated?api_key=#{ENV['tmdb_api_key']}")[:results]
+    get_url("movie/top_rated")[:results]
   end
 
   def search(keyword, page = 1)
-    get_url("/search/movie?api_key=#{ENV['tmdb_api_key']}&query=#{keyword}&page=#{page}")
+    # get_url("/search/movie?api_key=#{ENV['tmdb_api_key']}&query=#{keyword}&page=#{page}")
+    get_url("search/movie?&query=#{keyword}&page=#{page}")
   end
 
   def get_url(url)
-    response = @conn.get("https://api.themoviedb.org/3#{url}")
+    response = @conn.get("#{url}")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
