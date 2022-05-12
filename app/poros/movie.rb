ee867d38469d40129b2fbe85ed437a9add50a1e4
Katy Harrod <m.catherine.harrod@gmail.com>
2022-05-12 15:38:04 -0600

    Implement facade and poro

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,1 +1,5 @@
-
+class Movie
+  def initialize(data)
+    @original_title = data[:original_title]
+  end
+end
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe MovieService do
   it 'makes an API call' do
     result = MovieService.top_rated_movies
-    binding.pry
+
+    poro_factory = MovieService.top_rated_movies_list
+
   end
 end
