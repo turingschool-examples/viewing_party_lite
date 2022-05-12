eec9247f2b948a9e9d74151e8ac85fa1f3adf20a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-12 15:58:48 -0600

    Test: party model method #movie grabs a movie from the API using the party's movie_id

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -6,11 +6,11 @@ describe "User dashboard/show" do
     @user2 = User.create!(name: "User Two", email: "user2@test.com")
     @user3 = User.create!(name: "User Three", email: "user3@test.com")
 
-    @u1_vp = Party.create!(event_date: Time.new(2002, 04, 26, 1, 0, 0), duration: "230 mins", start_time: Time.new(2002, 04, 26, 1, 0, 0), user_id: @user1.id, movie_id: 24021)
+    @u1_vp = Party.create!(event_date: DateTime.new(2002, 04, 26, 6, 0, 0, "-07:00"), duration: "230 mins", start_time: DateTime.new(2002, 04, 26, 6, 0, 0, "-07:00"), user_id: @user1.id, movie_id: 24021)
     @u1_vp_inv_1 = Invitation.create!(user_id: @user1.id, party_id: @u1_vp.id)
     @u1_vp_inv_2 = Invitation.create!(user_id: @user3.id, party_id: @u1_vp.id)
 
-    @u2_vp = Party.create!(event_date: Time.new(2002, 02, 24, 2, 0, 0), duration: "230 mins", start_time: Time.new(2002, 02, 24, 2, 0, 0), user_id: @user2.id, movie_id: 809)
+    @u2_vp = Party.create!(event_date: DateTime.new(2002, 02, 24, 7, 0, 0, "-07:00"), duration: "230 mins", start_time: DateTime.new(2002, 02, 24, 7, 0, 0, "-07:00"), user_id: @user2.id, movie_id: 809)
     @u2_vp_inv = Invitation.create!(user_id: @user3.id, party_id: @u2_vp.id)
     visit "/users/#{@user1.id}"
   end
@@ -27,7 +27,7 @@ describe "User dashboard/show" do
     expect(current_path).not_to eq("/users/#{@user2.id}/discover")
   end
 
-  it "has a section that lists viewing parties" do
+  it "has a section that lists viewing parties", :vcr do
     within "#viewing_parties" do
       expect(page).to have_content("Party ##{@u1_vp.id}")
       expect(page).to have_link("Movie: The Twilight Saga: Eclipse")
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -5,4 +5,22 @@ describe Party, type: :model do
     it { should have_many(:invitations) }
     it { should have_many(:users).through(:invitations) }
   end
+
+  describe "instance methods" do
+    before do
+      @user1 = User.create!(name: "User One", email: "user1@test.com")
+      @party_1 = Party.create!(event_date: DateTime.new(2002, 04, 26, 6, 0, 0, "-07:00"), duration: "230 mins", start_time: DateTime.new(2002, 04, 26, 6, 0, 0, "-07:00"), user_id: @user1.id, movie_id: 24021)
+      @party_2 = Party.create!(event_date: DateTime.new(2002, 02, 24, 7, 0, 0, "-07:00"), duration: "230 mins", start_time: DateTime.new(2002, 02, 24, 7, 0, 0, "-07:00"), user_id: @user1.id, movie_id: 809)
+    end
+
+    context "#movie" do
+      it "returns movie details poros", :vcr do
+        movie = @party_1.movie
+
+        expect(movie).to be_a Movie
+        expect(movie.title).to eq("The Twilight Saga: Eclipse")
+        expect(movie.title).not_to eq("Shrek 2")
+      end
+    end
+  end
 end
