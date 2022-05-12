c27e4da8bf454a92afd43aaed0b5f164b8f87076
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-12 12:37:50 -0600

    test: movie_service and define method with endpoint

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -11,9 +11,12 @@ class TmdbService
     get_url("/search/movie?api_key=#{ENV['tmdb_api_key']}&query=#{keyword}&page=#{page}")
   end
 
+  def movie_details(movie_id)
+     get_url("/movie/#{movie_id}?api_key=#{ENV['tmdb_api_key']}&language=en-US")
+  end
+
   def get_url(url)
     response = @conn.get("https://api.themoviedb.org/3#{url}")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
-
--- spec/services/tmdb_service_spec.rb
+++ spec/services/tmdb_service_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe TmdbService do
     expect(results[0][:vote_average]).to eq(8.7)
   end
 
-  it 'gets movies from keyword search', :vcr do 
+  it 'gets movies from keyword search', :vcr do
     results = service.search('man')
 
     expect(results).to be_a Hash
@@ -22,4 +22,19 @@ RSpec.describe TmdbService do
     expect(results[:results][0][:title]).to eq("The King's Man")
     expect(results[:results][0][:vote_average]).to eq(6.9)
   end
+
+  it "movie_details", :vcr do
+     # VCR.use_cassette('movie_details') do
+       movie_details = service.movie_details(438631)
+
+       expect(movie_details).to be_instance_of(Hash)
+       expect(movie_details.first).to be_instance_of(Array)
+       expect(movie_details[:id]).to eq(438631)
+       expect(movie_details[:title]).to eq("Dune")
+       expect(movie_details[:runtime]).to eq(155)
+       expect(movie_details[:vote_average]).to eq(7.9)
+       expect(movie_details[:genres]).to eq([{:id=>878, :name=>"Science Fiction"}, {:id=>12, :name=>"Adventure"}])
+       expect(movie_details[:overview]).to be_instance_of(String)
+
+   end
 end
