b5a7a8b09ab5fac33825ea9db7dc08b7f02a9ddd
Drew Proebstel <d.proebstel@gmail.com>
2022-05-12 15:26:15 -0700

    wip

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -10,7 +10,7 @@ class MovieService
     put_returns_together(body_1[:results], body_2[:results])
     #returns an API response that includes the top 20 rated movies in the database.
     #turns the JSON into a hash both times
-    #concats results together 
+    #concats results together
   end
 
   def search_movies(search_param)
@@ -30,6 +30,7 @@ class MovieService
     def conn
       Faraday.new(url: "https://api.themoviedb.org") do |faraday|
         faraday.params['api_key'] = ENV['tmdb_api_key']
+        faraday.params[:query] = param unless param.nil?
       end
     end
 
@@ -41,3 +42,46 @@ class MovieService
       first + second
     end
 end
+# require 'faraday'
+# require 'json'
+#
+# class MovieService
+#   def top_movies
+#     response_1 = conn.get("/3/movie/top_rated?&language=en-US&page=1")
+#     response_2 = conn.get("/3/movie/top_rated?&language=en-US&page=2")
+#     body_1 = parse_json(response_1)
+#     body_2 = parse_json(response_2)
+#     put_returns_together(body_1[:results], body_2[:results])
+#     #returns an API response that includes the top 20 rated movies in the database.
+#     #turns the JSON into a hash both times
+#     #concats results together
+#   end
+#
+#   def search_movies(search_param)
+#     response = conn.get("/3/search/movie?&language=en-US&page=1&query=#{search_param}")
+#     # Takes the argument provided by the form and uses it as the query keyword param when sending request.
+#     body = parse_json(response)
+#   end
+#
+#   def find_movie(id)
+#     response = conn.get("/3/movie/#{id}?")
+#     body = parse_json(response)
+#   end
+#
+#
+#   private
+#
+#     def conn
+#       Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+#         faraday.params['api_key'] = ENV['tmdb_api_key']
+#       end
+#     end
+#
+#     def parse_json(response)
+#       JSON.parse(response.body, symbolize_names: true)
+#     end
+#
+#     def put_returns_together(first, second)
+#       first + second
+#     end
+# end
