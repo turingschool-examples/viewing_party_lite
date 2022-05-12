77527eface5a6c93a0e954a5a3e8704a5fe0a9b9
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-12 13:42:22 -0600

    add movie_review endpoint

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -7,13 +7,21 @@ class TmdbService
     get_url("/movie/top_rated?api_key=#{ENV['tmdb_api_key']}")[:results]
   end
 
-  def search(keyword, page=1)
+  def search(keyword, page = 1)
     get_url("/search/movie?api_key=#{ENV['tmdb_api_key']}&query=#{keyword}&page=#{page}")
   end
 
   def movie_details(movie_id)
-     get_url("/movie/#{movie_id}?api_key=#{ENV['tmdb_api_key']}&language=en-US")
+    get_url("/movie/#{movie_id}?api_key=#{ENV['tmdb_api_key']}&language=en-US")
   end
+
+  def movie_cast(movie_id)
+   get_url("/movie/#{movie_id}/credits?api_key=#{ENV['tmdb_api_key']}&language=en-US")
+ end
+
+ def movie_review(movie_id)
+  get_url("/movie/#{movie_id}/reviews?api_key=#{ENV['tmdb_api_key']}&language=en-US")
+end
 
   def get_url(url)
     response = @conn.get("https://api.themoviedb.org/3#{url}")
