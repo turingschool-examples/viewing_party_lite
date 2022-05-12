b6c22b579c0bf17f062895e19db6f9c778d32fd2
Susan Boyd <sueboyd922@gmail.com>
2022-05-12 15:32:24 -0400

    Move self.conn from base service to movie service

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
-    movies = MovieService.get_top_rated
   end
+  
   def results
 
     @user = User.find(params[:id])
@@ -11,9 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    #binding.pry
     @user = User.find(params[:user_id])
     @movie = MovieFacade.find_movie(params[:id])
-
   end
 end
