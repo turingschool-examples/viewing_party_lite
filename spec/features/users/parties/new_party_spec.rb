717d54ed5e70edb8eb4628471edd89dbe99b5101
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-12 14:49:45 -0600

    Test: add test for submitting a new party

--- spec/features/users/parties/new_party_spec.rb
+++ spec/features/users/parties/new_party_spec.rb
@@ -32,10 +32,25 @@ RSpec.describe "New Party Page" do
       expect(page).to have_field('Hazel', checked: false) 
       expect(page).to have_field('Ruby', checked: false) 
       expect(page).to have_field('Woobie', checked: false) 
-      
+    end 
+
+    it 'when a new party is created, I am redirected to my dashboard with new party shown' do
+      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
+      lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
+      hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
+
+      jaws_id = 578
+     
+      visit "/users/#{skeeter.id}/movies/#{jaws_id}/party/new"
+      fill_in "Duration of Party", with: 124
+      # fill_in "Day", with "2022/05/14"
+      page.find('#start_time').set("2022-04-05")
 
 
+      fill_in "Start Time", with: "8:00"
+      check("invites_#{lugnut.id}")
 
+      click_on "Create Party"
     end 
 
   end 
