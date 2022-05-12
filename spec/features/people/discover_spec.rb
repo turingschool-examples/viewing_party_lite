66c545e8dbabf8db4e8796619d4bab816898cfbc
Luke Pascale <lpluke5@gmail.com>
2022-05-11 22:57:46 -0500

    Feat: Begin to shape the discover page

---
+++ spec/features/people/discover_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "Discover Page" do
+  it 'has a button to discover top rated movies' do
+    person = Person.create!(name: "Billy Williams", email: "bwill@mailer.com")
+
+    party1 = Party.create!(host_id: 1, movie_id: 2, movie_name: "Star Wars", date: "May 4, 2022", time: "8:00pm", duration: 180)
+    party2 = Party.create!(host_id: 1, movie_id: 3, movie_name: "Tootsie", date: "June 12, 2022", time: "6:00pm", duration: 130)
+
+    PartyPerson.create(party_id: party2.id, person_id: person.id)
+    PartyPerson.create(party_id: party1.id, person_id: person.id)
+
+    visit discover_path(person.id)
+save_and_open_page
+    click_button "Find Top Rated Movies"
+  end
+
+  it 'has a text field to enter keywords to search by movie title' do
+    person = Person.create!(name: "Billy Williams", email: "bwill@mailer.com")
+
+    party1 = Party.create!(host_id: 1, movie_id: 2, movie_name: "Star Wars", date: "May 4, 2022", time: "8:00pm", duration: 180)
+    party2 = Party.create!(host_id: 1, movie_id: 3, movie_name: "Tootsie", date: "June 12, 2022", time: "6:00pm", duration: 130)
+
+    PartyPerson.create(party_id: party2.id, person_id: person.id)
+    PartyPerson.create(party_id: party1.id, person_id: person.id)
+
+    visit discover_path(person.id)
+
+    expect(page).to have_content("Search by movie title")
+
+    fill_in :search, with: "Top Gun"
+
+    click_button "Find Movies"
+  end
+
+  # it 'has a button to search by movie titles' do
+  #   person = Person.create!(name: "Billy Williams", email: "bwill@mailer.com")
+  #
+  #   party1 = Party.create!(host_id: 1, movie_id: 2, movie_name: "Star Wars", date: "May 4, 2022", time: "8:00pm", duration: 180)
+  #   party2 = Party.create!(host_id: 1, movie_id: 3, movie_name: "Tootsie", date: "June 12, 2022", time: "6:00pm", duration: 130)
+  #
+  #   PartyPerson.create(party_id: party2.id, person_id: person.id)
+  #   PartyPerson.create(party_id: party1.id, person_id: person.id)
+  #
+  #   visit discover_path(person.id)
+  #
+  #   click_button "Find Movies"
+  # end
+end
