0cfb24226bcb4845af34553e43553ea4ce58a323
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-12 14:43:08 -0600

    Feat: add checkboxes for all users to be added to the party

--- app/controllers/user_movie_parties_controller.rb
+++ app/controllers/user_movie_parties_controller.rb
@@ -1,8 +1,9 @@
 class UserMoviePartiesController < ApplicationController
 
   def new
-    movie_id = params[:movie_id].to_i
     @user = User.find(params[:user_id])
+    @users = User.all
+    movie_id = params[:movie_id].to_i
     conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
     end 
     response = conn.get("/3/movie/#{movie_id}?api_key=#{ENV['movie_db_key']}")
--- spec/features/users/parties/new_party_spec.rb
+++ spec/features/users/parties/new_party_spec.rb
@@ -7,13 +7,34 @@ RSpec.describe "New Party Page" do
     it 'it has the movie title and a form to create a new party' do
       skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
       jaws_id = 578
-      date = DateTime.now.strftime("%m/%d/%y")
+      # date = DateTime.now.strftime("%m/%d/%y")
       visit "/users/#{skeeter.id}/movies/#{jaws_id}/party/new"
-      save_and_open_page
+     
       expect(page).to have_content("Create a Movie Party for Jaws")
       expect(page).to have_field('Duration of Party', with: '124')
       expect(page).to have_field('Day')
       expect(page).to have_field('Start Time', with: '7:00')
+
+    end 
+
+    it 'the new party form has a checkbox list of all users to add them to the party' do
+      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com')
+      lugnut = User.create!(name: 'LugNut', email: 'fatdog@corgi.com')
+      hazel = User.create!(name: 'Hazel', email: 'hazelthehut@food.com')
+      ruby = User.create!(name: 'Ruby', email: 'rubythehut@food.com')
+      woobie = User.create!(name: 'Woobie', email: 'woobiethehut@food.com')
+      jaws_id = 578
+     
+      visit "/users/#{skeeter.id}/movies/#{jaws_id}/party/new"
+    
+      expect(page).to have_content('Invite Other Users')
+      expect(page).to have_field('LugNut', checked: false) 
+      expect(page).to have_field('Hazel', checked: false) 
+      expect(page).to have_field('Ruby', checked: false) 
+      expect(page).to have_field('Woobie', checked: false) 
+      
+
+
 
     end 
 
