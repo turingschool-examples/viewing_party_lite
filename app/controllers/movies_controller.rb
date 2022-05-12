52663aca8bb88107af6c174316715459f8ec8ad8
Ian Ross <ianrossdrums@gmail.com>
2022-05-12 16:26:31 -0600

    feat: add movies show crud

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
   def show
     @user = User.find(params[:user_id])
-    @movie = params[:movie_id]
+    @movie = MovieService.get_movie(params[:movie_id])
+    @cast = MovieFacade.top_10_cast(params[:movie_id])
+    @reviews = MovieFacade.reviews(params[:movie_id])
   end
 
   def new
