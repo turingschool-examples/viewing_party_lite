046ded06fc7320cc69a6772940770910b630b7ac
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-12 17:01:10 -0600

    refactor: tmdb_service

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -1,35 +1,29 @@
 class TmdbService
-  def initialize
-    @conn = Faraday.new
+  def self.top20
+    get_url("/movie/top_rated?")[:results]
   end
 
-  def top20
-    get_url("/movie/top_rated?api_key=#{ENV['tmdb_api_key']}")[:results]
-  end
-
-  def search(keyword, page = 1)
-    get_url("/search/movie?api_key=#{ENV['tmdb_api_key']}&query=#{keyword}&page=#{page}")
+  def self.search(keyword, page = 1)
+    get_url("/search/movie?&query=#{keyword}&page=#{page}")
   end
 
   def self.movie_details(movie_id)
-    get_url("/movie/#{movie_id}?api_key=#{ENV['tmdb_api_key']}&language=en-US")
+    get_url("/movie/#{movie_id}?&language=en-US")
   end
 
   def self.movie_cast(movie_id)
-    get_url("/movie/#{movie_id}/credits?api_key=#{ENV['tmdb_api_key']}&language=en-US")
+    get_url("/movie/#{movie_id}/credits?&language=en-US")
   end
 
   def self.movie_review(movie_id)
-    get_url("/movie/#{movie_id}/reviews?api_key=#{ENV['tmdb_api_key']}&language=en-US")
-  end
-
-  def get_url(url)
-    response = @conn.get("https://api.themoviedb.org/3#{url}")
-    JSON.parse(response.body, symbolize_names: true)
+    get_url("/movie/#{movie_id}/reviews?&language=en-US")
   end
 
   def self.get_url(url)
-    response = Faraday.get("https://api.themoviedb.org/3#{url}")
+    # response = Faraday.get("https://api.themoviedb.org/3#{url}")
+    response = Faraday.get("https://api.themoviedb.org/3#{url}") do |faraday|
+      faraday.params['api_key'] = ENV['tmdb_api_key']
+    end
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
 end
