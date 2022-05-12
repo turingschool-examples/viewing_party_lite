015e6021fb5213154dbc6bbe56fbb43ee0179edf
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-12 12:40:33 -0600

    test: movie_facade details method and define method

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -2,22 +2,22 @@ class MovieFacade
 
   def top20(data = top_movies_data)
   	data.map do |movie_data|
-  	  MovieBrief.new(movie_data)
+  	  MovieDetail.new(movie_data)
   	end
   end
 
   def search(keyword)
     search_movies_data(keyword)
-    if @first_20[:total_pages] == 0 
+    if @first_20[:total_pages] == 0
       "No movies found containing '#{keyword}'"
     else
       data = @first_20[:results]+@second_20[:results]
       data.map do |movie_data|
-        MovieBrief.new(movie_data)
+        MovieDetail.new(movie_data)
       end
     end
   end
- 
+
   def search_movies_data(keyword)
     @first_20 ||= service.search(keyword)
     @second_20 ||= service.search(keyword, 2)
@@ -25,6 +25,13 @@ class MovieFacade
   def top_movies_data
   	@top_movies_data ||= service.top20
   end
+
+  def details(movie_id)
+    # require 'pry'; binding.pry
+     # MovieDetail.new(TmdbService.movie_details(movie_id))
+     @details ||= service.movie_details(movie_id)
+
+   end
 
   def service
   	@service ||= TmdbService.new
--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -23,19 +23,34 @@ RSpec.describe MovieFacade do
     data = JSON.parse(json, symbolize_names: true)[:results]
     results = facade.top20(data)
     expect(results).to be_an Array
-    expect(results[0]).to be_a MovieBrief
+    expect(results[0]).to be_a MovieDetail
     expect(results[0].title).to eq('The Shawshank Redemption')
     expect(results[0].vote_average).to eq(8.7)
   end
 
-  it 'creates 40 movies from valid search', :vcr do 
+  it 'creates 40 movies from valid search', :vcr do
     results = facade.search('man')
 
     expect(results).to be_an Array
-    expect(results[0]).to be_a MovieBrief
+    expect(results[0]).to be_a MovieDetail
     expect(results.count). to eq(40)
     expect(results[0].title).to eq("The King's Man")
     expect(results[0].vote_average).to eq(6.9)
+  end
+
+  it "details", :vcr  do
+    # VCR.use_cassette('movie_details') do
+      movie_details = facade.details(438631)
+      # expect(movie_details).to be_instance_of(MovieDetail)
+      expect(movie_details).to be_instance_of(Hash)
+      expect(movie_details.first).to be_instance_of(Array)
+      expect(movie_details[:id]).to eq(438631)
+      expect(movie_details[:title]).to eq("Dune")
+      expect(movie_details[:runtime]).to eq(155)
+      expect(movie_details[:vote_average]).to eq(7.9)
+      expect(movie_details[:genres]).to eq([{:id=>878, :name=>"Science Fiction"}, {:id=>12, :name=>"Adventure"}])
+      expect(movie_details[:overview]).to be_instance_of(String)
+
   end
 
 end
