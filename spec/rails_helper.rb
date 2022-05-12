09b8f583478f19510aa28df65bc0d74690d8aef1
Kim G <xiaole.guo@yahoo.com>
2022-05-12 10:25:49 -0600

    add filters to ignore mailers channels jobs file in simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter "app/channels"
+  add_filter "app/mailers" 
+  add_filter "app/jobs"
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
