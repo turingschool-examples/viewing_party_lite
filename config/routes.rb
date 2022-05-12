1808f80c407eef4264f6cf9a79800998793e0b07
Joseph Senecal <jasenecal2@gmail.com>
2022-05-11 23:45:35 -0600

    replace new user path with /register

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get '/', to: 'landing_page#index'
 
-  get '/users/new', to: 'users#new'
-  post 'users', to: 'users#create'
+  get '/register', to: 'users#new'
+
+  post '/users', to: 'users#create'
 
   get '/users/:id/movies', to: 'movies#results'
-  get 'users/:id/discover', to: 'movies#index'
-  get 'users/:id', to: 'users#show'
+  get '/users/:id/discover', to: 'movies#index'
+  get '/users/:id', to: 'users#show'
 
 end
