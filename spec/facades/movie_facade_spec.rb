0aed3c4cb2975d46ead144e124a181e9902a79d5
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-12 21:30:21 -0600

    Feat: movie facade creates movie object from search by id

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Movie Facade" do
   it "returns top rated movies movies", :vcr do
     movies = MovieFacade.top_rated_movies
-  
+
     expect(movies).to be_a Array
     expect(movies).to be_all Movie
   end
@@ -13,5 +13,11 @@ RSpec.describe "Movie Facade" do
 
     expect(movies).to be_a Array
     expect(movies).to be_all Movie
+  end
+
+  it "finds movie details by id", :vcr do
+    movie = MovieFacade.movie_details(278)
+    
+    expect(movie).to be_a Movie
   end
 end
