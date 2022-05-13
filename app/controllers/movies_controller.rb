07751604205cc21a9b94175a13c18d9ee3c6b84b
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-12 21:20:18 -0600

    Feat: movies index lists movie title and vote average

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,15 @@
 class MoviesController < ApplicationController
   def index
-    
+    @user = User.find(params[:id])
+
+    if params[:keyword]
+      @movies = MovieFacade.search(params[:keyword])
+    else
+      @movies = MovieFacade.top_rated_movies
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
