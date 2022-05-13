1aa8e51bf2861276f36d17c60c728ef41648beb0
Joseph Senecal <jasenecal2@gmail.com>
2022-05-13 11:22:16 -0600

    minor changes, not working

---
+++ app/controllers/parties_controller.rb
@@ -0,0 +1,29 @@
+class PartiesController < ApplicationController
+
+  def new
+    movie_facade = MovieFacade.new
+    @movie = movie_facade.search_by_id(params[:id])
+    @users = User.all
+  end
+
+  def create
+    party = Party.create!(party_params)
+    params[:invite_users].each do |user_id|
+      UserParty.create!(user_id: user_id, party_id: party.id) if user_id != ""
+    end
+    # binding.pry
+    redirect_to users_dashboard_path(params[:user_id])
+  end
+
+private
+
+  def party_params
+    # params.permit(:id, :user_id, :start_time, :duration, :invite_users)
+    {
+      host_id: params[:user_id],
+      date: params[:start_time],
+      duration: params[:duration],
+      movie_id: params[:id]
+    }
+  end
+end
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "new viewing party page" do
   it "has the name of the movie and form to create new party" do
     json_response = File.read("./spec/fixtures/shawshank.json")
     user_1 = User.create!(name: "Twitch", email: "twitch@dogmail.com")
-    stub_request(:get, "https://api.themoviedb.org/3/movie/278?api_key=131d23d3e9d511ff6fce6fdc6799d9be").
+    stub_request(:get, "https://api.themoviedb.org/3/movie/278?api_key=131d23d3e9d511ff6fce6fdc6799d9be&append_to_response=credits,reviews").
          with(
            headers: {
        	  'Accept'=>'*/*',
@@ -12,6 +12,11 @@ RSpec.describe "new viewing party page" do
        	  'User-Agent'=>'Faraday v2.3.0'
            }).
          to_return(status: 200, body: json_response, headers: {})
-    visit "/users/#{user_1.id}/discover"
+    visit "/users/#{user_1.id}/movies/278/viewing-party/new"
+
+    expect(page).to have_field(:duration)
+    expect(page).to have_field(:start_time)
+    expect(page).to have_field("invite_users[]")
+    expect(page).to have_button("Create Party")
   end
 end
