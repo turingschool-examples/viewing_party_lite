a758dacc738f6abba5bd21b273201bbfb44f4dd1
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-13 02:32:46 -0600

    Test: User show page lists invitees for hosted and invited_to parties

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -7,18 +7,17 @@ RSpec.describe "user dashboard" do
     @user_3 = User.create!(name: "Ghastly Gary", email: "spooky@dead.org")
     @user_4 = User.create!(name: "Flake Fred", email: "wontshowup@oops.org")
 
+    @party_1 = Party.create!(date: "2022/07/01", start_time: "09:00 PM", duration: 300, host_id: @user_1.id, movie_id: 278)
+
     @party_2 = Party.create!(date: "2022/07/05", start_time: "10:00 PM", duration: 200, host_id: @user_2.id, movie_id: 278)
 
-    @pary_user = PartyUser.create!(party_id: @party_2.id, user_id: @user_1.id)
+    @pu_1 = PartyUser.create!(party_id: @party_1.id, user_id: @user_2.id)
+    @pu_2 = PartyUser.create!(party_id: @party_1.id, user_id: @user_4.id)
 
-    visit "/users/#{@user_1.id}/movies/278/viewing-party/new"
-    # fill_in "duration", with: "300"
-    fill_in "date", with: "2022/07/01"
-    fill_in "Start time", with: "09:00 PM"
-    check "invited[#{@user_2.id}]"
-    click_button "Submit"
+    @pu_3 = PartyUser.create!(party_id: @party_2.id, user_id: @user_1.id)
+    @pu_4 = PartyUser.create!(party_id: @party_2.id, user_id: @user_3.id)
 
-
+    visit "/users/#{@user_1.id}"
   end
 
   it "displays user name" do
@@ -33,6 +32,12 @@ RSpec.describe "user dashboard" do
 
   it "displays parties user is hosting" do
     within(".hosting") do
+      expect(page).to have_content(@user_2.name)
+      expect(page).to have_content(@user_4.name)
+
+      expect(page).to_not have_content(@user_1.name)
+      expect(page).to_not have_content(@user_3.name)
+
       expect(page).to have_content("The Shawshank Redemption")
       expect(page).to_not have_content(@party_2.date)
     end
@@ -41,6 +46,12 @@ RSpec.describe "user dashboard" do
   it "displays parties user is invited to" do
     within(".invited_to") do
       expect(page).to have_content("The Shawshank Redemption")
+      expect(page).to have_content(@user_1.name)
+      expect(page).to have_content(@user_3.name)
+
+      expect(page).to_not have_content(@user_2.name)
+      expect(page).to_not have_content(@user_4.name)
+
       expect(page).to have_content(@party_2.date)
     end
   end
