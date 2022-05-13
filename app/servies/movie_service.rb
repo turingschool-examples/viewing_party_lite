c037fb2056de525b8697ee6f9d1d7bf91f859f49
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-13 12:10:50 -0600

    Refactor: remove unnecessary do blocks from MovieService methods'

--- app/servies/movie_service.rb
+++ app/servies/movie_service.rb
@@ -17,8 +17,7 @@ class MovieService
   end 
 
   def self.find_by_id(movie_id)
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-    end 
+    conn = Faraday.new(url: "https://api.themoviedb.org")
     response = conn.get("/3/movie/#{movie_id}?api_key=#{ENV['movie_db_key']}")
     data = JSON.parse(response.body, symbolize_names: true)
   end 
@@ -26,8 +25,7 @@ class MovieService
   def self.find_movies(movie_ids)
     movies = []
     movie_ids.uniq.each do |id|
-      conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      end 
+      conn = Faraday.new(url: "https://api.themoviedb.org")
       response = conn.get("/3/movie/#{id}?api_key=#{ENV['movie_db_key']}")
       movies << JSON.parse(response.body, symbolize_names: true)
     end 
