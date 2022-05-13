494c1bb5ba691842c4236e11daa40fd95cef5e4c
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-12 23:44:07 -0600

    Test: Movies new page has form to create new party

---
+++ spec/features/parties/new_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Parties New Page" do
+  before :each do
+    @user_1 = User.create!(name: "Unreal Ursa", email: "thisaintreal@gotcha.org")
+    @user_2 = User.create!(name: "Fake Fanny", email: "nonsense@fake.com")
+    @user_3 = User.create!(name: "Ghastly Gary", email: "spooky@dead.org")
+    @user_4 = User.create!(name: "Flake Fred", email: "wontshowup@oops.org")
+
+    visit "/users/#{@user_1.id}/movies/278/viewing-party/new"
+  end
+
+  it "has form to create new party" do
+    # fill_in "duration", with: "300"
+    fill_in "date", with: "2022/07/01"
+    fill_in "Start time", with: "09:00 PM"
+    check "invited[#{@user_2.id}]"
+    click_button "Submit"
+
+    expect(current_path).to eq("/users/#{@user_1.id}")
+  end
+end
