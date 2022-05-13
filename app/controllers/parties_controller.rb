fb75f76894f6e21cafea19cdbe6ed44dc04aa62f
Tyler Ross <tross020890@gmail.com>
2022-05-13 09:01:56 -0500

    incomplete form for new party

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,6 +1,19 @@
 class PartiesController < ApplicationController
 
   def new
+    @movie = MovieFacade.find_movie(params[:id])
     @user = User.find(params[:id])
+    @users = User.all
   end
+
+  def create
+    party = Party.new(party_params)
+    if party.save
+      redirect_to
+  end
+
+private
+
+    def party_params
+      params.permit(:id, :duration, :date, :time, :host)
 end
