6af2561af97a750af9ffb78b126b771a1e7833d8
Susan Boyd <sueboyd922@gmail.com>
2022-05-13 12:37:10 -0400

    User Dashboard Page Test for hosting/inviting parties

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 RSpec.describe "Movie Details Page", type: :feature do
   it "has button to create viewing party", :vcr do
     user = User.create!(name: "Tim", email: "Tim@mail.com")
-    # movie = Movie.new(id: 1, title: "Movie", vote_average: "8.2")
     visit "/users/#{user.id}/movies/278"
     click_button "Create new viewing party"
 
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -22,4 +22,52 @@ RSpec.describe "User Dashboard/Show Page", type: :feature do
     click_on "Discover Movies"
     expect(current_path).to eq("/users/#{user1.id}/discover")
   end
+
+  it 'shows the viewing parties they were invited to' do
+    user_1 = User.create!(name: "Joe", email: "joe@mail.com")
+    user_2 = User.create!(name: "Amy", email: "amy@mail.com")
+
+    movie_1 = Movie.new(id: 1, title: "This Movie")
+    movie_2 = Movie.new(id: 2, title: "That Movie")
+    movie_3 = Movie.new(id: 3, title: "That Other Movie")
+
+    date = Date.today
+    time = Time.now
+    party_1 = Party.create!(date: date, time: time, duration: 97, movie_id: 1, host: user_2.id)
+    party_2 = Party.create!(date: date, time: time, duration: 120, movie_id: 2, host: user_1.id)
+    party_3 = Party.create!(date: date, time: time, duration: 100, movie_id: 3, host: user_2.id)
+
+    party_user_1 = PartyUser.create!(party: party_1, user: user_2, host: true)
+    party_user_2 = PartyUser.create!(party: party_1, user: user_1, host: false)
+    party_user_3 = PartyUser.create!(party: party_2, user: user_1, host: true)
+    party_user_4 = PartyUser.create!(party: party_2, user: user_2, host: false)
+    party_user_5 = PartyUser.create!(party: party_3, user: user_2, host: true)
+    party_user_6 = PartyUser.create!(party: party_3, user: user_1, host: false)
+
+    visit "/users/#{user_1.id}"
+    within ".invited_to" do
+      expect(page).to have_content(movie_1.title)
+      expect(page).to have_content(movie_3.title)
+      expect(page).not_to have_content(movie_2.title)
+    end
+
+    within ".hosting" do
+      expect(page).to have_content(movie_2.title)
+      expect(page).not_to have_content(movie_1.title)
+      expect(page).not_to have_content(movie_3.title)
+    end
+
+    visit "/users/#{user_2.id}"
+    within ".invited_to" do
+      expect(page).to have_content(movie_2.title)
+      expect(page).not_to have_content(movie_1.title)
+      expect(page).not_to have_content(movie_3.title)
+    end
+
+    within ".hosting" do
+      expect(page).to have_content(movie_1.title)
+      expect(page).to have_content(movie_3.title)
+      expect(page).not_to have_content(movie_2.title)
+    end
+  end
 end
