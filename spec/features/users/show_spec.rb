ef1bc2f1bd4fc9bad2cc4584fa0908941194fa00
Joseph Downs <93220002+josephdowns@users.noreply.github.com>
2022-05-13 13:35:03 -0600

    Update show_spec.rb
    change 27 to dynamic attribute

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "user/dashboard", type: :feature do
     @time = Time.now
     @user = User.create!(name: "Terra Branford", email: "terraff6@square.com")
     @party1 = Party.create!(host_id: @user.id, movie_id: 16, date: @date, start_time: @time, length: 120)
-    @party2 = Party.create!(host_id: 27, movie_id: 19, date: @date, start_time: @time, length: 120)
+    @party2 = Party.create!(host_id: @user.id, movie_id: 19, date: @date, start_time: @time, length: 120)
     UserParty.create!(user_id: @user.id, party_id: @party1.id, host: true)
     UserParty.create!(user_id: @user.id, party_id: @party2.id, host: false)
     visit "/users/#{@user.id}"
