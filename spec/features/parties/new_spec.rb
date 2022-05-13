00e450e4d51cd555fea071f21c1815c91c0809a2
Tyler Ross <tross020890@gmail.com>
2022-05-13 07:24:01 -0500

    incomplete new party formm

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe "New Viewing Party", type: :feature do
   it 'has a form to create new party' do
     user = User.create!(name: "Tim", email: "Tim@mail.com")
-    visit "/users/#{user.id}/movies/278/viewing-party/new"
+    visit "/users/#{user.id}/movies/278/viewing_party/new"
 end
