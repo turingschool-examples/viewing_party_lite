787868373a65e6c8bd1330ae6c71bd6dc8d5bd1c
craig cardwell <wizeduvcc@gmail.com>
2022-05-13 10:44:09 -0600

    refactor: edit tests and view for movie details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
     @movie_details = MovieFacade.single_movie(params[:id])
     @single_movie_credits = MovieFacade.single_movie_credits(params[:id])
     @single_movie_reviews = MovieFacade.single_movie_reviews(params[:id])
-    # require "pry"; binding.pry
   end
 
   private
