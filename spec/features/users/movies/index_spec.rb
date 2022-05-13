07e8a8df989276410066a65da5665109593d35df
Alex Psitos <apsitos08@gmail.com>
2022-05-12 21:26:37 -0400

    Test: users movies index spec accounts for sad paths of empty or no results

--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'the users index page' do
 
-  # before do
-  #   @user_1 = User.create!(name: 'Alfonso Diogenes', email: 'alphonsodiogenes420@hotmail.com')
+  before :each do
+    @user_1 = User.create!(name: 'Alfonso Diogenes', email: 'alphonsodiogenes420@hotmail.com')
   #   @user_2 = User.create!(name: 'Cradley Booper', email: 'cradleybooper420@hotmail.com')
   #   @user_3 = User.create!(name: 'Jangelina Aolie', email: 'jangelinaaolie420@hotmail.com')
   #   @party_1 = Party.create!(date: '2022-05-10', duration: 147, start_time: '7:00', movie: 'Jurassic Park', host: @user_1.id)
   #   @party_2 = Party.create!(date: '2022-05-11', duration: 112, start_time: '8:00', movie: 'Robocop', host: @user_1.id)
   #   @user_party_1 = UserParty.create!(user_id: @user_1.id, party_id: @party_1.id)
   #   @user_party_2 = UserParty.create!(user_id: @user_1.id, party_id: @party_2.id)
-  # end
+  end
 
   it 'shows top 40 movies' do
-    @user_1 = User.create!(name: 'Alfonso Diogenes', email: 'alphonsodiogenes420@hotmail.com')
     VCR.use_cassette("shows_top_40_movies") do
       visit user_discover_path(@user_1)
       click_button("Find Top Rated Movies")
@@ -24,13 +23,11 @@ RSpec.describe 'the users index page' do
   end
 
   it 'shows search results with vote averages and titles as links of movies that match search params' do
-    @user_1 = User.create!(name: 'Alfonso Diogenes', email: 'alphonsodiogenes420@hotmail.com')
     VCR.use_cassette("shows search results") do
       visit user_discover_path(@user_1)
 
       fill_in 'search', with: "fight club"
       click_button("Find Movies")
-      save_and_open_page
       expect(current_path).to eq(user_movies_path(@user_1))
 
       within "#movie-550" do
@@ -45,5 +42,21 @@ RSpec.describe 'the users index page' do
         expect(page).to have_link("GCW Fight Club")
       end
     end
+  end
+
+  it 'can return message if search left blank', :vcr do
+    visit user_discover_path(@user_1)
+    fill_in 'search', with: ""
+    click_button("Find Movies")
+    expect(current_path).to eq(user_movies_path(@user_1))
+    expect(page).to have_content("No results found")
+  end
+
+  it 'can return a message if search results return empty', :vcr do
+    visit user_discover_path(@user_1)
+    fill_in 'search', with: "gjfhdgdga"
+    click_button("Find Movies")
+    expect(current_path).to eq(user_movies_path(@user_1))
+    expect(page).to have_content("No results found")
   end
 end
