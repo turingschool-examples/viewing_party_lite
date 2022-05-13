7bcf45134125e85548456d91591e017c837f43b9
John Hennerich <john.hennerich@gmail.com>
2022-05-13 10:34:14 -0600

    Refactor: movie service full implement of inheritance
    Co-authored-by: Brylan Gannon <94757433+B-gann21@users.noreply.github.com>

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class ApplicationService
   def conn
-    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.params['api_key'] = ENV['tmdb_key']
     end
   end
 
   def parse_json(response)
-    JSON.parse(response, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
