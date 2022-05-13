6fd0a3a247e962eed96eeab8c29875279bee4101
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-13 02:52:53 -0600

    create layouts for reused buttons

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -3,16 +3,19 @@ require "rails_helper"
 RSpec.describe "Movies Show Page" do
   before :each do
     @user_1 = User.create!(name: "Unreal Ursa", email: "thisaintreal@gotcha.org")
-    @user_2 = User.create!(name: "Fake Fanny", email: "nonsense@fake.com")
-    @user_3 = User.create!(name: "Ghastly Gary", email: "spooky@dead.org")
-    @user_4 = User.create!(name: "Flake Fred", email: "wontshowup@oops.org")
 
     visit "/users/#{@user_1.id}/movies/278"
   end
 
   it "has button to create viewing party" do
     click_button "Create Viewing Party"
-    # save_and_open_page
+
     expect(current_path).to eq("/users/#{@user_1.id}/movies/278/viewing-party/new")
+  end
+
+  it "has button leading to discover page" do
+    click_button "Discover Page"
+
+    expect(current_path).to eq("/users/#{@user_1.id}/discover")
   end
 end
