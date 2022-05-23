964a9e28cc84ea2f3e3f297e700071bba85a01ae
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-23 13:32:01 -0600

    Test: add testing for login w/ correct info, takes to users dashboard

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -60,10 +60,22 @@ RSpec.describe 'Landing/Welcome Page' do
     it 'clicking link to login, takes me to login page /login' do
       skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
       visit "/"
-      save_and_open_page
       click_button 'Login'
-
+    
       expect(current_path).to eq('/login')
+    end
+    
+    it 'When I enter my email and password correctly I am taken to my dashboard' do
+      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
+      visit "/login"
+
+      fill_in :name, with: 'Skeeter'
+      fill_in :email, with: 'skeeter@example.com'
+      fill_in :password, with: 'test123'
+
+      click_on "Log In"   
+
+      expect(current_path).to eq("/users/#{skeeter.id}")
     end
 
 
