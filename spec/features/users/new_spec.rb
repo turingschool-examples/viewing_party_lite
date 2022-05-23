2e70be053665c9e1571b90b310d5827ff663278a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-23 12:16:24 -0600

    Feat: add/fix form on new user view to reflect new columns in User model

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe 'New User Page', type: :feature do
   describe 'happy path' do
     it 'filling in the form correctly redirects me to my new user dashboard/show page' do
       visit '/register'
-      fill_in :user_name, with: 'skeeter'
-      fill_in :user_email, with: 'skeeter@example.com'
-      fill_in :user_password, with: 'password'
-      fill_in :user_password_confirmation, with: 'password'
+      save_and_open_page
+      fill_in :name, with: 'skeeter'
+      fill_in :email, with: 'skeeter@example.com'
+      fill_in :password, with: 'password'
+      fill_in :password_confirmation, with: 'password'
       click_on 'Create User'
 
       new_user = User.last
