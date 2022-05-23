eb216479ad01e0ccd2235c0273b7042b9be338a6
James Harkins <jh.harkins@protonmail.com>
2022-05-23 13:11:48 -0600

    Feat: pass sad path tests for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,12 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       redirect_to "/users/#{user.id}"
+    elsif params[:password_confirmation].empty?
+      redirect_to "/register"
+      flash[:invalid_email] = "Please confirm your password in the 'Password confirmation' field."
+    elsif params[:password] != params[:password_confirmation]
+      redirect_to "/register"
+      flash[:invalid_email] = "Passwords must match."
     else
       redirect_to "/register"
       flash[:invalid_email] = "There is already an account associated with this e-mail address."
