02e1399b413918e277cae78c5edcda293e2e11a3
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-23 11:50:06 -0600

    Test: add model/validation test for password_digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest)}
+    it 'encrpyts a users password' do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end 
   end
 
   describe 'relationships' do
