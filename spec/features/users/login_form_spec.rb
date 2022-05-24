bc03b9d441643ac5f2fd2440379fcd1fd21de4dc
Alex Psitos <apsitos08@gmail.com>
2022-05-24 17:36:19 -0400

    Test: write test for user login sad path

--- spec/features/users/login_form_spec.rb
+++ spec/features/users/login_form_spec.rb
@@ -4,11 +4,27 @@ RSpec.describe 'login page' do
   it 'has fields to login that when correctly filled out I am taken to my dashboard' do
     user_1 = User.create!(name: 'Alex', email: 'alex@testexample.com', password: '1234', password_confirmation: '1234')
     visit '/login'
-save_and_open_page
+
     fill_in 'Email', with: "#{user_1.email}"
     fill_in 'Password', with: "#{user_1.password}"
     click_button("Login")
-    expect(current_path).to eq("/users/#{user_1.id}")
+    expect(current_path).to eq(user_dashboard_path(user_1))
 
+  end
+
+  context 'sad path' do
+    it 'redirects to login form if user fails to enter correct credentials' do
+      user_1 = User.create!(name: 'Alex', email: 'alex@testexample.com', password: '1234', password_confirmation: '1234')
+      visit '/login'
+
+      fill_in 'Email', with: "#{user_1.email}"
+      fill_in 'Password', with: "i forgot"
+      click_button("Login")
+
+      expect(current_path).to eq('/login')
+      within '.flash' do
+        expect(page).to have_content('Invalid email/password')
+      end
+    end
   end
 end
