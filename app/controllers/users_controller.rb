8c3f550193e5cb53d5e473da8c32ae38a92ecee8
James Harkins <jh.harkins@protonmail.com>
2022-05-24 14:40:11 -0600

    Feat: pass tests for dashboard authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(session[:user_id])
+    if current_user
+      @user = User.find(session[:user_id])
+    else
+      redirect_to "/"
+      flash[:login] = "You must be logged in to access the Dashboard."
+    end
   end
 
   def new
@@ -15,24 +20,6 @@ class UsersController < ApplicationController
       flash[:notice] = user.errors.full_messages.to_sentence
     end
   end
-
-  # def login_form
-  # end
-  #
-  # def login_user
-  #   user = User.find_by(email: params[:email])
-  #   if user
-  #     if user.authenticate(params[:password])
-  #       redirect_to "/users/#{user.id}"
-  #     else
-  #       flash[:invalid_password] = "Invalid password."
-  #       render :login_form
-  #     end
-  #   else
-  #     flash[:invalid_email] = "There is no account associated with this email address."
-  #     render :login_form
-  #   end
-  # end
 
   private
 
