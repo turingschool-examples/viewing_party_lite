07cffde1bee8f9ffab4dd673df83941b893dd391
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-24 13:59:18 -0600

    test: prep movies_results_spec for removal of /users/:id, set up before block for logging in user

--- spec/features/users/discover/movies_results_spec.rb
+++ spec/features/users/discover/movies_results_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Movies Results Page', type: :feature do
+  before :each do
+    @user1 = User.create!(name: 'Skeeter', email: 'skeeter@skeeter.com', password: 'test')
+    @user2 = User.create!(name: 'Alex', email: 'alex@alex.com', password: 'test123')
+    visit '/'
+
+    click_button 'Login'
+    fill_in 'Name:', with: 'Skeeter'
+    fill_in 'Email:', with: 'skeeter@skeeter.com'
+    fill_in 'Password', with: 'test'
+    click_on 'Log In'
+  end
 
   describe 'As a Visitor' do 
 
     it 'clicking Find Top Rated Movies displays the top rated movies and their vote average on the results page', :vcr do
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
-      visit "/users/#{skeeter.id}/discover"
+      # skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
+      visit "/discover"
 
       click_button "Find Top Rated Movies"
       expect(page).to have_content("Top Rated Movies") 
@@ -17,8 +28,8 @@ RSpec.describe 'Movies Results Page', type: :feature do
     end 
 
     it 'searching for a movie by title returns all movies with search word in title', :vcr do
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
-      visit "/users/#{skeeter.id}/discover"
+      # skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
+      visit "/discover"
 
       fill_in "Search by Movie Title", with: "jaws"
       click_on "Find Movies"
@@ -33,33 +44,33 @@ RSpec.describe 'Movies Results Page', type: :feature do
     end 
 
     it 'has a button to return back to the users discovery page', :vcr do
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
-      visit "/users/#{skeeter.id}/discover"
+      # skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
+      visit "/discover"
       click_button "Find Top Rated Movies"
 
       click_on "Discover Page"
-      expect(current_path).to eq("/users/#{skeeter.id}/discover")
+      expect(current_path).to eq("/discover")
     end 
 
     it 'each top movie result displayed is a link to that movies movie detail page', :vcr do
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
-      visit "/users/#{skeeter.id}/discover"
+      # skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
+      visit "/discover"
       click_button "Find Top Rated Movies"
  
       click_link "The Shawshank Redemption"
-      expect(current_path).to eq("/users/#{skeeter.id}/movies/278")
+      expect(current_path).to eq("/users/#{@user1.id}/movies/278")
     end 
 
     it 'each search result movie is a link to that movies detail page', :vcr do
 
-      skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
-      visit "/users/#{skeeter.id}/discover"
+      # skeeter = User.create!(name: 'Skeeter', email: 'skeeter@example.com', password_digest: 'test123', password_confirmation: 'test123')
+      visit "/discover"
   
       fill_in "Search by Movie Title", with: "jaws"
       click_on "Find Movies"
 
       click_link "Jaws 3-D"
-      expect(current_path).to eq("/users/#{skeeter.id}/movies/17692")
+      expect(current_path).to eq("/users/#{@user1.id}/movies/17692")
 
     end 
 
