4645998095f3307a6884e296185f733d35fef0ad
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-28 11:25:15 -0600

    feat: move movie details show page API call to movie facade and cast facade, change view to use movie and cast poro

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -20,22 +20,11 @@ class UserMoviesController < ApplicationController
 
   def show
     if current_user
-      conn = Faraday.new(url: "https://api.themoviedb.org")
-      response1 = conn.get("/3/movie/#{params[:movie_id]}?api_key=#{ENV['movie_db_key']}")
-      response2 = conn.get("/3/movie/#{params[:movie_id]}/credits?api_key=#{ENV['movie_db_key']}")
-      data1 = JSON.parse(response1.body, symbolize_names: true)
-      data2 = JSON.parse(response2.body, symbolize_names: true)
-
-      @movie_data = {movie: data1, cast: data2[:cast][0..9]}
+      @movie = MovieFacade.movie_id(params[:movie_id])
+      @cast = MovieFacade.cast(params[:movie_id])
     else
-      conn = Faraday.new(url: "https://api.themoviedb.org")
-      response1 = conn.get("/3/movie/#{params[:movie_id]}?api_key=#{ENV['movie_db_key']}")
-      response2 = conn.get("/3/movie/#{params[:movie_id]}/credits?api_key=#{ENV['movie_db_key']}")
-      data1 = JSON.parse(response1.body, symbolize_names: true)
-      data2 = JSON.parse(response2.body, symbolize_names: true)
-
-      @movie_data = {movie: data1, cast: data2[:cast][0..9]}
-
+      @movie = MovieFacade.movie_id(params[:movie_id])
+      @cast = MovieFacade.cast(params[:movie_id])
     end 
   end
   
--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,12 +1,15 @@
 class Movie 
-  attr_reader :title, :id, :vote_average, :runtime, :original_title, :poster_path
+  attr_reader :title, :id, :vote_average, :runtime, :original_title, :poster_path, :genres, :overview
 
   def initialize(data)
+
     @title = data[:title]
     @id = data[:id]
     @vote_average = data[:vote_average]
     @runtime = data[:runtime]
     @original_title = data[:original_title]
     @poster_path = data[:poster_path]
+    @genres = data[:genres].map { |genre| genre.values[1]}
+    @overview = data[:overview]
   end 
 end 
