c44ca7892b0a5381ae9daf9990edefa935f259b9
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-05 17:33:22 -0600

    Test: user viewing party model

---
+++ spec/models/user_viewing_party_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe UserViewingParty, type: :model do
+
+  describe "associations" do
+    it { should belong_to :user }
+    it { should belong_to :viewing_party }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :hosting }
+  end
+
+end
