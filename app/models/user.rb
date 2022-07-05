5cced7b09d50ed4662790bd8eae3a64d7bae8fda
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-05 17:24:26 -0600

    Test: viewing party model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_many :user_viewing_parties
   has_many :viewing_parties, through: :user_viewing_parties
 
-  validates_presence_of :name, :email
+  validates_presence_of :name
+  validates_uniqueness_of :email
 end
