52d7205f9ac7bc69c6541d1c181f76e2a1f97cc8
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-05 18:01:00 -0600

    Test: welcome page

--- spec/features/application/welcome_spec.rb
+++ spec/features/application/welcome_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'landing page', type: :feature do
 
   it "displays the title of the application, a button to create new users, and existing users" do
     
-    
     visit "/"
 
     expect(page).to have_content("Viewing Party Light")
