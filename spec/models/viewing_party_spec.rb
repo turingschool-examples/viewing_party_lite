cb97018d6f58f238097f8c7bbb57d38c7fb5e05a
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-05 18:55:59 -0600

    created model tests for User

---
+++ spec/models/viewing_party_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ViewingParty, type: :model do
+  describe 'Relationships' do
+    it { should have_many :attendees }
+    it { should have_many(:users).through(:viewing_partys) }
+  end
+end
