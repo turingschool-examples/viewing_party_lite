59da8bb752724fcb2a2957166b3c5920fdce6d1f
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-05 19:07:25 -0600

    Added validations for User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :attendees
   has_many :viewing_partys, through: :attendees
+  validates_presence_of :user_name, :email
+  validates_uniqueness_of :email
 end
