bb54837dda1593519a21dd611f163b1e9900d13e
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-05 18:27:04 -0600

    Feat: display info, links, and buttons on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,7 @@
 class WelcomeController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
 end
---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper' 
+
+RSpec.describe 'landing page', type: :feature do
+
+  it "displays the title of the application, a button to create new users, and existing users" do
+    user1 = User.create!(name: "Jane Powell", email: "jpowell38@gmail.com")
+    user2 = User.create!(name: "Ann Miller", email: "amiller@gmail.com")
+    visit "/"
+
+    expect(page).to have_content("Viewing Party Light")
+    expect(page).to have_link("Home")
+    click_link("Home")
+    expect(current_path).to eq("/")
+    expect(page).to have_button("Create a New User")
+    expect(page).to have_content("Existing Users:")
+    within "#user-0" do
+      expect(page).to have_link("Jane Powell's Dashboard")
+      expect(page).to_not have_link("Ann Miller's Dashboard")
+      # click_link("Jane Powell's Dashboard")
+      # expect(current_path).to eq("/users/#{user1.id}")
+      # visit "/"
+    end
+    within "#user-1" do
+      expect(page).to have_link("Ann Miller's Dashboard")
+      expect(page).to_not have_link("Jane Powell's Dashboard")
+      # click_link("Ann Miller's Dashboard")
+      # expect(current_path).to eq("/users/#{user2.id}")
+    end
+  end
+end
