606d7ec0954b7a5124ee3408e4dafeb1f27793cd
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-06 16:26:33 -0600

    All tests passing for model relationships and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do 
   describe 'relationships' do 
-    it { should have many :user_viewing_parties }
-    it { should have_many(:viewing_parties).through(user_viewing_parties) }
+    it { should have_many :user_viewing_parties }
+    it { should have_many(:viewing_parties).through(:user_viewing_parties) }
   end
 
   describe 'validations' do 
