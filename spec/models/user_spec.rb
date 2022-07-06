f3d21c471597c0af828d3954258a617f241f3260
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-05 19:06:00 -0600

    added validations tests to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe User, type: :model do
     it { should have_many :attendees }
     it { should have_many(:viewing_partys).through(:attendees) }
   end
+
+  describe 'Validations' do
+    it { should validate_presence_of :user_name }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+  end
 end
