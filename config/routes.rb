bd16aa89d1b12d4c125b5e8239f92e4d8dd82314
Jim Riddle <jimriddle0@gmail.com>
2022-07-06 16:56:25 -0500

    coverage, routes, schema

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "landing#home"
+
+  resources :users
+  resources :parties
+  # get '/admin', to: 'admins#dashboard'
 end
