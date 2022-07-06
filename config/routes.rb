2ccb01b1f3770b21b53c8355948f8eccacf9ea01
Bryce Wein <bryce.wein@gmail.com>
2022-07-05 22:58:10 -0400

    Refactor: change routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :landing, only: [:index]
+  root to: 'welcome#index'
 
-resources :users, only: %i[show create] 
+  resources :users, only: %i[new create]
 end
