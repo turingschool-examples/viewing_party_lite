de59bfcc3a11015ab9bf7953d6a5129c56c0283f
Bryce Wein <bryce.wein@gmail.com>
2022-07-06 15:07:08 -0400

    Feat: new user creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
-  resources :users, only: %i[new create]
+  get '/register', to: 'users#new'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users, only: %i[create show]
 end
