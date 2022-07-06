341e295e368845a2cfd78941fe763a96ea062dcc
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-06 16:20:39 -0600

    Relationship and validation tests for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :user_viewing_party
-  has_many :viewing_party, through: :user_viewing_party
+  has_many :user_viewing_parties
+  has_many :viewing_parties, through: :user_viewing_parties
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
--- spec/models/user_viewing_party_spec.rb
+++ spec/models/user_viewing_party_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe UserViewingParty, type: :model do
     it { should belong_to :user }
     it { should belong_to :viewing_party }
   end
+
+  describe 'validations' do 
+    it { should validate_presence_of(:host) }
+  end
 end
