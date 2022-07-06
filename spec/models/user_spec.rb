e980bb496b36bda5cab320014229496ec31d2ca4
Andrew King <dev.andrewking@gmail.com>
2022-07-06 17:25:48 -0500

    add party and user model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'relationships' do
+    it { should have_many :party_users }
+    it { should have_many(:parties).through(:party_users) }
+  end
+
+    describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+  end
+end
