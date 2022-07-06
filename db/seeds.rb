bd969fdd36eb8280686a6e361961374d0716b35d
Ian Ross <ianrossdrums@gmail.com>
2022-07-06 16:46:19 -0600

    feat: make seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(name: "Pabu", email: "pabu@pabu.com")
+user2 = User.create(name: "Loki", email: "loki@loki.com")
+user3 = User.create(name: "Thor", email: "thor@thor.com")
+user4 = User.create(name: Faker::JapaneseMedia::OnePiece.character ,email: "#{Faker::JapaneseMedia::OnePiece.island}@onepiece.com")
+user5 = User.create(name: Faker::JapaneseMedia::SwordArtOnline.real_name ,email: "#{Faker::JapaneseMedia::SwordArtOnline.game_name}@swordart.com")
+
+#viewing_party1 = ViewingParty.create(movie_id: 1, date: 2022-07-06, start_time: ,duration: 180)
