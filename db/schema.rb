b8c5e4b4d72ebfa1d5a04318a1157128843e89e6
Zachary P <zachary.s.prince@outlook.com>
2022-07-07 16:35:55 -0400

    Remove host from parties table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_707_002_240) do
+ActiveRecord::Schema.define(version: 20_220_707_203_532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'parties', force: :cascade do |t|
-    t.string 'host'
     t.string 'movie_name'
     t.integer 'movie_id'
     t.date 'date'
