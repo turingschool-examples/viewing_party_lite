aa487560f2cde7d40e7bbc0a24cb7b0b74c26e8e
Zachary P <zachary.s.prince@outlook.com>
2022-07-07 18:33:41 -0400

    Change root route formatting to fix heroku issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'welcome#index'
+  root to: 'welcome#index'
 
   resources :users, only: %i[show create]
   get 'register' => 'users#new'
