ff6e4aae6370054ca4e53c8c025f7ff67e0dd700
Jim Riddle <jimriddle0@gmail.com>
2022-07-07 18:37:12 -0500

    facade tests

---
+++ spec/facades/movie_facade_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'MovieFacade' do
+
+  it 'gives me an array of movie objects', :vcr do
+    movies = MovieFacade.top_20_movies
+
+    expect(movies).to be_a Array
+    expect(movies).to be_all Movie
+  end
+end
