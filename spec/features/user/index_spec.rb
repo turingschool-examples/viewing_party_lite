ec9b99fa46de96f9269a6e8ba467351b4ef56e5c
Wesley Garcia <wes@alltradestech.com>
2022-07-06 18:51:10 -0700

    Add User controller and User#index view

--- spec/features/user/index_spec.rb
+++ spec/features/user/index_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "User Index Page", type: :feature do
   describe "landing page" do
-    it "shows as the main landing page" do
+    it "has the title of the application and exists as our landing page" do
       visit '/'
 
-      expect(path).to eq('/users')
+      expect(page).to have_content("Viewing Party Lite")
     end
-
-    it "has the title of the application"
 
     it "has a button to create a new user"
 
