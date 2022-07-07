f75dc6dab55f98f3a86567fbb14ec74673ef9933
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-07 08:20:46 -0600

    Add partial, new, create, and happy path + flash messaging for registering a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
+  end
+
+  def create
+    user = User.create!(user_params)
+    redirect_to user_path(user.id), notice: "Welcome, #{user.name}!"
   end
 
   def show
 
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'New User Form' do 
   describe 'happy path' do 
+    it 'has a form to make a new user' do 
+      visit register_path
 
+      fill_in 'Name', with: 'Zachary'
+      fill_in 'Email', with: 'superbadismyfave@yahoo.com'
+      click_on 'Register'
+
+      user = User.last
+      expect(current_path).to eq(user_path(user.id))
+      expect(page).to have_content("Welcome, #{user.name}!")
+    end 
   end
 
   describe 'sad path' do 
