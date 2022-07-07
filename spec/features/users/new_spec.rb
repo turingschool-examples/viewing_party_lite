2ecb0c317a094fc4ef3c2affc16b69dfb4d80a86
Andrew King <dev.andrewking@gmail.com>
2022-07-07 01:22:40 -0500

    change test to button

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'register new user' do
 
   it "can create a new user" do
     visit '/'
-    expect(page).to have_link("Create New User")
-    click_link "Create New User"
+    expect(page).to have_button("Create New User")
+    click_button "Create New User"
     expect(current_path).to eq('/register')
     fill_in "Name", with: "Lee"
     fill_in "Email", with: "lee@gmail.com"
