88a6d282383290908383d492353768c48f323ce6
Tyler Ross <tross020890@gmail.com>
2022-07-07 15:39:53 -0500

    Feat: add blank movies discover/index page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -30,4 +30,14 @@ RSpec.describe "User Dashboard/Show Page", type: :feature do
 
     expect(page).to have_button("Discover Movies")
   end
+
+  it "Button to discover movies redirects to movie discover/index" do
+    user1 = User.create!(name: "Jim", email: 'jim@mail.com')
+    user2 = User.create!(name: "Tommy", email: 'tommy@mail.com')
+
+    visit "/users/#{user1.id}"
+    click_button "Discover Movies"
+
+    expect(current_path).to eq("/users/#{user1.id}/discover")
+  end
 end
