3bf113fa61ba3792907203a74674d63225e372ca
Zachary P <zachary.s.prince@outlook.com>
2022-07-06 21:41:13 -0400

    Add user create method and strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
   def new; end
+
+  def create
+    new_user = User.create!(user_params)
+
+    redirect_to "/users/#{new_user.id}"
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :name)
+  end
 end
