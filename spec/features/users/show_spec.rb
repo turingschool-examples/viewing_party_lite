51c5db28c1b19b596d9639c8dee8087e353c4103
Zachary P <zachary.s.prince@outlook.com>
2022-07-07 16:46:16 -0400

    Revise user show page etsts

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -17,11 +17,22 @@ RSpec.describe 'user dashboard page' do
     expect(page).to have_button('Discover Movies')
   end
 
-  # it 'has a section that lists viewing parties' do
-  #   user = User.create!(name: 'Tommy Tanktop', email: 'tanktoptitan@aol.com')
-  #   vparty1 = Party.new(host: )
-  #     visit "/users/#{user.id}"
+  it 'has a section that lists viewing parties' do
+    user = User.create!(name: 'Tommy Tanktop', email: 'tanktoptitan@aol.com')
 
-  #   expect(page).to have_content('Discover Movies')
-  # end
+    Party.new(movie_name: 'Fight Club', movie_id: 1, date: Time.now.strftime('%d/%m/%Y'),
+              time: Time.now.strftime('%H:%M'), duration: 120)
+    Party.new(movie_name: 'Fight Club 2', movie_id: 1,
+              date: Time.now.strftime('%d/%m/%Y'), time: Time.now.strftime('%H:%M'), duration: 120)
+    Party.new(movie_name: 'Fight Club 3', movie_id: 1, date: Time.now.strftime('%d/%m/%Y'),
+              time: Time.now.strftime('%H:%M'), duration: 120)
+
+    visit "/users/#{user.id}"
+
+    within '#viewingParties' do
+      expect(page).to have_content('Fight Club')
+      expect(page).to have_content('Fight Club 2')
+      expect(page).to have_content('Fight Club 3')
+    end
+  end
 end
