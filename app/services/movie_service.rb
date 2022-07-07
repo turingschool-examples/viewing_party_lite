6b724fc661503d1d3d9da446201409f47a4e3941
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-07 15:45:49 -0600

    Remove old method

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -15,10 +15,4 @@ class MovieService
     response = conn.get(endpoint)
     json = JSON.parse(response.body, symbolize_names: true)
   end
-  # def self.get_top_rated_movies
-  #   conn = Faraday.new(url: "https://api.themoviedb.org")
-  #   response = conn.get("/3/movie/top_rated?api_key=#{ENV['movie_api_key']}")
-  #   data = JSON.parse(response.body, symbolize_names: true)
-  #   data[:results]
-  # end
 end
