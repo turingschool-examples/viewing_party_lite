31bb7d628a9296356ff3a0421c2d78b7a66abe65
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-07 15:44:18 -0600

    Refa: MovieService search method and test for Controller

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,9 +1,24 @@
 class MovieService
 
-  def self.get_top_rated_movies
-    conn = Faraday.new(url: "https://api.themoviedb.org")
-    response = conn.get("/3/movie/top_rated?api_key=#{ENV['movie_api_key']}")
-    data = JSON.parse(response.body, symbolize_names: true)
-    data[:results]
+  def self.conn
+    Faraday.new('http://api.themoviedb.org/3/') do |faraday|
+      faraday.params["api_key"] = ENV['movie_api_key']
+    end
   end
+
+  def self.movie_query_results(query)
+    if query == "top%20rated"
+      endpoint = "movie/top_rated"
+    else
+      endpoint = "search/movie?query=#{query}"
+    end
+    response = conn.get(endpoint)
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+  # def self.get_top_rated_movies
+  #   conn = Faraday.new(url: "https://api.themoviedb.org")
+  #   response = conn.get("/3/movie/top_rated?api_key=#{ENV['movie_api_key']}")
+  #   data = JSON.parse(response.body, symbolize_names: true)
+  #   data[:results]
+  # end
 end
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -3,8 +3,27 @@ require 'rails_helper'
 RSpec.describe MovieService do
   describe "top rated movies endpoint" do
     it "gets the top rated movies from this endpoint", :vcr do
-      json = MovieService.get_top_rated_movies
-      expect(json[0]).to have_key(:title)
+      query = "top%20rated"
+      parsed_json = MovieService.movie_query_results(query)
+
+      expect(parsed_json).to be_a Hash
+      expect(parsed_json[:results]).to be_a Array
+
+      top_movie = parsed_json[:results].first
+
+      expect(top_movie).to include :title, :overview, :original_title, :vote_count #etc.
+      expect(top_movie[:title]).to be_a String
+      expect(top_movie[:overview]).to be_a String
+      expect(top_movie[:original_title]).to be_a String
+      expect(top_movie[:vote_count]).to be_a Integer
+    end
+
+    it "gets a searched movie via the search endpoint", :vcr do
+      query = "search/movie?query=Shaw"
+      parsed_json = MovieService.movie_query_results(query)
+
+      expect(parsed_json).to be_a Hash
+      expect(parsed_json[:results]).to be_a Array
     end
   end
 end
