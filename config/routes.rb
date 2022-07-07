ef4435672bde949e25641d36c9ea7372a2ef8db2
Zachary P <zachary.s.prince@outlook.com>
2022-07-07 19:05:49 -0400

    Add uers discover route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
-  resources :users, only: %i[show create]
+  resources :users, only: %i[show create] do
+    resources :discover, controller: :movies
+  end
   get 'register' => 'users#new'
 
   resources :movies, only: :index
