a6aa49ad65d256975b390815c07e75861be69b35
Drew Proebstel <d.proebstel@gmail.com>
2022-07-06 18:34:29 -0700

    dashboard discover movies done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'landing#index'
   get '/users/register', to: 'users#new'
-  get '/users/:id/discover', to: 'users#discover'
+  get '/users/:id/discover', to: 'users#search'
   get '/users/:id', to: 'users#show'
   post '/users/register', to: 'users#create'
 
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe "Dashboard Page" do
 
     expect(page).to have_content("Geddy's Dashboard")
   end
+
+  it 'has a button to navigate to discover movie page' do
+    user = User.create(name: 'Geddy', email: '2112@yyz.com')
+    visit "/users/#{user.id}"
+    click_button("Discover Movies")
+
+    expect(current_path).to eq("/users/#{user.id}/discover")
+  end
 end
