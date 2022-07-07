5bebbc702b35f83b718a7981a4ee1e3abfdf032b
Sarah Hall <swatson1994@yahoo.com>
2022-07-06 21:58:23 -0400

    Feat: Add route, controller, and temp view for successful user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    user = User.create!(name: params[:name], email: params[:email])
+    redirect_to "/users/#{user.id}"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
