0f86e7d497d6a928b30f411a8ca7d9fefe716bac
Luke Pascale <lpluke5@gmail.com>
2022-07-07 16:51:54 -0500

    Add testing and discover movies page. Try and finish all routes

--- spec/features/user/discover_spec.rb
+++ spec/features/user/discover_spec.rb
@@ -1,16 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "The User's Discover Movies Page", type: :feature do
-  describe 'dashboard' do
     it 'has VPL title and discover movies at the top' do
-      user = User.create!(name: "Rand", email: "randalthor@gmail.com")
+    user = User.create!(name: "Rand", email: "randalthor@gmail.com")
 
-      visit "/users/#{user.id}"
-      click_button "Discover Movies"
+    visit "/users/#{user.id}"
+    click_button "Discover Movies"
 
-      expect(current_path).to eq("/users/#{user.id}/discover")
-      expect(page).to have_content("Viewing Party Lite")
-      expect(page).to have_content("Discover Movies")
-    end
+    expect(current_path).to eq("/users/#{user.id}/discover")
+    expect(page).to have_content("Viewing Party Lite")
+    expect(page).to have_content("Discover Movies")
+  end
+
+  xit 'has a button that returns top rated movies' do
+    user = User.create!(name: "Rand", email: "randalthor@gmail.com")
+
+    visit "/users/#{user.id}/discover"
+
+    click_button "Find Top Rated Movies"
+  end
+
+  xit 'has a search field based on keyword' do
+    user = User.create!(name: "Rand", email: "randalthor@gmail.com")
+
+    visit "/users/#{user.id}/discover"
+
+    fill_in :search, with: "Fight"
+    click_button "Find Movies"
+
+    expect(page).to have_content("Fight Club")
   end
 end
