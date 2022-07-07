65ad892f104c569001234a6a446a90bea2303fc7
Tyler Ross <tross020890@gmail.com>
2022-07-06 20:03:43 -0500

    Feat: start landing page

---
+++ spec/features/landing_page_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Home page", type: :feature do
+  it "Has title of application" do
+    visit '/'
+
+    expect(page).to contain("Viewing Party")
+  end
+
+  it "Has button to create new user" do
+    visit '/'
+
+    expect(page).to have_button("Create user")
+  end
+
+  it "Has a list of existing users" do
+    user1 = User.create!(name: "Jimbo", email: "Jimbo@gmail.com")
+    visit '/'
+
+    expect(page).to have_link("Jimbo")
+  end
+
+  it "Has link to landing page" do
+    visit '/'
+
+    expect(page).to have_link("Home")
+  end
+
+
+end
