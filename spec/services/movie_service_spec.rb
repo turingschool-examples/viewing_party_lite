4948bad23ab0928ae5569f08b73fee6e813f7140
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-07 06:40:06 -0600

    Feat: create movie service
    Co-authored-by: Jenn Halloran <jenniferlhalloran@gmail.com>

---
+++ spec/services/movie_service_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe MovieService do
+  describe 'class methods' do
+    describe '#top_rated_movies' do
+      it "returns data for the top 20 movies", :vcr do
+        movies = MovieService.top_rated_movies('/3/movie/top_rated?api_key=02f6db89ebe8897a06342829374c182c&language=en-US&page=1')
+        
+        expect(movies[:results]).to be_an(Array)
+        expect(movies[:results].length).to eq(20)
+        first_movie = movies[:results].first
+
+        expect(first_movie[:title]).to be_a(String)
+        expect(first_movie[:vote_average]).to be_a(Float)
+      end
+    end
+  end
+end
