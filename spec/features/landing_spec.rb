692e8dd958ab2eea11ae54dfe6c9136146dbfdba
Deannah Burke <deannah@bierstadtlager.com>
2022-07-06 21:41:26 -0600

    Add second feature test, button to landing page

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe 'the landing page', type: :feature do
 
     expect(page).to have_content("Viewing Party Lite")
   end
+
+  it 'has button to create new user' do
+    visit '/'
+
+    expect(page).to have_button("Create New User")
+    #will add to this test to have button link to user registration page once this is pushed, then I can pull down user registration code :) -DB
+  end
 end
