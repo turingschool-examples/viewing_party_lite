644cfa091ea3f0f9f552e483f56a35bcb9df336c
Zachary P <zachary.s.prince@outlook.com>
2022-07-07 19:16:55 -0400

    Reorganize routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
+  get 'register' => 'users#new'
+
   resources :users, only: %i[show create] do
     resources :discover, controller: :movies
   end
-  get 'register' => 'users#new'
 end
