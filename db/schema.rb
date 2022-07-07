a77d176bf3f55ebf8773f240772e9b93969cdb5b
Stirling Hostetter <stirhoss@icloud.com>
2022-07-07 15:36:15 -0500

    Update database to fix broken model relationships

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema.define(version: 2022_07_05_224438) do
   enable_extension "plpgsql"
 
   create_table "user_viewing_parties", force: :cascade do |t|
-    t.bigint "users_id"
-    t.bigint "viewing_parties_id"
+    t.bigint "user_id"
+    t.bigint "viewing_party_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_user_viewing_parties_on_users_id"
-    t.index ["viewing_parties_id"], name: "index_user_viewing_parties_on_viewing_parties_id"
+    t.index ["user_id"], name: "index_user_viewing_parties_on_user_id"
+    t.index ["viewing_party_id"], name: "index_user_viewing_parties_on_viewing_party_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,6 +41,6 @@ ActiveRecord::Schema.define(version: 2022_07_05_224438) do
     t.integer "host_id"
   end
 
-  add_foreign_key "user_viewing_parties", "users", column: "users_id"
-  add_foreign_key "user_viewing_parties", "viewing_parties", column: "viewing_parties_id"
+  add_foreign_key "user_viewing_parties", "users"
+  add_foreign_key "user_viewing_parties", "viewing_parties"
 end
