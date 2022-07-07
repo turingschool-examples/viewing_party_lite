ddedfa716048a7297ed5e7bed83f5dfeb2ca9051
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-07 07:49:34 -0600

    Test: movie details page create viewing party button

---
+++ spec/features/users/movies/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'movie details page' do
+
+  it "has a button to create a new viewing party" do
+    user1 = User.create!(name: 'Jane Powell', email: 'jpowell38@gmail.com')
+    attributes = {
+      title: 'Titanic',
+      id: 1,
+      vote_average: 9.1,
+      runtime: 194
+    }
+    movie = Movie.new(attributes)
+    visit "/users/#{user1.id}/movies/#{movie.id}"
+
+    expect(page).to have_button("Create a Viewing Party")
+    click_button("Create a Viewing Party")
+    expect(current_path).to eq("/users/#{user1.id}/movies/#{movie.id}/viewing_party/new")
+  end 
+end
