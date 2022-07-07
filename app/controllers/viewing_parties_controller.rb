5bc7f5f369b16d9effe19c2af42daaeaa47bc825
Stirling Hostetter <stirhoss@icloud.com>
2022-07-07 17:26:54 -0500

    Comment out things we don't need

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,13 +1,18 @@
 class ViewingPartiesController < ApplicationController
 
-  def index
-    conn = Faraday.new(url: "https://api.moviedb.org/3”) do |faraday|
-      faraday.headers["X-API-KEY"] = ENV['api_key']
-    end
+  # def index
+  #   conn = Faraday.new(url: 'https://api.moviedb.org/3') do |faraday|
+  #     faraday.headers['X-API-KEY'] = ENV['api_key']
+  #   end
 
-    response = conn.get('/movie/top_rated')
+  #   response = conn.get('/movie/top_rated')
 
-    data = JSON.parse(response.body, symbolize_names: true)
+  #   data = JSON.parse(response.body, symbolize_names: true)
 
+  #   data.each do |movie|
+  #     Movie.new(movie)
+  #   end
+
+    @top_movies = MoviesService.top_movies
   end
 end
