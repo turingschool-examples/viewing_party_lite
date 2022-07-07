354e84b9273e4fb2e35fb1aa7239473a72b9f288
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-07 16:02:17 -0600

    Refa: MovieFacade query search, add vcr fixtures

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,11 +1,18 @@
 class MovieFacade
 
-  def self.top_rated
-    top_movies = []
-    data = MovieService.get_top_rated_movies
-    data.each do |movie|
-      top_movies << Movie.new(movie)
+  # def self.top_rated
+  #   top_movies = []
+  #   data = MovieService.get_top_rated_movies
+  #   data.each do |movie|
+  #     top_movies << Movie.new(movie)
+  #   end
+  #   top_movies
+  # end
+
+  def self.all_movies(query)
+    data = MovieService.movie_query_results(query)
+    data[:results].map do |movie_result|
+      Movie.new(movie_result)
     end
-    top_movies
   end
 end
