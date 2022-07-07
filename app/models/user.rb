024f84fc0786ff77c2fe0ee6159b9d94cd47ec79
Drew Proebstel <d.proebstel@gmail.com>
2022-07-06 19:06:43 -0700

    add many to many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :user_parties
+  has_many :parties, :through => :user_parties
   validates_presence_of :name, :email
   validates_uniqueness_of :email
 end
