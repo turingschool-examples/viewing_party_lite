fbcb384c64d79a418a71a6a8d76d7a193944168a
Zachary P <zachary.s.prince@outlook.com>
2022-07-06 21:19:26 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/register', to: 'users#new'
-  post '/users', to: 'users#create'
+  # get '/register', to: 'users#new'
+  # post '/users', to: 'users#create'
 
-  resources :users
+  resources :users, only: :show
   get 'register' => 'users#new'
 end
