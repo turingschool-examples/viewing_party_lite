2f0e60c7050ff3626353f9b92419b2da7148e3aa
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-07 13:20:05 -0600

    Beginning of rubocop fixes - viewing party spec to index spec done

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,43 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe 'Welcome Index' do 
+RSpec.describe 'Welcome Index' do
   describe 'landing page' do
-    it 'has the title of the application' do 
+    it 'has the title of the application' do
       visit root_path
 
       expect(page).to have_content('Viewing Party Lite')
     end 
 
-    it 'has a button to create a new user' do 
+    it 'has a button to create a new user' do
       visit root_path
       expect(page).to have_button('Create a New User')
 
-      click_button "Create a New User"
-      expect(current_path).to eq("/register")
+      click_button 'Create a New User'
+      expect(current_path).to eq('/register')
     end
 
-    it 'has a list of existing users which links to the users dashboard' do 
+    it 'has a link to go back to the landing page' do
+      visit root_path
+
+      click_link 'Home'
+      expect(current_path).to eq(root_path)
+    end
+  end 
+
+  describe 'user lists' do 
+    it 'has a list of existing users which links to the users dashboard' do
       user1 = User.create!(name: 'Deannah', email: 'rockyhorrorfan@gmail.com')
       user2 = User.create!(name: 'Sai', email: 'movieluvr55@hotmail.com')
 
       visit root_path
 
-      expect(page).to have_link "#{user1.name}"
-      expect(page).to have_link "#{user2.name}"
-      expect(page).to have_content "#{user2.email}"
+      expect(page).to have_link 'Deannah'
+      expect(page).to have_link 'Sai'
+      expect(page).to have_content 'movieluvr55@hotmail.com'
 
       within "#user-#{user1.id}" do
-        expect(page).to have_content(user1.email)
-        click_link "#{user1.name}"
+        expect(page).to have_content('rockyhorrorfan@gmail.com')
+        click_link 'Deannah'
       end
       expect(current_path).to eq(user_path(user1.id))
-    end
-
-    it 'has a link to go back to the landing page' do 
-      visit root_path
-
-      click_link "Home"
-      expect(current_path).to eq(root_path)
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do 
-  describe 'relationships' do 
+RSpec.describe User, type: :model do
+  describe 'relationships' do
     it { should have_many :user_viewing_parties }
     it { should have_many(:viewing_parties).through(:user_viewing_parties) }
   end
 
-  describe 'validations' do 
+  describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
