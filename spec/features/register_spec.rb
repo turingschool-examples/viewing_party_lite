0b4f8e5f2c734fb60fb351401c4d3b2fca4bcbd5
Sarah Hall <swatson1994@yahoo.com>
2022-07-06 21:43:11 -0400

    Test: Add test for user creation w/ form

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
+#save_and_open_page
 
 RSpec.describe "User Registration Page", type: :feature do
-  # before :each do
-  #   visit "/register"
-  # end
+  before :each do
+    user1 = User.create!(name: 'Sai', email: 'SaiLent@overlord.com')
+  end
 
   it 'has a form' do
     visit "/register"
     expect(page).to have_field(:name)
-    expect(page).to have_field(:name)
+    expect(page).to have_field(:email)
     expect(page).to have_button("Register")
+  end
+  it 'can register a new user' do
+    visit "/register"
+    last_user = User.all.last
+
+    fill_in :name, with: 'Casey'
+    fill_in :email, with: 'EternalPancakes@Geemail.com'
+    click_button("Register")
+
+    expect(page).to have_current_path("/users/#{last_user.id + 1}")
   end
 end
