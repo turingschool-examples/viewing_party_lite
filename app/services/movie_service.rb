2603a0d6f1e86d6afbc71bd8e1463ec27dd6ddea
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-07 13:23:44 -0600

    Ref: add api key to params in connection
    Co-authored-by: <jenniferlhalloran@gmail.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -5,9 +5,14 @@ class MovieService
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.find_movie(id)
+    response = connection.get("/3/movie/#{id}")
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.connection
     Faraday.new(url: 'https://api.themoviedb.org') do |f|
-      f.headers['api_key'] = ENV['tmdb_api_key']
+      f.params['api_key'] = ENV['tmdb_api_key']
     end
   end
 end
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -3,15 +3,27 @@ require 'rails_helper'
 RSpec.describe MovieService do
   describe 'class methods' do
     describe '#top_rated_movies' do
-      it "returns data for the top 20 movies", :vcr do
-        movies = MovieService.top_rated_movies('/3/movie/top_rated?api_key=02f6db89ebe8897a06342829374c182c&language=en-US&page=1')
-        
+      it 'returns data for the top 20 movies' do
+        movies = MovieService.top_rated_movies('/3/movie/top_rated?language=en-US&page=1')
+        # binding.pry
         expect(movies[:results]).to be_an(Array)
         expect(movies[:results].length).to eq(20)
         first_movie = movies[:results].first
-
+        
         expect(first_movie[:title]).to be_a(String)
         expect(first_movie[:vote_average]).to be_a(Float)
+      end
+    end
+    
+    describe '#find_movie' do
+      it 'returns details for a movie with the given id', :vcr do
+        movie = MovieService.find_movie(120)
+
+        expect(movie[:title]).to be_a(String)
+        expect(movie[:vote_average]).to be_a(Float)
+        expect(movie[:runtime]).to be_a(Integer)
+        expect(movie[:genres]).to be_a(Array)
+        expect(movie[:overview]).to be_a(String)
       end
     end
   end
