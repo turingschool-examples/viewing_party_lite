5d413fbd2aa0eda80cb9a6f847b568980a3c9cb6
Zachary P <zachary.s.prince@outlook.com>
2022-07-07 17:46:21 -0400

    Add welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/register', to: 'users#new'
-  # post '/users', to: 'users#create'
+  get '/', to: 'welcome#index'
 
   resources :users, only: %i[show create]
   get 'register' => 'users#new'
