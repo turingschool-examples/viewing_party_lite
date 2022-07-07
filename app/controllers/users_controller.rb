6ec014dd0a9685f4419f9e07180e4a1979417a3e
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-07 16:52:51 -0400

    Create the discover page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   end
 
   def discover 
+    @user = User.find(params[:id])
   end
 
   private
---
+++ spec/features/user_movies/discover_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'user discover page' do
+    before do 
+        @user1 = User.create!(name: 'Jane', email: 'eleven@upsidedown.com')
+        visit "users/#{@user1.id}/discover"
+    end 
+
+    it 'has a button to discover top rated movies' do
+        click_button("Find Top Rated Movies")
+        
+        expect(current_path).to eq("/users/#{@user1.id}/movies")
+        #will want to add test to check content of page once API is consumed
+    end
+end
