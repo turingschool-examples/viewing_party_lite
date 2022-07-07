e92202370e7e60ba16bcdc1f0e41d447ab5caa40
Stirling Hostetter <stirhoss@icloud.com>
2022-07-07 10:19:41 -0500

    Test: for having a title on the landing page

--- spec/features/landing_page/index_spec.rb
+++ spec/features/landing_page/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe LandingPage: :Index do
   it 'has a landing page' do
-    visit '/'
+     isit '/'
 
     expect(page).to have_content('Viewing Party!!(lite)')
   end
