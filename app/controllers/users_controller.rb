77ce7f01080930cbc8146703f9393ed56081eac0
Stephen Wilkens <stephenwilkens@gmail.com>
2022-07-06 20:40:14 -0600

    add routes, adjust controller name

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     render :new
+  #   else
+  #     flash[:alert] = "Error: invalid input, please try again"
+  #   end
+  # end
+  def show
+    @user = User.find(params[:id])
+  end
+
+def create
+    # user = user_params
+    # user[:email] = user[:email].downcase
+    new_user = User.new(user_params)
+    if new_user.save
+      flash[:success] = 'Account Successfully Created!'
+      session[:user_id] = new_user.id
+      redirect_to user_path(new_user.id)
+    else
+      flash[:error] = 'Email address is blank/already in use.'
+      redirect_to register_path
+    end
+  end
+
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.permit(:user_name, :email)
+  end
+end
