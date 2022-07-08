76c5d8e230eeccb0af993971b3a0ab1e9d337d0c
Stirling Hostetter <stirhoss@icloud.com>
2022-07-08 18:45:11 -0500

    Altered the method for validating email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates_presence_of :email, uniquness: true
+  validates_presence_of :email
   validates_presence_of :name
+  validates_uniqueness_of :email
 
   has_many :user_viewing_parties
   has_many :viewing_parties, through: :user_viewing_parties
