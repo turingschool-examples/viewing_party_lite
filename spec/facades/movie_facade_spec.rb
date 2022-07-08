36108c569bbf7feb8447fda932eebab37c8eb9a2
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-08 11:36:50 -0500

    Test: search by keyword test

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe 'MovieFacade' do
     expect(movies).to be_a Array
     expect(movies).to be_all Movie
   end
+
+  it "returns movies based on query", :vcr do
+    movies = MovieFacade.movie_by_keyword("Django")
+
+    expect(movies).to be_a Array
+    expect(movies).to be_all Movie
+
+    expect(movies.first.title).to eq("Django Unchained")
+    expect(movies.length).to eq(40)
+  end
 end
