61d92e29870da2bcc16dfbfe33091ab4eff26744
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-07 18:00:24 -0600

    Fix within block

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -9,22 +9,25 @@ RSpec.describe 'Discover Page', type: :feature do
       visit "/users/#{user1.id}/discover"
       expect(page).to have_content("#{user1.name}'s Discover Page")
     end
+  end 
 
-
+  describe 'top rated movies button' do 
     it 'has button to discover top rated movies' do
       user1 = User.create!(name: 'Parker', email: 'mangaforever@hootube.net')
 
       visit "/users/#{user1.id}/discover"
       expect(page).to have_button('Discover Top Rated Movies')
     end
+  end 
 
+  describe 'search field' do 
     it 'has a text field to enter keyword(s) to search by movie title' do
       user1 = User.create!(name: 'Parker', email: 'mangaforever@hootube.net')
 
       visit "/users/#{user1.id}/discover"
-        within '.movie_search'
-      expect(page).to have_field('search', placeholder: 'Search by movie title')
-
+      within '.movie_search' do 
+        expect(page).to have_field('search', placeholder: 'Search by movie title')
+      end 
     end
 
     it 'has a Button to Search by Movie Title' do
