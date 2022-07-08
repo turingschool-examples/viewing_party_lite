48eb78ef97b2a91b8452659776370fd2e0c46c30
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-08 17:41:17 -0500

    Feat: added more attributes

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 class Movie
-  attr_reader :title, :vote_average, :duration, :id
+  attr_reader :title, :vote_average, :runtime, :id, :overview
 
   def initialize(movie_data)
+    @data = movie_data
     @title = movie_data[:title]
     @vote_average = movie_data[:vote_average]
-    @duration = movie_data[:duration]
+    @runtime = movie_data[:runtime]
     @id = movie_data[:id]
+    @overview = movie_data[:overview]
+  end
+
+  def get_genre
+    @data[:genres].map do |genre|
+      genre[:name]
+    end
   end
 end
