a5aeb34295464adc15a1d8017b431c2b6490051c
Stirling Hostetter <stirhoss@icloud.com>
2022-07-08 10:44:47 -0500

    Add more comprehensive tests

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe MovieService do
-  it 'gets json and parses into hash', :vcr do
+  describe 'top_movies endpoint' do
+    it 'gets top_movies JSON and parses into hash', :vcr do
+      response = MovieService.top_rated
 
-    expect(MovieService.top_movies).to be_a Hash
+
+      expect(response).to be_a Hash
+      expect(response[:results]).to be_a Array
+      expect(response[:results].count).to eq 20
+
+      first_movie = response[:results][0]
+
+      expect(first_movie).to have_key :title
+      expect(first_movie).to have_key :overview
+      expect(first_movie).to have_key :vote_average
+    end
   end
 end
