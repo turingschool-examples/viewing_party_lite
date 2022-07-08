42ab7c58bc13d5ca71af0dd8ac5a7584a5d1275d
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-07-08 10:40:21 -0600

    feat: add discover action to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     redirect_to "/users/#{@user.id}"
   end
 
+  def discover
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
