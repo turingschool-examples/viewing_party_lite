6baa47e5b3de73b627b58649773b85a298a2d497
Luke Pascale <lpluke5@gmail.com>
2022-07-07 22:24:51 -0500

    Add VCR config to rails_helper

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "MovieFacade" do
   describe "MovieFacade#top_40" do
-    it "makes a service call and returns an array of movie POROs" do
+    it "makes a service call and returns an array of movie POROs", :vcr do
       movies = MovieFacade.top_40
 
       expect(movies).to be_a Array
