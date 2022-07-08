5791524b7da2637206dd83bb5a3afd3209f4ecb4
Drew Proebstel <d.proebstel@gmail.com>
2022-07-07 19:58:26 -0700

    test for movie title linking to movie show page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -39,6 +39,7 @@ RSpec.describe "Dashboard Page" do
       expect(page).to_not have_content("Jurassic Park")
       expect(page).to_not have_content("Neil")
     end
+
     within ".hosting" do
       expect(page).to have_content("Geddy")
       expect(page).to have_content("Neil")
@@ -48,5 +49,21 @@ RSpec.describe "Dashboard Page" do
       expect(page).to_not have_content("Jack Ass 3.5")
       expect(page).to_not have_content("Alex")
     end
+  end
+  it 'shows all viewing parties the user is invited or hosting with details' do
+    user1 = User.create(name: 'Geddy', email: '2112@yyz.com')
+    user2 = User.create(name: 'Alex', email: 'cygnus@xanadu.com')
+    user3 = User.create(name: 'Neil', email: 'bytor@snowdog.com')
+    party1 = Party.create(date: '2022-05-10', duration: 147,  movie: 'Jurassic Park', host: user2.id, movie_image: 'movie picture', host_name:user2.name, start_time: '7:00', movie_id: 123)
+    party2 = Party.create(date: '2022-05-14', duration: 147,  movie: 'Jack Ass 3.5', host: user1.id, movie_image: 'movie picture', host_name:user1.name, start_time: '1:00', movie_id: 124)
+    PartyUser.create(user_id: user1.id, party_id: party1.id )
+    PartyUser.create(user_id: user2.id, party_id: party1.id )
+    PartyUser.create(user_id: user3.id, party_id: party1.id )
+    PartyUser.create(user_id: user1.id, party_id: party2.id )
+    PartyUser.create(user_id: user2.id, party_id: party2.id )
+
+    visit "/users/#{user2.id}"
+    click_link("Jack Ass 3.5")
+    save_and_open_page
   end
 end
