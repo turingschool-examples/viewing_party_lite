5e40e7a95cd2b791bfa643977dbd1dcef0621a0e
Stirling Hostetter <stirhoss@icloud.com>
2022-07-08 10:44:23 -0500

    Switched to private methods with call(api)

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,13 +1,24 @@
 class MovieService
-
-  def self.top_movies
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-      faraday.headers['api_key'] = ENV['api_key']
+  class << self
+    def top_rated
+      call_api("/3/movie/top_rated?api_key=#{ENV['api_key']}")
     end
-    response = conn.get("/3/movie/top_rated?api_key=#{ENV['api_key']}")
 
-    data = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+  private
+
+    def call_api(path)
+      response = conn.get(path)
+      parsed_data(response)
+    end
+
+    def conn
+      conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+        faraday.headers['api_key'] = ENV['api_key']
+      end
+    end
+
+    def parsed_data(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
-
 end
