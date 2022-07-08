6b7d991865e057b4570e7c142493618e47104cf5
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-08 16:32:41 -0600

    edit top rate movie test

--- spec/features/discover_spec.rb
+++ spec/features/discover_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Discover do
   it 'has a button to display top rated movies' do
     user = User.create!(name: 'User', email: 'user@email.com')
 
-    visit user."#{user.id}".discover
+    visit user/"#{user.id}"/discover
 
     expect(page).to have_button("Discover Top Rated Movies")
   end
