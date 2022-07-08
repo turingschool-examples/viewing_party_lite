4273cc36834f10e071c01890fdb33b58a8e15ee1
Clay Ash <claycourtneyash@gmail.com>
2022-07-07 19:33:58 -0600

    Adjust movie controller index for views, add search result details to movie index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,9 +4,9 @@ class MoviesController < ApplicationController
     @user = User.find(params[:user_id])
     # @movies = MovieFacade.all 
     if params[:keyword].present?
-      MovieFacade.movie_search(params[:keyword])
+     @movies = MovieFacade.movie_search(params[:keyword])
     else 
-      MovieFacade.top_movies 
+     @movies = MovieFacade.top_movies 
     end 
   end
 end
