44665ca266888b2ef52fbbf09ce9327ab7ed38de
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-08 11:33:54 -0500

    Feat: added id attribute

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Movie
-  attr_reader :title, :vote_average, :duration
+  attr_reader :title, :vote_average, :duration, :id
 
   def initialize(movie_data)
     @title = movie_data[:title]
     @vote_average = movie_data[:vote_average]
     @duration = movie_data[:duration]
+    @id = movie_data[:id]
   end
 end
