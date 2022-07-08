16e50e7ea71e4d5f5581caccfc56b30f9fb76a75
Stirling Hostetter <stirhoss@icloud.com>
2022-07-08 18:46:33 -0500

    Tests: Landing page basic tests

--- spec/features/landing_page/index_spec.rb
+++ spec/features/landing_page/index_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe LandingPage: :Index do
-  it 'has a landing page' do
-     isit '/'
+  it 'has a landing page with a title' do
+    visit root_path
 
     expect(page).to have_content('Viewing Party!!(lite)')
+  end
+
+  it 'contains a link to register for the service' do
+    visit root_path
+
+    expect(page).to have_button('Create an Account')
+
+    click_on('Create an Account')
+
+    expect(current_path).to eq('/register')
   end
 end
