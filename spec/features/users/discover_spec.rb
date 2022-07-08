ecab28c08bb029ac762a5a75e55627cfcdb045d3
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-08 11:37:38 -0500

    Refactor: moved test into movie spec

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -13,42 +13,10 @@ RSpec.describe 'User Discover Page' do
     expect(current_path).to eq("/users/#{@user1.id}/discover")
   end
 
-  it 'tells details the user discover page' do
+  it 'has a button to see top 20 movies' do
     @user1 = User.create!(name: 'Jimar', email: 'jimar@jimar.com')
 
     visit "/users/#{@user1.id}/discover"
     expect(page).to have_button('Top Rated Movies')
-  end
-
-  it 'tells details the user discover page', :vcr do
-    @user1 = User.create!(name: 'Jimar', email: 'jimar@jimar.com')
-
-    visit "/users/#{@user1.id}/discover"
-
-    click_button 'Top Rated Movies'
-    expect(current_path).to eq("/users/#{@user1.id}/movies")
-
-    expect(page).to have_content('Top 20 Movies:')
-    expect(page).to have_content('Title: The Shawshank Redemption')
-    expect(page).to have_content('Average Vote: 8.7')
-
-    expect(page).to_not have_content('Title: Ariel')
-    expect(page).to_not have_content('Average Vote: 6.8')
-  end
-
-  it 'tells details a search param', :vcr do
-    @user1 = User.create!(name: 'Jimar', email: 'jimar@jimar.com')
-
-    visit "/users/#{@user1.id}/discover"
-
-    # click_button "Top Rated Movies"
-    # expect(current_path).to eq("/users/#{@user1.id}/movies")
-    #
-    # expect(page).to have_content("Top 20 Movies:")
-    # expect(page).to have_content("Title: The Shawshank Redemption")
-    # expect(page).to have_content("Average Vote: 8.7")
-    #
-    # expect(page).to_not have_content("Title: Ariel")
-    # expect(page).to_not have_content("Average Vote: 6.8")
   end
 end
