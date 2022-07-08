b645a5b58d4d5eccee6178911a221780d9a8e88f
Deannah Burke <deannah@bierstadtlager.com>
2022-07-08 15:44:05 -0600

    Add first feature test and view for discover movies

---
+++ spec/features/user/discover_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "User discover page", type: :feature do
+  before :each do
+    @user1 = User.create!(name: 'Sai', email: 'SaiLent@overlord.com')
+    @user2 = User.create!(name: 'Parker', email: 'GriffithDidNothing@Wrong.com')
+  end
+
+  it 'has a button to discover top rated movies' do
+    visit "/users/#{@user1.id}/discover"
+
+    expect(page).to have_button("Discover Top Rated Movies")
+  end
+
+  it 'has a search box and button to search by movie title' do
+    visit "/users/#{@user1.id}/discover"
+
+    expect(page).to have_field(:search)
+    expect(page).to have_button("Search by Movie Title")
+  end
+end
