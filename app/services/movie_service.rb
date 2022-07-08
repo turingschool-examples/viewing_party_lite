4787edbbd9d2c6c520c6138cf96178701fa3a1bd
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-08 17:42:17 -0500

    Feat: movie details connection added

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -12,14 +12,16 @@ class MovieService
   response1 = connection.get("/3/search/movie?query=#{query}&api_key=#{ENV['api_key']}")
   response2 = connection.get("/3/search/movie?query=#{query}&page=2&api_key=#{ENV['api_key']}")
 
-
   data1 = JSON.parse(response1.body, symbolize_names: true)
   data2 = JSON.parse(response2.body, symbolize_names: true)
 
-
   data1[:results] + data2[:results]
-
 end
+
+  def self.movie_details(id)
+    response = connection.get("/3/movie/#{id}?api_key=#{ENV['api_key']}")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
 
   def self.connection
     Faraday.new(url: 'https://api.themoviedb.org')
