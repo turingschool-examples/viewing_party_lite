b81d2f6392ba46aa260191feaf082927ee80da3a
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-08 17:02:47 -0600

    test discover page top rated movie button

--- spec/features/discover_spec.rb
+++ spec/features/discover_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Discover do
+RSpec.describe 'Discover' do
   it 'has a button to display top rated movies' do
     user = User.create!(name: 'User', email: 'user@email.com')
 
-    visit user/"#{user.id}"/discover
+    visit "user/#{user.id}/discover"
 
     expect(page).to have_button("Discover Top Rated Movies")
   end
 
-  end
 end
