894c2cab19554fe73b54e9879f1d784e1c468af8
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-08 17:53:33 -0400

    Add image path attribute to movie poro
    Co-authored-by: Thomas Haines <tjhaines@eckerd.edu>

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -4,18 +4,20 @@ require 'rails_helper'
 
 RSpec.describe Movie do
   it 'exists' do
-    attributes = {
-      title: 'Titanic',
-      id: 1,
-      vote_average: 9.1,
-      runtime: 194
-    }
+    
+    attributes = JSON.parse(File.read("./spec/fixtures/movie_details.json"), symbolize_names: true)
     movie = Movie.new(attributes)
 
     expect(movie).to be_a Movie
-    expect(movie.title).to eq('Titanic')
-    expect(movie.id).to eq(1)
-    expect(movie.vote_average).to eq(9.1)
-    expect(movie.runtime).to eq('3hr 14 min')
+    expect(movie.title).to eq("The Lord of the Rings: The Fellowship of the Ring")
+    expect(movie.id).to eq(120)
+    expect(movie.vote_average).to eq(8.4)
+    expect(movie.runtime).to eq('2hr 59 min')
+    expect(movie.runtime_mins).to eq(179)
+    expect(movie.genres).to be_a(Array)
+    expect(movie.reviews).to be_a(Array)
+    expect(movie.overview).to be_a(String)
+    expect(movie.cast).to be_a(Array)
+    expect(movie.image_uri).to eq("/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg")
   end
 end
