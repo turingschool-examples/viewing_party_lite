7200cf18db0bc328c37faac5a69362f2f8ea0967
Stephen Wilkens <stephenwilkens@gmail.com>
2022-07-07 21:26:13 -0600

    add services test

---
+++ spec/services/movie_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'MovieService' do
+  context 'happy path' do
+    it 'retrieves data and parses response', :vcr do
+
+      parsed_json = MovieService.top_40_rated_movies
+
+      expect(parsed_json).to be_a Array
+      movie = parsed_json.first
+
+      expect(movie).to include :title, :vote_average
+      expect(movie[:title]).to be_a(String)
+      expect(movie[:vote_average]).to be_a(Float)
+    end
+  end
+end
