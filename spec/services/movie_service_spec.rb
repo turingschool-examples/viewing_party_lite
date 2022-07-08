ef671ab59071411a37d6e9ecb57db32e358a0230
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-08 17:45:30 -0500

    Test: movie details connection

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -4,17 +4,13 @@ require 'rails_helper'
 
 RSpec.describe 'MovieService' do
   context 'happy path' do
-    # input - US state
-    # output - parsed JSON is the output
     it 'returns top 20 movies', :vcr do
       parsed_json = MovieService.top_20_movies
 
       expect(parsed_json).to be_a Array
-      # expect(parsed_json[:results]).to be_a Array
+
       movie = parsed_json.first
-      # binding.pry
-      #
-      # expect(movie).to have_key(:name)
+
       expect(movie).to include :title, :vote_average
       expect(movie[:title]).to be_a(String)
       expect(movie[:vote_average]).to be_a(Float)
@@ -22,7 +18,6 @@ RSpec.describe 'MovieService' do
 
     it "searches for movies", :vcr do
       parsed_json = MovieService.movie_by_keyword("Django")
-
       expect(parsed_json).to be_a Array
       movie = parsed_json.first
 
@@ -30,9 +25,14 @@ RSpec.describe 'MovieService' do
       expect(movie[:title]).to be_a(String)
       expect(movie[:vote_average]).to be_a(Float)
       expect(parsed_json.length).to eq(40)
-
-
     end
 
+    it "gets details for specific movie", :vcr do
+      fight_club = MovieService.movie_details(550)
+
+      expect(fight_club).to include :title, :vote_average, :runtime, :genres, :overview
+      expect(fight_club[:title]).to be_a(String)
+      expect(fight_club[:vote_average]).to be_a(Float)
+    end
   end
 end
