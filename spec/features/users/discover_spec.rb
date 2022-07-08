1fce304e9aabb02927f426a1d4cdda548c2da503
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-07-08 10:37:24 -0600

    test: discover page has a button to discover top rated movies and can search by movie title

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'discover page' do
   before do
-    visit "/users/#{user1.id}/discover"
-
     @user1 = User.create!(name: "Pabu", email: "pabu@pabu.com")
     @user2 = User.create!(name: Faker::JapaneseMedia::OnePiece.character, email: "#{Faker::JapaneseMedia::OnePiece.island}@onepiece.com")
     @user3 = User.create!(name: Faker::JapaneseMedia::SwordArtOnline.real_name, email: "#{Faker::JapaneseMedia::SwordArtOnline.game_name}@swordart.com")
+
+    visit "/users/#{@user1.id}/discover"
   end
 
-  it 'has a button to Discover Top Rated Movies' do
+  it 'has a button to Discover Top Rated Movies', :vcr do
     click_button "Discover Top Rated Movies"
   end
 
-  it 'has a text field to enter keyword(s) to search by movie title' do
-    expect(page).to have_field("MovieTitle")
+  it 'has a text field to enter keyword(s) to search by movie title', :vcr do
+    save_and_open_page
+    expect(page).to have_field("movie_title")
     expect(page).to have_button("Search By Movie Title")
   end
 end
