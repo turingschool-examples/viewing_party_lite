a68c205d7ab037c389290e35ddbbdf9336834e89
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-07 20:29:02 -0400

    Complete user_movies index view page

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -2,10 +2,10 @@
 
 class UserMoviesController < ApplicationController
   def index
-    # if params[:q] == "top rated"
-    #   @movies = MovieFacade.top_rated_movies ?
-    # elsif params[:search]
-    # blah blah
-    # end
+    if params[:q] == 'top rated'
+      @top_movies = MovieFacade.top_rated_movies
+    elsif params[:search]
+      @movie_search_results = MovieFacade.search(params[:search])
+    end
   end
 end
--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -8,18 +8,22 @@ describe 'user discover page' do
     visit "users/#{@user1.id}/discover"
   end
 
-  it 'has a button to discover top rated movies' do
+  it 'has a button to discover top rated movies', :vcr do
     click_button('Find Top Rated Movies')
 
     expect(current_path).to eq("/users/#{@user1.id}/movies")
-    # will want to add test to check content of page once API is consumed
+    expect(page).to have_content('Top Rated Movies')
+    expect(page).to have_content('The Shawshank Redemption')
+
+    expect(page).to_not have_content('Movie results for:')
   end
 
-  it 'has a text field to enter a keyword and a button to search by movie title ' do
+  it 'has a text field to enter a keyword and a button to search by movie title ', :vcr do
     fill_in 'search', with: 'titanic'
     click_button('Find Movies')
 
     expect(current_path).to eq("/users/#{@user1.id}/movies")
-    # will want to add test to check content of page once API is consumed
+    expect(page).to have_content('Movie results for: titanic')
+    expect(page).to have_content('Titanic 666')
   end
 end
