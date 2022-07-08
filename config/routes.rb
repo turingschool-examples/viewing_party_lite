0e51227e8fb3349171146f2627f74ba975a3cddf
Ian Ross <ianrossdrums@gmail.com>
2022-07-08 12:11:53 -0600

    merge main

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create]
   get '/users/:id/discover', to: 'users#discover'
 
+  get "/", to: "landing#index"
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
-
-  get "/", to: "landing#index"
 end
