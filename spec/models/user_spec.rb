90e231d4766b28197eb61759ee8aa4ed91390eaa
Stirling Hostetter <stirhoss@icloud.com>
2022-07-08 18:46:55 -0500

    Tests: Basic tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe User do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of :email }
   end
 
   describe 'relationships' do
     it { should have_many(:user_viewing_parties) }
-    it { should have_many(:viewing_parties).through(:user_viewing_parties)}
+    it { should have_many(:viewing_parties).through(:user_viewing_parties) }
   end
 end
