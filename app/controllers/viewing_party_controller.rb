464057a372571f58494e927ef13e1c1908de2dd5
Zachary P <zachary.s.prince@outlook.com>
2022-07-09 14:42:02 -0400

    Add reviews instance variable

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,5 +1,6 @@
 class ViewingPartyController < ApplicationController
   def new
     @movie = MoviesFacade.movie(params[:movie_id])
+    @reviews = MoviesFacade.movie_reviews(params[:movie_id])
   end
 end
