0c866bbe53b5759a37dd6694226c5e7c4dda3ef7
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-07-09 14:17:39 -0700

    Add: final touches to viewing party creation

---
+++ db/migrate/20220709203725_add_start_time_to_party.rb
@@ -0,0 +1,5 @@
+class AddStartTimeToParty < ActiveRecord::Migration[5.2]
+  def change
+    add_column :parties, :start_time, :string
+  end
+end
--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -30,4 +30,43 @@ RSpec.describe 'Viewing New Party Form' do
     expect(current_path).to eq(user_path(user_1.id))
     expect(page).to have_content('The Shawshank Redemption')
   end
+
+  it "lists parties on all invited guests" do
+    user_1 = User.create!(name: 'Zac Morris', email: 'zac@gmail.com')
+    user_2 = User.create!(name: 'Dee Torres', email: 'dee@gmail.com')
+    user_3 = User.create!(name: 'Rae Sull', email: 'rae@gmail.com')
+    visit new_user_movie_viewing_party_path(user_1.id, 278)
+
+    expect(page).to have_content('The Shawshank Redemption')
+    
+    fill_in :duration, with: 192
+    
+    fill_in :date, with: "2022/08/01"
+    fill_in :time, with: Time.new(2022, 8, 1, 18, 30).strftime("%T")
+    check "#{user_2.email}"
+    click_button "Create Party"
+    
+    visit user_path(user_2.id)
+    expect(page).to have_content('The Shawshank Redemption')
+  end
+  
+  it "does not create a party with duration is less than movie runtime" do
+    user_1 = User.create!(name: 'Zac Morris', email: 'zac@gmail.com')
+    user_2 = User.create!(name: 'Dee Torres', email: 'dee@gmail.com')
+    user_3 = User.create!(name: 'Rae Sull', email: 'rae@gmail.com')
+    visit new_user_movie_viewing_party_path(user_1.id, 278)
+
+    expect(page).to have_content('The Shawshank Redemption')
+    
+    fill_in :duration, with: 102
+    
+    fill_in :date, with: "2022/08/01"
+    fill_in :time, with: Time.new(2022, 8, 1, 18, 30).strftime("%T")
+    check "#{user_2.email}"
+    click_button "Create Party"
+    
+    expect(current_path).to eq(new_user_movie_viewing_party_path(user_1.id, 278))
+    expect(user_1.parties).to  eq([])
+    expect(page).to have_content("Party duration too short, try again.")
+  end
 end
