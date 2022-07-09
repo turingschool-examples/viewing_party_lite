d50dd3b1ae70a295d2b1b500f6383bf8d80831ad
Zachary P <zachary.s.prince@outlook.com>
2022-07-09 13:37:27 -0400

    Add viewing party new page

---
+++ spec/features/users/movies/viewing_parties/new_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Viewing Party New Page' do
+  it 'displays top 40 movies' do
+    user = User.create!(name: 'User', email: 'user@email.com')
+
+    visit "user/#{user.id}/discover"
+
+    click_button('Discover Top Rated Movies')
+    expect(page).to have_content('The Shawshank Redemption')
+    expect(page).to have_content('Vote Average: 8.7')
+    expect(page).to have_content("Schindler's List")
+    expect(page).to have_content('Vote Average: 8.6')
+  end
+end
