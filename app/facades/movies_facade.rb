8d83e3831ba07bda6d63f32278ec0996192ebbf5
Becky Nisttahuz <benistta@hotmail.com>
2022-07-08 22:29:37 -0600

    Update: movie facades with new methods for reviews and cast

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -3,4 +3,18 @@ class MoviesFacade
    json = MovieService.all_movie_id_search(id)
    Movie.new(json)
  end
+
+  def self.movie_cast(id)
+   json_2 = MovieService.get_movie_cast(id)
+   json_2[:cast].map do |actor|
+     Actor.new(actor)
+   end
+ end
+
+  def self.movie_reviews(id)
+   json_3 = MovieService.get_reviews(id)
+   json_3[:results].map do |author|
+     Review.new(author)
+   end
+ end
 end
