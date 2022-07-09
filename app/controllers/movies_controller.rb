ce37b6f070fe1cc1af86053e68d386140d6775c7
Becky Nisttahuz <benistta@hotmail.com>
2022-07-08 22:28:58 -0600

    Update: movie controller with movie review and cast

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,8 @@ class MoviesController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @movie = MoviesFacade.movie_id_search(params[:id])
+    @movie_cast = MoviesFacade.movie_cast(params[:id]).first(10)
+    @movie_review = MoviesFacade.movie_reviews(params[:id])
     # require "pry"; binding.pry
   end
 end
