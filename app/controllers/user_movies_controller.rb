b4d4a5c1ee3428ed1742fbe3644ee3112dba4606
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-09 00:57:57 -0600

    create search method in user_movie controller

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,6 +1,10 @@
 class UserMoviesController < ApplicationController
   def index
     @top_movies = MoviesFacade.top_rated
- 
+  end
+
+  def search
+    keyword = params[:search]
+    @search_results = MoviesFacade.search(keyword)
   end
 end
