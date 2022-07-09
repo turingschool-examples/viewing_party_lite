b0480aa10bc261dd88aa12bd83b6298927df1b25
Eric Espindola <eric_e_2012@hotmail.com>
2022-07-08 17:47:16 -0700

    created tests to search for movies using API

--- spec/facades/movies_facade_spec.rb
+++ spec/facades/movies_facade_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe MoviesFacade do
     expect(expected.class).to eq Array
     expect(expected).to be_all Movie
   end
+
+  it 'turns the search data in to poros', :vcr do
+    expected = MoviesFacade.search("the shawshank redemption")
+    expect(expected.class).to eq Array
+    expect(expected).to be_all Movie
+  end
 end
