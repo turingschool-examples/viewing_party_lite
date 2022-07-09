0acd70a83ce2842acb1544c5581c256a51f632d0
Sarah Hall <swatson1994@yahoo.com>
2022-07-09 16:59:07 -0400

    Test: Add test for dynamic page titles
    Co-authored-by: Deannah Burke <98491210+deannahburke@use
    rs.noreply.github.com>

--- spec/features/user/movies/index_spec.rb
+++ spec/features/user/movies/index_spec.rb
@@ -31,6 +31,13 @@ RSpec.describe "User Movie Results Page", type: :feature do
 
       expect(page).to have_current_path("/users/#{@user2.id}/movies/238")
     end
+    it 'has the correct page title', :vcr do
+      visit "/users/#{@user2.id}/discover"
+
+      click_button("Discover Top Rated Movies")
+
+      expect(page).to have_content("Top Rated Movies")
+    end
   end
   describe 'search bar' do
     it 'returns matches and partial matches based off entered keyword', :vcr do
@@ -43,6 +50,15 @@ RSpec.describe "User Movie Results Page", type: :feature do
       expect(page).to have_content('Winter Castle')
       expect(page).to have_content('The Glass Castle')
       expect(page).to_not have_content('The Godfather')
+    end
+
+    it 'has the correct page title', :vcr do
+      visit "/users/#{@user2.id}/discover"
+
+      fill_in(:q, with: 'castle')
+      click_button("Search by Movie Title")
+
+      expect(page).to have_content("Movie results for: castle")
     end
   end
 end
