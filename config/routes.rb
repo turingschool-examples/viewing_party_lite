b8cb37a9ca39f35956e7bedcb080b5abe4c246b1
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-09 00:56:28 -0600

    create route for search method - user_movies

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'register' => 'users#new'
   get '/user/:id/discover', to: 'users#discover'
+  post '/search', to: 'user_movies#search'#, controller: :user_movies
+
 
   resources :users, only: %i[show create] do
     resources :movies, only: [:index], controller: :user_movies
