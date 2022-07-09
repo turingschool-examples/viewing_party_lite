afd4f44cc28d0a3e4c11c62f13e74a0d5932abe3
Eric Espindola <eric_e_2012@hotmail.com>
2022-07-08 20:41:06 -0700

    update poro so that each Movie object has an ID attribute and use that ID to get the movie info which includes image path and runtime

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -5,9 +5,19 @@ class Movie
               :vote_average
 
   def initialize(data)
+    @id = data[:id]
     @title = data[:title]
     @genre_ids = data[:genre_ids]
     @popularity = data[:popularity]
     @vote_average = data[:vote_average]
+    @info = MoviesFacade.info(@id)
+  end
+
+  def image
+    @info[:poster_path]
+  end
+
+  def runtime
+    @info[:runtime]
   end
 end
