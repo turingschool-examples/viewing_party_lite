3e413dd0e01f450a6c4b6c2876bf9dd13b3470b1
Bryce Wein <bryce.wein@gmail.com>
2022-07-08 23:45:53 -0400

    Feat: create viewing parties

--- app/models/party_user.rb
+++ app/models/party_user.rb
@@ -1,2 +1,4 @@
 class PartyUser < ApplicationRecord
+  belongs_to :user
+  belongs_to :viewing_party
 end
--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,2 +1,4 @@
 class ViewingParty < ApplicationRecord
+  has_many :party_users
+  has_many :users, through: :party_users
 end
