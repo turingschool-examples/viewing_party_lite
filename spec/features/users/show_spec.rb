2dea22deff9d771cc8dd389bfaa7c768135c7f00
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-09 11:56:54 -0600

    Add test for show file showing movie attributes

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,30 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe 'User Dashboard Page', type: :feature do
-  before(:each) do
-    @user1 = User.create!(name: 'Parker', email: 'mangaforever@hootube.net')
-    @user2 = User.create!(name: 'Lola', email: 'lola@example.com')
-  end
 
   describe 'data is displayed on page'do
-
     it 'shows user name on dashboard' do
-      visit user_path(@user1.id)
-      expect(page).to have_content("#{@user1.name}'s Dashboard")
-      expect(page).to_not have_content("#{@user2.name}'s Dashboard")
+      user1 = User.create!(name: 'Parker', email: 'mangaforever@hootube.net')
+      user2 = User.create!(name: 'Lola', email: 'lola@example.com')
+
+      visit user_path(user1.id)
+      expect(page).to have_content("#{user1.name}'s Dashboard")
+      expect(page).to_not have_content("#{user2.name}'s Dashboard")
     end
 
     it 'has a button to Discover Movies' do
-      visit user_path(@user1.id)
+      user1 = User.create!(name: 'Parker', email: 'mangaforever@hootube.net')
+      user2 = User.create!(name: 'Lola', email: 'lola@example.com')
+
+      visit user_path(user1.id)
       expect(page).to have_button('Discover Movies')
       click_button('Discover Movies')
-      expect(current_path).to eq("/users/#{@user1.id}/discover")
-      expect(page).to have_content("#{@user1.name}'s Discover Page")
+      expect(current_path).to eq("/users/#{user1.id}/discover")
+      expect(page).to have_content("#{user1.name}'s Discover Page")
     end
 
     it "has a section that listing viewing parties" do
-      visit user_path(@user1.id)
+      user1 = User.create!(name: 'Parker', email: 'mangaforever@hootube.net')
+      user2 = User.create!(name: 'Lola', email: 'lola@example.com')
+
+      visit user_path(user1.id)
       expect(page).to have_content("Viewing Parties")
+    end
+  end
+
+  describe 'viewing parties section' do 
+    it 'shows the movie attributes' do 
+      user1 = User.create!(name: 'Parker', email: 'mangaforever@hootube.net')
+      user2 = User.create!(name: 'Lola', email: 'lola@example.com')
+
+      party1 = ViewingParty.create!(movie_id: 129, duration: 96, date: Date.new(2022,9,7), start_time: "16:00:00")
+      party1 = ViewingParty.create!(movie_id: 545611, duration: 175, date: Date.new(2023,4,4), start_time: "19:00:00")
+      party3 = ViewingParty.create!(movie_id: 284053, duration: 145, date: Date.new(2022,10,5), start_time: "17:30:00")
+
+      user_viewing1 = UserViewingParty.create!(user_id: user1.id, viewing_party_id: party1.id, host: false)
+      user_viewing1 = UserViewingParty.create!(user_id: user1.id, viewing_party_id: party2.id, host: false)
+      user_viewing1 = UserViewingParty.create!(user_id: user2.id, viewing_party_id: party3.id, host: false)
+
+      visit user_path(user1.id)
+      within '.invites' do 
+        expect(page).to have_content('Spirited Away')
+        expect(page).to have_content('Everything Everywhere All at Once')
+        expect(page).to_not have_content('Thor Ragnarok')
+        click_link 'Spirited Away'
+      end
+      expect(current_path).to eq(user_movie_path(user1.id, 129))
+
     end
   end
 end
