592fbd5ba61b0dfbf793ebbfe81a7443ab1a234f
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-08 19:03:42 -0600

    Add link to return to discover page

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -8,12 +8,23 @@ RSpec.describe 'Movie Index Page', type: :feature do
       
       visit user_movies_path(user1.id)
       expect(page.all('.movies').count).to eq(20)
-      save_and_open_page
+
       within '.topMovies' do 
         expect(page).to have_content('Spirited Away')
         expect(page).to have_content('Pulp Fiction')
         expect(page).to have_content(8.7)
       end
+    end
+  end
+
+  describe 'link to discovery page' do 
+    it 'links back to the discovery page', :vcr do 
+      user1 = User.create!(name: 'Andrew', email: 'concertenthusiast@musac.org')
+
+      visit user_movies_path(user1.id)
+      
+      click_link 'Discover Page'
+      expect(current_path).to eq(user_discover_path(user1.id))
     end
   end
 
