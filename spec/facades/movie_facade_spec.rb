9ccb5dec79e17a60a19db82356c5c2c0b1ef6df6
Clay Ash <claycourtneyash@gmail.com>
2022-07-09 09:33:29 -0600

    Add test for parties new page, users show page

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe MovieFacade do 
   let!(:top_rated) { MovieFacade.top_movies }
   let!(:search_movies) { MovieService.find_movies("Doctor Strange") }
+  let!(:search_id) { MovieFacade.search_by_id(453395) }
 
   it 'creates top rated movie poros', :vcr do
      expect(top_rated.first).to be_a(Movie)
@@ -11,4 +12,9 @@ RSpec.describe MovieFacade do
   it 'creates searched movies', :vcr do 
     expect(search_movies.first[:id]).to eq(453395)
   end
+
+  it 'creates search_id', :vcr do
+    expect(search_id.title).to eq("Doctor Strange in the Multiverse of Madness")
+  end
 end
+
---
+++ spec/features/parties/new_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "create party page" do 
+  let!(:user_1) { User.create!(name: "Sam", email: "sam@zmail.com" ) }
+  let!(:user_2) { User.create!(name: "Hank", email: "hank@zmail.com" ) } 
+  let!(:user_3) { User.create!(name: "Tom", email: "tom@zmail.com") }
+  let!(:movie_id) { 453395 }
+
+  it "has a form to create party that will be displayed on all invited users dashboards", :vcr do 
+    visit user_path(user_1.id)
+
+    expect(page).to_not have_content("Doctor Strange in the Multiverse of Madness")
+
+    visit new_user_movie_party_path(user_1.id, movie_id)
+
+    fill_in 'duration', with: 102
+    fill_in 'date', with: '07/09/2022'
+    fill_in 'start_time', with: '09:48 PM'
+
+    check 'Hank'
+    check 'Tom'
+    click_button 'Create Party'
+
+    expect(current_path).to eq(user_path(user_1.id))
+    expect(page).to have_content("Doctor Strange in the Multiverse of Madness")
+
+    visit user_path(user_2.id)
+   expect(page).to have_content("Doctor Strange in the Multiverse of Madness")
+  end
+end
