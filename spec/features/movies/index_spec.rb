bd88f68bc0d81761238d140e0111a2c7e282cdc3
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-08 18:57:33 -0600

    Add vote average testing and functionality

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Movie Index Page', type: :feature do 
   describe 'title section' do 
-    it 'has a list of the top rated films', :vcr do 
+    it 'has a list of the top rated films with a vote count', :vcr do 
       user1 = User.create!(name: 'Andrew', email: 'concertenthusiast@musac.org')
       user2 = User.create!(name: 'Sai', email: 'allthethings@hottooob.com')
       
       visit user_movies_path(user1.id)
       expect(page.all('.movies').count).to eq(20)
-
+      save_and_open_page
       within '.topMovies' do 
         expect(page).to have_content('Spirited Away')
         expect(page).to have_content('Pulp Fiction')
+        expect(page).to have_content(8.7)
       end
     end
   end
