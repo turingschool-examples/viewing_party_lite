08b63455145a17480aaf2b024b04ba01bf10442c
Zachary P <zachary.s.prince@outlook.com>
2022-07-09 17:14:01 -0400

    Move everything from new viewing party page to movie details page

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,7 +1,3 @@
 class ViewingPartyController < ApplicationController
-  def new
-    @movie = MoviesFacade.movie(params[:movie_id])
-    @reviews = MoviesFacade.movie_reviews(params[:movie_id])
-    @cast = MoviesFacade.ten_movie_cast(params[:movie_id])
-  end
+  def new; end
 end
