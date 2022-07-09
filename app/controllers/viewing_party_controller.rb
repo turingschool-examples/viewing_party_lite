0548e5d723bec99e6b5d1bc24a75ad1dba71b0e6
Zachary P <zachary.s.prince@outlook.com>
2022-07-09 15:04:51 -0400

    Add cast members instance variable to new method

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -2,5 +2,6 @@ class ViewingPartyController < ApplicationController
   def new
     @movie = MoviesFacade.movie(params[:movie_id])
     @reviews = MoviesFacade.movie_reviews(params[:movie_id])
+    @cast = MoviesFacade.ten_movie_cast(params[:movie_id])
   end
 end
