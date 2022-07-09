6fdfad446fc1d906220f3b82aa956ab600646061
Zachary P <zachary.s.prince@outlook.com>
2022-07-09 13:04:39 -0400

    Add get_movie service

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -1,22 +1,28 @@
 class MoviesService
   def self.get_url(url, keyword = nil)
-
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.params[:api_key] = ENV['movie_api_key']
-      faraday.params[:query] = keyword if !keyword.nil?
+      faraday.params[:query] = keyword unless keyword.nil?
     end
 
     response = conn.get(url)
 
     JSON.parse(response.body, symbolize_names: true)
-
   end
-  
+
   def self.get_top_rated
     get_url('3/movie/top_rated')[:results]
   end
 
   def self.find_movies(keyword)
-    get_url("3/search/movie", keyword)[:results]
+    get_url('3/search/movie', keyword)[:results]
+  end
+
+  def self.get_movie(movie_id)
+    get_url("3/movie/#{movie_id}")
+  end
+
+  def self.get_movie_reviews(movie_id)
+    get_url("3/movie/#{movie_id}/reviews")[:results]
   end
 end
