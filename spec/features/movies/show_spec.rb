f31c9d88893bcc08bf312fc006ad0e61d4fb91b3
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-08 18:22:44 -0600

    Feat: Movie Details Page displays required info

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -41,16 +41,17 @@ RSpec.describe "Movie Deatils/Show Page" do
       # Count of total reviews
       # Each review's author and information
 
-      # endpoints: images, reviews, cast
-
+      # endpoints: images, reviews, cast, genre
+save_and_open_page
+      expect(page).to have_content("The Shawshank Redemption's Details Page")
       expect(page).to have_content("Title: The Shawshank Redemption")
       expect(page).to have_content("Vote Average: 8.7")
-      expect(page).to have_content("Runtime: ")
-      expect(page).to have_content("Genres: ")
+      expect(page).to have_content("Runtime: 2:22")
+      expect(page).to have_content("Genre(s): Drama, Crime")
       expect(page).to have_content("Summary: Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.")
-      expect(page).to have_content("Total Reviews: 21715") #vote count? we'll find out
-      expect(page).to have_content("Cast: ")
-      expect(page).to have_content("Reviews: ")
+      expect(page).to have_content("Cast:")
+      expect(page).to have_content("Number of Reviews: 7")
+      expect(page).to have_content("Reviews:")
     end
   end
 end
