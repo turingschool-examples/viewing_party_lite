e990c5d1cbd299f2f5047cb75d3a7a196b9b7a1f
Zachary P <zachary.s.prince@outlook.com>
2022-07-09 13:03:11 -0400

    Add viewing party routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root to: 'welcome#index'
 
   get 'register' => 'users#new'
   get '/user/:id/discover', to: 'users#discover'
 
   resources :users, only: %i[show create] do
-    resources :movies, controller: :user_movies
+    resources :movies, controller: :user_movies do
+      resources :viewing_party
+    end
   end
 end
