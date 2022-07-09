a097804a16042ce3cf9c162d735b9437866e6bc0
Oscar Santos <oscar303data@gmail.com>
2022-07-08 18:53:42 -0600

    create party page

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,5 +1,7 @@
-class ViewingPartyController < ApplicationController 
+class ViewingPartyController < ApplicationController
     def new
-        
+      @host = User.find(params[:user_id])
+      @users = User.where("id != #{@host.id}")
+      @movie = MoviesFacade.get_details(params[:movie_id])
     end
 end
