fa5d8c7f77287b38166b3da96889e33a15628fbd
Stephen Wilkens <stephenwilkens@gmail.com>
2022-07-09 10:19:24 -0600

    Add test for movie facade movie search method

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe 'MovieFacade' do
     expect(movies).to be_a Array
     expect(movies).to be_all Movie
   end
+
+
+
+
+
+
+
+
+
+  it 'can create a new movie object from an api search', :vcr do
+    movie = MovieFacade.new.movie_search(278)
+
+    expect(movie).to be_a Movie
+    expect(movie.id).to be_a Integer
+    expect(movie.title).to be_a String
+    expect(movie.vote_average).to be_a Float
+  end
 end
