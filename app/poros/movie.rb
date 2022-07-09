0ee4997265b13153ca7178362835f14c5a251b04
Zachary P <zachary.s.prince@outlook.com>
2022-07-09 13:03:35 -0400

    Add extra instance variables

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,8 +1,23 @@
 class Movie
-  attr_reader :title, :vote_average
+  attr_reader :title,
+              :vote_average,
+              :runtime,
+              :genre,
+              :summary_description,
+              :movie_id
 
   def initialize(data)
     @title = data[:title]
     @vote_average = data[:vote_average]
+    @runtime = (time_conversion(data[:runtime]) if data[:runtime])
+    @genre = data[:genres][0][:name] if data[:genres]
+    @summary_description = data[:overview]
+    @movie_id = data[:id]
+  end
+
+  def time_conversion(minutes)
+    hours = minutes / 60
+    rest = minutes % 60
+    "#{hours}:#{rest}"
   end
 end
