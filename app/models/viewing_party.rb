160cb9b8a1b291c7df78808fff9dbcefd0642ea1
Sarah Hall <swatson1994@yahoo.com>
2022-07-10 15:16:00 -0400

    Feat: Add method for displaying title
    Co-authored-by: Deannah Burke <98491210+deannahburke@use
    rs.noreply.github.com>

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -6,4 +6,10 @@ class ViewingParty < ApplicationRecord
   validates_presence_of :date
   validates_presence_of :time
   validates_presence_of :movie_id
+
+
+  def display_title
+    movie = MovieFacade.create_movie_details(movie_id)
+    movie.title
+  end
 end
