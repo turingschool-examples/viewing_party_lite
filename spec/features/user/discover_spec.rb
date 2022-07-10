b236dd8fd12c397dee6e060355271a713c1f3bbe
Eric Espindola <eric_e_2012@hotmail.com>
2022-07-10 13:27:32 -0700

    update discover page to include search bar

--- spec/features/user/discover_spec.rb
+++ spec/features/user/discover_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe 'user discover page', :vcr do
     expect(page).to have_link("Discover")
   end
 
-  xit 'has search button' do
+  it 'has search button' do
     user1 = User.create!(email: "user1@example.com", name: "Jeff Casimir")
     visit "/users/#{user1.id}/discover"
     fill_in('Search', with: 'shrek')
     click_button 'Search'
 
-    expect(current_path).to eq("/users/#{user1.id}/movies?q=shrek")
+    expect(current_path).to eq("/users/#{user1.id}/movies")
 
     expect(page).to have_link("Shrek")
     expect(page).to have_content("Vote Average: 7.7")
