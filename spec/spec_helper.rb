ac13d1934faf6fec396cb96d18f5f0d2e3b689aa
Jim Riddle <jimriddle0@gmail.com>
2022-07-10 15:49:40 -0500

    update simple cov filters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,11 @@ require 'webmock/rspec'
 require 'simplecov'
 SimpleCov.start 'rails'
 SimpleCov.add_filter %w[spec config]
+SimpleCov.add_filter "app/channels/application_cable/channel.rb"
+SimpleCov.add_filter "app/channels/application_cable/connection.rb"
+SimpleCov.add_filter "app/jobs/application_job.rb"
+SimpleCov.add_filter "app/mailers/application_mailer.rb"
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
