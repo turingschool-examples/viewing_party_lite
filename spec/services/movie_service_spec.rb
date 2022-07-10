676a4b3ad9a08aa5d0f3a1f58e3d87ac7a94bcc9
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-10 11:58:37 -0600

    test movies service #find_movie

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe MoviesService do
     expect(movie[:title]).to be_a String
     expect(movie[:vote_average]).to be_a Float
   end
+  
+  it '#find_movies retrieves data and parses response', :vcr do
+    keyword = "Apple"
+    parsed_json = MoviesService.find_movies(keyword)
+
+    expect(parsed_json).to be_a Array
+    
+    movie = parsed_json.first
+    expect(movie).to be_a Hash 
+
+    parsed_json.each do |movie|
+        expect(movie[:title]).to be_a String
+        expect(movie[:title]).to include(keyword)
+    end
+  end
 end
