051e908c43f1416300c91e433ae2d35ade1b972f
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-09 23:00:15 -0600

    test user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe User, type: :model do
     it { should have_many(:party_users) }
     it { shoulld have_many(:parties).through(:party_users) }
   end
-  
+
+  describe 'validations'
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :name}
+    it {should valideate_uniqueness_of :email}
+  end
 end
