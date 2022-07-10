7790cd37f867e540436a8c2980595860182bf494
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-10 00:03:36 -0600

    test party model validations

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Party, type: :model do
     it { shoulld have_many(:users).through(:party_users) }
   end
 
+
+  describe 'validations' do
+    it { should validate_presence_of :movie_name }
+    it { should validate_presence_of :movie_id }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :start_time }
+    it { should validate_presence_of :duration }
+  end
 end
