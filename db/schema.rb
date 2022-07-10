4437c574431cce75cfffb0815478a4bf28a51652
Zachary P <zachary.s.prince@outlook.com>
2022-07-10 12:14:36 -0400

    Fix formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_07_203629) do
-
+ActiveRecord::Schema.define(version: 20_220_707_203_629) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "parties", force: :cascade do |t|
-    t.string "movie_name"
-    t.integer "movie_id"
-    t.date "date"
-    t.time "start_time"
-    t.integer "duration"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'parties', force: :cascade do |t|
+    t.string 'movie_name'
+    t.integer 'movie_id'
+    t.date 'date'
+    t.time 'start_time'
+    t.integer 'duration'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "party_users", force: :cascade do |t|
-    t.bigint "party_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "host", default: false
-    t.index ["party_id"], name: "index_party_users_on_party_id"
-    t.index ["user_id"], name: "index_party_users_on_user_id"
+  create_table 'party_users', force: :cascade do |t|
+    t.bigint 'party_id'
+    t.bigint 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.boolean 'host', default: false
+    t.index ['party_id'], name: 'index_party_users_on_party_id'
+    t.index ['user_id'], name: 'index_party_users_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "party_users", "parties"
-  add_foreign_key "party_users", "users"
+  add_foreign_key 'party_users', 'parties'
+  add_foreign_key 'party_users', 'users'
 end
