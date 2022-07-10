dcfb417fc51a9672256bdad0aed747a1f19240c4
Jim Riddle <jimriddle0@gmail.com>
2022-07-10 13:07:05 -0500

    modify party controller to create new party

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,13 +2,34 @@
 
 class PartiesController < ApplicationController
   def new
-    @party = Party.new
     @movie = MovieFacade.movie_details(params[:movie_id])
+    @host = User.find(params[:id])
+    @users = User.all
+  end
+
+  def create
+    # @party = Party.new
+    @movie = MovieFacade.movie_details(params[:movie_id])
+    @host = User.find(params[:id])
+    params[:invitees].delete("")
+    if @movie.runtime > params[:duration].to_i
+      flash[:notice] = 'Party duration must be greater than movie runtime'
+      redirect_to "/users/#{@host.id}/movies/#{@movie.id}/viewing-party/new"
+    elsif (params[:invitees].length > 0)
+      @party = Party.create!(party_params)
+      UserParty.create!(user_id: @host.id, party_id: @party.id, host: true)
+      params[:invitees].each do |invitee_id|
+        UserParty.create!(user_id: invitee_id.to_i, party_id: @party.id, host: false)
+      end
+    else
+      @party = Party.create!(party_params)
+      UserParty.create!(user_id: @host.id, party_id: @party.id, host: true)
+    end
   end
 
   private
 
   def party_params
-    # params.permit(:name, :email)
+    params.permit(:date, :duration, :title, :start)
   end
 end
