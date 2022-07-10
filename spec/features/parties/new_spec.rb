05983af2e19dcd9a457c67b0a949ebd5f61b44f7
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-10 13:55:50 -0600

    Added set fields for duration of parties, has to be above the movie runtime

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'Creating a viewing party from a movie show page', :vcr do
         
         expect(current_path).to eq(new_user_movie_party_path(@user1.id, 278))
         expect(page).to have_content('New Viewing Party for The Shawshank Redemption')
-        
+        expect(page).to have_field('Duration', with: 142)
         fill_in 'Date', with: '2014-12-01'
         fill_in 'Time', with: '01:29:18'
         fill_in 'User name', with: 'Jake'
