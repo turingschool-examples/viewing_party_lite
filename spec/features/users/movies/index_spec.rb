41c9a8ad110c500b91a7f57a195cf052a309f68b
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-09 22:00:40 -0400

    Update movie results spec

--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require 'rails_helper'
 
 describe 'movie results/index page' do
@@ -7,7 +6,9 @@ describe 'movie results/index page' do
     @user1 = User.create!(name: 'Jane', email: 'eleven@upsidedown.com')
     visit "users/#{@user1.id}/discover"
   end
-  it 'links to each movie results details/show page' do
-    # ##is there a way to test this? I'm blanking
+  it 'links to each movie results details/show page', :vcr do
+    click_button "Find Top Rated Movies"
+    click_link "The Shawshank Redemption"
+    expect(current_path).to eq("/users/#{@user1.id}/movies/278")
   end
 end
