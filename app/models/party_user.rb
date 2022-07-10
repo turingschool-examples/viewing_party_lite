954cd5d61b8994e94b1ab8f4e758914f57515a09
Bryce Wein <bryce.wein@gmail.com>
2022-07-10 13:07:45 -0400

    Refactor: move invite_guests to model and make test

--- app/models/party_user.rb
+++ app/models/party_user.rb
@@ -1,4 +1,13 @@
 class PartyUser < ApplicationRecord
   belongs_to :user
   belongs_to :viewing_party
+
+  def self.invite_guests(list, viewing_party_id)
+    unless list.nil?
+      list.each do |user_id|
+        PartyUser.create({ user_id: user_id, viewing_party_id: viewing_party_id,
+                           host: false })
+      end
+    end
+  end
 end
