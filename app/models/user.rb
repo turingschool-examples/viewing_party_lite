fa645eabfdd81c6bb857e061da0b4b7293357c70
Bryce Wein <bryce.wein@gmail.com>
2022-07-09 23:37:46 -0400

    Test: viewing party not filled in test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   has_many :viewing_parties, through: :party_users
 
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,8 @@
 class ViewingParty < ApplicationRecord
   has_many :party_users
   has_many :users, through: :party_users
+
+  validates :duration, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
 end
