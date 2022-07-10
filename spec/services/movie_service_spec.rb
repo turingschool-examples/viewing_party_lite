9fa8c75ee73cecf8e47ee4d4e3483df9afcac40a
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-07-10 12:52:55 -0600

    fix: flaky test in ::get_search_movie

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe MovieService do
   describe '::get_search_movie' do
     it 'makes a service call and returns data for one movie' do
-      movie = MovieService.get_search_movie('Spirited Away')
+      movie = MovieService.get_search_movie("The Art of 'Spirited Away'")
 
       expect(movie[0][:title]).to eq("The Art of 'Spirited Away'")
     end
