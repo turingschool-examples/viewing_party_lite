46c0f7872f4637bb589e30bbdbad7b774b3e11d1
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-10 02:42:02 -0600

    make corections to relationship and validation tests for models

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Party, type: :model do
-  decribe 'relationships' do
+  describe 'relationships' do
     it { should have_many(:party_users) }
-    it { shoulld have_many(:users).through(:party_users) }
+    it { should have_many(:users).through(:party_users) }
   end
-
 
   describe 'validations' do 
     it { should validate_presence_of :movie_name }
--- spec/models/party_user_spec.rb
+++ spec/models/party_user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PartyUser, type: :model do
-  decribe 'relationships' do
+  describe 'relationships' do
     it { should belong_to(:party) }
     it { should belong_to(:user) }
   end
