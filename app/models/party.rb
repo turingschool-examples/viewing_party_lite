762713c57a784d0bc8502970eedcbecb6fcb1928
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-10 02:43:04 -0600

    create party model validations and relationships

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,10 @@
 class Party < ApplicationRecord
+  validates_presence_of :movie_name
+  validates_presence_of :movie_id
+  validates_presence_of :date
+  validates_presence_of :start_time
+  validates_presence_of :duration
+
+  has_many :party_users
+  has_many :users, through: :party_users
 end
