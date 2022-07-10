f46ecb8feb4e7f4cb25c176770e37cfd5fa761d5
Zachary P <zachary.s.prince@outlook.com>
2022-07-10 13:24:53 -0400

    Create viewing party render test

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -41,4 +41,44 @@ RSpec.describe 'user dashboard page' do
       expect(page).to have_content('Fight Club 3')
     end
   end
+
+  it 'has a movie image, title link, who is hosting the event, and a list of invitees' do
+    user1 = User.create!(name: 'Tommy Tanktop', email: 'tanktoptitan@aol.com')
+    user2 = User.create!(name: 'Jerry Jasmond', email: 'jjasmond@aol.com')
+    user3 = User.create!(name: 'Ken Kappersmith', email: 'kkappersmith@aol.com')
+    party1 = Party.create!(movie_name: 'Fight Club', movie_id: 1, date: Time.now.strftime('%d/%m/%Y'),
+                           start_time: Time.now.strftime('%H:%M'), duration: 120)
+    party2 = Party.create!(movie_name: 'Armageddon', movie_id: 4,
+                           date: Time.now.strftime('%d/%m/%Y'), start_time: Time.now.strftime('%H:%M'), duration: 120)
+    party3 = Party.create!(movie_name: 'Superbad', movie_id: 3, date: Time.now.strftime('%d/%m/%Y'),
+                           start_time: Time.now.strftime('%H:%M'), duration: 120)
+    PartyUser.create(party_id: party1.id, user_id: user1.id, host: true)
+    PartyUser.create(party_id: party1.id, user_id: user2.id, host: false)
+
+    PartyUser.create(party_id: party2.id, user_id: user2.id, host: true)
+    PartyUser.create(party_id: party2.id, user_id: user1.id, host: false)
+    PartyUser.create(party_id: party2.id, user_id: user3.id, host: false)
+
+    PartyUser.create(party_id: party3.id, user_id: user3.id, host: true)
+    PartyUser.create(party_id: party3.id, user_id: user1.id, host: false)
+    PartyUser.create(party_id: party3.id, user_id: user2.id, host: false)
+
+    visit "/users/#{user.id}"
+
+    within '#viewingPartiesInvited' do
+      expect(page).to have_content('Host: jjasmond@aol.com')
+      expect(page).to have_content('Host: kkappersmith@aol.com')
+      expect(page).to have_content('Armageddon')
+      expect(page).to have_content('Superbad')
+      expect(page).to_not have_content('Fight Club')
+    end
+
+    within '#viewingPartiesCreated' do
+      expect(page).to have_content('Host: You')
+      expect(page).to_not have_content('Armageddon')
+      expect(page).to_not have_content('Superbad')
+      expect(page).to have_content('Fight Club')
+      expect(page).to have_content('jjasmond@aol.com')
+    end
+  end
 end
