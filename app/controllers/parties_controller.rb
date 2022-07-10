200029eb1b3e5e118d89c17eb31224c862dba9c1
Jim Riddle <jimriddle0@gmail.com>
2022-07-10 15:41:48 -0500

    modified party controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -8,7 +8,6 @@ class PartiesController < ApplicationController
   end
 
   def create
-    # @party = Party.new
     @movie = MovieFacade.movie_details(params[:movie_id])
     @host = User.find(params[:id])
     params[:invitees].delete("")
@@ -21,15 +20,25 @@ class PartiesController < ApplicationController
       params[:invitees].each do |invitee_id|
         UserParty.create!(user_id: invitee_id.to_i, party_id: @party.id, host: false)
       end
+      redirect_to "/users/#{@host.id}/viewing-party/dashboard"
     else
       @party = Party.create!(party_params)
       UserParty.create!(user_id: @host.id, party_id: @party.id, host: true)
+      redirect_to "/users/#{@host.id}/viewing-party/dashboard"
     end
+    # binding.pry
+  end
+
+  def dashboard
+    @user = User.find(params[:id])
+    
+    # @hosted_parties = @user.hosted_parties
+    # @invited_parties = @user.invited_parties
   end
 
   private
 
   def party_params
-    params.permit(:date, :duration, :title, :start)
+    params.permit(:date, :duration, :title, :start, :movie_id)
   end
 end
