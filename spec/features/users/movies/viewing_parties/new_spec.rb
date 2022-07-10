786a135264659608476b0e0f8747a36a97406358
Zachary P <zachary.s.prince@outlook.com>
2022-07-10 11:05:57 -0400

    Fix tests

--- spec/features/users/movies/viewing_parties/new_spec.rb
+++ spec/features/users/movies/viewing_parties/new_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Viewing Party New Page' do
   it 'Has a form to create a new viewing party', :vcr do
     user = User.create!(name: 'User', email: 'user@email.com')
+    User.create!(name: 'Tom Marryway', email: 'gloop@email.com')
     movie = MoviesFacade.movie(550)
 
     visit new_user_movie_viewing_party_path(user_id: user.id, movie_id: movie.movie_id)
-    save_and_open_page
-    expect('Fight Club').to appear_before('Duration of Party:')
 
-    fill_in :duration_of_party, with: '200'
-    fill_in :when, with: '200'
-    fill_in :time, with: '200'
-    fill_in :checkbox, with: '200'
+    expect('Fight Club').to appear_before('Duration')
+
+    fill_in :duration, with: '200'
+    fill_in :date, with: '07/10/2022'
+    fill_in :time, with: '11:59'
+    page.check
   end
 end
