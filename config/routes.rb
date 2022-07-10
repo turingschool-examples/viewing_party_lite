765fea55b2a696a73b42508ae8c4f5537ab98c23
Colin Reinhart <colinbreinhartgmail.com>
2022-07-10 14:17:54 -0600

    routing correction

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   get '/users/:id/movies', to: 'movies#index'
   get 'users/:user_id/movies/:movie_id/show', to: 'movies#show'
-  get 'users/:user_id/movies/:movie_id', to: 'parties#new'
+  get 'users/:user_id/movies/:movie_id', to: 'movies#show'
   post '/register', to: 'users#create'
   get 'users/:user_id/movies/:movie_id/viewing_party/new', to: 'parties#new'
   post 'users/:user_id/movies/:movie_id/viewing_party', to: 'parties#create'
