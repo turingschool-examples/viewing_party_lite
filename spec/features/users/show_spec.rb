3bdbdda85590b55c36392ca10007e32756b21828
Tyler Ross <tross020890@gmail.com>
2022-07-10 14:57:50 -0500

    Test: refactor user tests to avoid duplicate emails

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "User Dashboard/Show Page", type: :feature do
   it 'has the users info' do
-    user1 = User.create!(name: "Jim", email: 'jim@mail.com')
-    user2 = User.create!(name: "Tommy", email: 'tommy@mail.com')
+    user1 = User.create(name: "Jim", email: 'jim1@mail.com')
+    user2 = User.create(name: "Tommy", email: 'tommy1@mail.com')
 
     visit "/users/#{user1.id}"
 
@@ -14,13 +14,13 @@ RSpec.describe "User Dashboard/Show Page", type: :feature do
   end
 
   it "has section that lists viewing parties" do
-    user1 = User.create!(name: "Jim", email: 'jim@mail.com')
-    user2 = User.create!(name: "Tommy", email: 'tommy@mail.com')
-    user3 = User.create!(name: "Hoju", email: 'hoju@mail.com')
-    party1 = Party.create!(duration: 300, date: Date.today, start_time: Time.now, user_id: user1.id, movie_id: 298, movie_name: "Shawshank Redemption")
+    user1 = User.create(name: "Jim", email: 'jim2@mail.com')
+    user2 = User.create(name: "Tommy", email: 'tommy2@mail.com')
+    user3 = User.create(name: "Hoju", email: 'hoju2@mail.com')
+    party1 = Party.create(duration: 300, date: Date.today, start_time: Time.now, user_id: user1.id, movie_id: 298, movie_name: "Shawshank Redemption")
     partyuser1 = PartyUser.create!(party: party1, user: user1)
     partyuser2 = PartyUser.create!(party: party1, user: user2)
-    party2 = Party.create!(duration: 300, date: Date.today, start_time: Time.now, user_id: user2.id, movie_id: 19404, movie_name: "Parasite")
+    party2 = Party.create(duration: 300, date: Date.today, start_time: Time.now, user_id: user2.id, movie_id: 19404, movie_name: "Parasite")
     partyuser3 = PartyUser.create!(party: party2, user: user1)
     partyuser4 = PartyUser.create!(party: party2, user: user2)
 
@@ -45,8 +45,8 @@ RSpec.describe "User Dashboard/Show Page", type: :feature do
   end
 
   it "has button to discover movies" do
-    user1 = User.create!(name: "Jim", email: 'jim@mail.com')
-    user2 = User.create!(name: "Tommy", email: 'tommy@mail.com')
+    user1 = User.create(name: "Jim", email: 'jim3@mail.com')
+    user2 = User.create(name: "Tommy", email: 'tommy3@mail.com')
 
     visit "/users/#{user1.id}"
 
@@ -54,8 +54,8 @@ RSpec.describe "User Dashboard/Show Page", type: :feature do
   end
 
   it "Button to discover movies redirects to movie discover/index" do
-    user1 = User.create!(name: "Jim", email: 'jim@mail.com')
-    user2 = User.create!(name: "Tommy", email: 'tommy@mail.com')
+    user1 = User.create(name: "Jim", email: 'jim4@mail.com')
+    user2 = User.create(name: "Tommy", email: 'tommy4@mail.com')
 
     visit "/users/#{user1.id}"
     click_button "Discover Movies"
