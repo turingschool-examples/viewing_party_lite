f7552e10def37afc8a2ae2bf2e39bd6225b3363f
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-18 16:08:07 -0500

    Test: can validate unique email and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,8 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many(:user_parties) }
     it { should have_many(:parties).through(:user_parties) }
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:email)}
+    it { should have_secure_password }
   end
 end
