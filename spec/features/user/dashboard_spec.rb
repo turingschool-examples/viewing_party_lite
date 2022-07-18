7bf48cf726fd96226a72d60e511648fcc828939f
Stirling Hostetter <stirlhoss@proton.me>
2022-07-18 14:04:07 -0500

    Add password to user creation

--- spec/features/user/dashboard_spec.rb
+++ spec/features/user/dashboard_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'user dashboard' do
   it 'has discover movies button' do
-    user1 = User.create!(email: "user1@example.com", name: "Jeff Casimir")
+    user1 = User.create!(email: "user1@example.com", name: "Jeff Casimir", password: 'test123', password_confirmation: 'test123')
 
     visit "/users/#{user1.id}/dashboard"
     click_button "Discover Movies"
