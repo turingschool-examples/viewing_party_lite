521ceb73906229e86c8537bd664cdf8485b1ef7c
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-18 16:03:44 -0500

    Test: happy and sad path test for logging in

---
+++ spec/features/users/login_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Login page " do
+  before :each do
+    @jim = User.create(name: 'jim', email: 'jim@jim.com', password: 'Isuck', password_confirmation: 'Isuck')
+  end
+
+  it "can login a user" do
+    visit login_path
+    fill_in 'email', with: 'jim@jim.com'
+    fill_in 'password', with: 'Isuck'
+    click_button "Login"
+    expect(current_path).to eq(user_path(@jim))
+  end
+
+  it "takes you back to the login page if no match found" do
+    visit login_path
+    fill_in 'email', with: 'jim@jimmy.com'
+    fill_in 'password', with: 'Isuck'
+    click_button "Login"
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Invalid Credentials")
+  end
+
+end
