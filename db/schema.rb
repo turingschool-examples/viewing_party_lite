44b09511229607b8092a7d67e5d9e50387deedfe
Stirling Hostetter <stirlhoss@proton.me>
2022-07-18 13:12:50 -0500

    Schema updated with password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_224438) do
+ActiveRecord::Schema.define(version: 2022_07_18_180754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_07_05_224438) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "viewing_parties", force: :cascade do |t|
