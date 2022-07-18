250610ad883952e3a3893b8d93b4712a45568ca1
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-18 16:07:46 -0500

    Test: can register a user

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -7,7 +7,31 @@ RSpec.describe 'User Registration Page' do
     visit register_path
     fill_in 'name', with: 'Jamar'
     fill_in 'email', with: 'Jamar@sucks.com'
+    fill_in 'password', with: 'test123'
+    fill_in 'password_confirmation', with: 'test123'
     click_on 'Create User'
     expect(current_path).to eq(user_path(User.last.id))
   end
+
+  it 'gives an error when not all fields are filled out ' do
+    visit register_path
+    fill_in 'email', with: 'Jamar@sucks.com'
+    fill_in 'password', with: 'test123'
+    fill_in 'password_confirmation', with: 'test123'
+    click_on 'Create User'
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Name can't be blank")
+  end
+
+  it "gives an error when passwords dont match " do
+    visit register_path
+    fill_in 'name', with: 'Jamar'
+    fill_in 'email', with: 'Jamar@sucks.com'
+    fill_in 'password', with: 'test123'
+    fill_in 'password_confirmation', with: 'test456'
+    click_on 'Create User'
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+
 end
