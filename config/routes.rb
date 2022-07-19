962f772891eda496d59a05b2d105ad3a07a8ba18
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-19 13:54:10 -0500

    Cleanup extra spacing

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,16 @@ Rails.application.routes.draw do
 
   root 'landing#home'
   get '/users/dashboard', to: 'users#show'
-
   get '/users/discover', to: 'users#discover'
 
   resources :users, except: [:new, :show]
   resources :parties
-
 
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 
   get '/login', to: 'users#login_form'
   post '/login', to: 'users#login_user'
-  # get '/admin', to: 'admins#dashboard'
 
   get '/users/movies', to: 'users#movies'
   get '/users/movies/:movie_id', to: 'users#movie_details'
