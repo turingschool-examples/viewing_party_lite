4bf3974e9e5de55c1f71c161157c54dac9a3ff13
Thiago Silveira <thiago1992@yahoo.com>
2022-08-23 13:44:56 -0600

    Add: Migrations for users, view parties, and user view parties
    Co-authored-by: Shirley DeCesari <shirley.decesari@gmail.com>

---
+++ db/migrate/20220823193717_create_user_view_parties.rb
@@ -0,0 +1,10 @@
+class CreateUserViewParties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_view_parties do |t|
+      t.references :user, foreign_key: true
+      t.references :view_party, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
