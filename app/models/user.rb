c08a38e754ffca1efe1943a0da66bb3720a81224
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-23 16:42:40 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :viewing_party_users
+  has_many :viewing_parties, through: :viewing_party_users
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates :email, uniqueness: true
 end
