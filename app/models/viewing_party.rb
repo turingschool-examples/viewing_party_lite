baa39d5deb4fb865372c6634faccafaef8b13e8e
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-23 08:10:12 -0600

    test: viewing_party validations

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,6 @@
 class ViewingParty < ApplicationRecord
   has_many :viewing_party_users
   has_many :users, through: :viewing_party_users
+
+  
 end
--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -6,9 +6,10 @@ RSpec.describe ViewingParty do
     it { should have_many(:users).through(:viewing_party_users) }
   end
 
-  # describe 'validations' do
-  #   it { should validate_presence_of(:user_id) }
-  #   it { should validate_presence_of(:viewing_party_id) }
-  #   it { should validate_presence_of(:is_host) }
-  # end
+  describe 'validations' do
+    it { should validate_presence_of(:movie_id) }
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:start_time) }
+    it { should validate_presence_of(:duration) }
+  end
 end
