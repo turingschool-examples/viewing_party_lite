4f192f82fff05472c91d4dba73a373ec11ad489d
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-22 20:43:12 -0600

    feature/Add all existing users under exisiting users section

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  
   def index
+    @users = User.all
   end
 end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,19 +3,13 @@ require 'rails_helper'
 RSpec.describe 'landing page' do
   describe '#index' do
     it 'displays the title of the application' do
-      user_1 = User.create!(name: 'Cindy Lou', email: 'cidlou@gmail.com')
-      user_2 = User.create!(name: 'David Smith', email: 'daves@gmail.com')
-      user_3 = User.create!(name: 'Mary Jones', email: 'maryjones@gmail.com')
-
+ 
       visit '/'
 
       expect(page).to have_content('Viewing Party Lite')
     end
 
     it 'has button to create a new user' do
-      user_1 = User.create!(name: 'Cindy Lou', email: 'cidlou@gmail.com')
-      user_2 = User.create!(name: 'David Smith', email: 'daves@gmail.com')
-      user_3 = User.create!(name: 'Mary Jones', email: 'maryjonesu@gmail.com')
 
       visit '/'
 
@@ -23,15 +17,35 @@ RSpec.describe 'landing page' do
     end
     
     it 'click button to redirect to new user registration form' do
-      user_1 = User.create!(name: 'Cindy Lou', email: 'cidlou@gmail.com')
-      user_2 = User.create!(name: 'David Smith', email: 'daves@gmail.com')
-      user_3 = User.create!(name: 'Mary Jones', email: 'maryjonesu@gmail.com')
 
       visit '/'
 
       click_button('Create New User')
 
       expect(current_path).to eq('/register')
+    end
+
+    it 'displays all exisiting users by email address' do
+      user_1 = User.create!(name: 'Cindy Lou', email: 'cidlou@gmail.com')
+      user_2 = User.create!(name: 'David Smith', email: 'daves@gmail.com')
+      user_3 = User.create!(name: 'Mary Jones', email: 'maryjonesu@gmail.com')
+
+      visit '/'
+      
+      within "#users0" do
+        expect(page).to have_content("cidlou@gmail.com's Dashboard")
+        expect(page).to_not have_content("daves@gmail.com's Dashboard")
+      end
+
+      within "#users1" do
+        expect(page).to have_content("daves@gmail.com's Dashboard")
+        expect(page).to_not have_content("maryjonesu@gmail.com's Dashboard")
+      end
+
+      within "#users2" do
+        expect(page).to have_content("maryjonesu@gmail.com's Dashboard")
+        expect(page).to_not have_content("cidlou@gmail.com's Dashboard")
+      end
     end
   end
 end
