58aec07f19a5c481092d9079d94c8a04736299f7
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-23 14:15:54 -0600

    feat: add users discover path

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
+
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/discover', to: 'users#discover'
 end
