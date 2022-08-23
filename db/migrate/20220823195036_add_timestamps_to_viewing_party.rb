d77af2354b0d84aad71c8930e05747bd4635e60f
Sage Skaff <skaffsage@gmail.com>
2022-08-23 13:03:03 -0700

    Create database

---
+++ db/migrate/20220823195036_add_timestamps_to_viewing_party.rb
@@ -0,0 +1,6 @@
+class AddTimestampsToViewingParty < ActiveRecord::Migration[5.2]
+  def change
+    add_column :viewing_parties, :created_at, :datetime
+    add_column :viewing_parties, :updated_at, :datetime
+  end
+end
