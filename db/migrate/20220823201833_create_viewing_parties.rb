57dcc4df4c9128f77174757ebe5229b591d919aa
Justin Ramirez <jusrez528@gmail.com>
2022-08-23 16:20:11 -0400

    Add viewing party and viewing party users migrations to schema
    Co-authored-by: Mayu Takeda <mayu.takeda.utd@gmail.com>

---
+++ db/migrate/20220823201833_create_viewing_parties.rb
@@ -0,0 +1,12 @@
+class CreateViewingParties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :viewing_parties do |t|
+      t.integer :movie_id
+      t.time :duration
+      t.date :date
+      t.time :start_time
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_012016) do
+ActiveRecord::Schema.define(version: 2022_08_23_201852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,25 @@ ActiveRecord::Schema.define(version: 2022_08_23_012016) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "viewing_parties", force: :cascade do |t|
+    t.integer "movie_id"
+    t.time "duration"
+    t.date "date"
+    t.time "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "viewing_party_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "viewing_party_id"
+    t.boolean "host"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_viewing_party_users_on_user_id"
+    t.index ["viewing_party_id"], name: "index_viewing_party_users_on_viewing_party_id"
+  end
+
+  add_foreign_key "viewing_party_users", "users"
+  add_foreign_key "viewing_party_users", "viewing_parties"
 end
