94bfbe71e634ca4e7b006fe714611a36be2c6030
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-23 14:01:09 -0600

    test: uesr's dashboard displays a button that links to the user's movie discover page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "User's Dashboard Show Page" do
   before do
-    visit "/register"
-    
     user1 = User.create!(name: "#{Faker::TvShows::HowIMetYourMother.character}", email: "#{Faker::TvShows::HowIMetYourMother.character}@centuryfoxtv.com")
 
     visit "/users/#{user1.id}"
   end
 
-  it "displays a user's name and email" do
+  it "displays the user's name" do
     expect(page).to have_content("#{user1.name}'s Dashboard")
-    expect(page).to have_content("#{user1.email}'s Dashboard")
+    expect(page).to_not have_content("Joanne's Dashboard")
   end
 
-  
+  it "can click a button to link to the user's movie discover page" do
+    click_button "Discover Movies"
+    
+    expect(current_path).to eq("/users/#{@user.id}/discover")
+  end
 end
