a736a8305a1f67828459f4371a4032696ee4433f
Shirley DeCesari <shirley.decesari@gmail.com>
2022-08-23 15:34:38 -0700

    Add: gems
    Co-authored-by: Thiago Silveira <thiago1992@yahoo.com>

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,37 +12,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_193717) do
-
+ActiveRecord::Schema.define(version: 20_220_823_193_717) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "user_view_parties", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "view_party_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_view_parties_on_user_id"
-    t.index ["view_party_id"], name: "index_user_view_parties_on_view_party_id"
+  create_table 'user_view_parties', force: :cascade do |t|
+    t.bigint 'user_id'
+    t.bigint 'view_party_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_user_view_parties_on_user_id'
+    t.index ['view_party_id'], name: 'index_user_view_parties_on_view_party_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "view_parties", force: :cascade do |t|
-    t.datetime "date"
-    t.integer "duration"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_view_parties_on_user_id"
+  create_table 'view_parties', force: :cascade do |t|
+    t.datetime 'date'
+    t.integer 'duration'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.index ['user_id'], name: 'index_view_parties_on_user_id'
   end
 
-  add_foreign_key "user_view_parties", "users"
-  add_foreign_key "user_view_parties", "view_parties"
-  add_foreign_key "view_parties", "users"
+  add_foreign_key 'user_view_parties', 'users'
+  add_foreign_key 'user_view_parties', 'view_parties'
+  add_foreign_key 'view_parties', 'users'
 end
