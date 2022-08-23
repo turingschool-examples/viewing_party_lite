d882c7402c3fd69c247e166a568d4aa3f38c30c1
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-23 16:46:55 -0500

    Add test for registering a new user

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'New User Registration' do
+
+  it 'can create new user' do
+
+    visit "/register"
+
+    fill_in 'Name:', with: "Drew"
+    fill_in 'Email:', with: "drew@email.com"
+    click_button("Create New User")
+    user = User.last
+
+    expect(current_path).to eq("/users/#{user.id}")
+  end
+
+  it 'cannot create a new user if a name is not input' do
+
+    visit "/register"
+
+    fill_in 'Email:', with: "drew@email.com"
+    click_button("Create New User")
+    expect(current_path).to eq("/register")
+
+    fill_in 'Name:', with: "Drew"
+    fill_in 'Email:', with: "drew@email.com"
+    click_button("Create New User")
+    expect(current_path).to eq("/register")
+    user = User.last
+
+    expect(current_path).to eq("/users/#{user.id}")
+  end
+
+  it 'cannot create a new user if an email is not input' do
+
+    visit "/register"
+
+    fill_in 'Name:', with: "Drew"
+    click_button("Create New User")
+    expect(current_path).to eq("/register")
+
+    fill_in 'Name:', with: "Drew"
+    fill_in 'Email:', with: "drew@email.com"
+    click_button("Create New User")
+    expect(current_path).to eq("/register")
+    user = User.last
+
+    expect(current_path).to eq("/users/#{user.id}")
+  end
+end
