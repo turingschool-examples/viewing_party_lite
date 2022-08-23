47006f26b0199a941ac6b8872b3d0df94cd5687f
Alex Boyd <alexanderleeboyd@gmail.com>
2022-08-22 23:28:04 -0400

    Feat: landing page lists existing users

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,8 @@
 class LandingController < ApplicationController
 
   def index
-    
+    @users = User.all
+    # require 'pry'; binding.pry 
   end
 
 end
--- spec/features/landing/index_spec.rb
+++ spec/features/landing/index_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe 'landing page', type: :feature do
     expect(current_path).to eq("/register")
   end
 
+  it 'displays list of existing users, with links to each user\'s dashboard' do
+    user1 = User.create!(first_name: "David", last_name: "Lynch", email: "david-fake@test.com")
+
+    user2 = User.create!(first_name: "Steven", last_name: "Spielberg", email: "steven-fake@test.com")
+
+    user3 = User.create!(first_name: "Greta", last_name: "Gerwig", email: "greta-fake@test.com")
+
+    visit '/'
+
+    within "#existing-users" do
+      expect(page).to have_content("david-fake@test.com's Dashboard")
+      expect(page).to have_content("steven-fake@test.com's Dashboard")
+      expect(page).to have_content("greta-fake@test.com's Dashboard")
+    end
+
+    save_and_open_page
+
+    click_on "david-fake@test.com's Dashboard"
+
+    expect(current_path).to eq("/users/#{user1.id}")
+  end
+
 end
