0a8d91812ef88dfe9e2a623138c8abea15cb709f
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-23 08:08:29 -0600

    test: user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe User do
     it { should have_many(:viewing_parties).through(:viewing_party_users) }
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+  end
 end
