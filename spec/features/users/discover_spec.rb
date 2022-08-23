56a4a092145faede05768c733dd78b1b51d4d73a
Blake Saylor <blakesaylor@gmail.com>
2022-08-23 15:33:42 -0600

    Add test for presence of find top rated movies button

---
+++ spec/features/users/discover_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'discover page' do
+  it 'has a button to find top rated movies' do
+    user1 = User.create!(name: 'Geraldo Rivera', email: 'geraldo@trashtv.com')
+    
+    visit "users/#{user1.id}/discover"
+
+    save_and_open_page
+
+    expect(page).to have_button "Find Top Rated Movies"
+    click_button "Find Top Rated Movies"
+    expect(current_path).to eq "/users/#{user1.id}/movies?q=top%40rated"
+  end
+end
