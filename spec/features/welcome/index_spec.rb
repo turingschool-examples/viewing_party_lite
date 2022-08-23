019e7273df1d88f59cbc8bafa75940ec4b3366fa
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-22 20:21:08 -0600

    feat/Add button to create new user

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe 'landing page' do
     it 'displays the title of the application' do
       user_1 = User.create!(name: 'Cindy Lou', email: 'cidlou@gmail.com')
       user_2 = User.create!(name: 'David Smith', email: 'daves@gmail.com')
-      user_3 = User.create!(name: 'Mary Jones', email: 'cidlou@gmail.com')
+      user_3 = User.create!(name: 'Mary Jones', email: 'maryjones@gmail.com')
 
       visit '/'
-      save_and_open_page
+
       expect(page).to have_content('Viewing Party Lite')
+    end
+
+    it 'has button to create a new user' do
+      user_1 = User.create!(name: 'Cindy Lou', email: 'cidlou@gmail.com')
+      user_2 = User.create!(name: 'David Smith', email: 'daves@gmail.com')
+      user_3 = User.create!(name: 'Mary Jones', email: 'maryjonesu@gmail.com')
+
+      visit '/'
+
+      expect(page).to have_button('Create New User')
     end
   end
 end
