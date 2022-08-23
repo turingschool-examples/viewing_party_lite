4bedbb86c1143c717cc34b5f8c9c48219ebb397d
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-23 16:41:29 -0500

    Update schema back to original

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_042433) do
+ActiveRecord::Schema.define(version: 2022_08_22_223050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2022_08_23_042433) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "viewing_parties", force: :cascade do |t|
