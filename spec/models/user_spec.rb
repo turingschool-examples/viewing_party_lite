9897e52b5eea783397ed30d3b10ee6b7f51d9888
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-23 08:03:26 -0600

    test: user relationships

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'relationships' do
+    it { should have_many(:viewing_party_users) }
+    it { should have_many(:viewing_parties).through(:viewing_party_users) }
+  end
+
+end
