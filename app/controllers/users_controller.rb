7e3f7ab021bbe34a4af11d74d3f68725d49637fa
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-23 12:52:05 -0600

    New user created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,19 @@ class UsersController < ApplicationController
   end
 
   def new
-  end 
+  end
+
+  def create
+    @user = User.create(user_params)
+      if @user.save
+        redirect_to "/users/#{@user.id}"
+      else
+        render new
+      end
+  end
+
+  private
+   def user_params
+     params.permit(:name, :email, :password)
+   end
 end
