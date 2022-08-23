4da47e630bb32f3dac1fd90f6182c34115c46fc8
John Santosuosso <john.santosuosso1@gmail.com>
2022-08-22 22:42:03 -0400

    add validations and passing validation tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_parties
+  has_many :parties, through: :user_parties
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
