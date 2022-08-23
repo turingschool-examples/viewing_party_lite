e824f1e1ed19f69b69a5fb0ae35a6b04dbcb0457
Eli Sachs <easachs13@gmail.com>
2022-08-23 01:48:41 -0600

    Add user new/index feats

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,8 +1,7 @@
 class Party < ApplicationRecord
   has_many :party_users
   has_many :users, through: :party_users
-  validates_presence_of :movie
-  validates_presence_of :date
+  validates_numericality_of :movie_id
   validates_presence_of :start_time
   validates_numericality_of :duration
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :parties, through: :party_users
   validates_presence_of :name
   validates_presence_of :email
+  validates_uniqueness_of :email
 end
