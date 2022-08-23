b9b07e77a796f3cb8bf8fd0084f46df62dc51b3c
Justin Ramirez <jusrez528@gmail.com>
2022-08-23 17:34:25 -0400

    Add spec for user dashboard page
    Co-authored-by: Mayu Takeda <mayu.takeda.utd@gmail.com>

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'User Dashboard' do
+  @users = let!(:users) { create_list(:user, 3) }
+
+  describe 'When I visit /users/:id where :id is a valid user id' do
+    it 'I should see the user names dasboard at the top of the page, a button to discover movies, and a section that lists viewing parties' do
+      user1 = users[0]
+      user2 = users[1]
+      user3 = users[2]
+
+      visit user_path(user1)
+      
+      expect(page).to have_content("#{user1.name}'s Dashboard")
+      expect(page).to have_button("Discover Movies")
+      expect(page).to have_content("Viewing Parties")
+    end
+    
+  end
+end
