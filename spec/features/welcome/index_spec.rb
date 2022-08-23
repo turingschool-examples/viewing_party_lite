2043e88023c1de0ad99fa459202fc3c402e5e833
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-23 07:33:44 -0600

    test: Welcome index page lists all existing users

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -10,15 +10,20 @@ RSpec.describe "Welcome Index Page" do
     expect(page).to have_content('Welcome to Viewing Party!')
   end
 
-  it 'displays a button to create a new user' do
+  it 'can click a button to create a new user' do
     click_button 'Create a New User'    
     
     expect(current_path).to eq('/register')
   end
   
-  it 'displays a link to return to the home page at the top' do
+  it 'can click a link at the top of the page to return to the home page' do
     click_link 'Home'    
     
     expect(current_path).to eq('/')
+  end
+
+  it 'lists all existing users' do
+
+    # expect(page).to have_content
   end
 end
