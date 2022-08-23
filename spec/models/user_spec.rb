8d2276d55e1e12f068afdfcfbb7017992fc1247d
Bryce Simonds <bsimonds14@gmail.com>
2022-08-23 14:42:19 -0600

    Tests: validations and relationships for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:quantity_threshold) }
+  end
+
+  describe 'relationships' do
+    it { should have_many(:party_users) }
+    it { should have_many(:viewing_parties).through(:party_users) }
+  end
+end
+
