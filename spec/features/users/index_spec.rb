11980c5ceb0720f9aae808697d8bc17e76d4fcf4
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-22 20:38:25 -0600

    User index on landing page

---
+++ spec/features/users/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'the landing page' do
+  it 'shows a list of all users as links to their dashboard' do
+    user1 = User.create!(name: 'Geraldo Rivera', email: 'geraldo@trashtv.com')
+    user2 = User.create!(name: 'Maury Povich', email: 'maury@trashtv.com')
+    user3 = User.create!(name: 'Jenny Jones', email: 'jenny@trashtv.com')
+
+    visit root_path
+
+    within "#user-#{user1.id}" do
+      expect(page).to have_link('Geraldo Rivera')
+    end
+
+    within "#user-#{user2.id}" do
+      expect(page).to have_link('Maury Povich')
+    end
+
+    within "#user-#{user3.id}" do
+      expect(page).to have_link('Jenny Jones')
+    end
+  end
+end
