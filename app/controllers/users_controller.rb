ff8a3d625bbec22f1404c4a8196be8e41522585a
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-23 16:34:28 -0600

    feat: add top movies feature with test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,22 +26,26 @@ class UsersController < ApplicationController
             faraday.headers["api_key"] = ENV['tmdb_key']
         end
 
-        response = conn.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['tmdb_key']}&language=en-US&page=1&include_adult=false&query=#{params[:search]}")
+        if params[:movies] != nil  
+            response1 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['tmdb_key']}&language=en-US&page=1")
+            response2 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['tmdb_key']}&language=en-US&page=2")
+        else 
+            response1 = conn.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['tmdb_key']}&language=en-US&page=1&include_adult=false&query=#{params[:search]}&page=1")
+            response2 = conn.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['tmdb_key']}&language=en-US&page=1&include_adult=false&query=#{params[:search]}&page=2")
+        end 
         
-        data = JSON.parse(response.body, symbolize_names: true)
-        
-        @movies = data[:results][0..39]
+        data1 = JSON.parse(response1.body, symbolize_names: true)
+        data2 = JSON.parse(response2.body, symbolize_names: true)
+
+        @movies = data1[:results].concat(data2[:results])
 
         render 'users/movies'
         # redirect_to "/users/#{params[:user_id]}/movies/?=#{params[:search]}"
     end 
 
-    def top_movies
-        redirect_to "/users/#{params[:user_id]}/movies/?=top%20rated"
-    end 
-
-    def movies 
-    end 
+    # def top_movies
+    #     redirect_to "/users/#{params[:user_id]}/movies/?=top%20rated"
+    # end 
 
     private
     def user_params
--- spec/features/users/movies/movies_spec.rb
+++ spec/features/users/movies/movies_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'Movies Search' do
-  describe 'happy path' do
+RSpec.describe 'Movies Display Page' do
+  describe 'search function' do
     it 'allows user to search for movies', :vcr do
         user = User.create!(first_name: "Homer", last_name: "Simpson", email:"name@test.com", created_at: Time.now, updated_at: Time.now)
 
@@ -13,7 +13,28 @@ RSpec.describe 'Movies Search' do
         expect(page.status_code).to eq 200
         expect(page).to have_content("Title: Pulp Fiction")
         expect(page).to have_content("Vote Average: 8.5")
-        expect(page).to have_selector(".movies", count: 6)
+        expect(page).to have_selector(".movie", count: 6)
+    end
+  end
+
+  describe 'top movies function' do
+    it 'allows user to view top rated movies', :vcr do
+        user = User.create!(first_name: "Homer", last_name: "Simpson", email:"name@test.com", created_at: Time.now, updated_at: Time.now)
+
+        visit "/users/#{user.id}/discover"
+
+        click_button("Discover Top Rated Movies")
+
+        expect(page.status_code).to eq 200
+
+         within ("#movies") do 
+            expect(page.all('.movie')[0]).to have_content("The Shawshank Redemption")
+            expect(page.all('.movie')[0]).to have_content("Vote Average: 8.7")
+            expect(page.all('.movie')[1]).to have_content("Dilwale Dulhania Le Jayenge")
+            expect(page.all('.movie')[1]).to have_content("Vote Average: 8.7")
+        end 
+
+        expect(page).to have_selector(".movie", count: 40)
     end
   end
 end
