39404f0bcba2e842be7cef6a8571fb6e5c4a978e
Eli Sachs <easachs13@gmail.com>
2022-08-23 01:48:18 -0600

    Add user new/index specs

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Party, type: :model do
   describe "validations" do
-    it { should validate_presence_of :movie }
-    it { should validate_presence_of :date }
+    it { should validate_numericality_of :movie_id }
     it { should validate_presence_of :start_time }
     it { should validate_numericality_of :duration }
     
