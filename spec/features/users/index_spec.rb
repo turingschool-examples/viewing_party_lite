5153f47edf85c336348aaf12a748e4f99cc3df75
Blake Saylor <blakesaylor@gmail.com>
2022-08-23 14:15:06 -0600

    Various view refactoring

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -9,22 +9,22 @@ RSpec.describe 'the landing page' do
     visit root_path
 
     within "#user-#{user1.id}" do
-      expect(page).to have_link('Geraldo Rivera')
+      expect(page).to have_link('Geraldo Rivera', href: user_path(user1))
     end
 
     within "#user-#{user2.id}" do
-      expect(page).to have_link('Maury Povich')
+      expect(page).to have_link('Maury Povich', href: user_path(user2))
     end
 
     within "#user-#{user3.id}" do
-      expect(page).to have_link('Jenny Jones')
+      expect(page).to have_link('Jenny Jones', href: user_path(user3))
     end
 
     click_link('Geraldo Rivera')
-    expect(current_path).to eq("/users/#{user1.id}")
+    expect(current_path).to eq user_path(user1)
   end
 
-  it 'has a button to create a new user' do
+  it 'has a button to create a new user that directs to the register page' do
     user1 = User.create!(name: 'Geraldo Rivera', email: 'geraldo@trashtv.com')
     user2 = User.create!(name: 'Maury Povich', email: 'maury@trashtv.com')
     user3 = User.create!(name: 'Jenny Jones', email: 'jenny@trashtv.com')
@@ -32,5 +32,7 @@ RSpec.describe 'the landing page' do
     visit root_path
 
     expect(page).to have_button('New User')
+    click_button('New User')
+    expect(current_path).to eq register_path
   end
 end
