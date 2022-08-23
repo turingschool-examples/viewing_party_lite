1384737e51ef7b1cb77b838ea6d0d6fdc9b1515b
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-22 18:27:28 -0600

    feat: add button to create a new user to welcome page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,10 +70,10 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
-  config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['govt_api_key']}
-  config.default_cassette_options = { re_record_interval: 1.day }
-end
+# VCR.configure do |config|
+#   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+#   config.hook_into :webmock
+#   config.configure_rspec_metadata!
+#   config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['govt_api_key']}
+#   config.default_cassette_options = { re_record_interval: 1.day }
+# end
