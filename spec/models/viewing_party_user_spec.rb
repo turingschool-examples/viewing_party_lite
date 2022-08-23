cdc6fa5e9d2251aba9ff86c70d6a33a90457f495
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-23 08:12:01 -0600

    test: viewing_party_user validations

--- spec/models/viewing_party_user_spec.rb
+++ spec/models/viewing_party_user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe ViewingPartyUser do
     it { should belong_to(:user) }
     it { should belong_to(:viewing_party) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:viewing_party_id) }
+    it { should validate_presence_of(:is_host) }
+  end
 end
