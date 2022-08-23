1f3ff702b50cdb53857c405e5fa7658ce2dc82e9
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-08-23 15:04:45 -0700

    Installed rubocop/autocorrected offenses

--- db/migrate/20220823195036_add_timestamps_to_viewing_party.rb
+++ db/migrate/20220823195036_add_timestamps_to_viewing_party.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTimestampsToViewingParty < ActiveRecord::Migration[5.2]
   def change
     add_column :viewing_parties, :created_at, :datetime
--- db/migrate/20220823195109_add_timestamps_to_user_viewing_party.rb
+++ db/migrate/20220823195109_add_timestamps_to_user_viewing_party.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTimestampsToUserViewingParty < ActiveRecord::Migration[5.2]
   def change
     add_column :user_viewing_parties, :created_at, :datetime
