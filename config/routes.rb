813b066be865f8d4c16a4edf51b0627264c89177
Nicole Esquer <nicole.esquer5@gmail.com>
2022-08-22 22:33:36 -0700

    test: added more tests for uniqueness of email, organized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get '/', to: 'welcome#index'
-
+  get '/', to: 'welcome#index'
   get '/register', to: 'users#new'
-
-  resources :users
-
   get '/dashboard', to: 'users#show'
 
+  resources :users, only: [:create, :new, :show] do
+    resources :discover, only: [:index]
+  end
 end
