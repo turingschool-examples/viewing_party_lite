724cfa6a73eff47ed255fbc4f3ec80a8723870a9
Bryce Simonds <bsimonds14@gmail.com>
2022-08-23 17:54:35 -0600

    Test: creates user and redirects with valid data

--- spec/features/create_spec.rb
+++ spec/features/create_spec.rb
@@ -1,19 +1,31 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe 'user creation' do
   describe 'the user registration' do
     it 'renders the new form' do
       visit "/register"
-
+      
       expect(page).to have_content('Register a New User')
       expect(find('form')).to have_content('Name')
       expect(find('form')).to have_content('Email')
       expect(page).to have_button('Create New User')
     end
+
+    it 'creates the user and redirects to the user show page' do
+      visit "/register"
+      random_name = Faker::Name.name
+      random_email = Faker::Internet.email
+
+      fill_in 'Name', with: random_name
+      fill_in 'Email', with: random_email
+
+      click_button 'Create New User'
+      expect(page).to have_current_path("/users/:id")
+      expect(page).to have_content(random_name)
+    end
   end
 end 
-
-
 # When a user visits the '/register' path they should see a form to register.
 
 # The form should include:
