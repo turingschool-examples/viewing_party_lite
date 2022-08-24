2002039619b8cb342f76a311fadf2b26312cd940
Alex Boyd <alexanderleeboyd@gmail.com>
2022-08-23 23:19:35 -0400

    Feat: displays movie title

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -20,7 +20,16 @@ RSpec.describe "movie detail page", type: :feature do
     click_button("Discover Page")
 
     expect(current_path).to eq("/users/#{user1.id}/discover")
+  end
 
+  it 'displays the movie title', :vcr do
+    user1 = User.create!(first_name: "David", last_name: "Lynch", email: "david-fake@test.com")
+
+    visit "/users/#{user1.id}/movies/278"
+
+    within "#movie-title" do
+      expect(page).to have_content("The Shawshank Redemption")
+    end
   end
 
 end
