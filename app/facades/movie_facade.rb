13b517f766e0fe01a63c7d3e8291a25272a492da
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-24 11:45:26 -0600

    feat: add ::create_top_movie method

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -2,9 +2,8 @@ class MovieFacade
 
   def self.create_top_movies
     movies = MovieService.get_top_movies
-    movies[0..49].map do |movie|
+    movies[0..39].map do |movie|
       Movie.new(movie)
     end
   end
-
 end
