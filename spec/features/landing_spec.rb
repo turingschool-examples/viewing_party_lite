75eb2ccd2c3cd897e8c2b9e937b10575d1a7db3a
Parker Thomson <pdthomson1239@gmail.com>
2022-08-23 21:23:40 -0400

    test for create a user button on landing page, links to form in users

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'landing page' do
 
-  it "can display the apps name" do
-    visit '/'
+  describe 'ALL landing page functionality' do
+    it "can display the apps name" do
+      visit '/'
 
-    expect(page).to have_content('Viewing Party Lite')
+      expect(page).to have_content('Viewing Party Lite')
+    end
+
+    it "has a button to create a new user" do
+      visit '/'
+
+      expect(page).to have_button('Create a New User')
+      click_button('Create a New User')
+      expect(current_path).to eq('/register')
+    end
+
   end
-
-
 end
