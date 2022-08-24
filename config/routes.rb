fa997ca553cb6b465186caa472374e5cfb211a25
Nick Jones <nickjones9605@gmail.com>
2022-08-24 13:17:18 -0600

    Adds a search form to discover movie page for a user. Adds a test for searching for a movie. Adds a redirect for the page that user is searching for.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   
   get "/users/:id/discover", to: "users#discover"
   resources :users, only: [:show]
-
+  get "/users/:id/movies", to: "movies#index"
+  post '/users/:id/search', to: 'movies#search'
 end
--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -3,9 +3,20 @@ require 'rails_helper'
 
 RSpec.describe 'user discover movies page' do 
     it "has a button to find top rated movies" do 
-    user_1 = User.create!(name: "ODB", email: "testemail1@mail.com")
-    visit "/users/#{user_1.id}/discover"
-    save_and_open_page
+        user_1 = User.create!(name: "ODB", email: "testemail1@mail.com")
+        visit "/users/#{user_1.id}/discover"
+        click_on("Find Top Rated Movies")
+        expect(current_path).to eq("/users/#{user_1.id}/movies")
+    end
+
+    it "has a button to search for a particular movie" do 
+        user_1 = User.create!(name: "ODB", email: "testemail1@mail.com")
+        visit "/users/#{user_1.id}/discover"
+        fill_in :search, with: 'Sanders'
+        save_and_open_page
+        click_button 'Search'
+        # expect(page.status_code).to eq 200 < needs content, probably from a cassette fixture
+        expect(current_path).to eq("/users/#{@user.id}/search")
     end
 
 end
