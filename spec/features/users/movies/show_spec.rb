f310acd0614ee4024b683b61459636007d9c69ee
Alex Boyd <alexanderleeboyd@gmail.com>
2022-08-23 23:10:04 -0400

    Feat: movie detail page has button to ddiscover page

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "movie detail page", type: :feature do
   
   it 'has a button to create a viewing party', :vcr do
-
     user1 = User.create!(first_name: "David", last_name: "Lynch", email: "david-fake@test.com")
 
     visit "/users/#{user1.id}/movies/278"
@@ -11,6 +10,17 @@ RSpec.describe "movie detail page", type: :feature do
     click_button("Create Viewing Party for The Shawshank Redemption")
 
     expect(current_path).to eq("/users/#{user1.id}/movies/278/viewing-party/new")
+  end
+
+  it 'has a button to return to the discover page', :vcr do
+    user1 = User.create!(first_name: "David", last_name: "Lynch", email: "david-fake@test.com")
+
+    visit "/users/#{user1.id}/movies/278"
+
+    click_button("Discover Page")
+
+    expect(current_path).to eq("/users/#{user1.id}/discover")
+
   end
 
 end
