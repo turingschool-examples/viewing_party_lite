2445ccfba86d58ff9d5f3445afa8b26e220837c1
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-24 15:02:38 -0600

    feat: add viewing party movie title with feature test

---
+++ spec/features/viewing_party/new_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "viewing party new page", type: :feature do
+
+    it 'has a viewing party new page', :vcr do
+        user = User.create!(first_name: "Homer", last_name: "Simpson", email:"name@test.com", created_at: Time.now, updated_at: Time.now)
+       
+        visit "/users/#{user.id}/movies/278"
+
+        click_button("Create Viewing Party for The Shawshank Redemption")
+
+        expect(current_path).to eq("/users/#{user.id}/movies/278/viewing-party/new")
+        expect(page).to have_content("The Shawshank Redemption")
+    end
+end 
