3f2b6e9da4e55c501022a6cbe5901c5db7633279
Thiago Silveira <thiago1992@yahoo.com>
2022-08-24 17:02:42 -0600

    Test: discover movies button goes to user discover movies dash

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'show page', type: :feature do
+RSpec.describe 'user dashboard show page', type: :feature do
   it 'has user information on show page' do
     user1 = User.create!(name: "Bob", email: "bob@turing.edu")
     user2 = User.create!(name: "Sally", email: "sally@turing.edu")
@@ -11,6 +11,15 @@ RSpec.describe 'show page', type: :feature do
     expect(page).to have_content(user1.name)
     expect(page).to have_button("Discover Movies")
     expect(page).to have_content("Viewing Parties")
+  end
 
+  it 'can take user to users discover movies page' do
+    user1 = User.create!(name: "Bob", email: "bob@turing.edu")
+    
+    visit user_path(user1)
+
+    click_button 'Discover Movies'
+
+    expect(current_path).to eq(user_discover_index_path(user1))
   end
 end
