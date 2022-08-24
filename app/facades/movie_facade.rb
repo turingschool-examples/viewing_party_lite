036935c77e1cbc366392052d998e023a18b3f482
Nick Jones <nickjones9605@gmail.com>
2022-08-24 13:27:32 -0600

    Fixes typo, still running into issues with MovieService adding hashes

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,14 +1,13 @@
 class MovieFacade 
     def self.top_movies
         top_movies = MovieService.get_top_movies
-        binding.pry
         top_movies.map do |data|
             TopMovie.new(data)
         end
     end
     
     def self.movie(id)
-        movie = MoviceService.get_movie(id)
+        movie = MovieService.get_movie(id)
         Movie.new(movie)
     end
 
--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe 'user discover movies page' do
         user_1 = User.create!(name: "ODB", email: "testemail1@mail.com")
         visit "/users/#{user_1.id}/discover"
         fill_in :search, with: 'Sanders'
-        save_and_open_page
         click_button 'Search'
         # expect(page.status_code).to eq 200 < needs content, probably from a cassette fixture
         expect(current_path).to eq("/users/#{@user.id}/search")
