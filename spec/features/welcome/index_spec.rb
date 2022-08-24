035a7abbe613514962c730c32fb5b92576947b14
mkbonini <mkbonini@gmail.com>
2022-08-24 10:40:09 -0600

    test: pages display link to landing page and app title

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -16,15 +16,17 @@ RSpec.describe 'landing page' do
 
     it 'has a list of existing users' do
         user_1 = User.create!(name: "Mike", email: "email@email.com")
+        user_2 = User.create!(name: "Nick", email: "123@email.com")
 
         visit "/"
         
         within("#existing-users") do
             expect(page).to have_content("email@email.com's Dashboard")
+            expect(page).to have_content("123@email.com's Dashboard")
         end
     end
 
-    xit 'users emails link to user show page' do
+    it 'users emails link to user show page' do
         user_1 = User.create!(name: "Mike", email: "email@email.com")
 
         visit "/"
@@ -32,5 +34,19 @@ RSpec.describe 'landing page' do
         click_on("email@email.com's Dashboard")
 
         expect(current_path).to eq("/users/#{user_1.id}")
+    end
+
+    it ' has a link to to the landing page' do
+        visit "/"
+
+        click_on("Home")
+
+        expect(current_path).to eq("/")
+    end
+
+    it ' shows the app title' do
+        visit "/"
+
+        expect(page).to have_content("Viewing Party Light")
     end
 end
