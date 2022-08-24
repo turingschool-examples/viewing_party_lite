eb72c61440cd8945e2148ec05dbeb8b245e67c95
Thomas Turner <thomasturner482@yahoo.com>
2022-08-24 12:27:26 -0500

    routs changes

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -5,10 +5,11 @@ class RegisterController < ApplicationController
   end
 
   def create
-    if User.create!(name: params[:name],
-                     email: params[:email])
-                     flash.notice = 'User Has Been Created!'
-                     redirect_to "/register"
+    # binding.pry
+    user = User.new(name: params[:name], email: params[:email])
+    if user.save
+      flash.notice = 'User Has Been Created!'
+      redirect_to user_path(User.last.id)
     else
       flash.notice = 'User Has Not Been Created!'
       redirect_to "/register"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get '/register', to: 'register#new'
   post '/register', to: 'register#create'
-  get '/users/:id', to: 'users#show' 
+  get '/users/:id', to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'home_page#index'
    resources :users
+   resources :register
 end
