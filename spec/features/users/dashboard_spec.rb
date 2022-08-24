b7e012ade6492b115951502f6d0976e2e0489c39
philmarcu <philmark18@gmail.com>
2022-08-23 20:37:27 -0600

    Feat - add button for discover movies on user dashboard

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+RSpec.describe 'User Dashboard' do
+  context 'user discover movie button' do
+    it 'has a button to discover movies' do
+      @user1 = User.create!(name: "Jim Bob", email: "jimb@viewingparty.com")
+      @user2 = User.create!(name: "Cary Berry", email: "caryb@viewingparty.com")
+
+      visit user_path(@user1.id)
+
+      expect(page).to have_content("Jim Bob's Dashboard")
+      expect(page).to_not have_content("Cary Berry's Dashboard")
+      expect(page).to have_selector(:link_or_button, "Discover Movies")
+    end
+  end
+end
