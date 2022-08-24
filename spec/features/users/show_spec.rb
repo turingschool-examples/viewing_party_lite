dd2b99bd9589630192ec1feaa78d1eaa5a778815
Bryan Shears <brshears@gmail.com>
2022-08-23 19:08:39 -0600

    add feature test for discover movies button

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -8,11 +8,23 @@ RSpec.describe 'User Dashbboard', type: :feature do
         user_2 = User.create!(name: "Ben", email: "ben@ben.com")
 
         visit "/users/#{user_1.id}"
-
-        expect(page).to have_content("Bryan")
-        expect(page).to_not have_content("bryan@bryan.com")
-        expect(page).to_not have_content("Ben")
-        expect(page).to_not have_content("ben@ben.com")
+        within '#header' do 
+            expect(page).to have_content("Bryan")
+            expect(page).to_not have_content("bryan@bryan.com")
+            expect(page).to_not have_content("Ben")
+            expect(page).to_not have_content("ben@ben.com")
+        end 
+        within '#discover_movies_button' do 
+            expect(page).to have_button('Discover Movies')
+        end 
     end 
   
+    it 'should have a button to discover movies' do 
+        user_1 = User.create!(name: "Bryan", email: "bryan@bryan.com")
+        user_2 = User.create!(name: "Ben", email: "ben@ben.com")
+
+        visit "/users/#{user_1.id}"
+
+
+    end 
 end 
