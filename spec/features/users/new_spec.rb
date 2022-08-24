d5aacd05b29de0db3649a6dc8a2ebc275cf80878
Sage Skaff <skaffsage@gmail.com>
2022-08-23 17:16:01 -0700

    Test: form to register new user

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'User Registration Page' do
+  it 'has form to register new user' do
+    visit '/register'
+
+    fill_in :name, with: 'Gandalf'
+    fill_in :email, with: 'gandalfthegrey@wizard'
+
+    click_on 'Register'
+
+    expect(current_path).to eq("/users/#{User.all.first.id}")
+  end
+end
