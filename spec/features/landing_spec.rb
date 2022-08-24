262954a79a38b0761ed5f76ee88929d08d672028
Parker Thomson <pdthomson1239@gmail.com>
2022-08-23 21:45:24 -0400

    Added tests for displaying just the users email and the link to the DB

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -17,5 +17,30 @@ RSpec.describe 'landing page' do
       expect(current_path).to eq('/register')
     end
 
+    it "displays existing user emails only" do
+      user1 = User.create!(name: 'Hai Sall', email: 'shoe_eater@payless.com')
+      user2 = User.create!(name: 'Sryce Bimmons', email: 'valhiemhero@hotmail.com')
+
+      visit '/'
+
+      expect(page).to have_content('shoe_eater@payless.com')
+      expect(page).to have_content('valhiemhero@hotmail.com')
+      expect(page).to_not have_content('Hai Sall')
+      expect(page).to_not have_content('Sryce Bimmons')
+    end
+
+    it "links existing users to the user dashboard" do
+      user1 = User.create!(name: 'Hai Sall', email: 'shoe_eater@payless.com')
+      user2 = User.create!(name: 'Sryce Bimmons', email: 'valhiemhero@hotmail.com')
+
+      visit '/'
+save_and_open_page
+      click_link("shoe_eater@payless.com's Dashboard")
+      expect(current_path).to eq("/users/#{user1.id}")
+
+      visit '/'
+      click_link("valhiemhero@hotmail.com's Dashboard")
+      expect(current_path).to eq("/users/#{user2.id}")
+    end
   end
 end
