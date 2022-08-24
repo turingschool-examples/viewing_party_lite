ad2e3d634ff6c5208b6cc573d4eaedd48ca3450e
Thiago Silveira <thiago1992@yahoo.com>
2022-08-23 18:44:21 -0600

    Test: Renamed root to landing page + users and register routes
    Co-authored-by: Shirley DeCesari <shirley.decesari@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'landing_page#index'
+  
+  resources :users, only: [:show, :create] do
 
+  end
+
+  get '/register', to: 'users#new'
 end
