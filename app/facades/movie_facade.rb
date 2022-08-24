bc9752f3d77b828d52d794a59502ab809cf2896c
Blake Saylor <blakesaylor@gmail.com>
2022-08-24 11:48:54 -0600

    Add/update routing for API call for top movies
    Co-authored-by: Anna Marie Sterling <103849872+AMSterling@users.noreply.github.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,18 +1,21 @@
 class MovieFacade
   def self.top_rated
-    top_rated = service.top_rated_movies
-    top_movies = []
-    top_rated.each do |movie|
-      top_movies << movie[:original_title]
+    # top_movies_array = MovieService.top_rated_movies
+    # @top_movies = top_movies_array.map do |movie_data|
+    #   data = {}
+    #   data[:movie_id] = movie_data[:id]
+    #   data[:title] = movie_data[:title]
+    #   data[:rating] = movie_data[:vote_average]
+    #   Movie.new(data)
+    # end
+    # @top_movies
+
+    @top_movies = MovieService.top_rated_movies.map do |info|
+      TopMovie.new(info)
     end
-    top_movies
   end
 
   def self.service
     MovieService.new
   end
 end
-
-# movie_title = "Jurassic Park"
-# movie_id = "whatever integer the id is"
-# movie_rating = "7.9"
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,5 +1,5 @@
 class MovieService
-  def top_rated_movies
+  def self.top_rated_movies
     # 20 Movies to a page, will need two pages
     response = conn.get("movie/top_rated?&page=1")
     response2 = conn.get("movie/top_rated?&page=2")
@@ -8,8 +8,7 @@ class MovieService
     output = page1[:results] + page2[:results]
   end
 
-
-  def conn
+  def self.conn
     connection = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
       faraday.params['api_key'] = ENV['movie_api_key']
     end
