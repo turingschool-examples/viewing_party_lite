59bbfbbc8d1b13e306b52be1be6c0d914f3ee561
Nikky Rojas <rojas7313@gmail.com>
2022-08-24 09:20:02 -0600

    US6 UserDashboard Complete

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -5,13 +5,32 @@ RSpec.describe "User Dashboard Page", type: :feature do
         user1 = User.create!(name: "Micheal Jordan", email: "user1@gmail.com")
         user2 = User.create!(name: "Kobe Bryant", email: "user2@gmail.com")
 
-        visit "/users/#{user1.id}"
+        visit "/"
+        click_on "Micheal Jordan"
 
-        expect(page).to have_current_path(merchants_item_path(item_1))
-        expect(page).to have_content("Basket Ball")
-        expect(page).to have_content("Description: Wilson 29 in orange ball")
-        expect(page).to have_content("Price: 25000")
+        expect(page).to have_current_path("/users/#{user1.id}")
+        expect(page).to have_content("Micheal Jordan Dashboard")
+        expect(page).to have_content("Viewing Parties:")
+        expect(page).to have_button("Discover Movies")
         expect(page).to_not have_content("Kobe Bryant")
+    end
+    it 'a users dashboard shows all viewing parties' do
+        user1 = User.create!(name: "Micheal Jordan", email: "user1@gmail.com")
+        user2 = User.create!(name: "Kobe Bryant", email: "user2@gmail.com")
+    
+        party1 = ViewingParty.create!(duration: 90, date: "Mon, 29 Aug 2022 14:00:00 UTC +00:00", host_id: user2.id, movie_id: 244)
+        party2 = ViewingParty.create!(duration: 120, date: "Mon, 30 Aug 2022 17:00:00 UTC +00:00", host_id: user2.id, movie_id: 545)
+        userparty1= UserViewingParty.create!(user_id: user1.id, viewing_party_id: party1.id)
+        userparty2= UserViewingParty.create!(user_id: user1.id, viewing_party_id: party2.id)
+
+        visit "/users/#{user1.id}"
+        save_and_open_page
+        
+
+        expect(page).to have_content("Viewing Parties:")
+        expect(page).to have_content(party1.id)
+        expect(page).to have_content(party2.id)
+
     end
 
 end
