f6e55c97a9efb3ca9b3e458c605724f39e7d2456
Thiago Silveira <thiago1992@yahoo.com>
2022-08-24 17:03:36 -0600

    Feature: discover movies button takes user to discover movies dash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.create!(user_params)
-    # binding.pry
     redirect_to user_path(user.id)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'landing_page#index'
   
   resources :users, only: [:show, :create] do
-
+    resources :discover, only: [:index]
   end
 
   get '/register', to: 'users#new'
