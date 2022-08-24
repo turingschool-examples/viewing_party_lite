ee3fec285eae0c32daa898b50179589d727c626b
philmarcu <philmark18@gmail.com>
2022-08-24 16:09:29 -0600

    Feat - API calls made for top rated & search, html pages display results & tests pass

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   def search
+    @user = User.find(params[:user_id])
+
     conn = Faraday.new(url: "https://api.themoviedb.org") do |f|
       f.params["api_key"] = ENV['api_key']
     end
@@ -8,6 +10,22 @@ class MoviesController < ApplicationController
 
     data = JSON.parse(response.body, symbolize_names: true)
 
-    movies = data[:results]
+    @movies = data[:results]
+
+    render "users/discover"
+  end
+
+  def top_rated
+    @user = User.find(params[:user_id])
+
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |f|
+      f.params["api_key"] = ENV['api_key']
+    end
+    response = conn.get("/3/movie/top_rated")
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    @movies = data[:results]
+
+    render "movies/top_rated"
   end
 end
