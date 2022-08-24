ee105985d536f7f8bf95b8fc7cc3ffadb1a8c2f7
Nick Jones <nickjones9605@gmail.com>
2022-08-24 10:54:27 -0600

    Adds a new discover action to users controller and a discover route to routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,8 @@ class UsersController < ApplicationController
     def show 
         @user = User.find(params[:id])
     end 
+
+    def discover
+        @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: "welcome#index"
   
-
+  get "/users/:id/discover", to: "users#discover"
   resources :users, only: [:show]
 
 end
