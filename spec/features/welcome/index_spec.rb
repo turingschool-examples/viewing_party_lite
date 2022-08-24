64551db38b1c0fe304abc88e4ea5e3c9b8549026
mkbonini <mkbonini@gmail.com>
2022-08-23 19:31:54 -0600

    test: landing page has buttons and links

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'landing page' do
+    it 'has a button to create a new user' do
+        visit "/"
+
+        expect(page).to have_button("Create a New User")
+    end
+
+    it 'has a list of existing users' do
+        user_1 = User.create!(name: "Mike", email: "email@email.com")
+
+        visit "/"
+
+        within "#existing-users" do
+            expect(page).to have_link("email@emails.com's Dashboard")
+        end
+    end
+end
