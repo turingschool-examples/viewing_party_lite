2717ebce5b32bab55d96b7cfd922db1d79b2abb7
Blake Saylor <blakesaylor@gmail.com>
2022-08-24 11:50:06 -0600

    Update to encode as UTF-8 instead of Binary
    Co-authored-by: Anna Marie Sterling <103849872+AMSterling@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,6 +71,9 @@ Shoulda::Matchers.configure do |config|
 end
 
 VCR.configure do |config|
+  config.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
+  end
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.filter_sensitive_data('<API Key>') { ENV['movie_api_key'] }
