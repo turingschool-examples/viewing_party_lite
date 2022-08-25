6ef953bc5d4758820dc7bee9cf37be6e7ae1249e
Cory Bethune <corybbethune@gmail.com>
2022-08-25 12:23:02 -0700

    Added more tests to poro suit to test parsers. tests all pass.

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  it 'initializes with a hash of data' do
+
+  before(:each) do
     data = {
       id: 1,
       title: 'The Shawshank Redemption',
@@ -19,43 +20,43 @@ RSpec.describe Movie do
     }
     cast = {
       cast: [
-          {
+        {
           name: "a",
           character: "a"
         },
-          {
+        {
           name: "b",
           character: "b"
         },
-          {
+        {
           name: "c",
           character: "c"
         },
-          {
+        {
           name: "d",
           character: "d"
         },
-          {
+        {
           name: "e",
           character: "e"
         },
-          {
+        {
           name: "f",
           character: "f"
         },
-          {
+        {
           name: "g",
           character: "g"
         },
-          {
+        {
           name: "h",
           character: "h"
         },
-          {
+        {
           name: "i",
           character: "i"
         },
-          {
+        {
           name: "j",
           character: "j"
         }
@@ -65,38 +66,52 @@ RSpec.describe Movie do
       total_results: 3,
       results: [
         {
-            author_details: {
-              username: "user1"
-            },
-            content: "review 1"
+          author_details: {
+            username: "user1"
+          },
+          content: "review 1"
         },
         {
-            author_details: {
-              username: "user2"
-            },
-            content: "review 2"
+          author_details: {
+            username: "user2"
+          },
+          content: "review 2"
         },
         {
-            author_details: {
-              username: "user3"
-            },
-            content: "review 3"
+          author_details: {
+            username: "user3"
+          },
+          content: "review 3"
         }
       ]
     }
-    movie = Movie.new(data, cast, review)
+    @movie = Movie.new(data, cast, review)
+  end
 
-    expect(movie).to be_a(Movie)
-    expect(movie.id).to eq(1)
-    expect(movie.title).to eq('The Shawshank Redemption')
-    expect(movie.vote_average).to eq(9.3)
-    expect(movie.summary).to eq("Overview here")
-    expect(movie.runtime).to eq("2h 0min")
-    expect(movie.genre).to eq(["drama,", "thriller"])
+  it 'initializes with 3 hashes of data' do
+    expect(@movie).to be_a(Movie)
+    expect(@movie.id).to eq(1)
+    expect(@movie.title).to eq('The Shawshank Redemption')
+    expect(@movie.vote_average).to eq(9.3)
+    expect(@movie.summary).to eq("Overview here")
+  end
 
-    expect(movie.cast).to include("a as a")
-    expect(movie.cast).to include("b as b")
+  it "parses out the genre information and formats it for the view" do
+    expect(@movie.genre).to eq(["drama,", "thriller"])
+  end
 
-    expect(movie.review_count).to eq(3)
+  it "parses out runtime data and formats it for the view" do
+    expect(@movie.runtime).to eq("2h 0min")
+  end
+
+  it "parses out cast data and formats it for the view" do
+    expect(@movie.cast).to include("a as a")
+    expect(@movie.cast).to include("b as b")
+  end
+
+  it "parses out review data and formats it for the view" do
+    expect(@movie.review_count).to eq(3)
+    expect(@movie.reviews[0][:author]).to eq("user1")
+    expect(@movie.reviews[0][:review]).to eq("review 1")
   end
 end
