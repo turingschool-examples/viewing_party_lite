dc453c65aafda51e71393adc38d791baf4a09b67
Bryce Simonds <bsimonds14@gmail.com>
2022-08-25 15:26:13 -0600

    Refactor: deleted save and open

--- spec/features/keyword_results_spec.rb
+++ spec/features/keyword_results_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Keyword Results' do
     user1 = User.create!(name: Faker::Name.first_name, email: Faker::Internet.email)
 
     visit "/users/#{user1.id}/movies?q=Shawshank"
-    save_and_open_page
+
     expect(page.status_code).to eq 200
     expect(page).to have_content("The Shawshank Redemption Vote Average: 8.7")
     expect(page).to have_link("The Shawshank Redemption")
