78a0939c2de7d05944f86334d270e6cf5fa058b4
Bryce Simonds <bsimonds14@gmail.com>
2022-08-25 14:14:24 -0600

    Test: movie db service recieves and parses the data for page 2 of the top rated movies

--- spec/services/movie_db_service_spec.rb
+++ spec/services/movie_db_service_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe MovieDBService do
     expect(top_movie_data[:id]).to be_a Integer
     expect(top_movie_data[:vote_average]).to be_a Float
   end
+
+  it 'retrieves top rated movie data and parses the response for page 2', :vcr do
+    parsed_json = MovieDBService.top_rated_movies_page_2
+    expect(parsed_json).to be_a Hash
+    expect(parsed_json[:results]).to be_a Array
+    
+    top_movie_data = parsed_json[:results][0]
+    expect(top_movie_data).to include(:original_title, :id, :vote_average)
+    expect(top_movie_data[:original_title]).to be_a String
+    expect(top_movie_data[:id]).to be_a Integer
+    expect(top_movie_data[:vote_average]).to be_a Float
+  end
 end
