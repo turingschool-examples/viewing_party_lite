cba7c27ef68177ae40ffe40d58325a0f052d2be7
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-25 17:22:44 -0600

    refactor/Test refactor

--- spec/service/movie_service_spec.rb
+++ spec/service/movie_service_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe MovieService do
-  it 'exists' do
-    ms = MovieService.top_rated
-
-    expect(ms).to be_a(Hash)
-  end
-
   describe '#class methods'
     it 'top_rated' do
       ms = MovieService.top_rated
+      md = ms[:results].first
 
       expect(ms).to be_a(Hash)
       expect(ms[:results]).to be_a(Array)
+      expect(md).to have_key :title
+      expect(md[:title]).to be_a(String)
+      expect(md).to have_key :vote_average
+      expect(md[:vote_average]).to be_a(Float)
     end
 
     it "movies_by_keyword" do
