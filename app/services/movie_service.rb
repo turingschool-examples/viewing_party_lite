b21bc926649a1ab28d80668fc7473aec70664f9b
Eli Sachs <easachs13@gmail.com>
2022-08-25 12:16:29 -0600

    Refactor movie service

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -5,8 +5,8 @@ class MovieService
     response_1 = conn.get("/3/search/movie?query=#{search}")
     response_2 = conn.get("/3/search/movie?page=2&query=#{search}")
 
-    json_1 = parse_data(response_1)
-    json_2 = parse_data(response_2)
+    json_1 = parse_json(response_1)
+    json_2 = parse_json(response_2)
 
     search_results = json_1[:results] + json_2[:results]
   end
@@ -15,20 +15,20 @@ class MovieService
     response_1 = conn.get('/3/movie/top_rated')
     response_2 = conn.get('/3/movie/top_rated?page=2')
 
-    json_1 = parse_data(response_1)
-    json_2 = parse_data(response_2)
+    json_1 = parse_json(response_1)
+    json_2 = parse_json(response_2)
 
     top_40 = json_1[:results] + json_2[:results]
   end
 
   private
   def self.conn
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.params['api_key'] = ENV.fetch('tmdb_api_key', nil)
     end
   end
 
-  def self.parse_data(response)
+  def self.parse_json(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
