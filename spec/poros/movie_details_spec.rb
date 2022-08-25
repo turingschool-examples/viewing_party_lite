12c1827e4286b9d0288d041b109685fdcf3541b5
Nicole Esquer <nicole.esquer5@gmail.com>
2022-08-25 16:43:55 -0700

    cleaned up spacing and indents for tests

--- spec/poros/movie_details_spec.rb
+++ spec/poros/movie_details_spec.rb
@@ -3,25 +3,17 @@ require 'rails_helper'
 RSpec.describe MovieDetails do
   it "has lots of movie details" do
     data =  
-  {
-  
-      "genres": [
-        {
-            "id": 80,
-            "name": "Crime"
-        },
-        {
-            "id": 18,
-            "name": "Drama"
-        }
-      ],
+      {"genres": [{
+      "id": 80,
+      "name": "Crime"},
+      {"id": 18,
+      "name": "Drama"}],
       "id": 505,
       "overview": "A career criminal who has been deformed since birth is given a new face by a kindly doctor and paroled from prison. It appears that he has gone straight, but he is really planning his revenge on the man who killed his mentor and sent him to prison.",
       "runtime": 94,
       "title": "Johnny Handsome",
       "vote_average": 6.3,
-      "vote_count": 199
-      }
+      "vote_count": 199}
 
       movie_details = MovieDetails.new(data)
 
--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -1,35 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-   it 'has title and vote average' do
-      data =
-   {
-    "adult": false,
-    "backdrop_path": "/v7baGyne7CsLxnM2maMwIxKZdVF.jpg",
-    "genres": [
-        {
-            "id": 80,
-            "name": "Crime"
-        },
-        {
-            "id": 18,
-            "name": "Drama"
-        }
-    ],
-    "id": 505,
-    "original_language": "en",
-    "original_title": "Johnny Handsome",
-    "popularity": 8.888,
-    "poster_path": "/hdEIkpL17rvfZH0l34FJ8H9LAa.jpg",
-    "title": "Johnny Handsome",
-    "vote_average": 6.3,
-    "vote_count": 199
-   }
-      movie = Movie.new(data)
+    it 'has title and vote average' do
+        data =
+     {"adult": false,
+        "backdrop_path": "/v7baGyne7CsLxnM2maMwIxKZdVF.jpg",
+        "genres": 
+        [{"id": 80,
+        "name": "Crime"},
+        {"id": 18,
+        "name": "Drama"}],
+        "id": 505,
+        "original_language": "en",
+        "original_title": "Johnny Handsome",
+        "popularity": 8.888,
+        "poster_path": "/hdEIkpL17rvfZH0l34FJ8H9LAa.jpg",
+        "title": "Johnny Handsome",
+        "vote_average": 6.3,
+        "vote_count": 199}
 
-      expect(movie).to be_a(Movie)
-      expect(movie.id).to eq(505)
-      expect(movie.title).to eq("Johnny Handsome")
-      expect(movie.vote_average).to eq(6.3)
+        movie = Movie.new(data)
+
+        expect(movie).to be_a(Movie)
+        expect(movie.id).to eq(505)
+        expect(movie.title).to eq("Johnny Handsome")
+        expect(movie.vote_average).to eq(6.3)
   end
 end
