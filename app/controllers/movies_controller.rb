c9b8938c5a1dcb2cdef91488bdd246403b24bca1
Bryan Shears <brshears@gmail.com>
2022-08-25 14:53:57 -0600

    add movies index and movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,19 @@ class MoviesController < ApplicationController
     def index
         @user = User.find(params[:id])
     end 
+ 
+    def search
+        # conn = Faraday.new(url: "https://www.themoviedb.org/") do |faraday|
+        #     faraday.params["api_key"] = ENV['moviedb_api_key']
+        # end
+        # response = conn.get("/3/search/movie")
+
+        # json = JSON.parse(response.body, symbolize_names: true)
+
+        movies = json[:results][0][:movies]
+
+        found_movies = movies.find_all {|m| m[:original_title] == params[:search]}
+        @movie = found_movies.first
+        render "movies/index"
+    end
 end 
