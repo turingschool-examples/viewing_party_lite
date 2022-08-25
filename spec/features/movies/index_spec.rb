953a313878bdf6db3f9569fb47d636b1cea93f8c
Blake Saylor <blakesaylor@gmail.com>
2022-08-24 19:17:04 -0600

    Update tests for poros and filter certain folders to reach 100% coverage
    Co-authored-by: Anna Marie Sterling <103849872+AMSterling@users.noreply.github.com>

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Movies Index Page' do
-  xit 'can show the top 40 movies from the API', :vcr do
+  it 'can show the top 40 movies from the API', :vcr do
     user1 = User.create!(name: 'Geraldo', email: 'geraldo@trashtv.com')
     visit "users/#{user1.id}/discover"
 
     click_button "Find Top Rated Movies"
 
-    save_and_open_page
-
-    expect(current_path).to eq "/users/#{user1.id}/movies?q=top-rated"
-
+    expect(current_path).to eq "/users/#{user1.id}/movies"
+    expect(page).to have_css('table#top-movies tr', :count=>41)
+    expect(page).to have_content('Shawshank Redemption')
   end
 
   it 'can show the top 40 movies from the API based on a search string', :vcr do
@@ -24,6 +23,7 @@ RSpec.describe 'Movies Index Page' do
     expect(page.status_code).to eq 200
 
     # 1 Header row, 40 result rows
+    expect(current_path).to eq "/users/#{user1.id}/movies"
     expect(page).to have_content("Movie Results for: spongebob")
     expect(page).to have_css('table#searched-movies-table tr', :count=>41)
   end
