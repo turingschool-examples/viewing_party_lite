f33c9acf29cca255a53efdf236ed7d60774f0f2b
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-25 15:41:11 -0600

    feat: add top rated movies to movies index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   def error_message(error)
       error.full_messages.join(', ')
   end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
