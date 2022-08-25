f3ab2dee4a305daa5bb1890face1c445024d00a8
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-25 17:12:04 -0600

    test: add viewing party new test for runtime less than movie runtime

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -2,9 +2,11 @@ class UserMoviesController < ApplicationController
 
     def index 
         @user = User.find(params[:id])
-        
+        binding.pry 
         if params[:movies] != nil  
             @movies = MovieIndexTopResultFacade.service
+        elsif params[:search] == ""
+            redirect_to "/users/#{params[:user_id]}/movies/#{params[:movie_id]}/viewing-party/new", notice: "Error: Runtime must be longer than the movie length"
         else 
             @movies = MovieIndexSearchResultFacade.service(params[:search])  
         end 
--- app/controllers/viewingparties_controller.rb
+++ app/controllers/viewingparties_controller.rb
@@ -6,15 +6,19 @@ class ViewingpartiesController < ApplicationController
   end
 
   def create 
-    date = DateTime.parse("#{params[:date]} #{params[:time]}")
-    vp = ViewingParty.create!(start_time: date, party_duration_minutes: params[:runtime], movie_title: params[:movie_title], movie_duration_minutes: params[:runtime])
-    ViewingPartyUser.create!(user_id: params[:user_id], viewing_party_id: vp.id, status: 1)
-    if params[:user_ids] != nil 
-      params[:user_ids].each do |id|
-        ViewingPartyUser.create(user_id: id, viewing_party_id: vp.id, status: 0)
-      end 
-    end  
-    redirect_to "/users/#{params[:user_id]}"
+    if params[:runtime].to_i < params[:movie_runtime].to_i
+      redirect_to "/users/#{params[:user_id]}/movies/#{params[:movie_id]}/viewing-party/new", notice: "Error: Runtime must be longer than the movie length"
+    else 
+      date = DateTime.parse("#{params[:date]} #{params[:time]}")
+      vp = ViewingParty.create!(start_time: date, party_duration_minutes: params[:runtime], movie_title: params[:movie_title], movie_duration_minutes: params[:runtime])
+      ViewingPartyUser.create!(user_id: params[:user_id], viewing_party_id: vp.id, status: 1)
+      if params[:user_ids] != nil 
+        params[:user_ids].each do |id|
+          ViewingPartyUser.create(user_id: id, viewing_party_id: vp.id, status: 0)
+        end 
+      end  
+      redirect_to "/users/#{params[:user_id]}"
+    end 
   end 
 
 end
