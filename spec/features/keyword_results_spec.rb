69a2c82ac3e9d237c3dde8188959fbff59a1d776
Bryce Simonds <bsimonds14@gmail.com>
2022-08-25 15:23:53 -0600

    Feat: shows the keyword that was searched

--- spec/features/keyword_results_spec.rb
+++ spec/features/keyword_results_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe 'Keyword Results' do
     user1 = User.create!(name: Faker::Name.first_name, email: Faker::Internet.email)
 
     visit "/users/#{user1.id}/movies?q=Shawshank"
-
+    save_and_open_page
     expect(page.status_code).to eq 200
     expect(page).to have_content("The Shawshank Redemption Vote Average: 8.7")
     expect(page).to have_link("The Shawshank Redemption")
     expect(page).to_not have_content("दिलवाले दुल्हनिया ले जायेंगे Vote Average: 8.7")
     expect(page).to have_button('Discover Page')
+    expect(page).to have_content('Movie results for: Shawshank')
   end
 end
