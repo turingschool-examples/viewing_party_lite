1aa32b5ae39068472a62a4c2434ba2259552618f
Thiago Silveira <thiago1992@yahoo.com>
2022-08-26 11:24:45 -0600

    Test: Movie Facades, Services, Poros specs
    Co-authored-by: Shirley DeCesari <shirley.decesari@gmail.com>

---
+++ spec/facades/movie_facade_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe MovieFacade do
+  it 'returns an array of movie objects', :vcr do
+    movies = MovieFacade.get_top_movies
+    expect(movies).to be_a(Array)
+    expect(movies).to be_all(Movie)
+
+  end
+end
---
+++ spec/services/movie_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe MovieService do
+  describe 'methods' do
+    it 'returns the top rated movies', :vcr do
+      top_rated = MovieService.top_rated_movies
+      # binding.pry
+      expect(top_rated[:results]).to be_a(Array)
+      expect(top_rated[:results].first[:original_title]).to be_a(String)
+    end
+  end
+end
