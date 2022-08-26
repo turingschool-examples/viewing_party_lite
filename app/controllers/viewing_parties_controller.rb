c0fc3d743337a4f7eab59883338282518c84db70
Bryce Simonds <bsimonds14@gmail.com>
2022-08-26 12:33:40 -0600

    Test: redirects user to dashboard and sees viewing party as host

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,4 +1,7 @@
 class ViewingPartiesController < ApplicationController
   def new
+    @user = User.find(params[:id])
+    @users = User.all
+    @movie = MovieDBFacade.movie(params[:movie_id])
   end
 end 
