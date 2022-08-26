3a72342500fcc94859c831574c0b42f5c906594b
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-26 10:09:17 -0600

    feat: add routes to create a new viewing party

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   get '/users/:id/discover', to: 'users#discover'
 
-
-
   get 'users/:user_id/movies/:movie_id', to: 'movies#show'
   get '/users/:id/movies', to: 'movies#index'
 
+  get '/users/:user_id/movies/:movie_id/viewing-party/new', to: 'viewing_party#new'
+  post '/users/:user_id/movies/:movie_id/viewing-party/new', to: 'viewing_party#create'
 end
