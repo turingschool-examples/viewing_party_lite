b1d4d1a09f6d62d2bd549932b683556853af0ade
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-26 12:50:34 -0600

    Display viewing parties inline

--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -24,23 +24,4 @@ RSpec.describe 'New Viewing Party' do
 
     expect(current_path).to eq "/users/#{user1.id}"
   end
-
-  xit 'cannot select a duration shorter than runtime', :vcr do
-    user1 = User.create!(name: 'Geraldo', email: 'geraldo@trashtv.com', password: "password")
-    user2 = User.create!(name: 'Maury', email: 'maury@trashtv.com', password: "password")
-    user3 = User.create!(name: 'Jenny', email: 'jenny@trashtv.com', password: "password")
-
-    visit new_user_movie_viewing_party_path(user1, 238)
-
-    fill_in 'Duration of Party:', with: 50
-    fill_in 'Date', with: Date.tomorrow
-    fill_in 'Start Time', with: Time.now + 600
-
-    check("attendees_ids_#{user2.id}")
-    check("attendees_ids_#{user3.id}")
-
-    click_button 'Create Party'
-
-    expect(current_path).to eq new_user_movie_viewing_party_path(user1, 238)
-  end
 end
