29981445deba65b03aceeab46685b10f8eefb543
Justin Ramirez <jusrez528@gmail.com>
2022-08-26 14:09:49 -0400

    Fix search form
    Co-authored-by: Mayu Takeda <mayu.takeda.utd@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_823_201_852) do
+ActiveRecord::Schema.define(version: 2022_08_23_201852) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'viewing_parties', force: :cascade do |t|
-    t.integer 'movie_id'
-    t.integer 'duration'
-    t.date 'date'
-    t.time 'start_time'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "viewing_parties", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "duration"
+    t.date "date"
+    t.time "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'viewing_party_users', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'viewing_party_id'
-    t.boolean 'host'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_viewing_party_users_on_user_id'
-    t.index ['viewing_party_id'], name: 'index_viewing_party_users_on_viewing_party_id'
+  create_table "viewing_party_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "viewing_party_id"
+    t.boolean "host"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_viewing_party_users_on_user_id"
+    t.index ["viewing_party_id"], name: "index_viewing_party_users_on_viewing_party_id"
   end
 
-  add_foreign_key 'viewing_party_users', 'users'
-  add_foreign_key 'viewing_party_users', 'viewing_parties'
+  add_foreign_key "viewing_party_users", "users"
+  add_foreign_key "viewing_party_users", "viewing_parties"
 end
