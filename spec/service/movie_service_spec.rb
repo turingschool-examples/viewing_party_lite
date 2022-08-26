4c60b0c4eb919e2408a2b95e64038d61baa605e4
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-25 19:42:30 -0600

    test/Refactor movie details test in movie service spec

--- spec/service/movie_service_spec.rb
+++ spec/service/movie_service_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe MovieService do
     it "movies_by_keyword" do
       results = MovieService.movies_by_keyword("Fight Club")
       data = results[:results].first
-      
+
       expect(results).to be_a(Hash)
       expect(results[:results]).to be_a(Array)
       expect(data).to have_key :title
@@ -30,6 +30,16 @@ RSpec.describe MovieService do
       movie_details = MovieService.movie_details(505)
 
       expect(movie_details).to be_a(Hash)
+      expect(movie_details).to have_key :title
+      expect(movie_details[:title]).to be_a(String)
+      expect(movie_details).to have_key :vote_average
+      expect(movie_details[:vote_average]).to be_a(Float)
+      expect(movie_details).to have_key :runtime
+      expect(movie_details[:runtime]).to be_a(Integer)
+      expect(movie_details).to have_key :genres
+      expect(movie_details[:genres]).to be_a(Array)
+      expect(movie_details).to have_key :overview
+      expect(movie_details[:overview]).to be_a(String)
     end
 
     it 'movie_reviews' do
