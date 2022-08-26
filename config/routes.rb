31388f0f95ba16495723802744058ab2cd17a672
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-25 22:22:35 -0500

    Add routes for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
   root to: 'welcome#index'
-  
+
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 
   get '/users/:id', to: 'users#show'
   get '/users/:id/discover', to: 'users#discover'
 
+
   get '/movies', to: 'movies#index'
-  get '/movies/:id', to: 'movies#show'
+  get 'users/:user_id/movies/:movie_id', to: 'movies#show'
+  get '/users/:id/movies', to: 'movies#search'
 
 end
