93592198055c8845095059cd3017193a6f7468b2
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-25 23:52:13 -0600

    Viewing party partial form
    Co-authored-by: Blake Saylor 
    <47160926+blakesaylor@users.noreply.github.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -21,4 +21,48 @@ RSpec.describe 'the User show page' do
 
     expect(current_path).to eq "/users/#{user1.id}/discover"
   end
+
+  it 'has a section that displays viewing parties hosted by the user', :vcr do
+    user1 = User.create!(name: 'Geraldo', email: 'geraldo@trashtv.com', password: "password")
+    user2 = User.create!(name: 'Maury', email: 'maury@trashtv.com', password: "password")
+    user3 = User.create!(name: 'Jenny', email: 'jenny@trashtv.com', password: "password")
+
+    visit new_user_movie_viewing_party_path(user1, 278)
+
+    fill_in 'Duration of Party:', with: 180
+    fill_in 'Date', with: Date.today + 1
+    fill_in 'Start Time', with: '6:00 PM'
+
+    check("attendees_ids_#{user2.id}")
+
+    click_button 'Create Party'
+
+    expect(page).to have_content('The Shawshank Redemption')
+    expect(page).to have_content(Date.today + 1)
+    # expect(page).to have_content(Time.strftime('%1:%M %p'))
+    expect(page).to have_content('Hosting')
+  end
+
+  it 'displays viewing parties which the user is invited', :vcr do
+    user1 = User.create!(name: 'Geraldo', email: 'geraldo@trashtv.com', password: "password")
+    user2 = User.create!(name: 'Maury', email: 'maury@trashtv.com', password: "password")
+    user3 = User.create!(name: 'Jenny', email: 'jenny@trashtv.com', password: "password")
+
+    visit new_user_movie_viewing_party_path(user2, 238)
+
+    fill_in 'Duration of Party:', with: 180
+    fill_in 'Date', with: Date.today + 1
+    fill_in 'Start Time', with: '6:00 PM'
+
+    check("attendees_ids_#{user1.id}")
+
+    click_button 'Create Party'
+
+    visit user_path(user1)
+
+    expect(page).to have_content('The Godfather')
+    expect(page).to have_content(Date.today + 1)
+    # expect(page).to have_content(Time.strftime('%1:%M %p'))
+    expect(page).to have_content('Invited')
+  end
 end
