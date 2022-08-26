d6daa7bb2d43f23b544dcba12d5b78e532a63628
Nicole Esquer <nicole.esquer5@gmail.com>
2022-08-26 02:26:12 -0700

    feature: create new viewing party form shows up

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,4 +1,17 @@
 class ViewingPartyController < ApplicationController
    def new
+      @user = User.find(params[:user_id])
+      @movie_details = MovieFacade.get_movie_details(params[:id])   
    end
+
+   def create
+   end
+
+
+   private
+
+   def viewing_party_params
+      params.permit(:id, :title, :duration, :date, :start_time)
+   end
+
 end
