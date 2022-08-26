7d8bcf0870c56bed14b062b5d4cd4b9fc98e8a8a
Ben Silverstein <benjsilverstein@comcast.net>
2022-08-25 21:36:17 -0600

    Remove merge conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1ab1465486320222759f1dbe1b63ed872136638a
 class UsersController < ApplicationController
   def create
     new_user = User.create!(user_params)
@@ -19,15 +15,3 @@ class UsersController < ApplicationController
       params.permit(:name, :email)
     end
 end
-<<<<<<< HEAD
-=======
-class UsersController < ApplicationController 
-    def show 
-        @user = User.find(params[:id])
-        @hosts_parties = @user.hosting
-    end 
-end 
->>>>>>> 6d1eb516be86393508d1e996ad5dac6310678e6a
-=======
-
->>>>>>> 1ab1465486320222759f1dbe1b63ed872136638a
