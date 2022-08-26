5f4fe279d690be09f831086662e4623610140e02
Ben Silverstein <benjsilverstein@comcast.net>
2022-08-25 21:38:00 -0600

    Create moview controller with discover method, index method, and search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
-class MoviesController < ApplicationController 
+class MoviesController < ApplicationController
     def index
+      @user = User.find(params[:id])
+      # facade = MovieFacade.new
+      @movies = MovieFacade.top_movies
+    end
+
+    def discover
+      @user = User.find(params[:id])
+    end
+
+    def search
+        @movie = MovieFacade.search_movies(params[:search])
         @user = User.find(params[:id])
-    end 
-end 
+        render "movies/search"
+    end
+end
