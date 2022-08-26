6bcbb2cf756086675dbea25dbc0799829deaa748
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-25 18:06:37 -0600

    test: movie service test for cast data

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -22,5 +22,24 @@ describe MovieService, :vcr do
             expect(movie).to have_key :vote_average
             expect(movie[:vote_average]).to be_a(Float)
         end
+
+        it "returns cast data for a specific movie" do
+            movie = MovieService.cast_data(278)
+             
+            expect(movie).to be_a Hash
+
+            expect(movie).to have_key :cast
+            expect(movie[:cast]).to be_a(Array)
+
+            cast = movie[:cast].first
+
+            expect(movie[:cast].first).to be_a Hash
+
+            expect(movie[:cast].first).to have_key :name
+            expect(movie[:cast].first[:name]).to be_a(String)
+
+            expect(movie[:cast].first).to have_key :character
+            expect(movie[:cast].first[:character]).to be_a(String)
+        end
     end
 end
