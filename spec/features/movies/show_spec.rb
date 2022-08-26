bc8f755b28ce46a65225358661c9a949fbf68150
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-25 18:57:19 -0600

    test/Add testing for reviews section to make sure content is present

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Movie Details Page', type: :feature do
+  describe '#buttons' do
     it "has button to create a viewing party" do
       user = User.create!(name: "Nancy", email: "nancydrew@email.com")
       visit user_movie_path(user.id, 505)
@@ -14,4 +15,25 @@ RSpec.describe 'Movie Details Page', type: :feature do
       click_button "Return to Discover Page"
       expect(current_path).to eq(user_discover_index_path(user.id)) 
     end
+  end
+
+  describe '#reviews' do
+    it 'displays number of reviews and the authors' do
+      user = User.create!(name: "Nancy", email: "nancydrew@email.com")
+
+      visit user_movie_path(user.id, 500)
+
+      expect(page).to have_content("2 Reviews")
+
+      within "#reviews-0" do
+        expect(page).to have_content("Author: talisencrw")
+        expect(page).to_not have_content("Author: Wuchak")
+      end
+
+      within "#reviews-1" do
+        expect(page).to have_content("Author: Wuchak")
+        expect(page).to_not have_content("Author: talisencrw")
+      end
+    end
+  end
 end
