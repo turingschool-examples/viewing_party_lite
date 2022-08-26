5cc79a9c1662c51fc31b3c9e35daf66810854d0a
Bryce Simonds <bsimonds14@gmail.com>
2022-08-25 18:26:47 -0600

    Test: has a search field and can find movies matching the keyword

--- spec/features/discover_spec.rb
+++ spec/features/discover_spec.rb
@@ -15,5 +15,18 @@ RSpec.describe 'Discover Dashboard' do
       
       expect(page).to have_link('The Shawshank Redemption')
     end
+
+    it 'has a search field that brings you to a list of movies matching the search', :vcr do
+      user = User.create!(name: Faker::Name.first_name, email: Faker::Internet.email)
+      visit "/users/#{user.id}/discover"
+    
+      expect(page).to have_button('Find Movies')
+      expect(page).to_not have_link('The Shawshank Redemption')
+
+      fill_in 'Find Movies', with: "Shawshank"
+      click_on("Find Movies")
+
+      expect(page).to have_link('The Shawshank Redemption')
+    end
   end
 end 
