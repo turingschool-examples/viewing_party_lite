aa9adf2a147c28729ba251f4f7fa66f5248aa9a9
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-26 11:34:16 -0600

    Robustify tests fix date

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -30,16 +30,16 @@ RSpec.describe 'the User show page' do
     visit new_user_movie_viewing_party_path(user1, 278)
 
     fill_in 'Duration of Party:', with: 180
-    fill_in 'Date', with: Date.today + 1
-    fill_in 'Start Time', with: '6:00 PM'
+    fill_in 'Date', with: Date.tomorrow
+    fill_in 'Start Time', with: Time.now + 600
 
     check("attendees_ids_#{user2.id}")
 
     click_button 'Create Party'
 
     expect(page).to have_content('The Shawshank Redemption')
-    expect(page).to have_content(Date.today + 1)
-    # expect(page).to have_content(Time.strftime('%1:%M %p'))
+    expect(page).to have_content(Date.tomorrow.strftime('%B %e, %Y'))
+    # expect(page).to have_content(Time.strftime('%I:%M %p'))
     expect(page).to have_content('Hosting')
   end
 
@@ -51,8 +51,8 @@ RSpec.describe 'the User show page' do
     visit new_user_movie_viewing_party_path(user2, 238)
 
     fill_in 'Duration of Party:', with: 180
-    fill_in 'Date', with: Date.today + 1
-    fill_in 'Start Time', with: '6:00 PM'
+    fill_in 'Date', with: Date.tomorrow
+    fill_in 'Start Time', with: Time.now + 600
 
     check("attendees_ids_#{user1.id}")
 
@@ -61,8 +61,8 @@ RSpec.describe 'the User show page' do
     visit user_path(user1)
 
     expect(page).to have_content('The Godfather')
-    expect(page).to have_content(Date.today + 1)
-    # expect(page).to have_content(Time.strftime('%1:%M %p'))
+    expect(page).to have_content(Date.tomorrow.strftime('%B %e, %Y'))
+    # expect(page).to have_content(Time.strftime('%I:%M %p'))
     expect(page).to have_content('Invited')
   end
 end
