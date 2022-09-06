605babcde25439872487a1690336a34ad29113d1
Alex Boyd <alexanderleeboyd@gmail.com>
2022-09-06 14:11:12 -0400

    Test: password_digest validations

--- db/migrate/20220822220536_create_users.rb
+++ db/migrate/20220822220536_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email, unique: true 
+      t.string :email, unique: true
+      t.string :password_digest
 
       t.timestamps 
     end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_08_22_221412) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
