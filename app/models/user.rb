e30970c651f0e48030ccec124b66754028f43d10
Cory Bethune <corybbethune@gmail.com>
2022-09-07 01:12:16 -0700

    Update user model with password attribute. update model test. Test passes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true, uniqueness: true
+
+  has_secure_password
 
   def self.all_emails
     emails = []
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,16 +11,23 @@ RSpec.describe User, :type => :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:name) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest)}
+    it { should have_secure_password }
   end
 
   before(:each) do
-    @user1 = User.create!(name: "Cory", email: 'Cory@gmail.com')
-    @user1 = User.create!(name: "John", email: 'John@gmail.com')
+    @user1 = User.create!(name: "Cory", email: 'Cory@gmail.com', password_digest: 'abc')
+    @user1 = User.create!(name: "John", email: 'John@gmail.com', password_digest: '123')
 
     party1 = Party.create!(date: Date.today, start_time: '20:00:00', movie_id: 278)
     party2 = Party.create!(date: Date.today, start_time: '10:00:00', movie_id: 278)
     @up1 = @user1.user_parties.create!(party_id: party1.id, host: true)
     @up2 = @user1.user_parties.create!(party_id: party2.id, host: false)
+  end
+
+  it "creates an instance of the user" do
+    expect(@user1).to_not have_attribute(:password)
+    expect(@user1.password_digest).to_not eq('abc')
   end
 
   describe 'class methods' do
