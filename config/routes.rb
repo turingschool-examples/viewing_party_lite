3e100bf4108d13a6d08d1704bd07cd806e19e256
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-08 09:06:12 -0600

    separate routes for readability

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,11 @@ Rails.application.routes.draw do
 
   get '/login', to: 'users#login_form'
   post '/login', to: 'users#login_user'
-  get '/special', to: 'special#index'
+  get '/help', to: 'help#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
   get '/users/:id/discover', to: 'users#discover'
