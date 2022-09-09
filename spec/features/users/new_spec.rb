1e936d06cf6254f885f2b2d89dd966f2eefa23f3
Shirley DeCesari <shirley.decesari@gmail.com>
2022-09-08 17:46:55 -0700

    Complete registration sad path #2

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -3,8 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe 'new user registration page', type: :feature do
-  it 'has a form to create a new user then sends user to show page' do
+  before :each do 
+    @user = User.create!(name: 'Bob', email: 'bob@turing.edu', password: 'test123')
     visit register_path
+  end 
+  
+  it 'has a form to create a new user then sends user to show page' do
     fill_in :name, with: 'Sally'
     fill_in :email, with: 'sally@turing.com'
     fill_in :password, with: 'test123'
@@ -14,4 +18,26 @@ RSpec.describe 'new user registration page', type: :feature do
 
     expect(current_path).to eq(user_path(User.last.id))
   end
+
+  it 'has a unique email' do 
+    fill_in :name, with: 'Sarah'
+    fill_in :email, with: 'bob@turing.edu'
+    fill_in :password, with: 'test123'
+    fill_in :password_confirmation, with: 'test123'
+    click_button 'Register New User'
+
+    expect(page).to have_current_path(register_path)
+    expect(page).to have_content("Email or password are invalid")
+  end 
+
+  it 'has a unique password' do 
+    fill_in :name, with: 'Alex'
+    fill_in :email, with: 'alex@turing.edu'
+    fill_in :password, with: 'test123'
+    fill_in :password_confirmation, with: 'test234'
+    click_button 'Register New User'
+
+    expect(page).to have_current_path(register_path)
+    expect(page).to have_content("Email or password are invalid")
+  end 
 end
