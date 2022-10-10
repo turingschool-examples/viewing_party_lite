f9350c0e4b414980f4e6d2dd5b976dcf67b4f684
dominicod <dominicodonnell99@gmail.com>
2022-10-10 17:56:46 -0500

    Test: add model validations to test-suite

--- spec/models/user_viewing_party_spec.rb
+++ spec/models/user_viewing_party_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe UserViewingParty, type: :model do
     it { should belong_to :user }
     it { should belong_to :viewing_party }
   end
+  describe 'Validations' do
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :viewing_party_id }
+  end
 end
--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe ViewingParty, type: :model do
   describe 'Relationships' do
     it { should have_many :user_viewing_parties }
   end
+  describe 'Validations' do
+    it { should validate_presence_of :duration }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :movie_id }
+  end
 end
