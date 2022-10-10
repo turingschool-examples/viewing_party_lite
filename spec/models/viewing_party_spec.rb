418199ef9b9f6aa214ebfc03694753af11a32123
dominicod <dominicodonnell99@gmail.com>
2022-10-10 17:53:48 -0500

    Test: create tests for relationships

---
+++ spec/models/viewing_party_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ViewingParty, type: :model do
+  describe 'Relationships' do
+    it { should have_many :user_viewing_parties }
+  end
+end
