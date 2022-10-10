652b3aa9c3079691720f61e5159248ac16baed8f
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-10 17:05:53 -0600

    refactor validations

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,7 +1,5 @@
 class Party < ApplicationRecord
   has_many :user_parties
   has_many :users, through: :user_parties
-  validates :start_time, presence: true
-  validates :date, presence: true
-  validates :duration, presence: true
+  validates :start_time, :date, :duration, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, :email, presence: true
 end
 
