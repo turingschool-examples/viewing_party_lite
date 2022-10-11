7be955c9c0a30246bf626faed468baa044a1b15e
dominicod <dominicodonnell99@gmail.com>
2022-10-11 15:38:30 -0500

    Feat: add url_path to index.html, Test: add url_path's to test suite

--- spec/features/landing_page/index_spec.rb
+++ spec/features/landing_page/index_spec.rb
@@ -5,6 +5,9 @@ require 'rails_helper'
 RSpec.describe 'Landing Page | Index', type: :feature do
   describe 'As a user when I visit the Landing Page,' do
     before(:each) { visit root_path }
+    let(:user1) { User.create!(name: 'jojo binks', email: 'jojo_binks@gmail.com') }
+    let(:user2) { User.create!(name: 'bobby', email: 'bobby@yahoo.com') }
+    let(:user3) { User.create!(name: 'marissa nicole', email: 'marissa.nicole99@gmail.com') }
     it 'I see the title of the Applications' do
       within('#application_title') do
         expect(page).to have_content 'Viewing Party'
@@ -12,29 +15,27 @@ RSpec.describe 'Landing Page | Index', type: :feature do
     end
     it 'I see a button to create a new user' do
       within('#user_creation') do
-        expect(page).to have_button '#create_user' # , href:=>"creation_path")
-        # TODO: Create expectation clause to ensure create user button links to creation path
+        expect(page).to have_button('#create_user', href: new_user_path)
       end
     end
     it 'I see a list of existing users which links to each users dashboard' do
       within('#current_users') do
         within('#user-1') do
-          expect(page).to have_link("jojo_binks@gmail.com's dashboard") # , href:=>"user_dashboard_path")
-          expect(page).to_not have_link("bobby@yahoo.com's dashboard") # , href:=>"user_dashboard_path")
-          expect(page).to_not have_link("marissa.nicole99@gmail.com's dashboard") # , href:=>"user_dashboard_path")
+          expect(page).to have_link("#{user1.email}'s dashboard", href: user_path(user1.id))
+          expect(page).to_not have_link("#{user2.email}'s dashboard", href: user_path(user2.id))
+          expect(page).to_not have_link("#{user3.email}'s dashboard", href: user_path(user3.id))
         end
         within('#user-2') do
-          expect(page).to have_link("bobby@yahoo.com's dashboard") # , href:=>"user_dashboard_path")
-          expect(page).to_not have_link("jojo_binks@gmail.com's dashboard") # , href:=>"user_dashboard_path")
-          expect(page).to_not have_link("marissa.nicole99@gmail.com's dashboard") # , href:=>"user_dashboard_path")
+          expect(page).to have_link("#{user2.email}'s dashboard", href: user_path(user2.id))
+          expect(page).to_not have_link("#{user1.email}'s dashboard", href: user_path(user1.id))
+          expect(page).to_not have_link("#{user3.email}'s dashboard", href: user_path(user3.id))
         end
         within('#user-3') do
-          expect(page).to have_link("marissa.nicole99@gmail.com's dashboard") # , href:=>"user_dashboard_path")
-          expect(page).to_not have_link("jojo_binks@gmail.com's dashboard") # , href:=>"user_dashboard_path")
-          expect(page).to_not have_link("bobby@yahoo.com's dashboard") # , href:=>"user_dashboard_path")
+          expect(page).to have_link("#{user3.email}'s dashboard", href: user_path(user3.id))
+          expect(page).to_not have_link("#{user1.email}'s dashboard", href: user_path(user1.id))
+          expect(page).to_not have_link("#{user2.email}'s dashboard", href: user_path(user2.id))
         end
       end
-      # TODO: Create expectation clause to ensure user dashboard links to the correct user dashboard path
     end
     it 'I see a link back to the home page' do
       within('#home_page_link') do
