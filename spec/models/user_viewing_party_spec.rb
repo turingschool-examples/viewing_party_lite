8bda0a4e31ca1aa83a9671ebbe8f3bc4c5a677d6
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-10-11 13:49:26 -0600

    Create model specs for user, user_viewing_party, viewing_party

---
+++ spec/models/user_viewing_party_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe UserViewingParty, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:user) }
+    it { should belong_to(:viewing_parties) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:viewing_party_id) }
+    it { should validate_presence_of(:role) }
+  end
+end
