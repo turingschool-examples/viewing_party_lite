e6a922f917fdf1df31231c14105858fd687de4e7
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-10 20:18:42 -0600

    test: add first test for landing page.

---
+++ spec/features/landing_page/landing_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Landing Page" do
+    describe "As a user when I visit the Landing Page" do
+        it 'I see the header for the page' do
+            visit root_path
+            
+            expect(page).to have_content("Viewing Party Light")
+        end
+    end
+end
