8f2d987fd2a14f0e8423f9144a4dc7d749929b6f
Thomas Musselman <tmusselman@gmail.com>
2022-10-11 17:11:52 -0600

    Testing for sad path validations

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -15,5 +15,30 @@ RSpec.describe "New User Page", type: :feature do
     expect(current_path).to eq(user_path(benji.id))
   end
 
-  
+  it 'validates uniqueness of email address' do
+    user = create(:user, email: "user@gmail.com")
+    fill_in :user_name, with: "Benji"
+    fill_in :user_email, with: "user@gmail.com"
+    click_button "Register"
+
+    expect(page).to have_content("Email has already been taken")
+    expect(User.last).to eq(user)
+  end
+
+  it 'validates presence of name and email' do
+    click_button "Register"
+
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Email can't be blank")
+    expect(User.last).to be nil
+  end
+
+  it 'validates format of email address' do
+    fill_in :user_name, with: "Benji"
+    fill_in :user_email, with: "user.gmail.com"
+    click_button "Register"
+
+    expect(page).to have_content("Email is not formatted correctly")
+    expect(User.last).to be nil
+  end
 end
