ce0b9596bc41b45a73768b75c9827a9fcda1a6a1
Erik Riggs <riggs.erik@gmail.com>
2022-10-11 17:07:50 -0600

    feat: completed dashboard user story
    co-authored-by Erik riggs.erik@gmail.com Sandy sandyymari3@gmail.com

--- spec/factories/factory_bot.rb
+++ spec/factories/factory_bot.rb
@@ -3,4 +3,17 @@ FactoryBot.define do
     name {Faker::Name.name}
     email {Faker::Internet.email}
   end
+
+  factory :party, class: Party do
+    date {Faker::Date.between(from: Date.today, to: 1.year.from_now)}
+    duration {Faker::Number.within(range: 20..200)}
+    movie_id {Faker::Number.within(range: 1..1000)}
+    start_time {Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :afternoon)}
+  end
+
+  factory :user_parties, class: UserParty do
+    association :user, factory: :user
+    association :party, factory: :party
+  end
+
 end
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,7 +3,20 @@ require 'rails_helper'
 RSpec.describe 'User Dashboard Page' do
   describe 'user visits the dashboard page' do
     before :each do
-      @users_1 = create_list(:user, 20)
+      @user_1 = create(:user)
+      @user_2 = create(:user)
+
+      @party_1 = create(:party)
+      @party_2 = create(:party)
+      @party_3 = create(:party)
+      @party_4 = create(:party)
+      @party_5 = create(:party)
+
+      create(:user_parties, user_id: @user_1.id, party_id: @party_1.id)
+      create(:user_parties, user_id: @user_1.id, party_id: @party_2.id)
+      create(:user_parties, user_id: @user_1.id, party_id: @party_3.id)
+      create(:user_parties, user_id: @user_2.id, party_id: @party_4.id)
+      create(:user_parties, user_id: @user_2.id, party_id: @party_5.id)
     end
 
 #     "<user's name>'s Dashboard" at the top of the page
@@ -11,17 +24,44 @@ RSpec.describe 'User Dashboard Page' do
 # A section that lists viewing parties**
 
     it 'shows users name at the top of the page' do
-      visit user_path(@users_1[0])
-      expect(page).to have_content("#{@users_1[0].name}'s Dashboard")
+      visit user_path(@user_1)
+      expect(page).to have_content("#{@user_1.name}'s Dashboard")
 
-      visit user_path(@users_1[2])
-      expect(page).to have_content("#{@users_1[2].name}'s Dashboard")
+      visit user_path(@user_2)
+      expect(page).to have_content("#{@user_2.name}'s Dashboard")
     end
 
     it 'has a button to discover movies' do
-      visit user_path(@users_1[0])
+      visit user_path(@user_1)
       click_button "Discover Movies"
-      expect(current_path).to eq(user_discover_path(@users_1[0]))
-    end 
+      expect(current_path).to eq(user_discover_path(@user_1))
+
+      visit user_path(@user_2)
+      click_button "Discover Movies"
+      expect(current_path).to eq(user_discover_path(@user_2))
+    end
+
+    it 'has a section for users current viewing parties' do
+      visit user_path(@user_1)
+
+      within("#party-#{@party_1.id}") do
+        expect(page).to have_content(@party_1.id)
+      end
+      within("#party-#{@party_2.id}") do
+        expect(page).to have_content(@party_2.id)
+      end
+      within("#party-#{@party_3.id}") do
+        expect(page).to have_content(@party_3.id)
+      end
+
+      visit user_path(@user_2)
+
+      within("#party-#{@party_4.id}") do
+        expect(page).to have_content(@party_4.id)
+      end
+      within("#party-#{@party_5.id}") do
+        expect(page).to have_content(@party_5.id)
+      end
+    end
   end
 end
