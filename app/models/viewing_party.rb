c464845b22a731e963d29b85be1cf03386dd39bd
Nikky Rojas <rojas7313@gmail.com>
2022-10-11 08:30:06 -0600

    added relationship and validations

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,5 +1,6 @@
 class ViewingParty < ApplicationRecord
   has_many :user_viewing_parties
+  has_many :users, through: :user_viewing_parties
   validates_presence_of :duration,
                         :date,
                         :movie_id
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Relationships' do
     it { should have_many :user_viewing_parties }
+    it { should have_many(:viewing_parties).through(:user_viewing_parties) }
   end
   describe 'Validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :password_digest }
     it { should validate_presence_of :email }
+    it {should validate_uniqueness_of :email }
   end
 end
