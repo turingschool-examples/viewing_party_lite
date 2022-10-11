6e32d635ea4c666f434b31daa7db38997405ee26
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-11 00:29:36 -0600

    add functionality to create a new user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def show
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      flash.alert = @user.errors.full_messages.to_sentence
+      redirect_to ('/register')
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
