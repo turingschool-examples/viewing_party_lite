58aecec0c9e3afcdc38a6d963ce16d60ab20f9bc
Anna Marie Sterling <sterling.am@gmail.com>
2022-10-11 14:00:10 -0600

    User show page content

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
 
-  resources :users, only: [:new]
+  resources :users, only: [:new] do
+    get '/dashboard', to: 'dashboard#show', as: :dashboard
+    get '/discover', to: 'discover#index', as: :discover
+  end
 end
