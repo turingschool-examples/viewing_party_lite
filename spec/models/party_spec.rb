6cca187a2064f8655ca41a856b8c8d4c353f0420
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-11 12:21:15 -0600

    test: added model tests

---
+++ spec/models/party_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Party, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :duration }
+    it { should validate_presence_of :start_time }
+  end
+  describe 'relationships' do
+    it { should have_many(:user_parties) }
+    it { should have_many(:users).through(:user_parties) }
+  end
+end
