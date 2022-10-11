7a5a80753a5da58425529582e32f0351cf8f5c9e
Astrid Hecht <silentaviary@gmail.com>
2022-10-11 16:30:11 -0600

    feat: Landing page complete, 2 tests fail becasue of missing features, but should fix itself when added

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'When I visit the landing page', type: :feature do
+  let!(:erin) { User.create!(name: 'Erin', email: 'epintozzi@turing.edu') }
+  let!(:mike) { User.create!(name: 'Mike', email: 'mike@turing.edu') }
+  let!(:meg) { User.create!(name: 'Meg', email: 'mstang@turing.edu') }
+  before(:each) {visit '/'}
+
   describe ' the header content' do
     it 'has a link "Home" to landing page' do
-      within 'header' do
+      save_and_open_page
+      within '#nav' do
         expect(page).to have_link('Home', href: '/')
       end
     end
   end
 
   describe 'the page content' do
-    let(:erin) { User.create!(name: 'Erin', email: 'epintozzi@turing.edu') }
-    let(:mike) { User.create!(name: 'Mike', email: 'mike@turing.edu') }
-    let(:meg) { User.create!(name: 'Meg', email: 'mstang@turing.edu') }
 
     it 'has a button to create new user' do
-      expect(page).to have_button('Create a New User', href: '/register')
+      expect(page).to have_button('Create a New User')
       click_button
       expect(current_path).to eq('/register')
     end
