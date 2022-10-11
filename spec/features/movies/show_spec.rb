b93ad5133152958f9831f96e01eb18ea0e17b01b
Candace Eckels <candace.eckels@gmail.com>
2022-10-11 13:33:02 -0600

    Mock for movie show created

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -3,10 +3,11 @@ require 'faker'
 
 RSpec.describe "Movie Show Page" do
     describe "As a user when I visit the movie show page" do
-        xit 'has a header for the movie title' do
+        it 'has a header for the movie title' do
             @alex = User.create!(user_name: "Alex", email: Faker::Internet.email , password_digest: Faker::Internet.password)
-            # @inglorious_bastards = double(id: 67, title: "Inglorious Bastards")
-            visit user_movie_path(@alex, @inglorious_bastards)
+            @inglorious_bastards = double( id: 1, title: "Inglorious Bastards")
+            
+            visit user_movie_path(@alex, @inglorious_bastards.id)
 
             expect(page).to have_content("Inglorious Bastards")
         end
