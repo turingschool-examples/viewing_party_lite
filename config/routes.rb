ca4601006c96c77d026f1182f0e160b27402a959
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-11 15:10:02 -0600

    test: added test for registration story

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'landings#index'
-  resources :users, only: [:new, :show]
+
+  # get '/', to: 'landings#index' is the same?
+  root 'landings#index'
+
+  resources :users, only: [:show, :create], path: 'dashboard'
+  resources :users, only: :new, path: 'register'
 end
