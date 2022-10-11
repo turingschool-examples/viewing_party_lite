d20504f2b1c44e8e9fb72284bc8f4c043b2386d9
Riley McCullough <rileybmccullough@gmail.com>
2022-10-10 18:10:02 -0600

    Create landing page and controller

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2022_10_10_225705) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
