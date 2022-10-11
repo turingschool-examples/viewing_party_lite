bd9a47c0bd36ddf2df6ef260e5a6e4c86277f2b7
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-11 00:39:48 -0600

    refactor: uncomment and unskip tests for issue #5 and correct formating for all pages per Rubocop

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "User Registration Page" do
-  describe "As a user" do
+RSpec.describe 'User Registration Page' do
+  describe 'As a user' do
     describe "When a user visits the '/register' path" do
       it 'They should see a form to register. The form should include: Name, Email, Register Button, once the user registers they should be taken to a dashboard page /users/:id' do
         visit '/register'
 
-        fill_in "Name", with: "Melissa"
-        fill_in "Email", with: "melissa.j@aol.com"
-        click_button "Register"
+        fill_in 'Name', with: 'Melissa'
+        fill_in 'Email', with: 'melissa.j@aol.com'
+        click_button 'Register'
 
         expect(current_path).to eq(user_path(User.find_by(name: 'Melissa')))
       end
@@ -16,16 +16,15 @@ RSpec.describe "User Registration Page" do
       it 'The email must be unique' do
         visit '/register'
 
-        User.create!(name:'Janny', email: 'Jane.doe@aol.com')
+        User.create!(name: 'Janny', email: 'Jane.doe@aol.com')
 
-        fill_in "Name", with: "Jane"
-        fill_in "Email", with: "Jane.doe@aol.com"
-        click_button "Register"
+        fill_in 'Name', with: 'Jane'
+        fill_in 'Email', with: 'Jane.doe@aol.com'
+        click_button 'Register'
 
         expect(current_path).to eq('/register')
         expect(page).to have_content('Email has already been taken')
       end
-
     end
   end
 end
