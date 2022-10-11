775cc3a978132e6940c75638fdfd46409f3b2fd3
Astrid Hecht <silentaviary@gmail.com>
2022-10-11 16:09:41 -0600

    test: landing page feature tests complete

---
+++ spec/features/layout_spec.rb
@@ -0,0 +1,19 @@
+# require 'rails_helper'
+
+# RSpec.describe 'application layout', type: :feature do
+#   describe 'header content' do
+#     let(:routes_info) { descibed_class.routes }
+#     let(:routes_get) { routes_info.select { |r| r.verb == 'GET' } }
+
+#     it 'has a link "Home" back to landing page' do
+#       alex = User.create!(name: 'Alex', email: 'alex@turing.edu')
+
+#       routes_get.each do |path|
+#         visit path
+#         within 'header' do
+#           expect(page).to have_link('Home', href: '/')
+#         end
+#       end
+#     end
+#   end
+# end
