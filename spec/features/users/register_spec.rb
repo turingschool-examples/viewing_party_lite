6dfda46971fa6d6fbbf9104bc6b191c088c0cb31
Ken Lenhart <omnipaid@gmail.com>
2022-10-11 15:09:12 -0700

    Feat/Test: Add users_controller.rb, users/new.html.erb, show.html.erb, flash message testing

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'User Registration page' do 
+RSpec.describe 'User Registration page' do
+  before :each do
+    @user_1 = create(:user)
+    @user_2 = create(:user)
+  end
   # When a user visits the '/register' path they should see a form to register.
 
   # The form should include:
@@ -12,11 +16,34 @@ RSpec.describe 'User Registration page' do
   
   describe 'User Registration Page #5' do
     it 'When a user visits the register path they should see a form to register.' do
+      visit register_path
       
+      fill_in 'name', with: 'James Dean'
+      fill_in 'email', with: 'jimmydean1979@goodinternet.net'
+
+      click_on 'Save'
+
+      user = User.last 
+      expect(current_path).to eq(user_path(user))
     end
 
-    it 'Once the user registers they should be taken to a dashboard page /users/:id, where :id is the id for the user that was just created' do
+    it 'has happy path' do
+      visit register_path
 
+      fill_in 'name', with: 'James Dean'
+      fill_in 'email', with: 'jimmydean1979@goodinternet.net'
+
+      click_on 'Save'
+
+      expect(page).to have_content("User Registered Successfully")
+    end
+
+    it 'has sad path' do
+      visit register_path
+
+      click_on 'Save'
+
+      expect(page).to have_content("Name can't be blank and Email can't be blank")
     end
   end
 end
