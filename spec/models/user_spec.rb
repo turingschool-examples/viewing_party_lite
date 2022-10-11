9f5231cbce4022da9f9a70511934080335962d0b
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-10 23:53:33 -0600

    test: tests for user registeration form and user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe User, type: :model do
       it { should have_many(:user_parties) }
       it { should have_many(:parties).through(:user_parties) }
    end
+
+   describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+   end
 end
