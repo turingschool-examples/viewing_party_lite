fea9ca6a60b624baa58600d4884c65f958945d23
Carter Ball <carter.k.ball@gmail.com>
2022-10-11 16:05:13 -0400

    Basic model spec setup

---
+++ spec/models/user_party_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe UserParty, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :party_id }
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :user_status }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :user}
+    it { should belong_to :party}
+  end
+
+end
