89876843805c89354e9ef19d83ed476c765f944d
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-10-11 14:01:45 -0600

    Create model validations and relationships

--- spec/models/user_viewing_party_spec.rb
+++ spec/models/user_viewing_party_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe UserViewingParty, type: :model do
   describe 'relationships' do
     it { should belong_to(:user) }
-    it { should belong_to(:viewing_parties) }
+    it { should belong_to(:viewing_party) }
   end
 
   describe 'validations' do
