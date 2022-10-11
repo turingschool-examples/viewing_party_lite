64ce85929219950102955e903f712481ac144c06
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-10 18:08:15 -0600

    passing first test

--- spec/features/users/welcome_spec.rb
+++ spec/features/users/welcome_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe 'application welcome page', type: :feature do
     describe 'When I visit the root path/welcome page' do
 
       it 'I can see the application titlem  a button to create a new user, & a link to go back to the landing page' do
-        #button should link to  '/register' 
 
         visit root_path
         expect(page).to have_content('Viewing Party Lite')
@@ -16,7 +15,7 @@ RSpec.describe 'application welcome page', type: :feature do
         expect(current_path).to eq(root_path)
       end
 
-      it 'I can also see a list of existing users which links to the user dashboard & ' do
+      xit 'I can also see a list of existing users which links to the user dashboard & ' do
         visit root_path
         #make users
         expect(page).to have_content('alaina.movies@gmail.com')
@@ -25,7 +24,7 @@ RSpec.describe 'application welcome page', type: :feature do
         expect(page).to have_content('alyssa.movies@gmail.com')
       end
 
-      it 'The register a new user button should lead to the registration page' do
+      xit 'The register a new user button should lead to the registration page' do
         visit root_path
         click_on('New User')
         expect(current_path).to eq('/registration')
