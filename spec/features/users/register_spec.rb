749e032c5698484ac54a726ae9905627264d9840
Ken Lenhart <omnipaid@gmail.com>
2022-10-11 12:17:28 -0700

    test: add test for user registration page story

---
+++ spec/features/users/register_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'User Registration page' do 
+  # When a user visits the '/register' path they should see a form to register.
+
+  # The form should include:
+  
+  # Name
+  # Email (must be unique)
+  # Register Button
+  # Once the user registers they should be taken to a dashboard page '/users/:id', where :id is the id for the user that was just created.
+  
+  describe 'User Registration Page #5' do
+    it 'When a user visits the register path they should see a form to register.' do
+      
+    end
+
+    it 'Once the user registers they should be taken to a dashboard page /users/:id, where :id is the id for the user that was just created' do
+
+    end
+  end
+end
