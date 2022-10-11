457304a9788cc0b3fd78b681d02c5cf764b287ba
Sid Mann <sjmann2@outlook.com>
2022-10-11 16:13:22 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#landing'
   get '/register', to:'users#new'
-  resources :users, only: [:index, :show, :create] do
-    resources 'discover', only: [:index]
-    resources :movies, only: [:show] do
+  resources :users, only: [:index, :show, :create], module: 'users' do
+    post '/movies_top_rated', to: 'movies#top_rated'
+    post '/movies_search', to: 'movies#search'
+    resources :discover, only: [:index]
+    resources :movies, only: [:index, :show] do
       resources :viewing_parties, only: [:new, :show]
     end
   end
