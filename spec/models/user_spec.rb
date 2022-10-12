b06b1ff7932638d71f90cb5c4db9ff9620d2a9fc
dominicod <dominicodonnell99@gmail.com>
2022-10-12 15:56:13 -0500

    Test: create test for model method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
   end
+  describe 'Class Methods' do
+    describe '.other_users' do
+      it 'returns all other users given the argument of user obj' do
+        user1 = User.create!(name: 'jojo binks', email: 'jojo_binks@gmail.com'))
+        user2 = User.create!(name: 'bobby', email: 'bobby@yahoo.com')
+        user3 = User.create!(name: 'marissa nicole', email: 'marissa.nicole99@gmail.com')
+        expect(User.other_users(user1)).to eq [user2, user3]
+      end
+    end
+  end
 end
