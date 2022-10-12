b334df55084e225a0ab3692895b38c6a110e7985
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-11 21:11:06 -0600

    test: add tests for register new user page

--- spec/features/register/new_spec.rb
+++ spec/features/register/new_spec.rb
@@ -7,19 +7,29 @@ RSpec.describe 'The register new user page' do
 
   describe 'As a user when I visit this page' do
     it 'I see a Register New User header' do
-
+        expect(page).to have_content("Register a New User")
     end
 
     it 'I see a form to register a new user' do
+        expect(page).to have_field(:user_name)
+        expect(page).to have_field(:email)
 
+        expect(page).to have_content("Name:")
+        expect(page).to have_content("Email:")
     end
 
-    it 'when I fill out the registration correctly I can click the register button' do
-
+    it 'after I fill out the registration I then click the register button' do
+        fill_in "Name:", with: "Kat"
+        fill_in "Email:", with: "kit.kat@guhmail.com"
+        click_button "Register"
     end
 
     it 'when I click the register button I am redirected to the dashboard page' do
-        
+        fill_in "Name:", with: "Kat"
+        fill_in "Email:", with: "kit.kat@guhmail.com"
+        click_button "Register"
+
+        expect(current_path).to eq()
     end
   end
 end
