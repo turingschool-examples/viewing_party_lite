9cda2d4a3365da5cbd35b9fca32728ecc49750ec
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-11 20:53:19 -0600

    add route for register page and write skeleton tests for new registration

---
+++ spec/features/register/new_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'The register new user page' do
+  before(:each) do
+    visit register_path
+  end
+
+  describe 'As a user when I visit this page' do
+    it 'I see a Register New User header' do
+
+    end
+
+    it 'I see a form to register a new user' do
+
+    end
+
+    it 'when I fill out the registration correctly I can click the register button' do
+
+    end
+
+    it 'when I click the register button I am redirected to the dashboard page' do
+        
+    end
+  end
+end
