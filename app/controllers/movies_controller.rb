a0d45a5ef117486776b1a47e8c384d36c11c43d8
Thomas Hayes <hayes.thomas610@gmail.com>
2022-10-12 14:21:58 -0600

    build out movies index method and show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,15 @@
 class MoviesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.params['api_key'] = ENV['movie_api_key']
+    end
+    response = conn.get('3/movie/top_rated')
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    @top_rated_movies = data[:results]
+
     if params[:search] == ''
       redirect_to user_discover_index_path(@user), notice: 'Search field cannot be blank'
     end
