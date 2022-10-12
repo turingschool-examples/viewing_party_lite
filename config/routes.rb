19cfab31d625c78f865a02a6414872cea0b5641c
Candace Eckels <candace.eckels@gmail.com>
2022-10-12 10:48:57 -0600

    Navigation bar complete with appropriate link

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "landing#index"
 
+  get '/about', to: 'landing#show'
+
   resources :users, only: [:show] do
     resources :movies, only: [:show]
     get "/discover", to: "discover#search"
--- spec/features/landing_page/landing_spec.rb
+++ spec/features/landing_page/landing_spec.rb
@@ -7,5 +7,32 @@ RSpec.describe "Landing Page" do
             
             expect(page).to have_content("Viewing Party Light")
         end
+
+        describe 'Navigation Bar' do
+            it 'should have a navigation bar' do
+                visit root_path
+
+                expect(page).to have_css('.topnav', text: 'Home', visible: true)
+                expect(page).to have_css('.topnav', text: 'About', visible: true)
+                expect(page).to have_css('.topnav', text: 'Viewing Party Light', visible: true)
+            end
+
+            it 'home button should navigate to landing page' do
+                visit root_path
+
+                # save_and_open_page
+                click_link 'Home'
+                expect(current_path).to eq root_path
+            end
+
+            it 'about button should navigate to about page' do
+                visit root_path
+
+                click_link 'About'
+
+                expect(current_path).to eq about_path
+            end
+
+        end
     end
 end
