ee63eb1a310a22cb83a277c924405c447e240260
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-12 16:06:00 -0600

    refactor: progress on user show page
    co-authored-by Erik riggs.erik@gmail.com Sandy sandyymari3@gmail.com

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-
+    # @users = User.all
     @movies = @user.parties.map do |party|
       MovieFacade.movie_details(party.movie_id)
     end
