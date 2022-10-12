948c0c098214ca5cba421c869f98094162216a90
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-11 21:37:21 -0600

    test: added tests for Viewing Parties user story

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -19,10 +19,6 @@ RSpec.describe 'User Dashboard Page' do
       create(:user_parties, user_id: @user_2.id, party_id: @party_5.id)
     end
 
-#     "<user's name>'s Dashboard" at the top of the page
-# A button to Discover Movies*
-# A section that lists viewing parties**
-
     it 'shows users name at the top of the page' do
       visit user_path(@user_1)
       expect(page).to have_content("#{@user_1.name}'s Dashboard")
@@ -62,6 +58,53 @@ RSpec.describe 'User Dashboard Page' do
       within("#party-#{@party_5.id}") do
         expect(page).to have_content(@party_5.id)
       end
+    end
+
+#     As a user,
+# When I visit a user dashboard,
+# I should see the viewing parties that the user has been invited to with the following details:
+
+# Movie Image
+# Movie Title, which links to the movie show page
+# Date and Time of Event
+# who is hosting the event
+# list of users invited, with my name in bold
+
+
+  it 'shows the viewing parties the user has been invited to with details' do
+    visit user_path(@user_1)
+    require 'pry'; binding.pry
+    # expect(page).to have_content(PICTUREHERE) #update with API
+    expect(page).to have_content(@party_1.movie_id) #change to name with API
+    expect(page).to have_content(@party_1.duration)
+    expect(page).to have_content("Host: #{@user_1.name}")
+    ##test for invitees
+
+    click_link "#{@party_1.movie_id}"
+
+    expect(current_path).to eq(movie_path(@party_1.movie_id)) #fix routing
+
+  end
+
+# I should also see the viewing parties that the user has created with the following details:
+
+# Movie Image
+# Movie Title, which links to the movie show page
+# Date and Time of Event
+# That I am the host of the party
+# List of friends invited to the viewing party
+    it 'shows the viewing parties the user has created with details' do
+          visit user_path(@user_2)
+        # expect(page).to have_content(PICTUREHERE) #update with API
+        expect(page).to have_content(@party_4.movie_id) #change to name with API
+        expect(page).to have_content(@party_4.duration)
+        expect(page).to have_content("Host: #{@user_2.name}") 
+        ##test for invitees
+
+        click_link "#{@party_4.movie_id}"
+
+        expect(current_path).to eq(movie_path(@party_4.movie_id)) #fix routing
+
     end
   end
 end
