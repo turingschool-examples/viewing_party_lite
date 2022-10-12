3efe8e039146d152b2806394992459afe8525a2a
Noah van Ekdom <noahvanekdom@gmail.com>
2022-10-12 05:56:38 -0600

    Add tests for users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
   resources :users, only: [:create, :show]
+
+  get '/register', to: 'users#new'
 end
---
+++ spec/features/users/create_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'User Registration' do
+  describe 'when a user visits /register' do
+    before(:each) do
+      visit '/register'
+    end
+
+    it 'they see a form to create a new user' do
+      expect(page).to have_content("We're excited to have you join us, please fill out this form!")
+    end
+
+    it 'the form includes fields for name and email' do
+      expect(page).to have_field(:name)
+      expect(page).to have_field(:email)
+    end
+
+    describe 'when I fill out that form with the correct information and hit submit' do
+      before(:each) do
+        fill_in "Name", with: "Jon Duttko"
+        fill_in "Email", with: "jduttko149@gmail.com"
+      end
+      it 'I am taken to the dashboard page for that user' do
+        click_button "Submit"
+        #expect(current_path).to eq(user_path(JOND))
+      end
+    end
+
+    describe 'when I do not fill out every field and click submit' do
+      it 'returns me to the new user form' do
+      end
+      it 'flashes a message saying all fields must contain information' do
+      end
+    end
+
+    describe 'when I add a non-unique email and click submit' do
+      it 'returns me to the new user form' do
+      end
+
+      it 'flashes a message that an email cannot be used twice' do
+      end
+    end
+  end
+end
