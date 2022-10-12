cdba3382fcaaf2cd9c96b1214b98a6db6a22ac37
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-10-12 07:16:30 -0600

    remove variables in MovieService.

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,6 +1,6 @@
 class MovieService
   def self.conn
-    conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
+    Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
       faraday.params['api_key'] = ENV['movie_api_key']
     end
   end
@@ -8,7 +8,7 @@ class MovieService
   def self.top_forty
     response_1 = conn.get('movie/top_rated?&page=1')
     response_2 = conn.get('movie/top_rated?&page=2')
-    output = combined_response(response_1, response_2)
+    combined_response(response_1, response_2)
   end
 
   def self.movie_search(keyword_string)
@@ -20,11 +20,11 @@ class MovieService
   def self.combined_response(response_1, response_2)
     page1 = JSON.parse(response_1.body, symbolize_names: true)[:results]
     page2 = JSON.parse(response_2.body, symbolize_names: true)[:results]
-    output = page1 + page2
+    page1 + page2
   end
 
   def self.movie_details(movie_id)
     response = conn.get("movie/#{movie_id}?&append_to_response=credits,reviews")
-    json = JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
