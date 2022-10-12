62ba35975209cd2004010155c601b609db0460ce
Thomas Hayes <hayes.thomas610@gmail.com>
2022-10-12 14:48:18 -0600

    add conditional view title for movies index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,14 +4,15 @@ class MoviesController < ApplicationController
     conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.params['api_key'] = ENV['movie_api_key']
     end
-    response = conn.get('3/movie/top_rated')
-
-    data = JSON.parse(response.body, symbolize_names: true)
-
-    @top_rated_movies = data[:results]
-
+    
     if params[:search] == ''
       redirect_to user_discover_index_path(@user), notice: 'Search field cannot be blank'
+    elsif !params[:search].nil?
+      response = conn.get("/3/search/movie?query=#{params[:search]}")
+    else
+      response = conn.get('3/movie/top_rated')
     end
+    data = JSON.parse(response.body, symbolize_names: true)
+    @movies = data[:results]
   end
 end
