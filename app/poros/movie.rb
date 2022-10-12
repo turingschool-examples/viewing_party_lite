7f2604172220ac8b0f8f234a90aba8179eefb650
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-12 12:53:19 -0600

    refactor: working on story and API consumption

---
+++ app/poros/movie.rb
@@ -0,0 +1,20 @@
+class Movie
+  attr_reader :id, 
+              :original_title, 
+              :runtime,
+              :vote_count,
+              :vote_average,
+              :overview,
+              :image
+
+    def initialize(data)
+      @id = data[:id]
+      @original_title = data[:original_title]
+      @runtime = data[:runtime]
+      @vote_count = data[:vote_count]
+      @vote_average = data[:vote_average]
+      @overview = data[:overview]
+      @image = data[:poster_path]
+    end
+  
+end
