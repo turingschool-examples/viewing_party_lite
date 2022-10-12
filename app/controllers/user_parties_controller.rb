8c3b781bc4311f1561b5d837c623769d8c41ff80
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-12 15:37:54 -0600

    Complete the New viewing Party Page

--- app/controllers/user_parties_controller.rb
+++ app/controllers/user_parties_controller.rb
@@ -9,5 +9,26 @@ class UserPartiesController < ApplicationController
   end
 
   def create
+    @user = User.find(params[:id])
+    @movie_id = params[:movie_id]
+    @party = Party.create!(party_params)
+    @movie_party = UserParty.new(user_id: @user.id, party_id: @party.id, is_host: true)
+    conn = Faraday.new(url: "https://api.themoviedb.org") 
+    response = conn.get("/3/movie/#{@movie_id}?api_key=#{ENV["movies_api_key"]}")
+    @movie_information  = JSON.parse(response.body, symbolize_names: true)
+    if @movie_information[:runtime] > @party.duration
+      redirect_to "/users/#{@user.id}/movies/#{@movie_id}/viewing-party/new"
+      flash[:alert] = "The duration can not be shorter than the run time of the movie, silly."
+    elsif @movie_party.save
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Something went wrong"
+      render :new
+    end
+  end
+
+  private
+  def party_params
+      params.permit(:id, :start_time, :duration, :date, :movie_id)
   end
 end
--- spec/features/user_parties/new_spec.rb
+++ spec/features/user_parties/new_spec.rb
@@ -33,8 +33,7 @@ RSpec.describe 'new user party page', type: :feature do
         end
       end
 
-      xit 'a viewing party should NOT be created if set to a value less than the duration of the movie', :vcr do
-
+      it "When the party is created, the user should be redirected back to the dashboard where the new event is shown. The event should also be listed on any other user's dashbaords that were also invited to the party.", :vcr do
         user1 = create(:user)
         user2 = create(:user)
         user3 = create(:user)
@@ -42,21 +41,21 @@ RSpec.describe 'new user party page', type: :feature do
         user5 = create(:user)
 
         visit "/users/#{user1.id}/movies/550/viewing-party/new"
-        fill_in(:duration, with: 2000)
-        fill_in(:date, with: "date")
-        fill_in(:start_time, with: "7pm")
+        fill_in(:duration, with: 1000)
+        fill_in(:date, with: "31/10/2022")
+
+        select "18", from: "[start_time(4i)]" #got this by inspecting the page
         within('#potential_invitees') do
-          click_on(user2)
-          click_on(user3)
-        end
-        #<div id='order_<%=order.id %>'> #change to doublequotes
-        #end the div before the iteration ends
-        expect(current_path).to eq("/users/#{user1.id}/movies/550/viewing-party/new")
-        expect(page).to have_content("error message details")
+          find(:css, "#invitees_#{user2.id}").set(true)
+          find(:css, "#invitees_#{user3.id}").set(true)
+          end
+        click_button('Create Party')
+
+        expect(current_path).to eq(user_path(user1))
+        expect(page).to have_content("Date: 2022-10-31")
       end
 
-
-      xit "When the party is created, the user should be redirected back to the dashboard where the new event is shown. The event should also be listed on any other user's dashbaords that were also invited to the party.", :vcr do
+      it 'a viewing party should NOT be created if set to a value less than the duration of the movie', :vcr do
         user1 = create(:user)
         user2 = create(:user)
         user3 = create(:user)
@@ -64,17 +63,18 @@ RSpec.describe 'new user party page', type: :feature do
         user5 = create(:user)
 
         visit "/users/#{user1.id}/movies/550/viewing-party/new"
-        fill_in(:duration, with: 2000)
-        fill_in(:date, with: "date")
-        fill_in(:start_time, with: "7pm")
+        fill_in(:duration, with: 1)
+        fill_in(:date, with: "31/10/2022")
+
+        select "18", from: "[start_time(4i)]" #got this by inspecting the page
         within('#potential_invitees') do
-            click_on(user2)
-            click_on(user3)
+          find(:css, "#invitees_#{user2.id}").set(true)
+          find(:css, "#invitees_#{user3.id}").set(true)
           end
-        expect(current_path).to eq(user_path(user1))
-        expect(page).to have_content("Fight Club")
-        visit user_path(user2)
-        expect(page).to have_content('Fight Club')
+        click_button('Create Party')
+
+        expect(current_path).to eq("/users/#{user1.id}/movies/550/viewing-party/new")
+        expect(page).to have_content("The duration can not be shorter than the run time of the movie, silly.")
       end
 
     end
