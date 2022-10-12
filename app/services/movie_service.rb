03b6cf1bdb9036fbb6062d2c94054104fa415f8f
Anna Marie Sterling <sterling.am@gmail.com>
2022-10-11 23:48:13 -0600

    Refactor movie service/movies controller index WIP

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -2,27 +2,25 @@ class MovieService
   def self.conn
     conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
       faraday.params['api_key'] = ENV['movie_api_key']
-    end 
+    end
   end
 
   def self.top_forty
     response_1 = conn.get('movie/top_rated?&page=1')
     response_2 = conn.get('movie/top_rated?&page=2')
-
-    json1 = JSON.parse(response_1.body, symbolize_names: true)[:results]
-    json2 = JSON.parse(response_2.body, symbolize_names: true)[:results]
-
-    json1 + json2
+    output = combined_response(response_1, response_2)
   end
 
   def self.movie_search(keyword_string)
     response_1 = conn.get("search/movie?query=#{keyword_string}&page=1")
     response_2 = conn.get("search/movie?query=#{keyword_string}&page=2")
+    combined_response(response_1, response_2)
+  end
 
-    json1 = JSON.parse(response_1.body, symbolize_names: true)[:results]
-    json2 = JSON.parse(response_2.body, symbolize_names: true)[:results]
-
-    json1 + json2
+  def self.combined_response(response_1, response_2)
+    page1 = JSON.parse(response_1.body, symbolize_names: true)[:results]
+    page2 = JSON.parse(response_2.body, symbolize_names: true)[:results]
+    output = page1 + page2
   end
 
   def self.movie_details(movie_id)
