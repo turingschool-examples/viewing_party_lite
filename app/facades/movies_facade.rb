ce0573b412ffc0787df65e7c83184cc651f3914b
Thomas Musselman <tmusselman@gmail.com>
2022-10-12 17:03:21 -0600

    WIP: movies poro and facade

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,8 +1,32 @@
 class MoviesFacade
   def self.get_movie(movie_id)
-    #title
-    #id = movie_id
-    
-    Movie.new
+    attributes = {id: movie_id}
+    details = MoviesService.movie_details(movie_id)
+    images_data = MoviesService.movie_images(movie_id)
+    credits_data = MoviesService.movie_credits(movie_id)
+    reviews_data = MoviesService.movie_reviews(movie_id)
+
+    attributes[:title] = details[:title]
+    attributes[:runtime] = details[:runtime]
+    attributes[:summary] = details[:overview]
+    attributes[:vote_average] = details[:vote_average]
+    attributes[:genres] = details[:genres].map{ |genre| genre[:name] }
+    attributes[:image_path] = images_data[:posters].first[:file_path]
+    attributes[:cast] = credits_data[:cast][0..9].map do |cast_member|
+      {name: cast_member[:name], character: cast_member[:character]}
+    end
+    attributes[:reviews] = reviews_data[:results].map do |review|
+      {author: review[:author], content: review[:content]}
+    end
+
+    Movie.new(attributes)
+  end
+
+  def self.get_top_20_movies
+
+  end
+
+  def self.get_search_results_movies(search_query)
+
   end
 end
--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,3 +1,21 @@
 class Movie
-  
+  attr_reader :title,
+              :id,
+              :vote_average,
+              :genres,
+              :summary,
+              :cast, 
+              :reviews
+              :image_path
+
+  def initialize(attributes)
+    @title = attributes[:title]
+    @id = attributes[:id]
+    @vote_average = attributes[:vote_average]
+    @genres = attributes[:genres]
+    @summary = attributes[:summary]
+    @cast = attributes[:cast]
+    @reviews = attributes[:reviews]
+    @image_path = attributes[:image_path]
+  end
 end
