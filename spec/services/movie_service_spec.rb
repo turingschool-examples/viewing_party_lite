494a8a586cfcccd815b1d9bf94f7b65817fee289
Andrew Mullins <mullinsand15@gmail.com>
2022-10-12 15:03:29 -0600

    finalize vcr use withouth api_key showing

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -7,15 +7,17 @@ RSpec.describe MovieService do
   describe 'class methods' do
     describe 'get_movie_data' do
       it 'get the movie data for a specific movie' do
-        movie_id = 550 #fight club
-        response = MovieService.get_movie_data(movie_id)
+        VCR.use_cassette('fight_club_movie_data_v1') do
+          movie_id = 550 #fight club
+          response = MovieService.get_movie_data(movie_id)
 
-        expect(response).to be_a(Hash)
-        expect(response[:title]).to be_a(String)
-        expect(response[:runtime]).to be_a(Integer)
-        expect(response[:vote_average]).to be_a(Float)
-        expect(response[:images][:posters]).to be_an(Array)
-        expect(response[:reviews][:results]).to be_an(Array)
+          expect(response).to be_a(Hash)
+          expect(response[:title]).to be_a(String)
+          expect(response[:runtime]).to be_a(Integer)
+          expect(response[:vote_average]).to be_a(Float)
+          expect(response[:images][:posters]).to be_an(Array)
+          expect(response[:reviews][:results]).to be_an(Array)
+        end
       end
     end
   end
