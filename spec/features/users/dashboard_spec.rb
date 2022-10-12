ae8533358e84ae5074d4da98736486e7ea27e43a
Ken Lenhart <omnipaid@gmail.com>
2022-10-11 22:53:12 -0700

    Add webmock to gemfile, add movies/index.html.erb, search.html.erb, add webmock stubbing

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 RSpec.describe 'User Dashboard Page' do
   describe 'When I visit /users/:id where :id is a valid user id' do 
     before :each do
+      stub_request(:get, "https://api.themoviedb.org/top_rated?api_key=89bc67a83f7f75e306b0ade64dcbefee").
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v2.6.0'
+        }).
+      to_return(status: 200, body: "", headers: {})
+      
       @user_1 = create(:user)
       @user_2 = create(:user)
 
