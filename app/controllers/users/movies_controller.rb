44af618a1124e01f071945b1b3b652c827270559
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-12 11:32:51 -0400

    add ids from api to movies

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,9 +1,14 @@
 class Users::MoviesController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
     if params[:top_rated]
       @movies = MoviesFacade.top_rated
     elsif params[:search]
       @movies = MoviesFacade.search(params[:search])
     end
+  end
+
+  def show
+    
   end
 end
--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,10 +1,11 @@
 class Movie
-  attr_reader :title, :vote_average
+  attr_reader :id,
+              :title, 
+              :vote_average
 
   def initialize(attributes)
+    @id = attributes[:id]
     @title = attributes[:title]
     @vote_average = attributes[:vote_average]
   end
-
-  
 end
