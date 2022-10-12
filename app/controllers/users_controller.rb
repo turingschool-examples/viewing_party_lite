f817c5161f043f401ebadafce71ddcf433b5bdf8
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-11 21:33:46 -0600

    feat: create skeleton methods in user controller and add new form for register page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    private
+
+    def user_params
+        params.permit(:user_name, :email, :password_digest)
+    end
 end
