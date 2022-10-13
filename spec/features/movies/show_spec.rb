620dda802081c8bd563097fda88b58b43279ae8d
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-13 13:29:33 -0600

    test: add layout expectations for movie details page

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -9,50 +9,54 @@ RSpec.describe 'Movie Details Page' do
       visit user_movie_path(@alex, @movie.id)
     end
     it 'I see a button to create a viewing party' do
-
+      expect(page).to have_button("Create Viewing Party for #{@movie.title}")
     end
 
     it 'I see a button to return to the Discover Page' do
-
+      expect(page).to have_button("Discover Page")
     end
 
-    xit 'has a header for the movie title' do
-      @alex = User.create!(user_name: 'Alex', email: Faker::Internet.email,
-                           password_digest: Faker::Internet.password)
+    it 'has a header for the movie title' do
+      # @alex = User.create!(user_name: 'Alex', email: Faker::Internet.email,
+      #                      password_digest: Faker::Internet.password)
 
-      @inglorious_bastards = double(id: 1, title: 'Inglorious Bastards')
+      # @inglorious_bastards = double(id: 1, title: 'Inglorious Bastards')
 
-      movie_data = ''
+      # movie_data = ''
 
-      allow(Movie).to receive(:new).with(movie_data).and_return(@inglorious_bastards)
+      # allow(Movie).to receive(:new).with(movie_data).and_return(@inglorious_bastards)
 
-      visit user_movie_path(@alex, @inglorious_bastards.id)
+      # visit user_movie_path(@alex, @inglorious_bastards.id)
 
-      expect(page).to have_content('Inglorious Bastards')
+      expect(page).to have_content("#{@movie.title}")
     end
 
     it 'I see the vote Average of the movie' do
-
+      expect(page).to have_content("Vote Average: #{@movie.vote_average}")
     end
 
     it 'I see the runtime in hours & minutes of the movie' do
-
+      expect(page).to have_content("Runtime: #{@movie.runtime}")
     end
 
     it 'I see the genre(s) associated to the movie' do
+      expect(page).to have_content("Genre(s): #{[@movie.genres]}")
+    end
 
+    it 'I see the list the summary for the movie' do
+      expect(page).to have_content("Summary")
     end
 
     it 'I see the list the first 10 cast members (characters&actress/actors) in the movie' do
-
+      expect(page).to have_content("Cast")
     end
 
     it 'I see the count of total reviews for the movie' do
-
+      expect(page).to have_content("8 Reviews")
     end
 
     it 'I see each reviews author and information for the movie' do
-
+      expect(page).to have_content("Author:")
     end
   end
 end
