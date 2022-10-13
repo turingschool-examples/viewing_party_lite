7a302f656db335f68070530199ad1b706af153c6
dominicod <dominicodonnell99@gmail.com>
2022-10-13 17:26:43 -0500

    Test: write test for viewers and host in user show

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -8,10 +8,13 @@ RSpec.describe 'User | Show', type: :feature do
       @user1 = User.create!(name: 'jojo binks', email: 'jojo_binks@gmail.com')
       @user2 = User.create!(name: 'bobby', email: 'bobby@yahoo.com')
       @user3 = User.create!(name: 'marissa nicole', email: 'marissa.nicole99@gmail.com')
+      @user4 = User.create!(name: 'dominic od', email: 'domo99@gmail.com')
       @vp1 = ViewingParty.create!(duration: 100, date: '2022-10-11 17:19:58', time: '17:08', movie_id: 36_600)
       @vp2 = ViewingParty.create!(duration: 180, date: '2022-10-12 16:18:58', time: '19:08', movie_id: 34_601)
       @user_view1 = UserViewingParty.create!(user_id: @user1.id, viewing_party_id: @vp1.id, role: 1)
       @user_view2 = UserViewingParty.create!(user_id: @user1.id, viewing_party_id: @vp2.id, role: 0)
+      @user_view3 = UserViewingParty.create!(user_id: @user2.id, viewing_party_id: @vp2.id, role: 0)
+      @user_view3 = UserViewingParty.create!(user_id: @user3.id, viewing_party_id: @vp2.id, role: 1)
       visit user_path(@user1.id)
     end
     it 'I see the users name at the top of the page' do
@@ -46,6 +49,23 @@ RSpec.describe 'User | Show', type: :feature do
           within('#role') do
             expect(page).to have_content('Invited')
             expect(page).to_not have_content('Host')
+          end
+        end
+      end
+    end
+    it 'I see the invited viewers, host, and my name in bold' do
+      within('#viewing_parties') do
+        within("#party-#{@vp2.id}") do
+          within('#host') do
+            expect(page).to have_content(@user3.name)
+          end
+          within('#viewers') do
+            expect(page).to have_content(@user1.name)
+            expect(page).to have_content(@user2.name)
+            expect(page).to_not have_content(@user3.name)
+            current_user = find('#current_user')
+            expect(current_user.text).to eq @user1.name
+            expect(current_user.tag_name).to eq 'strong'
           end
         end
       end
