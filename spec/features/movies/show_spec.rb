1bde821bcf512f5f58f42f3a1a0fb577b1d7db21
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-13 13:20:27 -0600

    test: add skeleton tests for movie details page

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -3,10 +3,17 @@
 require 'rails_helper'
 require 'faker'
 
-RSpec.describe 'Movie Show Page' do
-  describe 'As a user when I visit the movie show page' do
-    before :each do
-      # allow_any_instance_of(Movie).to receive(:find) {}
+RSpec.describe 'Movie Details Page' do
+  describe 'As a user when I visit the movie details page' do
+    before(:each) do
+      visit user_movie_path(@alex, @movie.id)
+    end
+    it 'I see a button to create a viewing party' do
+
+    end
+
+    it 'I see a button to return to the Discover Page' do
+
     end
 
     xit 'has a header for the movie title' do
@@ -22,6 +29,30 @@ RSpec.describe 'Movie Show Page' do
       visit user_movie_path(@alex, @inglorious_bastards.id)
 
       expect(page).to have_content('Inglorious Bastards')
+    end
+
+    it 'I see the vote Average of the movie' do
+
+    end
+
+    it 'I see the runtime in hours & minutes of the movie' do
+
+    end
+
+    it 'I see the genre(s) associated to the movie' do
+
+    end
+
+    it 'I see the list the first 10 cast members (characters&actress/actors) in the movie' do
+
+    end
+
+    it 'I see the count of total reviews for the movie' do
+
+    end
+
+    it 'I see each reviews author and information for the movie' do
+
     end
   end
 end
