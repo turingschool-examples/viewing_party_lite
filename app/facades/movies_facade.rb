8e266c46c551b72ed16d6c4539227d5e80620f9b
Noah van Ekdom <noahvanekdom@gmail.com>
2022-10-13 06:37:02 -0600

    Add API endpoints to access all necessary movie details

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,6 +1,6 @@
 class MoviesFacade
   def self.top_rated_movies
-    json = MoviesService.top_movies
+    json = MoviesService.top_rated
 
     @movies = json[:results].map do |movie_data|
       Movie.new(movie_data)
