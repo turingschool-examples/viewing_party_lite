5d2b11a61d2e94b3be573b5338731aed8fcdf5f2
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-13 12:17:49 -0400

    add self.invited users method to handle params hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
 
   validates_presence_of :name, :email
   validates_uniqueness_of :email
+
+  def friends
+    User.where.not(id: self.id)
+  end
+
+  def self.invited_users(param_hash)
+    param_hash.filter_map do |user_id,status|
+      user = User.find(user_id.to_i)
+      status = status.to_i
+      if status == 1
+        user
+      end
+    end
+  end
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -11,4 +11,38 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email)}
     it { should validate_uniqueness_of(:email)}
   end
+
+  describe 'instance methods' do
+    describe '#friends' do
+      let!(:user) { create :user }
+      let!(:friend_1) { create :user }
+      let!(:friend_2) { create :user }
+
+      # this could be refactored in the future to return only "friends", but currently is meant to return all users except user
+      it 'returns all users EXCEPT the current user' do
+        expect(user.friends).to include(friend_1, friend_2)
+        expect(user.friends).not_to include(user)
+      end
+    end
+  end
+
+  describe 'class methods' do
+    describe 'invited_users' do
+      let!(:user) { create :user }
+      let!(:friend_1) { create :user }
+      let!(:friend_2) { create :user }
+      let!(:friend_3) { create :user }
+
+      it 'accepts a hash from params and returns an array of user objects where original hash value = 1' do
+        params_hash = {
+          friend_1.id.to_s => "1",
+          friend_2.id.to_s => "1",
+          friend_3.id.to_s => "0"
+        }
+
+        expect(User.invited_users(params_hash)).to include(friend_1, friend_2)
+        expect(User.invited_users(params_hash)).not_to include(friend_3)
+      end
+    end
+  end
 end
