d494d0b1d628c054ef2551b28428e2312cfb6548
cballrun <106977961+cballrun@users.noreply.github.com>
2022-10-13 18:20:16 -0400

    Revert "Revert "Feat/movie poro and discover page""

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'landing#index'
  
   get '/register', to: 'users#register'
+  get 'users/:id/discover', to: 'users#discover'
 
   resources :users, only: [:create, :show]
 
