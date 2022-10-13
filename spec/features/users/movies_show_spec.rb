1be05d147bfa45c75e072df9d7ffcb4362a73302
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-13 10:16:29 -0600

    Tests created for movie details about reviewers on page

--- spec/features/users/movies_show_spec.rb
+++ spec/features/users/movies_show_spec.rb
@@ -44,14 +44,26 @@ RSpec.describe 'a users movies show page', type: :feature do
         expect(page).to have_content("Drama")
       end
 
-      xit 'has the movie total review count, reviewed information, and top 10 cast members for the movie', :vcr do
+      it 'has the movie total review count, reviewed information, and top 10 cast members for the movie', :vcr do
         user = create(:user)
         visit "/users/#{user.id}/movies/550"
-        expect(page).to have_content('Fight Club')
-        expect(page).to have_content('2:19')
-        expect(page).to have_content(8.433)
-        expect(page).to have_content("A ticking-time-bomb insomniac")
-        expect(page).to have_content("Drama")
+
+        expect(page).to have_content("Review Count: 7")
+        expect(page).to have_content("Brett Pascoe") #author
+        expect(page).to have_content("In my top 5 of all time favourite movies. Great story line and a movie you can watch over and over again.") #content
+
+        expect(page).to have_content("Edward Norton")
+        expect(page).to have_content("Brad Pitt")
+        expect(page).to have_content("Helena Bonham Carter")
+        expect(page).to have_content("Meat Loaf")
+        expect(page).to have_content("Jared Leto")
+        expect(page).to have_content("Zach Grenier")
+        expect(page).to have_content("Holt McCallany")
+        expect(page).to have_content("Eion Bailey")
+        expect(page).to have_content("Eion Bailey")
+        expect(page).to have_content("Richmond Arquette")
+        expect(page).to have_content("David Andrews")
+
       end
 
     end
