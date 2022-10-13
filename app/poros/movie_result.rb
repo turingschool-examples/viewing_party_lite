39d2def0675a4e13bdc88c341b1c077d8c05a99a
Mary Ballantyne <mballantyne777@gmail.com>
2022-10-13 16:04:39 -0600

    complete movie results page with top rated movies and search features working

--- app/poros/movie_result.rb
+++ app/poros/movie_result.rb
@@ -1,7 +1,8 @@
 class MovieResult
-  attr_reader :title, :vote_average
+  attr_reader :id, :title, :vote_average
   
   def initialize(data)
+    @id = data[:id]
     @title = data[:title]
     @vote_average = data[:vote_average]
   end
--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -9,14 +9,24 @@ RSpec.describe 'Discover page' do
     visit "/users/#{@user.id}/discover"
   end
 
-  it 'has a button for top rated movies', :vcr do
+  it 'has a button for top rated movies', vcr: 'top_rated.json' do
     expect(page).to have_button('Top Rated Movies')
     click_button 'Top Rated Movies'
     expect(current_path).to eq("/users/#{@user.id}/movies")
   end
 
-  it 'has a search field an button for searching for a movie by title' do
+  it 'has a search field and button for searching for a movie by title', :vcr do
     expect(page).to have_content('Movie Title:')
     expect(page).to have_button('Search')
+    fill_in 'Movie Title:', with: 'Spirited Away'
+    click_button 'Search'
+    expect(current_path).to eq("/users/#{@user.id}/movies")
+    expect(page).to have_content('Spirited Away (8.5)')
+  end
+
+  it 'has a button to return to the discover page', :vcr do
+    visit "/users/#{@user.id}/movies"
+    click_button 'Return to Discover'
+    expect(current_path).to eq("/users/#{@user.id}/discover")
   end
 end
