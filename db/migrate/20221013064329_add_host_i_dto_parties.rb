b85ad5ab84716e854e3b63130df1f243e6f994b0
nicole-esquer <nicole.esquer5@gmail.com>
2022-10-13 00:08:58 -0700

    feat: add host_id to parties

---
+++ db/migrate/20221013064329_add_host_i_dto_parties.rb
@@ -0,0 +1,5 @@
+class AddHostIDtoParties < ActiveRecord::Migration[5.2]
+  def change
+    add_column :parties, :host_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_10_221328) do
+ActiveRecord::Schema.define(version: 2022_10_13_064329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 2022_10_10_221328) do
   create_table "parties", force: :cascade do |t|
     t.integer "movie_id"
     t.integer "duration"
-    t.date "date"
-    t.time "start_time"
-    t.integer "host_id"
+    t.datetime "date"
+    t.datetime "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "host_id"
   end
 
   create_table "user_parties", force: :cascade do |t|
