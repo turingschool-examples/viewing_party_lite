12b64c31ec94509c921f223a2b1316f411b31146
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-13 13:29:51 -0400

    add logic to assign viewing_party_user entries when users are invited to or hosting a party

--- app/controllers/users/viewing_parties_controller.rb
+++ app/controllers/users/viewing_parties_controller.rb
@@ -1,22 +1,35 @@
 class Users::ViewingPartiesController < ApplicationController
   def new
-    @users = User.all
-    @user = User.find(params[:user_id]) 
+    @host = User.find(params[:user_id]) 
+    @friends = @host.friends
     @movie = MoviesFacade.details(params[:movie_id])
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @viewing_party = @user.viewing_parties.new(viewing_party_params)
+    host = User.find(params[:user_id])
+    invitees = User.invited_users(params[:users])
+    viewing_party = ViewingParty.new(viewing_party_params)
     if params[:duration] >= params[:movie_duration] 
-      if @viewing_party.save
-      redirect_to user_path(@user)
+      if viewing_party.save
+        ViewingPartyUser.create!(
+          user_id: host.id,
+          viewing_party_id: viewing_party.id,
+          hosting: true
+        )
+        invitees.each do |invitee|
+          ViewingPartyUser.create!(
+            user_id: invitee.id,
+            viewing_party_id: viewing_party.id,
+            hosting: false
+          )
+        end
+        redirect_to user_path(host)
       else
-        redirect_to new_user_movie_viewing_party_path(@user, params[:movie_id])
-        flash[:alert] = "Error: #{error_message(@viewing_party.errors)}"
+        redirect_to new_user_movie_viewing_party_path(host, params[:movie_id])
+        flash[:alert] = "Error: #{error_message(viewing_party.errors)}"
       end
     else
-      redirect_to new_user_movie_viewing_party_path(@user, params[:movie_id])
+      redirect_to new_user_movie_viewing_party_path(host, params[:movie_id])
       flash[:alert] = "Error: Duration cannot be shorter than movie runtime"
     end
   end
@@ -24,6 +37,6 @@ class Users::ViewingPartiesController < ApplicationController
   private
 
   def viewing_party_params
-    params.permit(:duration, :start_time, :movie_id)
+    params.permit(:duration, :start_time, :movie_id, :movie_title)
   end
 end
