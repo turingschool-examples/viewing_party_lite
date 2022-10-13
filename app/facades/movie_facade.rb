dc94d4b5d36004950d945706db45c06f0ff2bc05
Gavin Carew <gjcarew@gmail.com>
2022-10-12 23:23:01 -0700

    Cast method in MovieFacade and tests

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -5,9 +5,14 @@ class MovieFacade
       Movie.new(movie_data)
     end
   end
-  
+
   def self.movie_by_id(id)
     json = MovieService.movie_by_id(id)
     Movie.new(json)
+  end
+
+  def self.cast(id)
+    json = MovieService.cast(id)
+    json[:cast][0..9].map { |actor| actor[:name] }
   end
 end
