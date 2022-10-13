2fde260756ac8661103eb14075b6cced7a754149
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-10-13 14:31:13 -0700

    Add tests for discover movies page

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Discover Movies Dashboard", type: :feature do
   before :each do
+    @user_1 = create(:user)
+    visit user_discover_path(@user_1.id)
   end
 
-  it 'has a button to discover tope rated movies' do
-
+  it 'has a button to discover top rated movies', :vcr do
+    click_button('Find Top Rated Movies')  
+    expect(current_path).to eq(user_movies_path(@user_1))
   end
-
-  it 'has a text field to search movie titles by keyword' do
-
-  end
-
-  it 'has a button to search by movie title' do
-
+  
+  it 'has a button to search by movie title', :vcr do
+    fill_in :search, with: 'The Evil Dead'
+    click_button('Find Movies')  
+    expect(current_path).to eq(user_movies_path(@user_1))
   end
 end
