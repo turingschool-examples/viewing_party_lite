c3126ccf7e0db96b5f64a54a268cc4877c56b920
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-13 08:12:04 -0600

    added tests for sad path and top 20 movies results

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -15,22 +15,44 @@ RSpec.describe 'Discover movies page' do
       expect(page).to have_button('Search by Movie Title')
     end
 
-    it 'When I search for a movie I am taken to the Movie Results Page', :vcr do
-      # json_response = File.read("spec/fixtures/movies_query_data.json")
-      # stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movies_api_key']}&query=fight").to_return(status: 200, body: json_response)
+    describe 'happy path'
+      it 'When I search for a movie I am taken to the Movie Results Page', :vcr do
+        # json_response = File.read("spec/fixtures/movies_query_data.json")
+        # stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movies_api_key']}&query=fight").to_return(status: 200, body: json_response)
 
-      user = create(:user)
-      visit "/users/#{user.id}/discover"
-      # Details: There should only be a maximum of 40 results. The above details should be listed for each movie.
-      fill_in('Search by Movie Title', with: 'fight')
-      click_button('Search by Movie Title')
-      
-      expect(current_path).to eq("/users/#{user.id}/movies")
-      expect(page).to have_content('Average Votes:')
-      expect(page).to have_link('Fight Club')
-      expect(page).to have_button('Return to Discover Page')
+        user = create(:user)
+        visit "/users/#{user.id}/discover"
+        # Details: There should only be a maximum of 40 results. The above details should be listed for each movie.
+        fill_in('Search by Movie Title', with: 'fight')
+        click_button('Search by Movie Title')
+        
+        expect(current_path).to eq("/users/#{user.id}/movies")
+        expect(page).to have_content('Average Votes:')
+        expect(page).to have_link('Fight Club')
+        expect(page).to have_button('Return to Discover Page')
+      end
+
+      it 'When I click top rated movies I see the current top 20 movies', :vcr do
+        user = create(:user)
+        visit "/users/#{user.id}/discover"
+        click_button('Discover Top Rated Movies')
+        
+        expect(current_path).to eq("/users/#{user.id}/movies")
+        expect(page).to have_link('Godfather')
+        expect(page).to_not have_link('Matilda')
+      end
     end
 
+    describe 'sad path'
+      it 'When I fill in nothing and click Search by Movie Title I recieve an error message', :vcr do
+        user = create(:user)
+        visit "/users/#{user.id}/discover"
+        click_button('Search by Movie Title')
+        
+        expect(current_path).to eq("/users/#{user.id}/movies")
+        expect(page).to have_content('You must fill in a title.')
+      end
+    end
   end
 end
 
