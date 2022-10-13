c35d061251a45d0fba528a2deefe01c382d3be63
Sid Mann <sjmann2@outlook.com>
2022-10-13 18:15:07 -0500

    Edit simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,13 @@
 #
 require 'simplecov'
 require 'webmock/rspec'
-SimpleCov.start 'rails'
+# SimpleCov.start 'rails'
+SimpleCov.start do
+  add_filter './app/channels/application_cable/channel.rb'
+  add_filter './app/channels/application_cable/connection.rb'
+  add_filter './app/jobs/application_job.rb'
+  add_filter './app/mailers/application_mailer.rb'
+end
 SimpleCov.add_filter ['spec', 'config']
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
