645c115b8e084cdcbc87dce2fbe4ece712f49c24
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-13 10:04:20 -0400

    refactor before / let statements

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,19 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "user dashboard" do
-  let!(:user) { create :user }
-  let!(:viewing_parties) { create_list(:viewing_party, 3)}
-
+  
   describe 'when I visit a user dashboard' do
+    let!(:user) { create :user }
+  
     before :each do
-      viewing_parties.each do |party|
-        user.viewing_parties << party
+      VCR.use_cassette('user_dashboard') do
+        @movies = MoviesFacade.top_rated
+        @movie_1 = @movies[0]
+        @movie_2 = @movies[1]
+        @movie_3 = @movies[2]
+        @viewing_party_1 = user.viewing_parties.create!(duration: 300, start_time: Faker::Time.forward(days: 7, period: :evening), movie_id: @movie_1.id)
+        @viewing_party_2 = user.viewing_parties.create!(duration: 300, start_time: Faker::Time.forward(days: 7, period: :evening), movie_id: @movie_2.id)
+        @viewing_party_3 = user.viewing_parties.create!(duration: 300, start_time: Faker::Time.forward(days: 7, period: :evening), movie_id: @movie_3.id)
       end
-
-      visit user_path(user)
     end
 
-    xit 'displays a title' do
+    it 'displays a title' do
+      visit user_path(user)
+      require 'pry'; binding.pry
       expect(page).to have_content("#{user.name}'s Dashboard")
     end
 
