8bb66322a451cf5e98f888c455b8f56c7436b252
Thomas Musselman <tmusselman@gmail.com>
2022-10-13 15:59:37 -0600

    feature: movie info on user dashboard

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -2,4 +2,8 @@ class ViewingParty < ApplicationRecord
     belongs_to :host, class_name: 'User'
     has_many :viewing_party_invitees
     has_many :users, through: :viewing_party_invitees
+
+    def movie
+        MoviesFacade.get_movie(movie_id)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ronda = User.create(name: "Ronda Reynolds", email: "rr@gmail.com")
+bob = User.create(name: "Bob Boylan", email: "bb@gmail.com")
+terry = User.create(name: "Terry Tinkerson", email: "tt@gmail.com")
+
+vp_1 = ViewingParty.create(
+  movie_id: 17473,
+  duration: 110,
+  date: "2022-10-31",
+  start_time: "16:00:00",
+  host: ronda
+)
+
+vp_2 = ViewingParty.create(
+  movie_id: 36685,
+  duration: 180,
+  date: "2022-11-18",
+  start_time: "18:30:00",
+  host: bob
+)
+
+vp_1.users << [bob, terry]
+vp_2.users << [ronda, terry]
+
