5d942ee70b8673e82a5d9836b58fb46976e84416
Carter Ball <carter.k.ball@gmail.com>
2022-10-13 17:33:04 -0400

    Begin working on movie poro

---
+++ spec/poros/movie_spec.rb
@@ -0,0 +1,17 @@
+# require 'rails_helper'
+# require 'teammate'
+
+# RSpec.describe Movie do
+#   it 'exists and has attributes' do
+
+#     # VCR.use_cassette("teammate_data", :allow_playback_repeats => true) do
+#     #   teammate_hash = GithubService.get_data("contributors").first
+#     #   teammate = Teammate.new(teammate_hash)
+#     #   expect(teammate).to be_instance_of(Teammate)
+#     #   expect(teammate.username).to be_instance_of(String)
+#     #   expect(teammate.username).to eq("cballrun")
+#     #   expect(teammate.commits).to be_instance_of(Integer)
+#     #   expect(teammate.commits).to eq(73)
+#     end
+#   end
+# end
