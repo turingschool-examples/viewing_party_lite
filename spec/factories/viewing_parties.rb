9281903d153f88b42279bf3576d504c6716b9fd4
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-13 13:32:01 -0400

    add new attributes

--- spec/factories/viewing_parties.rb
+++ spec/factories/viewing_parties.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :viewing_party do
     start_time { Faker::Time.forward(days: 14, period: :evening) }
-    duration { Faker::Number.between(from: 90, to: 240)}
+    duration { Faker::Number.between(from: 90, to: 240) }
+    movie_id { Faker::Number.unique.number(digits: 6) }
+    movie_title { Faker::DcComics.title }
   end
 end
