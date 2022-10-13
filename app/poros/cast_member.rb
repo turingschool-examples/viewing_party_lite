3680b1bba6d99c9baefac5e29d0bdc94332d9786
Jake Kim <imjakekim@gmail.com>
2022-10-12 18:07:22 -0700

    created cast member class and refactored movie facade to be cleaner

---
+++ app/poros/cast_member.rb
@@ -0,0 +1,10 @@
+class CastMember
+    attr_reader :name,
+                :character
+
+    def initialize(data)
+        @name = data[:name]
+        @character = data[:character]
+    end
+end
+  
--- app/services/movie_db_service.rb
+++ app/services/movie_db_service.rb
@@ -1,40 +1,36 @@
 class MovieDbService
-    def self.top_rated_movies
+    def self.connection
         conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
             faraday.params["api_key"] = ENV['themoviedb_key']
-          end
-          response = conn.get("3/movie/top_rated")
+        end
+    end
 
-          data = JSON.parse(response.body, symbolize_names: true)
+    def self.top_rated_movies
+        response = self.connection.get("3/movie/top_rated")
+        data = JSON.parse(response.body, symbolize_names: true)
     end
 
     def self.movies_search(keyword)
         conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
             faraday.params["api_key"] = ENV['themoviedb_key']
             faraday.params["query"] = keyword
-          end
-          response = conn.get("3/search/movie")
-
-          data = JSON.parse(response.body, symbolize_names: true)
+        end
+        response = conn.get("3/search/movie")
+        data = JSON.parse(response.body, symbolize_names: true)
     end
 
     def self.find_movie_by_id(id)
-      conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-          faraday.params["api_key"] = ENV['themoviedb_key']
-        end
-        response = conn.get("3/movie/#{id}")
-
+        response = self.connection.get("3/movie/#{id}")
         data = JSON.parse(response.body, symbolize_names: true)
-
     end
 
     def self.find_movie_reviews(id)
-      conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-          faraday.params["api_key"] = ENV['themoviedb_key']
-        end
-        response = conn.get("3/movie/#{id}/reviews")
-
+        response = self.connection.get("3/movie/#{id}/reviews")
         data = JSON.parse(response.body, symbolize_names: true)
+    end
 
+    def self.cast(movie_id)
+        response = self.connection.get("3/movie/#{id}/credits")
+        data = JSON.parse(response.body, symbolize_names: true)
     end
 end
