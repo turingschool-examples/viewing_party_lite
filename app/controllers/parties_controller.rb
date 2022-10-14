c7caa64ccc48c771f17bf213796262498639ac95
Astrid-Hecht <106942456+Astrid-Hecht@users.noreply.github.com>
2022-10-14 00:21:21 -0600

    Update parties_controller.rb
    Just getting rid of unnecessary variable in controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -3,7 +3,6 @@ class PartiesController < ApplicationController
   def new
     @movie = MovieFacade.find_movie(params[:movie_id])
     @users = User.all
-    @host = User.find(params[:user_id])
   end
 
   def create
