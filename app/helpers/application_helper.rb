ad1e54eda5d4e39bddb08799c6b2802e5d4ae309
Thomas Musselman <tmusselman@gmail.com>
2022-10-13 19:36:46 -0600

    implement runtime view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def runtime_view_helper(int_runtime)
+    hrs = int_runtime / 60
+    mins = int_runtime % 60
+    return_value = ""
+    if hrs > 0
+      if hrs == 1
+        return_value += "1hr"
+      else
+        return_value += "#{hrs}hrs"
+      end
+    end
+    if mins > 0
+      return_value += " #{mins}mins"
+    end
+    return_value
+  end
 end
