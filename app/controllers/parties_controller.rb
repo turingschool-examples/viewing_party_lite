69d07be08ff520f9fc9d62d1dd35313622fed8e6
Sunny Moore <moore.sunny@gmail.com>
2022-10-14 15:06:21 -0700

    create new parties

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -9,4 +9,39 @@ class PartiesController < ApplicationController
     @users = User.all
     @movie = MovieFacade.find(params[:movie_id])
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @movie = MovieFacade.find(params[:movie_id])
+    
+    if params[:duration].to_i < @movie.runtime
+      flash[:alert] = "Error: Duration is less than movie duration"
+    elsif parse_date_params <= Time.now
+      flash[:alert] = "Error: Please pick a time in the future"
+    elsif params[:user_ids].keys == []
+      flash[:alert] = "Error: Please select users to invite"
+    else
+      @party = Party.create!(party_params)
+      PartyUser.create!(party: @party, user_id: params[:user_id])
+    end
+
+    invited = params[:user_ids].keys
+    invited.each do |user, _invited|
+      PartyUser.create!(party: @party.id, user_id: @user.id)
+    end
+    redirect_to user_path(@user)
+  end
+
+  def parse_date_params
+    DateTime.new(params["start_time(1i)"].to_i, 
+                params["start_time(2i)"].to_i,
+                params["start_time(3i)"].to_i,
+                params["start_time(4i)"].to_i,
+                params["start_time(5i)"].to_i)
+  end
+  private
+
+  def party_params
+    params.permit(:movie_id, :movie_title, :start_time, :duration, :host_id)
+  end
 end
--- spec/features/users/movies/parties/new_spec.rb
+++ spec/features/users/movies/parties/new_spec.rb
@@ -18,7 +18,36 @@ RSpec.describe 'parties/new' do
   it 'has a date and time fields', vcr: 'viewing_party' do
     movie = MovieFacade.find(550)
     visit new_user_movie_party_path(@user.id, movie.id)
-    save_and_open_page
     expect(page).to have_select('_start_time_3i', with_selected: Time.now.day.to_s)
+    expect(page).to have_select('_start_time_2i', with_selected: Time.now.strftime('%B'))
+    expect(page).to have_select('_start_time_1i', with_selected: Time.now.year.to_s)
+    expect(page).to have_select('_start_time_4i', with_selected: Time.now.strftime("%I %p"))
+  end
+  it 'has checkboxes for all users that can be invited', vcr: 'viewing_party' do
+    user2 = User.create(name: 'Mary', email: 'mary@email.com')
+    user3 = User.create(name: 'Bob', email: 'Bob@email.com')
+    user4 = User.create(name: 'Phillip', email: 'Phillip@email.com')
+    movie = MovieFacade.find(550)
+    visit new_user_movie_party_path(@user.id, movie.id)
+    # save_and_open_page
+    expect(page).to have_content('mary@email.com')
+  end
+  it 'form can create a new viewing party', vcr: 'viewing_party' do
+    user2 = User.create(name: 'Mary', email: 'mary@email.com')
+    user3 = User.create(name: 'Bob', email: 'Bob@email.com')
+    user4 = User.create(name: 'Phillip', email: 'Phillip@email.com')
+    
+    visit new_user_movie_party_path(@user.id, 550)
+
+    fill_in 'Duration', with: '140'
+    select(Time.now.strftime('%Y'), from: '_start_time_1i')
+    select(Time.now.strftime('%B'), from: '_start_time_2i')
+    select( 3.days.from_now.strftime('%d'), from: '_start_time_3i')
+    select('06 PM', from: '_start_time_4i')
+    select('15', from: '_start_time_5i')
+    check "user_ids[#{user2.id}]"
+    check "user_ids[#{user3.id}]"
+    click_button 'Create Party'
+    expect(current_path).to eq(user_path(@user.id))
   end
 end
