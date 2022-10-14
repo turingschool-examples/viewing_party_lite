8f9ec93e087c719a4dbd056967b7c0d9ab31f3bd
Stephen Fabian <fabianstephen@gmail.com>
2022-10-14 11:20:39 -0600

    adds new viewing party feature

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -13,25 +13,27 @@ class ViewingPartiesController < ApplicationController
     @movie = MovieFacade.details_poro(params[:movie_id])
     @users = User.all.where.not(name: @user.name)
 
-    if params[:duration].to_i > @movie.runtime.to_i
-      party = ViewingParty.create(duration: params[:duration], date: params[:date], time: params[:time], host_id: @user.id, movie_id: params[:movie_id] )
+    party = ViewingParty.new(duration: params[:duration], date: params[:date], time: params[:time], host_id: @user.id, movie_id: params[:movie_id])
+    if params[:duration].to_i < @movie.runtime.to_i 
+      flash[:notice] = "Party duration must be greater than movie runtime and all fields must be filled in "
+      render :new
+    elsif party.save
+      invited_users = []
+      params[:user].select do |user_id|
+        if user_id != "0"
+          invited_users << user_id.to_i
+        end
+      end
+      invited_users << @user.id
+      invited_users.each do |user|
+        ViewingPartyUser.create(viewing_party_id: party.id, user_id: user)
+      end
+      redirect_to user_path(@user)
     else
-      flash[:notice] = "Party duration must be greater than movie runtime "
-     render :new
+      flash.alert = party.errors.full_messages.to_sentence
+      render :new
     end
-
-    # if party.save!
-    #   flash.alert = party.errors.full_messages.to_sentence
-    #   render :new
-    # else 
-    #   flash.notice = "Yay ptay was scheduled"
-    # end
-
-    invited_users = params[:user].select do |user_id|
-      user_id != "0"
-    end
-    test = ViewingPartyUser.create(viewing_party_id: party.id, user_id: invited_users )
-    redirect_to user_path(@user)
+ 
   end
 
   def viewing_party_params
--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'New Viewing Party Page' do
     before :each do
       @user_1 = create(:user)
       @user_2 = create(:user)
+      @user_3 = create(:user)
     end
 
     it 'I see the name of the movie title' do
@@ -18,12 +19,26 @@ RSpec.describe 'New Viewing Party Page' do
       expect(current_path).to eq(user_discover_path(@user_1))
     end
 
-    it 'Has form field for duration, day, time. And checkbox to invite users' do
+    it 'Has form fields for duration, day, time. And checkbox to invite users' do
       visit new_user_movie_viewing_party_path(@user_1, 278)
-      fill_in "Duration", with: "140"
-      fill_in "Date", with: "12/30/2022"
+      expect(@user_1.viewing_parties).to eq([])
+      fill_in "Duration", with: "143"
+      fill_in "Date", with: "Tue, 11 Oct 2022"
       fill_in "Time", with: "6:00 PM"
       check "user_#{@user_2.id}" 
+      check "user_#{@user_3.id}"
+      click_button("Create Party")
+      expect(@user_1.viewing_parties.reload.present?).to eq(true) 
     end
+
+    # it 'each user has a checkbox to invite user to party, labeled by user name/email' do
+    #   within "#user_#{@user_1.id}" do
+    #     expect(page).to have_content(@user_1.name)
+    #     expect(page).to have_content(@user_1.email)
+    #     check "#user_#{@user_1.id}" 
+    #     expect(page).to_not have_content(@user_2.name)
+    #     expect(page).to_not have_content(@user_2.email)
+    #   end
+    # end
   end
 end
