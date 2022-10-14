1d4f3844aa88a22acce606ed3e2f4e361c00fef1
Riley McCullough <rileybmccullough@gmail.com>
2022-10-13 23:23:35 -0600

    Add to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'Mr. Kim', email: 'jakechicken@gmail.com')
+User.create(name: 'Mr Mac', email: 'macblue@gmail.com')
