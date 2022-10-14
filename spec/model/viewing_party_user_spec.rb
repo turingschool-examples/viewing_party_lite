376166c591c572f4ca75aa17da3336084da5606a
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-14 09:26:34 -0400

    add validation for hosting attribute

--- spec/model/viewing_party_user_spec.rb
+++ spec/model/viewing_party_user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe ViewingPartyUser, type: :model do
     it { should belong_to(:viewing_party)}
     it { should belong_to(:user)}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:hosting) }
+  end
 end
