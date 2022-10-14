47bbecc9e52933ffd107ca080b70dc85347ee1d2
Noah van Ekdom <noahvanekdom@gmail.com>
2022-10-14 14:42:02 -0600

    Add host status for user viewing party dashboard, modify verification of boolean

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
 
   has_many :user_parties
   has_many :parties, through: :user_parties
+
+  def host_status(party_id)
+    user_parties.where(party_id: party_id).pluck("host_status").first
+  end
 end
