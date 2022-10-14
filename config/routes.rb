344369c58e8a5acac0b3f37e5e4c728f02af791c
Carter Ball <carter.k.ball@gmail.com>
2022-10-14 01:10:39 -0400

    Form working to create a new party

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'users/:id/discover', to: 'users#discover'
   get '/users/:user_id/movies/:movie_id', to: 'user_movies#show' #namespace?
   get "/users/:user_id/movies/:movie_id/viewing-party/new", to: 'parties#new'
+  post "/users/:user_id/movies/:movie_id/viewing-party/create", to: 'parties#create'
   
   resources :users, only: [:create, :show] 
 
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -12,6 +12,24 @@ RSpec.describe 'the New Party Page' do
         expect(find('form')).to have_content('Start Time')
         expect(find('form')).to have_button('Create Party')
 
+        
+      end
+    end
+
+    it 'the form creates a new party' do
+      VCR.use_cassette("new_party", :allow_playback_repeats => true) do
+        user = create(:user, id: 50, name: "Bubbles", email: "bubbles@sunnyvale.ca")
+        visit "/users/#{user.id}/movies/550/viewing-party/new"
+        fill_in "Duration of Party", with: 180
+        fill_in "Day", with: Date.today
+        fill_in "Start Time", with: Time.now
+
+        expect { click_on 'Create Party' }.to change { Party.count }.by(1)
+        party = Party.first
+
+        expect(party.duration).to eq(180)
+        expect(party.date).to eq(Date.today)
+        expect(party.movie_id).to eq(550)
       end
     end
   end
