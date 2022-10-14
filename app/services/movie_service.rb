6e66daab92b1802aba485bf60107944b0fce46bc
Mary Ballantyne <mballantyne777@gmail.com>
2022-10-13 20:08:23 -0600

    complete movie details page

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -14,4 +14,19 @@ class MovieService
     response = conn.get('/3/search/movie', query: movie_title)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.movie_details(movie_id)
+    response = conn.get("/3/movie/#{movie_id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_reviews(movie_id)
+    response = conn.get("/3/movie/#{movie_id}/reviews")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_credits(movie_id)
+    response = conn.get("/3/movie/#{movie_id}/credits")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
