93690675ad2a598542ac33b2a6e7e6bd2db680b3
Carter Ball <carter.k.ball@gmail.com>
2022-10-13 23:54:37 -0400

    New party form exists

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,6 +1,6 @@
 class PartiesController < ApplicationController
 
   def new
-    
+    @movie = MovieFacade.find_movie(params[:movie_id])
   end
 end
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe 'the New Party Page' do
 
   describe 'party creation' do
     it 'has a form to create a new party' do
-      # VCR.use_cassette("user_movies_show", :allow_playback_repeats => true) do
+      VCR.use_cassette("new_party", :allow_playback_repeats => true) do
         user = create(:user, id: 50, name: "Bubbles", email: "bubbles@sunnyvale.ca")
         visit "/users/#{user.id}/movies/550/viewing-party/new"
-      # end
+        expect(page).to have_content('Create a Movie Party for Fight Club')
+        expect(find('form')).to have_content('Day')
+        expect(find('form')).to have_content('Start Time')
+        expect(find('form')).to have_button('Create Party')
+
+      end
     end
   end
 end
