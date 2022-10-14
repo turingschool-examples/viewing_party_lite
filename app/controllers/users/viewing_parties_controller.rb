7649902bb60ad4392b9de8f5ce2661e7499ec957
A.J. Krumholz <krumholz.alan@gmail.com>
2022-10-14 15:43:39 -0400

    bug fix datetime

--- app/controllers/users/viewing_parties_controller.rb
+++ app/controllers/users/viewing_parties_controller.rb
@@ -6,6 +6,7 @@ module Users
       @host = User.find(params[:user_id])
       @friends = @host.friends
       @movie = MoviesFacade.details(params[:movie_id])
+      @current_time = DateTime.now
     end
 
     def create
