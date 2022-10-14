9dffe94dcfe104572d23126a43688fa0816725c6
Thomas Musselman <tmusselman@gmail.com>
2022-10-14 12:46:33 -0600

    add validations, sad path, and refactor viewing party creation

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -2,8 +2,19 @@ class ViewingParty < ApplicationRecord
     belongs_to :host, class_name: 'User'
     has_many :viewing_party_invitees
     has_many :users, through: :viewing_party_invitees
+    validates_presence_of :duration, :date, :start_time, :movie_id, :host_id
+    validates :duration, numericality: { only_integer: true } 
+    validate :duration_longer_than_runtime
 
     def movie
         MoviesFacade.get_movie(movie_id)
+    end
+
+    private
+
+    def duration_longer_than_runtime
+        if movie.runtime > duration
+            errors.add(:duration, "must be greater than movie runtime")
+        end
     end
 end
