2fccfb7f7ffd37c919b541d2de77d38bc552322a
Sid Mann <sjmann2@outlook.com>
2022-10-24 12:10:28 -0500

    Add password digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,38 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_221_013_163_612) do
+ActiveRecord::Schema.define(version: 2022_10_24_170842) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
-  create_table 'viewing_parties', force: :cascade do |t|
-    t.integer 'duration'
-    t.date 'date'
-    t.time 'start_time'
-    t.bigint 'movie_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'host_id'
-    t.string 'movie_title'
+  create_table "viewing_parties", force: :cascade do |t|
+    t.integer "duration"
+    t.date "date"
+    t.time "start_time"
+    t.bigint "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "host_id"
+    t.string "movie_title"
   end
 
-  create_table 'viewing_party_users', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'viewing_party_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'hosting', default: false
-    t.index ['user_id'], name: 'index_viewing_party_users_on_user_id'
-    t.index ['viewing_party_id'], name: 'index_viewing_party_users_on_viewing_party_id'
+  create_table "viewing_party_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "viewing_party_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "hosting", default: false
+    t.index ["user_id"], name: "index_viewing_party_users_on_user_id"
+    t.index ["viewing_party_id"], name: "index_viewing_party_users_on_viewing_party_id"
   end
 
-  add_foreign_key 'viewing_party_users', 'users'
-  add_foreign_key 'viewing_party_users', 'viewing_parties'
+  add_foreign_key "viewing_party_users", "users"
+  add_foreign_key "viewing_party_users", "viewing_parties"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password)}
+    it { should validate_presence_of(:password_confirmation)}
+
+    it { should have_secure_password }
   end
 
   describe 'instance methods' do
