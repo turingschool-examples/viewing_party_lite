57c98ec6994d355b43537ca3f5128cd26c3b5143
Sid Mann <sjmann2@outlook.com>
2022-10-24 17:06:31 -0500

    Change validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :viewing_party_users
   has_many :viewing_parties, through: :viewing_party_users
 
-  validates_presence_of :name, :email, :password, :password_confirmation
+  validates_presence_of :name, :email, :password_digest
   validates_uniqueness_of :email
 
   has_secure_password
