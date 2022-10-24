280f4327df54c940dd56fa7ea9a09ff9b2f078f6
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-24 15:40:19 -0400

    add tests to not reveal a users password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
-  validates :name, :email, :password, presence: true
+  validates :name, :email, :password, :password_digest, presence: true
   validates_uniqueness_of :email
   has_secure_password
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should validate_uniqueness_of(:email) }
     it { should have_secure_password }
+    it { should validate_presence_of(:password_digest)}
+  end
+
+  describe 'password characteristics' do
+     it 'should not reveal a users password' do
+      user = User.create(name: 'Alaina', email: 'alaina@test.com', password: 'password123', password_confirmation: 'password123')
+
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq("password123")
+     end
   end
 
   describe 'instance methods' do
