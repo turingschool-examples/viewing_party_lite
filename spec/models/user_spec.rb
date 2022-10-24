67efb670d3b6511f2b4a8d0a2273f742b36325a5
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-24 15:34:02 -0400

    testing for user model add secure password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_uniqueness_of(:email) }
+    it { should have_secure_password }
   end
-
 
   describe 'instance methods' do
     describe '#friends' do
@@ -25,5 +27,4 @@ RSpec.describe User, type: :model do
      end
     end
   end
-
 end
