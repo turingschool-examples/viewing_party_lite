52fd002f6499a502e087a8f33dd139671da80834
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-25 17:15:11 -0600

    Feat: Add user id to session after login or profile creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    if !current_user
+      flash[:error] = "You are not a registered user"
+      redirect_to root_path
+    end
+  end
 end
