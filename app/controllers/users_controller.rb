06d9149ce897f16f69fd62ae6c53ea83b8e006f5
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-25 14:07:33 -0400

    updated schema to rollback addition to column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
     if !User.find_by(email: params[:email]).nil?
       @user = User.find_by(email: params[:email])
       if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
         flash[:success] = "Welcome, #{@user.name}!"
         session[:user_id] = @user.id
         redirect_to user_path(@user)
