034fd4884c20c961a12d00f827134d4d61c6b698
Anna Marie Sterling <sterling.am@gmail.com>
2022-11-16 23:57:58 -0700

    Fix sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :current_admin?
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   def require_user
