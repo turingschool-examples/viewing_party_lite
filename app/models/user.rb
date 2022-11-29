0524a76495e46af3ff3c3c763bb82ed26518fc61
Astrid Hecht <silentaviary@gmail.com>
2022-11-29 13:19:07 -0700

    Feat: User registration pages complete and tested, Write User controller and model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+  validates_presence_of :name
+  
   has_many :user_parties
   has_many :parties, through: :user_parties
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
-Rspec.describe 'User Registration Page' do
+RSpec.describe 'User Registration Page' do
   before :each do
     visit '/register'
   end
 
   describe 'the user registration form' do
     it 'show the correct form with fields for name, email, and a submit button' do
+      # save_and_open_page
       expect(page).to have_content('New User Registration')
       expect(find('form')).to have_content('Name')
       expect(find('form')).to have_content('E-mail')
-      expect(find('form')).to have_content('Register')
+      expect(page).to have_button('Register')
     end
   end
 
   describe 'registration submission' do
     context 'if filled out corectly' do
       it 'creates new user' do
-        fill_in 'Name', with: 'Kenz'
-        fill_in 'E-mail', with: 'kenz_mail@gmail.com'
+        fill_in 'name', with: 'Kenz'
+        fill_in 'email', with: 'kenz_mail@gmail.com'
 
         expect { click_on 'Register' }.to change { User.count }.by(1)
         user = User.last
@@ -31,26 +32,26 @@ Rspec.describe 'User Registration Page' do
     context 'if filled out incorrectly, it flashes appropriate error and doesnt create user' do
       it 'with empty fields' do
         expect { click_on 'Register' }.to change { User.count }.by(0)
-        expect(page).to have_content("Name invalid: field can't be blank")
-        expect(page).to have_content("E-mail invalid: field can't be blank")
+        expect(page).to have_content("Name can't be blank")
+        expect(page).to have_content("Email can't be blank")
         expect(current_path).to eq('/register')
       end
 
       it 'with non-email string in email field' do
-        fill_in 'Name', with: 'Cat'
-        fill_in 'E-mail', with: 'What is email'
+        fill_in 'name', with: 'Cat'
+        fill_in 'email', with: 'What is email'
         expect { click_on 'Register' }.to change { User.count }.by(0)
-        expect(page).to have_content('E-mail invalid: not an email address')
+        expect(page).to have_content('')
         expect(current_path).to eq('/register')
       end
 
       it 'given a duplicate E-mail' do
         User.create!(name: 'Astrid', email: 'Astrid-mail@gmail.com')
-        fill_in 'Name', with: 'Astrid_2'
-        fill_in 'E-mail', with: 'Astrid-mail@gmail.com'
+        fill_in 'name', with: 'Astrid_2'
+        fill_in 'email', with: 'Astrid-mail@gmail.com'
 
         expect { click_on 'Register' }.to change { User.count }.by(0)
-        expect(page).to have_content('E-mail invalid: already in use')
+        expect(page).to have_content('Email has already been taken')
         expect(current_path).to eq('/register')
       end
     end
