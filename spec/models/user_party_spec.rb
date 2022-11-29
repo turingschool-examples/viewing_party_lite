2c985ee600112442e99d7e00fea665b298f6a494
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-11-28 20:02:35 -0700

    Add model associations and tests
    Co-authored-by: Mostafa <msakr3@gmail.com>”

--- spec/models/user_party_spec.rb
+++ spec/models/user_party_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UserParty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it {should belong_to :user }
+    it {should belong_to :party }
+  end
 end
