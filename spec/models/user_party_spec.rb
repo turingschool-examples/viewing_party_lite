8c3f518424a576cef7e3a18be0e35a5b81c87eb9
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-28 21:15:29 -0700

    add coverage to gitignore file

--- spec/models/user_party_spec.rb
+++ spec/models/user_party_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UserParty, type: :model do
-  it { should belong_to(:user) }
-  it { should belong_to(:party) }
+  describe 'relationships' do
+    it { should belong_to(:user) }
+    it { should belong_to(:party) }
+  end
 end
