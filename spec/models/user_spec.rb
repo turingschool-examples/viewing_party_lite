53235279e815d241e76a43ac7405f9887d28c1e4
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-28 18:16:35 -0700

    Fix formatting on relationship testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'relationships' do
-    it {should have_many(:user_view_parties)}
-    it {should have_many(:view_parties).through(:user_view_parties)}
+    it { should have_many(:user_view_parties) }
+    it { should have_many(:view_parties).through(:user_view_parties) }
   end
 end
--- spec/models/view_party_spec.rb
+++ spec/models/view_party_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ViewParty, type: :model do
   describe 'relationships' do
-    it {should have_many(:user_view_parties) }
-    it {should have_many(:users).through(:user_view_parties) }
+    it { should have_many(:user_view_parties) }
+    it { should have_many(:users).through(:user_view_parties) }
   end
 end
