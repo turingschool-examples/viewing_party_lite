bd4a1ee866f887696a4c363db9a2e3e98ab6c252
Kevin Ta <kvnta@yahoo.com>
2022-11-29 12:00:48 -0700

    create table migrations

---
+++ db/migrate/20221129185918_create_user_parties.rb
@@ -0,0 +1,11 @@
+class CreateUserParties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_parties do |t|
+      t.integer :user_status
+      t.references :user, foreign_key: true
+      t.references :viewing_party, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
