320139e6a04bea1933126b45b004cdcbeee5c253
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-28 21:05:07 -0700

    create user_parties table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_29_033115) do
+ActiveRecord::Schema.define(version: 2022_11_29_040320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2022_11_29_033115) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_parties", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "party_id"
+    t.integer "user_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["party_id"], name: "index_user_parties_on_party_id"
+    t.index ["user_id"], name: "index_user_parties_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -31,4 +41,6 @@ ActiveRecord::Schema.define(version: 2022_11_29_033115) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_parties", "parties"
+  add_foreign_key "user_parties", "users"
 end
