31276d898377461e72f1f3e4ee1bf5e8e738be33
Eli Fuchsman <efuchsman@gmail.com>
2022-11-28 19:07:43 -0700

    Complete Landing page

--- spec/landing_spec.rb
+++ spec/landing_spec.rb
@@ -1,12 +1,13 @@
-# When a user visits the root path they should be on the landing page ('/') which includes:
-
-# Title of Application
-# Button to Create a New User
-# List of Existing Users which links to the users dashboard
-# Link to go back to the landing page (this link will be present at the top of all pages)
 require "rails_helper"
 
 RSpec.describe "Landing Page" do
+  before :each do
+    @user1 = create(:user)
+    @user2 = create(:user)
+    @user3 = create(:user)
+    @user4 = create(:user)
+  end
+
   describe "As a user" do
     describe "When I visit '/'" do
       it "Then I expect to see Title of Application" do
@@ -19,6 +20,41 @@ RSpec.describe "Landing Page" do
         visit "/"
 
         expect(page).to have_button("Create New User")
+      end
+
+      it "I see a List of Existing User email links" do
+
+        visit "/"
+        #  save_and_open_page
+
+        expect(page).to have_link(@user1.email)
+        expect(page).to have_link(@user2.email)
+        expect(page).to have_link(@user3.email)
+        expect(page).to have_link(@user4.email)
+      end
+    end
+
+    describe "When I click an email link" do
+      it "I am directed to that users show page" do
+        visit "/"
+
+        click_link(@user1.email)
+
+        expect(current_path).to eq("/users/#{@user1.id}")
+      end
+    end
+
+    describe "I see a home link" do
+      describe "When I click this link" do
+        it "I am taken to the landing page" do
+          visit "/"
+
+          expect(page).to have_link("Home")
+
+          click_link("Home")
+
+          expect(current_path).to eq("/")
+        end
       end
     end
   end
