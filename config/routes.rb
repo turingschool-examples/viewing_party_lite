ce43e6f8ff575e5ae25a6567292c3d9bdba61674
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-29 14:35:13 -0700

    Updates routes
    - [ ] I have performed a self-review of my code
    - [ ] If it is a core feature, I have added thorough tests.
    - [ ] Will this be part of a product update? If yes, please write one phrase about this update.
    
    Co-authored-by: Madeline Mauser <106927896+MadelineMauser@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
+
+  resources :users, only: %i[index show]
 
 end
