356f01c7f2a07b9ae205b7a1159f17a3dc701f5b
Will Wang <willy.j.wang@gmail.com>
2022-11-29 15:02:30 -0600

    Create discover movies page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe 'User Dashboard Page' do 
-  before :each do 
+RSpec.describe 'User Dashboard Page' do
+  before :each do
     @user1 = User.create!(name: "William", email: "William@gmail.com")
     @user2 = User.create!(name: "Ashley", email: "Ashley@gmail.com")
     @user3 = User.create!(name: "Abdul", email: "Abdul@gmail.com")
-    visit "users/#{@user1.id}"
+    visit(user_path(@user1.id)) #{}"users/#{@user1.id}"
   end
 
-  describe 'when I visit users/:id' do 
-    it 'I see the user names dashboard header at top of page' do 
-      
+  describe 'when I visit users/:id' do
+    it 'I see the user names dashboard header at top of page' do
+
       expect(page).to have_content("#{@user1.name}s Dashboard")
       expect(page).to_not have_content("#{@user2.name}s Dashboard")
     end
 
-    it 'I see a button to discover movies' do 
+    it 'I see a button to discover movies' do
       expect(page).to have_button("Discover Movies")
     end
 
-    it 'has a section that lists viewing parties' do 
+    it 'has a section that lists viewing parties' do
       expect(page).to have_content("#{@user1.name}s Viewing Parties")
       # save_and_open_page
     end
