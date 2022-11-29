9921ad11c6f0a540fe99ab5a199bc3d1d97351ff
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-28 17:59:22 -0700

    Add existing users with links to each users dashboard

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'the Welcome Page', type: :feature do
+  let!(:user_1) {User.create!(name: 'Naomi', email: 'nomi@yahoo.com')}
+  let!(:user_2) {User.create!(name: 'Joe', email: 'joe@hotmail.com')}
+  let!(:user_3) {User.create!(name: 'Zuko', email: 'zuko@gmail.com')}
+
   describe 'When I visit the welcome page' do
     it 'I see the title of the application' do
       visit root_path
@@ -16,12 +20,18 @@ RSpec.describe 'the Welcome Page', type: :feature do
       expect(current_path).to eq(register_path)
     end
 
-    xit 'I see a list of existing users which links to the users dashboard' do
+    it 'I see a list of existing users which links to the users dashboard' do
       visit root_path
-
+      
       within "#existing-users" do
-        expect(page).to have()
+        expect(page).to have_content(user_1.email)
+        expect(page).to have_content(user_2.email)
+        expect(page).to have_content(user_3.email)
+
+        click_link user_1.email
       end
+      save_and_open_page
+      expect(current_path).to eq(user_path(user_1))
     end
 
     it 'I see a link to go back to the landing page' do
