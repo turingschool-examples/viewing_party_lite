5ba46f22a2b6d10729c86d4a47ae772f986824b4
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-11-29 13:35:57 -0700

    Add user and party model validations with tests

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Party, type: :model do
     it {should have_many :user_parties }
     it {should have_many(:users).through(:user_parties) }
   end
+
+  describe 'validations' do
+    it {should validate_presence_of(:movie_title)}
+    it {should validate_presence_of(:duration)}
+    it {should validate_numericality_of(:duration)}
+    it {should validate_presence_of(:date)}
+    it {should validate_presence_of(:time)}
+    it {should validate_presence_of(:host_id)}
+  end
 end
