48697ce46e2b4c847dd81de0415e7852a253926e
Rich Kaht <adamselene1776@protonmail.com>
2022-11-28 19:01:42 -0700

    Feat: Add migrations
    Co-authored-by: Rich Kaht <108554663+Freeing3092@users.noreply.github.com>
    Co-authored-by: Sean Culliton <108320490+smculliton@users.noreply.github.com>

---
+++ db/migrate/20221129015446_create_movie_parties.rb
@@ -0,0 +1,12 @@
+class CreateMovieParties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movie_parties do |t|
+      t.string :movie_title
+      t.integer :duration
+      t.datetime :start_time
+      t.integer :movie_id
+
+      t.timestamps
+    end
+  end
+end
