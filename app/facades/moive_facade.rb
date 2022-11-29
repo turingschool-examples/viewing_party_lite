9bdd73acc766bd921f36735177e063459091a0ad
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-29 15:12:48 -0700

    movie facade methods

---
+++ app/facades/moive_facade.rb
@@ -0,0 +1,11 @@
+require_relative 'services/movie_service'
+
+class MovieFacade
+  def self.movie_title(movie_id)
+    MovieService.movie_details(movie_id)[:title]
+  end
+
+  def self.movie_image(movie_id)
+    MovieService.movie_details(movie_id)[:backdrop_path]
+  end
+end
