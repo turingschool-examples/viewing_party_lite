d3307775f2b54f03907f4366dd63768d8fa7a35a
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-29 16:55:36 -0700

    Tests features for discover#index to link to movies
    - [ ] I have performed a self-review of my code
    - [ ] If it is a core feature, I have added thorough tests.
    - [ ] Will this be part of a product update? If yes, please write one phrase about this update.
    
    Co-authored-by: Madeline Mauser <106927896+MadelineMauser@users.noreply.github.com>

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -9,9 +9,15 @@ RSpec.describe 'Discover Movies Page' do
   end
 
   describe 'index' do
-    it 'has a link to discover top rated movies' do
+    it 'has a link to discover top rated movies that links to movies results page' do
       expect(page).to have_link('Discover Top Rated Movies')
+      click_link('Discover Top Rated Movies')
+      expect(current_path).to eq("/users/#{@user.id}/movies")
     end
+
+    xit 'has a search bar that links to movies results page' do
+    end
+
 
   end
 
