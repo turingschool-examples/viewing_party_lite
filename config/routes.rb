f12e7f4f51066af156b8c4a3a5e493dbd2d5ad91
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-29 16:54:25 -0700

    Adds Movies resources to routes and controllers
    - [x] I have performed a self-review of my code
    - [x] If it is a core feature, I have added thorough tests.
    - [x] Will this be part of a product update? If yes, please write one phrase about this update.
    
    movies resource, controller, and views added in preparation for API data
    
    Co-authored-by: Madeline Mauser <106927896+MadelineMauser@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
   resources :users, only: %i[index show] do
     resources :discover, only: %i[index]
+    resources :movies, only: %i[index show]
   end
 
 end
