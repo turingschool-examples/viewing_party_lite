726d0883f84e0ad4d30defefa1618eb87d602e7d
Will Wang <willy.j.wang@gmail.com>
2022-11-28 18:04:39 -0600

    Create tables and relationships

---
+++ db/migrate/20221128234209_create_viewing_parties.rb
@@ -0,0 +1,13 @@
+class CreateViewingParties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :viewing_parties do |t|
+      t.string :movie_title
+      t.integer :movie_id
+      t.integer :duration
+      t.date :date
+      t.time :start_time
+
+      t.timestamps
+    end
+  end
+end
