a2761a084330610677a40fd0d758eb51a09d38ae
Eli Fuchsman <efuchsman@gmail.com>
2022-11-28 20:01:25 -0700

    Create migrations for viewing party and viewing party users, create user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :viewing_party_users
+  has_many :viewing_parties, through: :viewing_party_users
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_uniqueness_of :email
 end
