93c980365b2f7a9d2f574c88a54343c90e2d9f43
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-29 16:19:48 -0700

    Adds discover resource and corrects navbar link
    - [x] I have performed a self-review of my code
    - [ ] If it is a core feature, I have added thorough tests.
    - [ ] Will this be part of a product update? If yes, please write one phrase about this update.
    
    Co-authored-by: Madeline Mauser <106927896+MadelineMauser@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show] do
+    resources :discover, only: %i[index]
+  end
 
 end
