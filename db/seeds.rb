29d506a12eaf9aa0839adcfee093707f3adf0d58
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-29 15:07:26 -0700

    Adds seed data
    - [ ] I have performed a self-review of my code
    - [ ] If it is a core feature, I have added thorough tests.
    - [ ] Will this be part of a product update? If yes, please write one phrase about this update.
    
    Co-authored-by: Madeline Mauser <106927896+MadelineMauser@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+5.times do
+  User.create!(name: Faker::Name.unique.name, email: Faker::Internet.unique.free_email)
+end
