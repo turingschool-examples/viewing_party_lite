6966acd437abba3c84d2ccb0f8ec038782727c2f
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-29 00:37:09 -0700

    add all testing details to welcome index spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :create]
+
+  get '/register', to: 'users#new', as: 'new_user'
+  get '/users/:id/discover', to: 'users#discover', as: 'discover'
 end
