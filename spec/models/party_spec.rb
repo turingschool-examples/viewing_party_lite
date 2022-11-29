a405f3f0710cb764a0fcc2c118bcedb1d6571550
Darby Smith <newman.darby2@gmail.com>
2022-11-28 21:38:46 -0500

    Validations for party

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Party, type: :model do
     it { should have_many :user_parties }
     it { should have_many(:users).through(:user_parties) }
   end
+
+  describe 'validations' do
+    it { should validate_numericality_of :duration }
+    it { should validate_numericality_of :day }
+    it { should validate_numericality_of :start_time }
+    it { should validate_presence_of :movie_title }
+  end
 end
