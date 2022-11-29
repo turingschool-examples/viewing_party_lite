91d0f7d17f8e5a107a171ddd62f152e41db2502e
Eli Fuchsman <efuchsman@gmail.com>
2022-11-28 20:28:46 -0700

    Create model spec for viewing party user

--- app/models/viewing_party_user.rb
+++ app/models/viewing_party_user.rb
@@ -1,4 +1,7 @@
 class ViewingPartyUser < ApplicationRecord
   belongs_to :user
   belongs_to :viewing_party
+
+  validates_presence_of :host
+  validates_inclusion_of :host, :in => [true, false]
 end
