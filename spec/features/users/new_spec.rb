a67a70d84033456a311d0a857e1d7999c73ba861
Astrid Hecht <silentaviary@gmail.com>
2022-11-29 12:26:58 -0700

    fix: add missing user count snippet to sad path

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -20,7 +20,7 @@ Rspec.describe 'User Registration Page' do
         fill_in 'Name', with: 'Kenz'
         fill_in 'E-mail', with: 'kenz_mail@gmail.com'
 
-        expect {click_on 'Register'}.to change { User.count }.by(1)
+        expect { click_on 'Register' }.to change { User.count }.by(1)
         user = User.last
         expect(user.name).to eq('Kenz')
         expect(user.email).to eq('kenz_mail@gmail.com')
@@ -30,7 +30,7 @@ Rspec.describe 'User Registration Page' do
 
     context 'if filled out incorrectly, it flashes appropriate error and doesnt create user' do
       it 'with empty fields' do
-        expect { click_on 'Register' }.to change {User.count}.by(0)
+        expect { click_on 'Register' }.to change { User.count }.by(0)
         expect(page).to have_content("Name invalid: field can't be blank")
         expect(page).to have_content("E-mail invalid: field can't be blank")
         expect(current_path).to eq('/register')
@@ -39,17 +39,18 @@ Rspec.describe 'User Registration Page' do
       it 'with non-email string in email field' do
         fill_in 'Name', with: 'Cat'
         fill_in 'E-mail', with: 'What is email'
-        expect(page).to have_content("E-mail invalid: not an email address")
+        expect { click_on 'Register' }.to change { User.count }.by(0)
+        expect(page).to have_content('E-mail invalid: not an email address')
         expect(current_path).to eq('/register')
       end
 
       it 'given a duplicate E-mail' do
-        User.create!(name: "Astrid", email: "Astrid-mail@gmail.com")
+        User.create!(name: 'Astrid', email: 'Astrid-mail@gmail.com')
         fill_in 'Name', with: 'Astrid_2'
         fill_in 'E-mail', with: 'Astrid-mail@gmail.com'
 
-        expect { click_on 'Register' }.to change {User.count}.by(0)
-        expect(page).to have_content("E-mail invalid: already in use")
+        expect { click_on 'Register' }.to change { User.count }.by(0)
+        expect(page).to have_content('E-mail invalid: already in use')
         expect(current_path).to eq('/register')
       end
     end
