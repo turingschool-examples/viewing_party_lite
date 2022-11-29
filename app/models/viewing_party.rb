3edbba3db3001d699db4414799abb72ecd7aca9c
Annie Pulzone <ajpulzone@gmail.com>
2022-11-28 21:05:05 -0800

    Add additional validations to users and viewing parties

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -3,4 +3,11 @@
 class ViewingParty < ApplicationRecord
   has_many :user_viewing_parties
   has_many :users, through: :user_viewing_parties
+
+  validates_presence_of :movie_title
+  validates_presence_of :duration
+  validates_presence_of :date
+  validates_presence_of :start_time
+  validates_presence_of :status
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe User, type: :model do
     it { should have_many :user_viewing_parties }
     it { should have_many(:viewing_parties).through(:user_viewing_parties) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :email}
+    it { should validate_uniqueness_of :email}
+  end
 end
