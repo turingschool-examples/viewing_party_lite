7beb6a4318e41b3dfa8353496b86f9ff204f0b9d
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-28 20:32:03 -0700

    create parties table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_29_032743) do
+ActiveRecord::Schema.define(version: 2022_11_29_033115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "parties", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "duration"
+    t.date "date"
+    t.time "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
