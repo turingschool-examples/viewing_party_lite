8fa12206f2709467c248aff41f5f4aeee85697e8
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-30 12:32:29 -0700

    Begins to stub the API calls for testing with webmock
    - [x] I have performed a self-review of my code
    - [x] If it is a core feature, I have added thorough tests.
    - [x] Will this be part of a product update? If yes, please write one phrase about this update.
    
    Many products will be using data from the API
    
    Co-authored-by: Madeline Mauser <106927896+MadelineMauser@users.noreply.github.com>

--- spec/features/discover/tmdb_services_spec.rb
+++ spec/features/discover/tmdb_services_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe 'TMDB Search' do
 
   describe 'happy path' do
     it 'allows users to search for top rated movies' do
-
+      json_response = File.read('spec/fixtures/top_rated_movies.json')
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV.fetch('tmdb_api_key', nil)}").to_return(status: 200, body: json_response)
       visit user_discover_index_path(@user.id)
       click_link 'Discover Top Rated Movies'
 
