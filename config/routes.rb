3c6d3899b889212c17d3c25ce771dbeb3f348efe
Amanda Ross <amikaross@gmail.com>
2022-11-30 06:55:33 -0700

    Minor updates to have pages match the wireframes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   get "/users/:id/discover", to: "movies#discover"
 
-  get '/welcome', to: 'welcome#index'
+  get '/', to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
