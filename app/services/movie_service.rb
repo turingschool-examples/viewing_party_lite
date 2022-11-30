45c7724a6057529f17b0b93434c1c99184b78570
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-30 11:39:03 -0700

    fix api again

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -3,7 +3,6 @@ require 'faraday'
 class MovieService
   def self.movie_details(movie_id)
     conn = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
-      # faraday.headers["api_key"] = ENV["moviedb_api_key"]
       faraday.adapter Faraday.default_adapter
     end
 
