98484c4968151a40b63b74ea905508a5eec8fb2d
Mostafa Sakr <msakr3@gmail.com>
2022-11-29 17:50:15 -0700

    Add test and implementation code for User Dashboard initial set up for US 6

---
+++ spec/features/user/show_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'the user show page' do
+  before :each do
+    @mostafa = User.create!(name: "Mostafa", email: "sasa2020@hotmail.com")
+    @yuji = User.create!(name: "Yuji", email: "yuyuhakusho2020@hotmail.com")
+    @bryan = User.create!(name: "Bryan", email: "breakingbad2020@hotmail.com")
+  end
+
+  it "has <user's name>'s Dahsboard at the top of the page" do
+    visit "/users/#{@yuji.id}"
+
+    expect(page).to have_content("Yuji's Dashboard")
+    expect(page).to_not have_content("Bryan's Dashboard")
+
+    visit "/users/#{@bryan.id}"
+
+    expect(page).to have_content("Bryan's Dashboard")
+    expect(page).to_not have_content("Yuji's Dashboard")
+  end
+
+  it "has a button to discover movies" do
+    visit "/users/#{@yuji.id}"
+
+    expect(page).to have_button("Discover Movies")
+  end
+
+  it "has a section for viewing parties" do
+    visit "/users/#{@yuji.id}"
+
+    expect(page).to have_content("Viewing Parties")
+  end
+end
