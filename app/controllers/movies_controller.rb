da4c9e9c7c1900071641b4ceb91d511367cf3406
Emily Port <emilylport@gmail.com>
2022-11-30 09:12:51 -0700

    user discover path works to movies index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
-class MoviesController 
+class MoviesController < ApplicationController 
   def index 
+    @user = User.find(params[:user_id])
+  end
+  def top_rated 
     connection = Faraday.new(url: "https://api.themoviedb.org/3") do |faraday|
     faraday.params["api_key"] = ENV['movies_api_key']
     end
@@ -7,9 +10,10 @@ class MoviesController
     response = connection.get("/discover/movie?sort_by=popularity.desc")
     json_body = response.body 
     parsed_response = JSON.parse(json_body, symbolize_names: true)
+    @popular = parsed_response[:results][0][:original_title]
+  
 
    
   end
-  require 'pry'; binding.pry
 end
 
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
-RSpec.describe "movies " do 
+RSpec.describe "discover movies page or movies index page " do
+  describe 'discover page' do 
+    before :each do 
+      @steve = User.create!(name: "Steve", email: "steve.smith@gmail.com")
+      @mary = User.create!(name: "Mary", email: "mary.smith@gmail.com")
+    end
+    it 'has a button to find top rated movies' do 
+      visit user_discover_path(@steve)
+      require 'pry'; binding.pry
+
+      click_button "Find Top Rated Movies"
+      expect(page.status_code).to eq 200 
+      expect(page).to have_content("Black Adam")
+    end
+  end
   
 end
