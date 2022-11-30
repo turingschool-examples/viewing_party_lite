e6accfe1cfd061a54cd2e99542d7136e6fd57748
Sage Skaff <skaffsage@gmail.com>
2022-11-30 15:23:05 -0800

    Update landing page controller, routes, and test for discover movies button

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing_page#index'
-  
+
   get '/register', to: 'users#new'
   post '/register', to: 'users#new'
 
-  resources :users
-
-  resources :users, path_names: { new: 'register' }
+  resources :users do
+    # shallow_path: '/register', shallow: true do
+    # get'/register', to: 'users#new', shallow: true
+    # post '/register', to: 'users#new', shallow: true
+    get '/discover', to: 'movies#discover'
+    resources :movies do
+      resources :viewing_parties
+    end
+  end
   # get '/register', to: 'users#index'
 end
---
+++ spec/features/users/discover_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover Movies Page' do
+  # As a user,
+  # When I go to a user dashbaord,
+  # and click "Discover Movies" button,
+  # I am redirected to a discover page '/users/:id/discover',
+  # where :id is the user id of the user who's dashboard I was just on.
+end
