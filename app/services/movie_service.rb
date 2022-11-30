9cd6f1005ea687add25e39cba14dec6b2aae7a7a
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-30 11:20:55 -0700

    fix api consumption

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -2,12 +2,12 @@ require 'faraday'
 
 class MovieService
   def self.movie_details(movie_id)
-    conn = Faraday.new(url: "https://api.themoviedb.org/3") do |faraday|
+    conn = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
       # faraday.headers["api_key"] = ENV["moviedb_api_key"]
       faraday.adapter Faraday.default_adapter
     end
 
-    response = conn.get("/movies/#{movie_id}?api_key=#{ENV["moviedb_api_key"]}")
+    response = conn.get("movie/#{movie_id}?api_key=#{ENV["moviedb_api_key"]}")
     json = JSON.parse(response.body, symbolize_names: true)
   end
 end
