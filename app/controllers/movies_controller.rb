d1b7e80ea3cc3b455cee4a5a7a1aab12c90b6c5f
Ashley <ashuhleyt@gmail.com>
2022-11-30 09:39:47 -0800

    updated PR template

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   def index
     if params[:title] != nil
       @movies = MovieFacade.search(params[:title])
-      # require "pry"; binding.pry
+      
     else
       @movies = MovieFacade.top_rated
     end
