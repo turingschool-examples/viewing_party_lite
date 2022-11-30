5aa75429e51d95d189d31a7886574f44405c54ab
Rich Kaht <adamselene1776@protonmail.com>
2022-11-30 14:47:20 -0700

    Feat/Test: Add MovieHeavy PORO

---
+++ app/poros/movie_heavy.rb
@@ -0,0 +1,27 @@
+require './app/services/movies_service'
+
+class MovieHeavy
+  attr_reader :id,
+              :movie_title,
+              :vote_average,
+              :runtime,
+              :genres,
+              :summary,
+              :first_10_cast_members,
+              :reviews_count
+
+  def initialize(data)
+    @id = data[:id]
+    @movie_title = data[:title]
+    @vote_average = data[:vote_average]
+    @runtime = data[:runtime]
+    @genres = data[:genres]
+    @summary = data[:overview]
+    @first_10_cast_members = MoviesService.credits(data[:id])[:cast].first(10)
+    @reviews_count = MoviesService.reviews(data[:id])[:results].size
+  end
+
+  def get_cast
+    MoviesService.credits(@id)[:cast].first(10)
+  end
+end
