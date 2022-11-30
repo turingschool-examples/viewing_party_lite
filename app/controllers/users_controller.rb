624b90d4ce19d89e03d5704d3c0601fe81f63211
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-29 18:15:54 -0700

    Add user controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    new_user = User.create(user_params)
+    if new_user.exists?
+      redirect_to user_path(new_user)
+    else
+      render '/register'
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
 end
