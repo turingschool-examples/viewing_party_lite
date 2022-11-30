cb867bb200c64a7393f32a64fd16ecdcec3491c3
Lucas Colwell <colwellc37@gmail.com>
2022-11-30 14:29:12 -0700

    Add controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   def index
+    #draft/practice
     conn = Faraday.new(
       url: "https://api.themoviedb.org",
       params: {api_key: Figaro.env.tmdb_api_key}
@@ -8,7 +9,7 @@ class MoviesController < ApplicationController
     response = conn.get("/3/movie/top_rated")
     json_body = response.body
     parsed_response = JSON.parse(json_body, symbolize_names: true)
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
 
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to "/users/#{@user.id}"
+    else
+      render new
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
 end
