90669d06659e7111649dfdc5f38a7b58a622f5ad
Alex M <alexm0717@hotmail.com>
2022-11-30 14:14:06 -0800

    added top rated movie search spec

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -12,6 +12,11 @@ RSpec.describe("discover movies page or movies index page ") do
       it("shows a list of top rated movies from the api") do
         visit(user_discover_path(@steve))
         click_button("Find Top Rated Movies")
+        expect(current_path).to(eq("/users/#{@steve.id}/movies"))
+        expect(page.status_code).to(eq(200))
+        expect(page).to(have_content("The Godfather Vote Average: 8.7"))
+        expect(page).to(have_content("20th Century Girl Vote Average: 8.7"))
+        expect(page).to(have_content("Dilwale Dulhania Le Jayenge Vote Average: 8.6"))
       end
     end
 
