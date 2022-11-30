e7725028193ca56669f411c9ef32ad4c8ba75b79
Annie Pulzone <ajpulzone@gmail.com>
2022-11-29 18:57:24 -0800

    Add functionality to add new user and associated testing/views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      redirect_to "/register", alert: "Your request could not be completed, please fill out all fields/choose an email that has not already been claimed"
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
+
 end
---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,58 @@
+require "rails_helper"
+
+RSpec.describe "New Users Page", type: :feature do 
+
+  before(:each) do
+    @user1 = User.create!(name: 'Chad', email: 'chad1@gmail.com')
+    @user2 = User.create!(name: 'Jessica', email: 'jessica2@gmail.com')
+    @user3 = User.create!(name: 'Fiona', email: 'Fiona3@gmail.com')
+  end
+
+  it "has a form to register a new user, that includes name, email (which is unique) and a 
+    'Register' button" do
+      visit "/register"
+
+      expect(page).to have_field(:name)
+      expect(page).to have_field(:email)
+      expect(page).to have_button("Register")
+  end
+
+  it "once the form is properly filled out, the user will be taken to their dashboard page ('/users/:id') where the id
+    is for the user that was just created" do
+      visit "/register"
+
+      fill_in :name,	with: "Max"
+      fill_in :email,	with: 'WhatsForDinner@gmail.com'
+      click_button "Register"
+
+      @user = User.last
+
+      expect(current_path).to eq(user_path(@user.id))
+  end
+
+  it "if the form is not completely filled in, the user will be redirected back to the '/register' page
+    and see an alert to complete the form" do
+      visit "/register"
+
+      expect(page).to have_no_content("Your request could not be completed, please fill out all fields/choose an email that has not already been claimed")
+      
+      fill_in :name,	with: ''
+      fill_in :email,	with: 'pieisdelici0us@gmail.com'
+      click_button "Register"
+
+      expect(current_path).to eq("/register")
+      expect(page).to have_content("Your request could not be completed, please fill out all fields/choose an email that has not already been claimed")
+  end
+
+  it "if the email is not unique, the user will be redirected to back to the '/register page and see
+    an alert that the email they entered has already been taken, choose another" do
+      visit "/register"
+
+      expect(page).to have_no_content("Your request could not be completed, please fill out all fields/choose an email that has not already been claimed")
+      fill_in :name,	with: "Tammy"
+      fill_in :email,	with: 'chad1@gmail.com'
+      click_button "Register"
+
+      expect(page).to have_content("Your request could not be completed, please fill out all fields/choose an email that has not already been claimed")
+  end
+end
