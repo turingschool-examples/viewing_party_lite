1f54211dd1a694b178ac587440e5117af462bd2c
James White <whitejaad@gmail.com>
2022-11-30 10:40:30 -0800

    fixes viewing party model test

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -23,6 +23,6 @@ class ViewingParty < ApplicationRecord
     #this will return the url path for the movie image for that party
     movie = MovieSearch.new.movie(movie_id)
     config_path = ConfigSearch.new.base_path
-    "#{config_path}w200/#{movie.partial_image_path}"
+    "#{config_path}w200#{movie.partial_image_path}"
   end
 end
