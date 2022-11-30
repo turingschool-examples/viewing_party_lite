a1beec25ff1a68b3a6b514acadb5407aa2d060f3
Annie Pulzone <ajpulzone@gmail.com>
2022-11-29 18:55:44 -0800

    Add flash message authorization

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing_page#index'
   
+  get '/register', to: 'users#new'
   post '/register', to: 'users#new'
-  
+
   resources :users
+
 end
