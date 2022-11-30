63bf14405d7d9260a142dfea4d392980b1a8c5eb
Sean Culliton <smculliton@gmail.com>
2022-11-29 20:33:00 -0700

    Fix tests

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -8,13 +8,17 @@ RSpec.describe 'the movie discover page' do
   end
 
   it 'has a button to discover top rated movies' do 
-    click_button 'Discover Top Rated Movies'
-    expect(current_path).to eq(user_movies_path(@user))
+    VCR.use_cassette('top_rated_movies') do 
+      click_button 'Discover Top Rated Movies'
+      expect(current_path).to eq(user_movies_path(@user))
+    end
   end
 
   it 'has a search field to search by a movie title' do 
-    expect(page).to have_field('Search')
-    click_button 'Search by Movie Title'
-    expect(current_path).to eq(user_movies_path(@user))
+    VCR.use_cassette('top_rated_movies') do 
+      expect(page).to have_field('Search')
+      click_button 'Search by Movie Title'
+      expect(current_path).to eq(user_movies_path(@user))
+    end
   end
 end
