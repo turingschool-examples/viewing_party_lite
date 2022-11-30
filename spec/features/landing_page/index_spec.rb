630dfd518baa9ff724e6ba478ba12676d6df5b67
Lucas Colwell <colwellc37@gmail.com>
2022-11-29 19:05:45 -0700

    Test: add landing page test

---
+++ spec/features/landing_page/index_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe 'landing Page', type: :feature do
+  describe 'As a User' do
+    describe 'When I visit the Landing Page' do
+      before(:each) do
+        @user1 = User.create!(name: 'Mary', email: 'mary@gmail.com')
+        @user2 = User.create!(name: 'Larry', email: 'larry@hotmail.com')
+        @user3 = User.create!(name: 'Sherri', email: 'sherri@aol.com')
+
+        visit root_path
+      end
+
+      it 'I see the title of the applications' do
+        within ('#app_title') do
+          expect(page).to have_content 'Viewing Party'
+        end
+      end
+
+      it 'I see a button to create a new user' do
+        within('#user_creation') do
+          expect(page).to have_button('Create User')
+        end
+      end
+
+      it 'I see a list of existing users, as a link to each users dashboards' do
+        within('#existing_users') do
+          within("#user-#{@user1.id}") do
+            expect(page).to have_link("#{@user1.email}'s dashboard'")
+            expect(page).to_not have_link("#{@user2.email}'s dashboard'")
+            expect(page).to_not have_link("#{@user3.email}'s dashboard'")
+          end
+          within("#user-#{@user2.id}") do
+            expect(page).to have_link("#{@user2.email}'s dashboard'")
+            expect(page).to_not have_link("#{@user1.email}'s dashboard'")
+            expect(page).to_not have_link("#{@user3.email}'s dashboard'")
+          end
+          within("#user-#{@user3.id}") do
+            expect(page).to have_link("#{@user3.email}'s dashboard'")
+            expect(page).to_not have_link("#{@user1.email}'s dashboard'")
+            expect(page).to_not have_link("#{@user2.email}'s dashboard'")
+          end
+        end
+      end
+
+      it 'I see a link back to the home page' do
+        expect(page).to have_link('Home')
+        click_link 'Home'
+        expect(page.current_path).to eq root_path
+      end
+    end
+  end
