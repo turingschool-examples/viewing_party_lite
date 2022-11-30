88164a4147ba6243f2878b50e0d037cbaf66cc80
Darby Smith <newman.darby2@gmail.com>
2022-11-30 11:13:03 -0500

    WIP discover movies page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :discover, only: [:index]
+    resources :movies, only: [:index]
   end
 
   resources :register, only: [:new, :create], controller: "users"
---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'user discover movies page' do
+  before :each do
+    @user_1 = User.create!(name: 'Jim', email: 'jim.halpert@gmail.com')
+    @user_2 = User.create!(name: 'Pam', email: 'pam.halpert@gmail.com')
+  end
+  
+  it 'has a button to discover top rated movies' do
+    visit user_discover_index_path(@user_1)
+
+    expect(page).to have_button("Find Top Rated Movies")
+
+    click_on "Find Top Rated Movies"
+
+    expect(current_path).to eq("/users/#{@user_1.id}/movies?q=top%20rated")
+  end
+
+  it 'has a text field to enter keywords and button to search by movie title' do
+    visit user_discover_index_path(@user_1)
+
+    expect(page).to have_field(:search)
+    expect(page).to have_button("Find Movies")
+    
+    fill_in :search, with: "Nemo"
+    click_on "Find Movies"
+
+    expect(current_path).to eq("/users/#{@user_1.id}/movies?q=Nemo")
+  end
+end
