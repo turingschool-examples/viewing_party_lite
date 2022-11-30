e0e599b4df977ce1938578607aab38f5ee7a900e
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-30 11:22:59 -0700

    Implement webmock/VCR with movie results testing

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -3,18 +3,27 @@ require 'rails_helper'
 RSpec.describe 'the Movies Index Page', type: :feature do
   let!(:user_1) { create(:user) }
   before(:each) do
+    VCR.insert_cassette "top rated"
     visit "/users/#{user_1.id}/discover"
     click_button 'Find Top Rated Movies'
+  end
+  after(:each) do
+    VCR.eject_cassette
   end
 
   describe 'After clicking the Top Movies button or Search button to lead me to the movies index page' do
     it 'I should see the movie title as a link to the movie details page and the vote average' do
+      expect(page.status_code).to eq 200
       expect(current_path).to eq(user_movies_path(user_1))
 
       within "#movie-results" do
         expect(page).to have_content("The Godfather")
         expect(page).to have_content("8.7")
       end
+    end
+
+    it 'should have a maximum of 20 movie results' do
+      expect(page).to have_css(".results", count: 20)
     end
 
     it 'I see a button that takes me back to the Discover Page' do
--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -5,23 +5,33 @@ RSpec.describe 'the Discover Movies Page', type: :feature do
   before(:each) do
     visit "/users/#{user_1.id}/discover"
   end
-
+  after(:each) do
+    VCR.eject_cassette
+  end
   describe 'When I visit the user discover path' do
     it 'I see a button to Discover Top Rated Movies that takes me to the movies results page' do
+      VCR.insert_cassette "top rated"
+      visit "/users/#{user_1.id}/discover"
+
       within "#top-rated" do
         click_button 'Find Top Rated Movies'
       end
       
+      expect(page.status_code).to eq 200
       expect(current_path).to eq(user_movies_path(user_1))
     end
 
     it 'I see a text field to enter keyword(s) and a button to search by movie title that takes me to the movies results page' do
+      VCR.insert_cassette "searches"
+      visit "/users/#{user_1.id}/discover"
+
       within "#find-movie" do
         fill_in :search, with: 'princess'
 
         click_button 'Find Movies'
       end
-
+      
+      expect(page.status_code).to eq 200
       expect(current_path).to eq(user_movies_path(user_1))
     end
   end
