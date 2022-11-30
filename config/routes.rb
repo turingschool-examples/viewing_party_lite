bfeb2d5d5153332a5d6ae44166cb040fe1fcb331
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-29 17:50:11 -0700

    Add user create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show create]
 
 end
