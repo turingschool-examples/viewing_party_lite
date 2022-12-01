bd59b256cc53058376828b47d9821ae895b685d2
Rich Kaht <adamselene1776@protonmail.com>
2022-11-30 18:31:07 -0700

    Feat/Test: Add Movie Details View

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,9 @@ class MoviesController < ApplicationController
     @user = User.find(params[:user_id])
     @movies = MoviesFacade.top_rated
   end
+
+  def show
+    @user = User.find(params[:user_id])
+    @movie = MovieHeavy.new(MoviesService.details(params[:id]))
+  end
 end
