3ca77ed18f9159646836d4813064ede8f68014f6
Rich Kaht <adamselene1776@protonmail.com>
2022-12-01 16:14:19 -0700

    Feat/Test: Add Review PORO

---
+++ spec/poros/review_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Review do
+  describe 'initialize' do
+    it 'has readable attributes' do
+      VCR.use_cassette('movie_reviews') do
+        data = MoviesService.reviews(238)[:results].first
+        @review = Review.new(data)
+      end
+
+      expect(@review).to be_a Review
+      expect(@review.author).to be_a String
+      expect(@review.content).to be_a String
+    end
+  end
+end
