f5e0724f0772e4ee5b0e001530e87bc474ec6d38
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-01 10:50:46 -0700

    Add .invited_users to party

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,8 @@
 class Party < ApplicationRecord
   has_many :party_users
   has_many :users, through: :party_users
+
+  def invited_users
+    users.where('party_users.host = ?', false).pluck(:name)
+  end
 end
