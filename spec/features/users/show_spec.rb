f9870e7c8129dd07017d1bdb024b6a64b9ba0c03
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-30 18:11:57 -0700

    Add test setup

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -4,8 +4,32 @@ require 'faker'
 RSpec.describe 'Users' do
   before(:each) do
     @user1 = User.create!(name: Faker::Name.unique.name, email: Faker::Internet.unique.free_email)
+    @user2 = User.create!(name: Faker::Name.unique.name, email: Faker::Internet.unique.free_email)
+    @user3 = User.create!(name: Faker::Name.unique.name, email: Faker::Internet.unique.free_email)
+    @user4 = User.create!(name: Faker::Name.unique.name, email: Faker::Internet.unique.free_email)
 
-    visit "/users/#{@user.id}"
+    @party1 = Party.create!(date: (DateTime.now + rand(90).days), start_time: "#{rand(1..24)}:00", movie_title: "Up", duration: (240 + rand(30)))
+    @party2 = Party.create!(date: (DateTime.now + rand(90).days), start_time: "#{rand(1..24)}:00", movie_title: "Alien", duration: (240 + rand(30)))
+    @party3 = Party.create!(date: (DateTime.now + rand(90).days), start_time: "#{rand(1..24)}:00", movie_title: "Whiplash", duration: (240 + rand(30)))
+    @party4 = Party.create!(date: (DateTime.now + rand(90).days), start_time: "#{rand(1..24)}:00", movie_title: "Toy Story", duration: (240 + rand(30)))
+    @party5 = Party.create!(date: (DateTime.now + rand(90).days), start_time: "#{rand(1..24)}:00", movie_title: "Brave", duration: (240 + rand(30)))
+    
+    @party_user1 = PartyUser.create!(user_id: @user1.id, party_id: @party1.id, host: true)
+    @party_user2 = PartyUser.create!(user_id: @user1.id, party_id: @party2.id, host: true)
+    @party_user3 = PartyUser.create!(user_id: @user1.id, party_id: @party3.id, host: false)
+    @party_user4 = PartyUser.create!(user_id: @user1.id, party_id: @party4.id, host: false)
+
+    @party_user5 = PartyUser.create!(user_id: @user2.id, party_id: @party3.id, host: true)
+    @party_user6 = PartyUser.create!(user_id: @user3.id, party_id: @party4.id, host: true)
+
+    @party_user7 = PartyUser.create!(user_id: @user2.id, party_id: @party1.id, host: false)
+    @party_user8 = PartyUser.create!(user_id: @user2.id, party_id: @party2.id, host: false)
+    @party_user9 = PartyUser.create!(user_id: @user3.id, party_id: @party1.id, host: false)
+
+    @party_user10 = PartyUser.create!(user_id: @user4.id, party_id: @party5.id, host: true)
+    @party_user11 = PartyUser.create!(user_id: @user3.id, party_id: @party5.id, host: false)
+    
+    visit "/users/#{@user1.id}"
   end
 
   describe 'show' do
