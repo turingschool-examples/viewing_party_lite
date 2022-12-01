b649ce59c9c0747696bca2e680b0553a2522a552
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-01 16:09:27 -0700

    Create initial discover page test file, move discover page view to users folder

---
+++ spec/features/users/discover/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'The Discover Movies Page' do
+  describe 'As a User' do
+    describe 'When I visit the Discover Movies Page' do
+      before(:each) do
+        @user1 = User.create!(name: 'Mary', email: 'mary@gmail.com')
+        @user2 = User.create!(name: 'Larry', email: 'larry@hotmail.com')
+        @user3 = User.create!(name: 'Sherri', email: 'sherri@aol.com')
+      end
+
+      it "displays a button to 'Find Top Rated Movies'" do
+        visit user_discover_index_path(@user1)
+
+        expect(page).to have_button('Find Top Rated Movies')
+      end
+    end
+  end
+
+end
