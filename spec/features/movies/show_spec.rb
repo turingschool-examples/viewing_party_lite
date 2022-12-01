f990087f0935564f0243527fa38cb0c50111746f
Mackenzie Leng <kenz.joy@outlook.com>
2022-12-01 16:44:04 -0700

    create testing shell for movies show spec

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper' 
+
+RSpec.describe 'movie detail page' do
+  describe 'as a user' do
+    describe 'when I visit user_movie_path(user_id, movie_id)' do
+      it '- shows a button to create a viewing party' do
+
+      end
+
+      it '- shows a button to return to the discover page' do
+
+      end
+
+      it '- shows the movie title' do
+
+      end
+
+      it '- shows the vote average of the movie' do
+
+      end
+
+      it '- shows the runtime in hours & minutes' do
+
+      end
+
+      it '- shows the genre(s) associated to the movie' do
+
+      end
+
+      it '- shows the summary description' do
+
+      end
+
+      it '- shows the first 10 cast members (characters & actors)' do
+
+      end
+
+      it '- shows the count of total reviews' do 
+
+      end
+
+      it '- shows each reviews author and information' do 
+        
+      end
+    end
+  end
+end
