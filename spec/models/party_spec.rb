8e4c3b4f2c1964c5ab0e77484ecc78eb417f38b3
Mostafa Sakr <msakr3@gmail.com>
2022-11-30 23:28:00 -0700

    Set up before each block for US 8.

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe Party, type: :model do
     it {should validate_presence_of(:time)}
     it {should validate_presence_of(:host_id)}
   end
+
+  describe 'methods' do
+    before :each do
+      @mostafa = User.create!(name: "Mostafa", email: "sasa2020@hotmail.com")
+      @yuji = User.create!(name: "Yuji", email: "yuyuhakusho2020@hotmail.com")
+      @bryan = User.create!(name: "Bryan", email: "breakingbad2020@hotmail.com")
+
+      @party1 = Party.create!(movie_title: "The Godfather", duration: 175, date: "12/1/2022", time: "19:00", host_id: @mostafa.id)
+      @party2 = Party.create!(movie_title: "Young Adam", duration: 93, date: "12/4/2022", time: "18:00", host_id: @mostafa.id)
+      @party3 = Party.create!(movie_title: "Natural Born Killers", duration: 118, date: "12/7/2022", time: "20:00", host_id: @mostafa.id)
+      @party4 = Party.create!(movie_title: "Pulp Fiction", duration: 154, date: "12/8/2022", time: "19:30", host_id: @yuji.id)
+      @party5 = Party.create!(movie_title: "Up", duration: 96, date: "12/13/2022", time: "17:00", host_id: @bryan.id)
+    end
+  end
 end
