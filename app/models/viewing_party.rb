f3964ae863ce4d81fcc557dc52cf5c436f527506
Eli Fuchsman <efuchsman@gmail.com>
2022-11-30 19:59:52 -0700

    Create model methods and tests for finding a viewing party movie poster
    Co-authored-by: Kristen Nestler <Kristen.Nestler@gmail.com>

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -10,4 +10,16 @@ class ViewingParty < ApplicationRecord
   validates_numericality_of :duration, greater_than: 0
   validates_presence_of :date
   validates_presence_of :start_time
+
+  def viewing_party_movie
+    MovieService.movie_by_id(movie_id)
+  end
+
+  def poster_path
+    viewing_party_movie[:poster_path]
+  end
+
+  def full_poster_path
+    "https://image.tmdb.org/t/p/original#{poster_path}"
+  end
 end
