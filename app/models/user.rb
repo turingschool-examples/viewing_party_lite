e0b24b25867c16c87240126b47291dd6cb39d65d
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-01 10:55:27 -0700

    Add .parties_invited_to to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  def parties_invited_to
+    parties.where('party_users.host = ?', false)
+  end
 end
