cda873d0908fa6d993f33c8a972f7b0455c9ba62
Emily Port <emilylport@gmail.com>
2022-11-30 21:32:04 -0700

    delete unused model file

--- app/models/discover.rb
+++
@@ -1,3 +0,0 @@
-class Discover < ApplicationRecord
-  
-end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -9,11 +9,9 @@ RSpec.describe "movies show page" do
   it 'displays a movies information from multiple API calls on a movie show page', :vcr do 
     visit(user_discover_path(@steve))
     click_button("Find Top Rated Movies")
-    # expect(current_path).to eq("users/#{@steve.id}/movies?q=top+rated")
     expect(page).to have_link("The Godfather")
     click_link "The Godfather"
     expect(page.status_code).to eq 200
-    # expect(current_path).to eq("users/#{@steve.id}/movies/#{result[:id]}")
     expect(page).to have_content("Genre: [\"Drama\", \"Crime\"]")
     expect(page).to have_content("Vote: 8.715")
     expect(page).to have_content("Actor Name: Marlon Brando Character Name: Don Vito Corleone")
@@ -21,7 +19,5 @@ RSpec.describe "movies show page" do
     expect(page).to have_button("Discover Movies")
     click_button "Discover Movies"
     expect(current_path).to eq(user_discover_path(@steve))
-    # expect(page).to have_button("Create Viewing Party for The Godfather")
-
   end
 end
