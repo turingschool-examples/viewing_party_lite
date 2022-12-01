6f9c1b789eaa8de786a6518f759d01ff76bb5d0b
Naomi Yocum <naomiyocum24@gmail.com>
2022-12-01 09:48:38 -0700

    Complete testing for the View Partiew new spec

--- spec/features/view_parties/new_spec.rb
+++ spec/features/view_parties/new_spec.rb
@@ -13,11 +13,16 @@ RSpec.describe 'New View Party Page', type: :feature do
 
   before(:each) do
     VCR.insert_cassette 'view party'
+    VCR.insert_cassette 'movie show'
+    VCR.insert_cassette 'image url'
     visit new_user_movie_view_party_path(user_1, movie_1.id)
   end
   after(:each) do
     VCR.eject_cassette
+    VCR.eject_cassette
+    VCR.eject_cassette
   end
+
   describe 'When I visit the new viewing party page' do
     it 'I should see the name of the movie title rendered above a form and a button to the discover page' do
       within '#header' do
@@ -28,12 +33,11 @@ RSpec.describe 'New View Party Page', type: :feature do
       expect(current_path).to eq("/users/#{user_1.id}/discover")
     end
 
-    xit 'the form should include the following fields: name, duration, when, start time, checkboxes next to each existing user in the system, after submitting I should be taken back to my dashboard where I see the new event' do
+    it 'the form should include the following fields: name, duration, when, start time, checkboxes next to each existing user in the system, after submitting I should be taken back to my dashboard where I see the new event' do
       within '#form' do
         fill_in :duration, with: 400
         fill_in :datetime, with: '2023-02-02 01:00:00 UTC '
-        check user_2.id.to_s
-
+        check "invite_users_#{user_2.id}"
         click_button 'Create Party'
       end
 
@@ -41,17 +45,28 @@ RSpec.describe 'New View Party Page', type: :feature do
 
       recent_view_party = ViewParty.last
       within "##{recent_view_party.movie_id}" do
-        expect(page).to have_content(view_party_1.movie_name)
-        expect(page).to have_content(view_party_1.datetime.strftime('%A, %d %B %Y'))
+        expect(page).to have_content(recent_view_party.movie_name)
+        expect(page).to have_content(recent_view_party.datetime.strftime('%A, %d %B %Y'))
         expect(page).to have_content('Hosting')
       end
     end
 
-    xit 'the duration in the form cannot be set to a value less than the duration of the movie' do
+    it 'if fields are filled out incorrectly the user will be redirected to the same page and see error messages' do
+      within '#form' do
+        fill_in :duration, with: 200
+        fill_in :datetime, with: '2019-02-02 01:00:00 UTC '
+        click_button 'Create Party'
+      end
+
+      expect(current_path).to eq(new_user_movie_view_party_path(user_1, movie_1.id))
+      expect(page).to have_content('Datetime cannot be in the past')
+    end
+
+    it 'the duration in the form cannot be set to a value less than the duration of the movie' do
       within '#form' do
         fill_in :duration, with: 1
         fill_in :datetime, with: '2023-02-02 01:00:00 UTC '
-
+        check "invite_users_#{user_2.id}"
         click_button 'Create Party'
       end
 
@@ -59,7 +74,31 @@ RSpec.describe 'New View Party Page', type: :feature do
       expect(page).to have_content('Party duration cannot be shorter than the movie duration')
     end
 
-    xit 'the new view party should also show on dashboards of users that were invited' do
+    it 'the new view party should also show on dashboards of users that were invited' do
+      within '#form' do
+        fill_in :duration, with: 400
+        fill_in :datetime, with: '2023-02-02 01:00:00 UTC '
+        check "invite_users_#{user_2.id}"
+        click_button 'Create Party'
+      end
+
+      recent_view_party = ViewParty.last
+      
+      VCR.eject_cassette
+      VCR.eject_cassette
+      VCR.eject_cassette
+
+      VCR.insert_cassette 'view party'
+      VCR.insert_cassette 'movie show'
+      VCR.insert_cassette 'image url'
+
+      visit user_path(user_2)
+
+      within "##{recent_view_party.movie_id}" do
+        expect(page).to have_content(recent_view_party.movie_name)
+        expect(page).to have_content(recent_view_party.datetime.strftime('%A, %d %B %Y'))
+        expect(page).to have_content('Invited')
+      end
     end
   end
 end
