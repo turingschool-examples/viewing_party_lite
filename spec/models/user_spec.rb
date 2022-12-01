719b937356e11d3a948f7691a514e239516e8c7b
Lucas Colwell <colwellc37@gmail.com>
2022-12-01 16:08:04 -0700

    Test: add validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many :viewing_party_users }
     it { should have_many(:viewing_parties).through(:viewing_party_users) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   end
 
   describe 'validations' do
