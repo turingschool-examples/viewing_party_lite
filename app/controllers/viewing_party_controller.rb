22b5ebcd87d73cc7a36fd3ec018ce03b2bf88f37
Will Wang <willy.j.wang@gmail.com>
2022-12-01 16:57:35 -0600

    Create new viewing party page

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,5 +1,23 @@
 class ViewingPartyController < ApplicationController
   def new
-    
+    @user = User.find(params[:user_id])
+    @users = User.all
+    @movie = MovieFacade.find_movie(params[:movie_id])
+  end
+  def create
+    user = User.find(params[:user_id])
+    movie = MovieFacade.find_movie(params[:movie_id])
+    viewing_party = ViewingParty.create!(movie_id: movie.id, movie_title: movie.title, date: params[:date], start_time: params[:time], duration: params[:duration])
+
+    params[:users].each do |user_id|
+      if user_id == params[:user_id]
+        UserViewingParty.create!(user_id: user_id, viewing_party_id: viewing_party.id, host: true)
+      else
+        UserViewingParty.create!(user_id: user_id, viewing_party_id: viewing_party.id, host: false)
+      end
+    end
+
+    redirect_to user_path(user.id)
+    # require "pry"; binding.pxry
   end
 end
