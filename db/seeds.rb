07b0a48fd1001e67f284dadbf1e2632c83cb11ea
Eli Fuchsman <efuchsman@gmail.com>
2022-12-01 09:48:19 -0700

    Seed more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user1 = User.create!(name: "Eli Fuchsman", email: "eli@gmail.com")
+user2 = User.create!(name: "Kristen Nestler", email: "kristen@gmail.com")
+user3 = User.create!(name: "John John", email: "John@gmail.com")
+user4 = User.create!(name: "Luke Skywalker", email: "luke@gmail.com")
+user5 = User.create!(name: "Michael Corleone", email: "mike@gmail.com")
+user6 = User.create!(name: "Tony Montana", email: "tony@gmail.com")
+user7 = User.create!(name: "Bruce Wayne", email: "bruce@gmail.com")
+user8 = User.create!(name: "Harvey Dent", email: "harvey@gmail.com")
+
 viewing_party1 = ViewingParty.create!(movie_id: 11, duration: 200, date: 'November 30, 2022',
                                        start_time: '7 PM')
 viewing_party2 = ViewingParty.create!(movie_id: 550, duration: 150, date: 'December 14, 2022',
