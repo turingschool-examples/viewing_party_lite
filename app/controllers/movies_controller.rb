00a2275663f1f832c32bb0bc27d316bd2bb6bb2c
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-12-01 19:00:46 -0700

    Add discover page button to movies index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,17 +2,15 @@ require './app/services/movies_service'
 
 class MoviesController < ApplicationController
   def index
-  
     @user = User.find(params[:id])
     if params[:q] == "top_rated"
       data = MoviesService.new.search("top_rated")
       movies = data[:results][1..20]
-      @top_20_rated = movies.pluck(:original_title, :id, :vote_average)
+      @movies = movies.pluck(:original_title, :id, :vote_average)
     else
-      params[:q]
       data = MoviesService.new.keyword_search(params[:q])
       movies = data[:results][1..20]
-      @top_20_rated = movies.pluck(:original_title, :id, :vote_average)
+      @movies = movies.pluck(:original_title, :id, :vote_average)
     end
   end
 
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe 'Movie index page' do
       expect(page).to have_button("Find Top Rated Movies")
       
       click_button "Find Top Rated Movies"
-  #  save_and_open_page
+ 
       expect(current_path).to eq("/users/#{@jim.id}/movies")
       expect(page).to have_link("20세기 소녀")
       expect(page).to have_content("Vote Average:8.7")
@@ -49,11 +49,37 @@ RSpec.describe 'Movie index page' do
     visit "/users/#{@jim.id}/discover"
 
     expect(page).to have_button("Submit")
+    
 
     fill_in('q', with: "hi")
     click_button('Submit')
 
     expect(current_path).to eq("/users/#{@jim.id}/movies")
     expect(page).to have_content("Hi Stranger")
+  end
+
+  it 'has a discover page button that routes to users/:id/discover' do 
+    json_response = File.read('spec/fixtures/hi_search.json')
+
+        stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=fcffd3018e92893c2d9bde84c969cedc&query=hi").
+        with(
+          headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v2.7.1'
+          }).
+        to_return(status: 200, body: json_response, headers: {})
+      visit "/users/#{@jim.id}/discover"
+
+      expect(page).to have_button("Submit")
+      
+
+      fill_in('q', with: "hi")
+      click_button('Submit')
+      
+    expect(page).to have_button("Discover Page")
+
+    click_button('Discover Page')
+    expect(current_path).to eq("/users/#{@jim.id}/discover")
   end
 end
