65e8dc9da45cf141866ec0833de184196c86ca41
Sage Skaff <skaffsage@gmail.com>
2022-12-02 09:37:39 -0800

    Add runtime to movie attributes test

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -6,38 +6,32 @@ RSpec.describe 'Movie Details Page' do
     @user2 = User.create!(name: 'Jessica', email: 'jessica2@gmail.com')
     @user3 = User.create!(name: 'Fiona', email: 'Fiona3@gmail.com')
   end
-  
-  it " has a button to create a viewing party that takes the user to (/users/:user_id/movies/:movie_id/viewing-party/new)" do
+
+  it ' has a button to create a viewing party that takes the user to (/users/:user_id/movies/:movie_id/viewing-party/new)' do
     visit  user_movie_path(@user1.id, 238)
   end
 
-  it "has a button to return to the discover page" do
+  it 'has a button to return to the discover page' do
     visit  user_movie_path(@user1.id, 238)
   end
 
-  it "has the following attributes of the movie: title, vote averarage, run time in hours/minutes,
+  it "has the following attributes of the movie: title, vote average, run time in hours/minutes,
     genres associated with the movie, summary description, first 10 cast members and the characters
     they play, total review count and each review's author and information" do
-      visit  user_movie_path(@user1.id, 238)
+    visit user_movie_path(@user1.id, 238)
 
-      within("#review-crastana") do
-        expect(page).to have_content("Author: crastana")
-        expect(page).to have_content("Review: The best movie ever")
-      end
+    within('#runtime') do
+      expect(page).to have_content('2 hours 55 minutes')
+    end
 
-      within("#review-futuretv") do
-        expect(page).to have_content("Author: futuretv")
-        expect(page).to have_content("Review: The Godfather Review by Al Carlson")
-      end
-  end
+    within('#review-crastana') do
+      expect(page).to have_content('Author: crastana')
+      expect(page).to have_content('Review: The best movie ever')
+    end
 
-  it 'lists the first 10 cast members and the names of their characters' do
-    visit user_discover_path(@user1.id)
-    fill_in :keyword, with: 'Godfather'
-    click_button('Search by Movie Title')
-
-    within('#movie_238') do
-      click_link('The Godfather')
+    within('#review-futuretv') do
+      expect(page).to have_content('Author: futuretv')
+      expect(page).to have_content('Review: The Godfather Review by Al Carlson')
     end
 
     within('#cast-members') do
@@ -46,4 +40,20 @@ RSpec.describe 'Movie Details Page' do
       expect(page).to_not have_content('Chris Pratt as Mario')
     end
   end
+
+  # it 'lists the first 10 cast members and the names of their characters' do
+  #   visit user_discover_path(@user1.id)
+  #   fill_in :keyword, with: 'Godfather'
+  #   click_button('Search by Movie Title')
+
+  #   within('#movie_238') do
+  #     click_link('The Godfather')
+  #   end
+
+  #   within('#cast-members') do
+  #     expect(page).to have_content('Marlon Brando as Don Vito Corleone')
+  #     expect(page).to have_content('Al Pacino as Don Michael Corleone')
+  #     expect(page).to_not have_content('Chris Pratt as Mario')
+  #   end
+  # end
 end
