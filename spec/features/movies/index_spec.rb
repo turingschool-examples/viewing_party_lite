45c00ee0f3122b43c2bbe85ab8dbd414f4f186b3
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-02 16:26:40 -0700

    Remove save and open page from testing

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe 'The Movies Index Page' do
 
         it 'has each movie title as a link to its show page' do
           expect(page).to have_link('Jojo Rabbit')
-          save_and_open_page
+
           click_link('Jojo Rabbit')
 
           expect(current_path).to eq(user_movie_path(@user1, 515001))
