f51075264bf274c5d74b22a4e85186bda65cd468
Gabe Nunez <108249540+MisterJackpots@users.noreply.github.com>
2022-12-01 18:05:06 -0700

    Update user_spec.rb
    Moved your email uniqueness validation to the validations section of the model testing, also added spacing in between curly braces to match formatting.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many :viewing_party_users }
     it { should have_many(:viewing_parties).through(:viewing_party_users) }
-    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   end
 
   describe 'validations' do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :email}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   end
 end
