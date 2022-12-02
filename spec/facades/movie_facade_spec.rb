a9403f5490a916f624420434cd55826e4061d934
Mackenzie Leng <kenz.joy@outlook.com>
2022-12-01 18:59:33 -0700

    creates testing shell for new movie facade methods

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -1,21 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe MovieFacade, :vcr do
-  # before(:each) do
-  #   @party_1 = Party.create!(movie_id: 550, duration: 170, date: Date.new(2022,12,20), start_time: DateTime.new(2022,12,20,18,30,00))
-  #   @party_2 = Party.create!(movie_id: 700, duration: 160, date: Date.new(2022,12,27), start_time: DateTime.new(2022,12,27,20,00,00))
-  # end
-
   describe 'class methods' do 
     describe '#movie_title(movie_id)' do 
       it '- returns the movie title of a given movie id' do
         expect(MovieService.movie_details(550)[:title]).to eq("Fight Club")
       end
+    end
 
+    describe '#movie_image(movie_id)' do
       it '- returns the movie image of a given movie id' do
         expect(MovieService.movie_details(550)[:poster_path]).to eq("/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg")
-      end    
+      end
+    end
+
+    describe '#vote_average(movie_id)' do
+      it '- returns the vote average of a given movie id' do
+
+      end
+    end
+
+    describe '#runtime(movie_id)' do
+      it '- returns the runtime in hours & minutes of a given movie id' do
+
+      end
+    end
+
+    describe '#genres(movie_id)' do
+      it '- returns the genres associated to a given movie id' do
+
+      end
+    end
+
+    describe '#summary_description(movie_id)' do
+      it '- returns the summary description of a given movie id' do
+
+      end
+    end
+
+    describe '#first_10_cast_members(movie_id)' do
+      it '- returns a list of the first 10 cast members (characters & actors)' do
+
+      end
+    end
+
+    describe '#total_reviews(movie_id)' do
+      it '- returns a count of the total reviews of a given movie id' do
+
+      end
     end
   end
-
 end
