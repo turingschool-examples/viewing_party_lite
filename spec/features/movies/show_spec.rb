74d7b382957efd4474546140ca42dece2a43f200
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-12-01 18:01:33 -0700

    Implements VCR with testing
    - [x] I have performed a self-review of my code
    - [x] If it is a core feature, I have added thorough tests.
    - [x] Will this be part of a product update? If yes, please write one phrase about this update.
    
    Co-authored-by: Madeline Mauser <106927896+MadelineMauser@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -4,29 +4,25 @@ RSpec.describe 'Movies Show Page' do
   before(:each) do
     @user = User.create!(name: Faker::Name.unique.name, email: Faker::Internet.unique.free_email)
 
-    json_response = File.read('spec/fixtures/godfather_movie.json')
-    stub_request(:get, 'https://api.themoviedb.org/3/movie/238?api_key=0c4fca0a6457377ef48a02ec2df80f2f&language=en-US').to_return(status: 200, body: json_response)
     visit user_movie_path(@user.id, 238)
   end
 
   describe 'Movies#show' do
-
-    it 'has a button back to Discover Movies' do
+    it 'has a button back to Discover Movies', :vcr do
       expect(page).to have_link('Discover Movies')
       click_link 'Discover Movies'
       expect(current_path).to eq("/users/#{@user.id}/discover")
     end
 
-    xit 'has a button to Create Viewing Party' do
+    xit 'has a button to Create Viewing Party', :vcr do
       expect(page).to have_link('Create Viewing Party')
       click_link 'Create Viewing Party'
-      expect(current_path).to eq("/users/#{:user_id}/movies/#{:movie_id}/viewing-party/new")
+      expect(current_path).to eq("/users/#{user_id}/movies/#{movie_id}/viewing-party/new")
     end
 
-
-    it 'has details for the movie on display' do
+    it 'has details for the movie on display', :vcr do
       expect(page).to have_content('The Godfather')
-      expect(page).to have_content('Average vote:')
+      expect(page).to have_content('Vote average:')
     end
   end
 end
