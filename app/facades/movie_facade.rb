95635f33743fb2d9d3f92b68825d1d4c244038cd
Astrid Hecht <silentaviary@gmail.com>
2022-12-02 13:07:04 -0700

    feat: create poro (last commit was write tests) and refactor code to work with it

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,11 +1,27 @@
 require_relative '../services/movie_service'
+require_relative '../poros/min_movie'
 
 class MovieFacade
-  def self.movie_title(movie_id)
-    MovieService.movie_details(movie_id)[:title]
+
+  def self.movie_card(movie_id)
+    MinMovie.new(MovieService.movie_details(movie_id))
   end
 
-  def self.movie_image(movie_id)
-    MovieService.movie_details(movie_id)[:poster_path]
+  
+
+  def self.top_rated(page = 1)
+    movies = []
+    MovieService.top_rated(page)
+                .map { |hash| hash[:results] }
+                .each { |result| movies << MinMovie.new(result) }
+    movies
+  end
+
+  def self.search(query, page = 1)
+    movies = []
+    MovieService.search(query, page)
+                .map { |hash| hash[:results] }
+                .each { |result| movies << MinMovie.new(result) }
+    movies
   end
 end
