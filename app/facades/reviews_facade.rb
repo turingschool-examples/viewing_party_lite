f9b0ee55b07967005ba30a156083184cf5124c45
Rich Kaht <adamselene1776@protonmail.com>
2022-12-01 17:09:21 -0700

    Fix: remove erroneous hardcoded id in ReviewsFacade

--- app/facades/reviews_facade.rb
+++ app/facades/reviews_facade.rb
@@ -1,6 +1,6 @@
 class ReviewsFacade
   def self.reviews(movie_id)
-    json = MoviesService.reviews(238)
+    json = MoviesService.reviews(movie_id)
 
     reviews = json[:results].map do |review|
       Review.new(review)
