bbccb7fbee9c02c74aea07395e543adedadc64c7
Mackenzie Leng <kenz.joy@outlook.com>
2022-12-02 09:43:18 -0700

    create movie poro before testing

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -12,13 +12,14 @@ class MovieFacade
   def self.movie_details(movie_id)
     Movie.new(MovieService.movie_details(movie_id), MovieFacade.first_10_cast_members(movie_id), MovieFacade.total_reviews(movie_id))
   end
-  # def self.vote_average(movie_id)
-  #   MovieService.movie_details(movie_id)[:vote_average]
-  # end
 
-  # def self.runtime(movie_id)
-  #   MovieService.movie_details(movie_id)[:runtime]
-  # end
+  def self.vote_average(movie_id)
+    MovieService.movie_details(movie_id)[:vote_average]
+  end
+
+  def self.runtime(movie_id)
+    MovieService.movie_details(movie_id)[:runtime]
+  end
 
   # def self.genres(movie_id)
   #   MovieService.movie_details(movie_id)[:genres].map do |key|
@@ -26,22 +27,22 @@ class MovieFacade
   #   end
   # end
 
-  # def self.summary_description(movie_id)
-  #   MovieService.movie_details(movie_id)[:overview]
+  def self.summary_description(movie_id)
+    MovieService.movie_details(movie_id)[:overview]
+  end
+
+  # def self.first_10_cast_members(movie_id)
+  #   MovieService.movie_credits(movie_id)[:cast].sort_by{ |actor| actor[:order].to_i }.map do |actor|
+  #     [actor[:name], actor[:character]]
+  #   end.first(10)
   # end
 
-  def self.first_10_cast_members(movie_id)
-    MovieService.movie_credits(movie_id)[:cast].sort_by{ |actor| actor[:order].to_i }.map do |actor|
-      [actor[:name], actor[:character]]
-    end.first(10)
-  end
+  # def self.total_reviews(movie_id)
+  #   MovieService.movie_reviews(movie_id)[:total_results]
+  # end
 
-  def self.total_reviews(movie_id)
-    MovieService.movie_reviews(movie_id)[:total_results]
-  end
-
-  def self.review_info(movie_id)
-    MovieService.movie_reviews(movie_id)[:results]
-  end
+  # def self.review_info(movie_id)
+  #   MovieService.movie_reviews(movie_id)[:results]
+  # end
 
 end
--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,3 +1,46 @@
 class Movie
-  
+  attr_reader :id,
+              :movie_title,
+              :movie_image,
+              :vote_average,
+              :runtime, 
+              :genres,
+              :summary_description,
+              :vote_average,
+              :total_reviews,
+              :reviews,
+              :cast
+
+  def initialize(movie_details, movie_reviews, movie_credits)
+    @id = movie_details[:id]
+    @movie_title = movie_detials[:title]
+    @movie_image = movie_detials[:poster_path]
+    @vote_average = movie_details[:vote_average]
+    @runtime = movie_detials[:runtime]
+    @genres = get_genres(movie_details)
+    @summary_description = movie_details[:overview]
+    @vote_average = movie_details[:vote_average]
+    @total_reviews = movie_reviews[:total_results]
+    @reviews = get_reviews(movie_reviews)movie_reviews[:results]
+    @cast = get_cast(movie_credits)
+  end
+
+  def get_genres(movie_details)
+    movie_details[:genres].map do |key|
+      key[:name]
+    end
+  end
+
+  def get_cast(movie_credits)
+    movie_credits[:cast].sort_by{ |actor| actor[:order].to_i }.map do |actor|
+      [actor[:name], actor[:character]]
+    end.first(10)
+  end
+
+  def get_reviews(movie_reviews)
+    review_hash = {}
+    movie_reviews.each do |review|
+      review_hash[review[:author]] = review[:content]
+    end
+  end
 end
