ffb5226f9881549ddf3bd9c7fd95c3d547d36c6b
Astrid Hecht <silentaviary@gmail.com>
2022-12-02 13:13:39 -0700

    test: add validation tests for all models

--- spec/models/user_party_spec.rb
+++ spec/models/user_party_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UserParty, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :user_status }
+    it { should define_enum_for(:user_status).with_values([:host, :invited, :accepted, :declined]) }
+  end
+  
   describe 'relationships' do
     it { should belong_to(:user) }
     it { should belong_to(:party) }
