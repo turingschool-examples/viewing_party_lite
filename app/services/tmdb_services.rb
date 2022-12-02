ac8db65ccaf34ac524427b8ad72a95b50010e29e
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-12-02 12:25:51 -0700

    Refactors to use Faraday

--- app/services/tmdb_services.rb
+++ app/services/tmdb_services.rb
@@ -1,28 +1,33 @@
-require 'httparty'
-
 class TmdbService
   def top_rated
-    get_url("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["tmdb_api_key"]}")
+    get_url("/3/movie/top_rated")
   end
 
   def search_movies(query)
-    get_url("https://api.themoviedb.org/3/search/movie?api_key=#{ENV["tmdb_api_key"]}&language=en-US&query=#{query}&page=1&include_adult=false")
+    get_url("/3/search/movie", { query: "#{query}", page: 1, include_adult: false })
   end
 
   def get_movie(movie_id)
-    get_url("https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV["tmdb_api_key"]}&language=en-US")
+    get_url("/3/movie/#{movie_id}")
   end
 
   def get_credits(movie_id)
-    get_url("https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=#{ENV["tmdb_api_key"]}&language=en-US")
+    get_url("/3/movie/#{movie_id}/credits")
   end
 
   def get_reviews(movie_id)
-    get_url("https://api.themoviedb.org/3/movie/#{movie_id}/reviews?api_key=#{ENV["tmdb_api_key"]}&language=en-US")
+    get_url("/3/movie/#{movie_id}/reviews")
   end
 
-  def get_url(url)
-    response = HTTParty.get(url)
+  def get_url(url, params = nil)
+    response = conn.get(url, params)
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params['api_key'] = ENV['tmdb_api_key']
+      faraday.params['language'] = 'en-US'
+    end
   end
 end
