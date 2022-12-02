4133038e06de93094d7afed73d8f18854e844d09
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-02 00:31:47 -0700

    Add json parsing to movies controller pending refactor, update movies show page view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,8 +4,15 @@ class MoviesController < ApplicationController
   end
 
   def show
+    movie_id = params[:id]
+    conn = Faraday.new(url: 'https://api.themoviedb.org/3') do |faraday|
+      faraday.params['api_key'] = ENV['movie_api_key']
+    end
+
+    response = conn.get("movie/#{movie_id}")
+
+    @movie = JSON.parse(response.body, symbolize_names: true)
     @user = User.find(params[:user_id])
-    @movie = params[:id]
   end
 
 end
