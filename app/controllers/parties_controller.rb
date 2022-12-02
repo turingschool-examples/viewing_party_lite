f480444ef8d651554c38b3257f307ef78e6dcc46
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-02 14:03:11 -0700

    Change minor formatting

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -16,8 +16,8 @@ class PartiesController < ApplicationController
       PartyUser.create(party_id: new_party.id, user_id: user.id, host: true)
       params[:users].each do |id|
         PartyUser.create(party_id: new_party.id, user_id: id.to_i, host: false)
-        p "test"
       end
+      redirect_to user_path(user)
       redirect_to user_path(user)
     else
       redirect_to new_user_movie_party_path(user, movie.id)
