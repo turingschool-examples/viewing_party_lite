12d77631c452c1368a94854bdfd40cb9ff0fe998
Sean Culliton <smculliton@gmail.com>
2022-12-12 13:19:09 -0700

    Create session for login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,22 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    # require 'pry'; binding.pry
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+
       redirect_to user_path(user)
       flash[:alert] = "Welcome, #{user.name}! You are now logged in"
     else
       flash[:alert] = 'Unrecognized email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'You have been logged out'
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 end
