bc945cc234954dd6517576c9798903b6d1fb5ba6
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-12 12:26:15 -0700

    Update user model validations for bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
 
   validates_presence_of :name, :email
   validates_uniqueness_of :email, case_sensitive: false
+  validates_presence_of :password
 
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,10 +10,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
-    it { should validate_presence_of :password_confirmation }
     it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { should validate_presence_of(:password_digest)}
-    it { should validates_confirmatin_of(:password) }
+    it { should validate_confirmation_of(:password) }
     it { should have_secure_password }
   end
 end
