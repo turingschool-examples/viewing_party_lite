92dd57403eb4626a517b9956fcbe09f19fe05cae
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-12 13:19:11 -0700

    Add password_digest validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   validates_presence_of :name, :email
   validates_uniqueness_of :email, case_sensitive: false
   validates_presence_of :password
+  validates_presence_of :password_digest
 
   has_secure_password
 end
