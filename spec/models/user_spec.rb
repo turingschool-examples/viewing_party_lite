899ed6eaf264c24acf865352e84803049c368aac
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-12 12:10:41 -0700

    Create user model validation tests for adding password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :password_confirmation }
     it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { should validate_presence_of(:password_digest)}
+    it { should validates_confirmatin_of(:password) }
+    it { should have_secure_password }
   end
 end
