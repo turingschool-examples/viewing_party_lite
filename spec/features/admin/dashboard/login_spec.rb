4cec928d7c82417cd4c54684499d6b3501093652
bkeener <bkeener7@gmail.com>
2022-12-13 12:42:20 -0700

    Add sad path for non-admin

--- spec/features/admin/dashboard/login_spec.rb
+++ spec/features/admin/dashboard/login_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Admin login' do
   describe 'happy path' do
     it 'can log in as admin and get to my dashboard' do
-      admin = user = create(:user, role: 2)
+      admin = create(:user, role: 2)
 
       visit login_path
       fill_in :email, with: admin.email
@@ -11,6 +11,16 @@ RSpec.describe 'Admin login' do
       click_button 'Log In'
 
       expect(current_path).to eq(admin_dashboard_index_path)
+    end
+  end
+
+  describe 'sad path' do
+    it 'does not allow default user to see admin dashboard index' do
+      non_admin = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(non_admin)
+      visit admin_dashboard_index_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
 end
