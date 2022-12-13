83855678846ae7c61d3d9ceda7682bf594cca25e
Darby Smith <newman.darby2@gmail.com>
2022-12-12 21:25:32 -0500

    Refactor for user login

--- spec/factories/party.rb
+++ spec/factories/party.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :party do
     duration { Faker::Number.within(range: 120..430)}
     day { Faker::Date.between(from: '2022-12-25', to: '2024-12-25') }
-    start_time { Faker::Time.forward(days: 365, period: :evening, format: :short)}
+    start_time { Faker::Time.between(from: DateTime.now, to: DateTime.now + 1).strftime("%H:%M:%S") }
     movie_title { Faker::Movie.title }
   end
 end
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'users dashboard page' do
   before :each do
-    @user_1 = User.create!(name: 'Jim', email: 'jim.halpert@gmail.com')
-    @user_2 = User.create!(name: 'Pam', email: 'pam.halpert@gmail.com')
-    @party_1 = Party.create!(duration: 176, day: Date.today, start_time: Time.now, movie_title: "The Godfather")
-    @party_2 = Party.create!(duration: 125, day: Date.today, start_time: Time.now, movie_title: "Grinch")
+    @user_1 = create(:user)
+    @user_2 = create(:user)
+    @party_1 = create(:party)
+    @party_2 = create(:party)
     @user_party_1 = UserParty.create!(user_id: @user_1.id, party_id: @party_1.id, host: true)
     @user_party_2 = UserParty.create!(user_id: @user_1.id, party_id: @party_2.id, host: false)
   end
@@ -30,6 +30,7 @@ RSpec.describe 'users dashboard page' do
     visit user_path(@user_1)
 
     expect(page).to have_content("Viewing Parties")
+    
     within("#party-#{@party_1.id}") do
       expect(page).to have_content(@party_1.day)
       expect(page).to have_content(@party_1.start_time)
@@ -42,6 +43,7 @@ RSpec.describe 'users dashboard page' do
     visit user_path(@user_1)
 
     expect(page).to have_content("Viewing Parties")
+    
     within("#party-#{@party_2.id}") do
       expect(page).to have_content(@party_2.day)
       expect(page).to have_content(@party_2.start_time)
@@ -50,4 +52,3 @@ RSpec.describe 'users dashboard page' do
     end
   end
 end
-# page.should have_css('img', text: "image1.jpg")
