8184ce9bbc0da61ca900d75767bf0d935848ee3d
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-31 16:25:39 -0700

    Created sad path testing for non-unique emails

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -2,23 +2,41 @@ require 'rails_helper'
 
 RSpec.describe 'user registration page' do
   before :each do
-    User.delete_all
     visit register_path
   end
   
-  it 'has a form to create a new user' do
-    expect(page).to have_field('Name')
-    expect(page).to have_field('Email')
-    expect(page).to have_button('Submit')
-  end
-  
-  it 'can be filled in and submitted' do
-    fill_in('Name', with: 'Jeff Goldblum')
-    fill_in('Email', with: 'JurassicSnark@gmail.com')
-    click_on 'Submit'
+  describe 'initial tests' do
+    it 'has a form to create a new user' do
+      User.delete_all
+      
+      expect(page).to have_field('Name')
+      expect(page).to have_field('Email')
+      expect(page).to have_button('Submit')
+    end
     
-    expect(current_path).to eq(user_path(User.last.id))
+    it 'can be filled in and submitted' do
+      fill_in('Name', with: 'Jeff Goldblum')
+      fill_in('Email', with: 'JurassicSnark@gmail.com')
+      click_on 'Submit'
+      
+      expect(current_path).to eq(user_path(User.last.id))
+      
+      expect(page).to have_content('Jeff Goldblum')
+      expect(page).to have_content('User has been created!')
+    end
     
-    expect(page).to have_content('Jeff Goldblum')
+    it 'returns an error if the email has an existing user' do
+      fill_in('Name', with: 'Jeff Goldblum')
+      fill_in('Email', with: 'JurassicSnark@gmail.com')
+      click_on 'Submit'
+      visit register_path
+      
+      fill_in('Name', with: 'John Doe')
+      fill_in('Email', with: 'JurassicSnark@gmail.com')
+      click_on 'Submit'
+      
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content('Cannot use existing email')
+    end
   end
 end
