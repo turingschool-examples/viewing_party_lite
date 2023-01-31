64eb5c5a06d79ce689e92df22d9bff3f65ea8008
Samuel Cox <samc1253@gmail.com>
2023-01-31 13:39:57 -0700

    Fix when column in validations for viewing parties

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ViewingParty, type: :model do
   end
 
   describe 'Validations' do
-    it { should validate_presence_of(:when) }
+    it { should validate_presence_of(:date_time) }
     it { should validate_numericality_of(:duration) }
     it { should validate_numericality_of(:host) }
   end
