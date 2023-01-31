3b7a97141a6bc149829cdffd212a8af70fc2a0d9
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-31 13:22:25 -0700

    Adds validations for User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :email}
+    it {should validate_uniqueness_of :email}
+  end
+
+  before :each do
+    load_test_data
+  end
+end
