af1c0b68204009b71c87a2798eaf3cb5e831e6f5
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-31 14:47:20 -0800

    Add new user path and page
    Co-authored-by: Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  # resources :users, only: [:new]
+  get "/register", to: "users#new"
 end
--- spec/features/home/index_spec.rb
+++ spec/features/home/index_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "index page" do
-  it 'The title of the application' do
-    visit root_path
+  describe "page title" do
+    it 'The title of the application' do
+      visit root_path
 
-    expect(page).to have_content("Viewing Party")
+      expect(page).to have_content("Viewing Party")
+    end
+  end
+
+  describe "Create new user button" do
+    it 'has a button to create a new user' do
+      visit root_path
+
+      expect(page).to have_button("Create New User")
+    end
+
+    it 'will load the New User Page' do
+      visit root_path
+
+      expect(current_path).to eq(root_path)
+
+      click_button "Create New User"
+
+      expect(current_path).to eq(register_path)
+    end
   end
 end
