6c118b286ac266b43062d6e84ec9840df86718b1
Bryan Flanagan <bflanagan78@yahoo.com>
2023-01-31 16:55:17 -0700

    feat: Create user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+  def show
+
+  end
+  
   def new
     @user = User.new
+  end
+
+  def create
+    # require 'pry'; binding.pry
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user)
+      flash[:alert] = "User Created Successfully"
+    else
+      redirect_to register_path
+      flash[:alert] = user.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+    # params.permit(:name, :email)
   end
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -2,10 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "register page" do
   it 'has a form with fields to fill in' do
-    visit 'register'
-save_and_open_page
+    visit register_path
     expect(page).to have_field("Name")
     expect(page).to have_field("Email")
     expect(page).to have_button("Create New User")
+  end
+  
+  it 'takes user to new dashboard page where :id is id for new user created' do
+    user1 = User.create!(name: "Dennis Franz", email: "dennis@aol.com")
+    id = User.maximum(:id)
+    # require 'pry'; binding.pry
+    
+    visit register_path
+    
+    fill_in "Name", with: "Ted Bundy"
+    fill_in "Email", with: "ted@gmail.com"
+    click_button "Create New User"
+    
+    save_and_open_page
+    expect(current_path).to eq(user_path((user1.id)+1))
   end
 end
