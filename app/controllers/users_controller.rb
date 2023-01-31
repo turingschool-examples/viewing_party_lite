a337158a0f885bab7a3283d05f4bbd893027bc94
J Seymour <JustJakeSeymour@gmail.com>
2023-01-31 15:30:17 -0700

    complete user create to happy path spec
    Co-authored-by: Nigel AM <nigel.aungmyint@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
+  def show
+  end
+  
   def new
+    @user = User.new
   end
 
-  def show
+  def create
+    user = User.new(user_params)
     
+    if user.save
+      flash[:notice] = "User successfully created"
+      redirect_to user_path(user.id)
+    else
+    end
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -10,5 +10,26 @@ RSpec.describe "User Register Page" do
       expect(page).to have_field("Email")
       expect(page).to have_button("Create New User")
     end
+    
+    describe "happy path" do
+      it "successfully creates user" do
+        visit register_path
+
+        fill_in("Name", with: "Test")
+        fill_in("Email", with: "valid@email.com")
+
+        click_button("Create New User")
+
+        user = User.all.last
+
+        expect(current_path).to eq("/users/#{user.id}")
+        expect(page).to have_content("User successfully created")
+      end  
+    end
+    
+    describe "sad path" do
+      
+    end
+
   end
 end
