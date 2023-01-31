05ddd630b16274e2ff89d99766ac9b579a690581
Thomas Casady <tscasady@gmail.com>
2023-01-30 19:33:56 -0600

    Add relations to user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'relations' do
-    
+    it { should have_many :viewing_party_users } 
+    it { should have_many(:viewing_parties).through(:viewing_party_users) } 
   end
 
   describe 'validations' do
