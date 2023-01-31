ef5239df9ec27452faba203c1979d9e15b98ddf3
Samuel Cox <samc1253@gmail.com>
2023-01-31 14:11:29 -0700

    Add landing page

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Landing Page' do
+  let!(:users) { create_list(:user, 10) }
+
+  it 'Has all relevant content' do
+    visit root_path
+
+    random_user = users.shuffle.shuffle.last
+    expect(page).to have_content('Viewing Party')
+    expect(page).to have_link(users.first.email, href: user_path(users.first))
+    expect(page).to have_link(users.last.email, href: user_path(users.last))
+    expect(page).to have_link(random_user.email, href: user_path(random_user))
+  end
+
+  it 'has a has a create button' do
+    visit root_path
+    expect(page).to have_button('Create a New User')
+  end
+
+  it 'has navigation link' do
+    visit root_path
+    expect(page).to have_link('Back to Home', href: root_path)
+  end
+end
