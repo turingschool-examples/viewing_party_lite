97efa90eff742dbe687296d05debd6007fee3a19
Alex Pitzel <pitzelalex@gmail.com>
2023-01-30 21:19:19 -0500

    feat: Create landing page list of registered users and nav link back to landing page

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ end
 
 group :test do
   gem 'capybara'
+  gem 'faker'
   gem 'launchy'
   gem 'rspec-rails'
   gem 'shoulda-matchers'
--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -19,9 +19,41 @@ RSpec.describe 'Landing page', type: :feature do
         expect(current_path).to eq new_user_path
       end
 
-      it 'has a list of existing users with links to their dashboards'
+      it 'has a list of existing users with links to their dashboards' do
+        u1 = create(:user)
+        u2 = create(:user)
+        u3 = create(:user)
+        u4 = create(:user)
+        u5 = create(:user)
 
-      it 'has a link to return to the landing page'
+        visit '/'
+
+        within "#user-#{u1.id}" do
+          expect(page).to have_link(u1.name, href: user_path(u1))
+        end
+
+        within "#user-#{u2.id}" do
+          expect(page).to have_link(u2.name, href: user_path(u2))
+        end
+
+        within "#user-#{u3.id}" do
+          expect(page).to have_link(u3.name, href: user_path(u3))
+        end
+
+        within "#user-#{u4.id}" do
+          expect(page).to have_link(u4.name, href: user_path(u4))
+        end
+
+        within "#user-#{u5.id}" do
+          expect(page).to have_link(u5.name, href: user_path(u5))
+        end
+      end
+
+      it 'has a link to return to the landing page' do
+        visit '/'
+
+        expect(page).to have_link('Landing Page', href: '/')
+      end
     end
   end
 end
