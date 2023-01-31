196f96ac2e53135225b8931c7d749a1255361dc9
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-31 15:52:04 -0600

    remove user story comment
    Co-authored-by: J Seymour <110737252+JustJakeSeymour@users.noreply.github.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
-# When a user visits the root path they should be on the landing page ('/') which includes:
-# Title
-# button for new user
-# list of existing users which links to the users dashboard
-# link to go back to the landing page(this shows on every page)
+
 RSpec.describe 'Landing Page' do
   it 'displays title of application' do
     visit root_path
