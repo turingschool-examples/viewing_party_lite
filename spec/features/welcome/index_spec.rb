6d0229e763ff4e69bac6eb120d11619d9248bcd8
Joe King <j.king92@gmail.com>
2023-01-31 15:39:51 -0700

    refactor: Add styling to center content and add heading to all pages

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Landing Page' do
     user2 = User.create!(name: 'Jane Smith', email: 'jane@gmail.com')
     
     visit root_path
-
+    save_and_open_page
     expect(page).to have_content('Existing Users:')
     
     within '#existing_users' do
@@ -24,7 +24,7 @@ RSpec.describe 'Landing Page' do
 
   it 'has a link to the landing page' do
     visit root_path
-save_and_open_page
+
     expect(page).to have_link('Home', href: root_path)
   end
 end
