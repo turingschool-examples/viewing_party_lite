3e86cb37a486ea9f3ad23fadc7aeab095dab2f80
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-31 13:59:14 -0800

    Test: user registration sad path

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,24 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration page' do
-  it 'creates/registers new user' do
-    visit register_path
+  describe 'happy path' do
+    it 'creates/registers new user' do
+      visit register_path
 
-    expect(page).to have_field('user[name]')
-    expect(User.all).to eq([])
-    expect(User.count).to eq(0)
+      expect(page).to have_field('user[name]')
+      expect(User.all).to eq([])
+      expect(User.count).to eq(0)
 
-    fill_in 'user[name]', with: 'Kaylah Rose'
-    fill_in 'user[email]', with: '1234@valid.com'
-    click_button 'Register'
+      fill_in 'user[name]', with: 'Kaylah Rose'
+      fill_in 'user[email]', with: '1234@valid.com'
+      click_button 'Register'
 
-    expect(User.count).to eq(1)
-    expect(current_path).to eq(user_path(User.last))
-    expect(page.status_code).to eq 200
+      expect(User.count).to eq(1)
+      expect(current_path).to eq(user_path(User.last))
+      expect(page.status_code).to eq 200
+    end
   end
 
-  it 'does not create a new user with empty name field'
-  it 'does not create a user with empty email field'
-  it 'does not create a user with empty name and email fields'
-  it 'does not create a user with duplicate email'
+  describe 'sad path' do
+    it 'does not create a new user with empty name field' do
+      visit register_path
+      expect(User.all).to eq([])
+      expect(User.count).to eq(0)
+
+      fill_in 'user[email]', with: '1234@ldskfj'
+
+      click_button 'Register'
+
+      expect(User.all).to eq([])
+      expect(User.count).to eq(0)
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content('User not registered: Required information missing.')
+      expect(page).to have_button('Register')
+    end
+    it 'does not create a user with empty email field'
+    it 'does not create a user with empty name and email fields'
+    it 'does not create a user with duplicate email'
+  end
 end
