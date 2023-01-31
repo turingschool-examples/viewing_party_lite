eec67f8b29e4aa3ccf19d998378c21abc2ce8a62
Drew Layton <dlayton66@gmail.com>
2023-01-31 15:50:27 -0700

    feat/test: Creates user registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'welcome#index'
-  resources :users, only: [:new, :show]
+  
+  resources :users, only: [:show, :create]
+  get '/register', to: 'users#new'
 end
