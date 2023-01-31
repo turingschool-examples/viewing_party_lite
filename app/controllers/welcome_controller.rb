f73541e623e3d98181d33031590c13388aef0ef1
Kelsie Porter <krporter22@gmail.com>
2023-01-31 16:49:49 -0700

    Create button to create a new user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,12 @@ class WelcomeController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+
+  end
+
+  def create
+    
+  end
 end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,25 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'The landing page index' do 
   describe 'landing page index' do 
-    it 'displays the application title' do 
+    before :each do 
+      @user1 = create(:user)
+      create_list(:user, 5)
+    
       visit root_path
+    end
+    it 'displays the application title' do 
 
       expect(page).to have_content("Viewing Party")
     end
 
-    xit 'displays a button to create a new user' do 
-      visit root_path
+    it 'displays a button to create a new user' do 
 
+      expect(page).to have_button("Create a New User")
     end
+
     it 'displays existing users which link to the users dashboard' do 
-      user1 = create(:user)
-      create_list(:user, 5)
-    
-      visit root_path
 
       within(".users") do
-        within("section#user-#{user1.id}") do 
-          expect(page).to have_content(user1.email)
+        within("section#user-#{@user1.id}") do 
+          expect(page).to have_content(@user1.email)
         end
         expect(page).to have_selector('section', count: 6)
       end
