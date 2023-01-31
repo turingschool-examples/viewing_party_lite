30a2fc4143dd2dde1a5cbed904cd79e927d0393f
Lacey Weaver <jlweave89@gmail.com>
2023-01-31 14:59:31 -0700

    Add section for viewing parties

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -20,7 +20,9 @@ RSpec.describe "User Show Page", type: :feature do
     end
 
     it "has a section that lists viewing parties" do
+      visit user_path(@user1.id)
 
+      expect(page).to have_css("section#viewing_parties")
     end
   end
 
