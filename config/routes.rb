7c759e9d503428b9a5313155cc3e88fc82574d42
Kelsie Porter <krporter22@gmail.com>
2023-01-31 16:23:05 -0700

    Create list of existing users on landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'welcome#index'
+
+  resources :users do 
+    resources :dashboard, only: [:index]
+  end
 end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,5 +7,23 @@ RSpec.describe 'The landing page index' do
 
       expect(page).to have_content("Viewing Party")
     end
+
+    xit 'displays a button to create a new user' do 
+      visit root_path
+
+    end
+    it 'displays existing users which link to the users dashboard' do 
+      user1 = create(:user)
+      create_list(:user, 5)
+    
+      visit root_path
+
+      within(".users") do
+        within("section#user-#{user1.id}") do 
+          expect(page).to have_content(user1.email)
+        end
+        expect(page).to have_selector('section', count: 6)
+      end
+    end
   end
 end
