5c706de9e00539cccb76a7f8058425b663d0f589
Lacey Weaver <jlweave89@gmail.com>
2023-01-30 18:46:04 -0700

    Add fix to gem file, missing shoulda-matchers

---
+++ spec/features/landing_page_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+
+RSpec.describe "Landing Page" do
+  describe "when the user visits the root path" do
+    it "will have a title" do
+      visit "/"
+
+      expect(page).to have_content("Viewing Party")
+    end
+
+    it "has a button to create a new user" do
+
+    end
+
+    it "list of existing users" do
+
+    end
+
+    it "has a user be a link to the user's dashboard" do
+
+    end
+
+    it "has a link to go back to the landing page" do
+
+    end
+  end
+end
