b9b53387ab71ad7841ed2fc32ea3cbb4345d4b43
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-31 13:58:40 -0700

    test: welcome/landing page contents

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'welcome index' do
+  before(:each) do
+    visit root_path
+  end
+
+  describe 'When a user visits the root path they should be on the landing page' do
+    it 'routes to expected path' do
+      expect(current_path).to eq(root_path)
+    end
+
+    describe 'Title of Application' do
+      it 'Title of Application' do
+        expect(page).to have_content('Viewing Party Lite')
+      end
+
+      it 'Button to Create a New User' do
+        expect(page).to have_button('Create User')
+      end
+
+      it 'Lists Existing Users which links to the users dashboard' do
+        # expect(page).to have_link(name_users)
+        # expect(page).to have_link(name_users)
+        # expect(page).to have_link(name_users)
+      end
+
+      it 'Links to go back to the landing page which will be present at the top of all pages' do
+        expect(page).to have_link('Home')
+      end
+    end
+  end
+end
