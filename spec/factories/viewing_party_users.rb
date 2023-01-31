b1ad26882fac45bc588ae304fd2d72e6b7ec7cf5
Thomas Casady <tscasady@gmail.com>
2023-01-31 17:35:48 -0600

    Update factories with better defaults.

--- spec/factories/viewing_party_users.rb
+++ spec/factories/viewing_party_users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :viewing_party_user do
-    user { nil }
-    viewing_party { nil }
+    user { create(:user) }
+    viewing_party { create(:viewing_party) }
   end
 end
