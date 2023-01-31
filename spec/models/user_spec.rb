b5fa264360cd3f2b44d99cef90b05af5db36dff1
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-30 18:41:08 -0700

    Created more model spec files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe User do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
-    it { should validate_uni_of :email }
+    it { should validate_uniqueness_of :email }
   end
   
 end
