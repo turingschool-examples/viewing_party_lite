8103d21e0e5ab6f9d53a3fb9b131944d78213a99
Brandon Gray <brandonjoe88@hotmail.com>
2023-02-01 16:40:09 -0700

    feat: discover buttons and fields

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -8,6 +8,11 @@ class MovieService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def top_rated
+    response = @conn.get("/3/movie/top_rated?api_key=#{ENV['movie_api_key']}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def image(poster_path)
     "https://image.tmdb.org/t/p/h100#{poster_path}"
   end
