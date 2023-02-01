921aaae458d6e39cb3a071a053a8fc3b3bcd735f
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-02-01 17:14:34 -0600

    viewing party

--- spec/features/users/movies/viewing_parties/new_spec.rb
+++ spec/features/users/movies/viewing_parties/new_spec.rb
@@ -5,14 +5,31 @@ RSpec.describe 'Viewing Party New' do
   let(:user) { users.first }
 
   it 'can create a new viewing party' do
+    json_response = File.read('spec/fixtures/the_godfather.json')
+    stub_request(:get, "https://api.themoviedb.org/3/movie/238?api_key=#{ENV['movie_api_key']}&language=en-US").
+      to_return(status: 200, body: json_response)
     visit new_user_movie_viewing_party_path(user, 238)
+    start_time = Time.now
     
     expect(page).to have_field('viewing_party[duration]', with: 175 )
-    fill_in('user[name]', with: 'Samuel Cox')
-    fill_in('user[email]', with: 'samuel@example.com')
-    click_button('Register')
 
-    expect(User.exists?(name: 'Samuel Cox')).to eq true
-    expect(User.exists?(email: 'samuel@example.com')).to eq true
+    fill_in('viewing_party[date]', with: Date.today)
+    fill_in('viewing_party[start_time]', with: start_time)
+
+    page.check("viewing_party[user_#{users.second.id}]")
+    page.check("viewing_party[user_#{users.fourth.id}]")
+
+    click_button('Create Viewing Party')
+
+    expect(current_path).to eq(user_path(user))
+    viewing_party = ViewingParty.first
+
+    expect(viewing_party.date).to eq(Date.today)
+    expect(viewing_party.time).to eq(start_time)
+    expect(viewing_party.users).to eq([users.first, users.second, users.fourth])
+    user_viewing_party_1 = UserViewingParty.find_by(user_id: user.id)
+    user_viewing_party_2 = UserViewingParty.find_by(user_id: users.second.id)
+    expect(user_viewing_party_1.hosting).to eq(true)
+    expect(user_viewing_party_2.hosting).to eq(false)
   end
 end
