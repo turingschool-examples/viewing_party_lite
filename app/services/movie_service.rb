c24bc0e4032fed21c601fc49760b2df3642f376b
Ryan Canton <ryancanton999@gmail.com>
2023-02-01 16:05:05 -0700

    Test: Add test for movie details page

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -25,7 +25,7 @@ class MovieService
     reviews_url = "https://api.themoviedb.org/3/movie/#{id}/reviews?api_key=#{@api_key}&language=en-US&page=1"
     reviews_response = Faraday.get(reviews_url)
     movie_data = JSON.parse(movie_response.body, symbolize_names: true)
-    cast_data = JSON.parse(cast_response.body, symbolize_names: true)[:cast][0..10]
+    cast_data = JSON.parse(cast_response.body, symbolize_names: true)[:cast][0..9]
     reviews_data = JSON.parse(reviews_response.body, symbolize_names: true)
     data = [movie_data, cast_data, reviews_data]
   end
