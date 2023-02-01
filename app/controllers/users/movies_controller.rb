50036168f9a1d7913ca7b03f56408dcb95dc30ae
Samuel Cox <samc1253@gmail.com>
2023-02-01 10:21:45 -0700

    Add sad path for movie search if nothing is inputted

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -7,17 +7,22 @@ module Users
 
       if params[:q] == 'top rated'
 
-        response = conn.get("discover/movie?api_key=2f8f6c343a2a2acbd770dfbfbb00e38a&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1000")
+        response = conn.get("discover/movie?api_key=#{ENV['movie_api_key']}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1000")
 
       elsif params[:commit]
 
-        response = conn.get("search/movie?api_key=2f8f6c343a2a2acbd770dfbfbb00e38a&language=en-US&query=#{params[:title]}&page=1&include_adult=false")
+        response = conn.get("search/movie?api_key=#{ENV['movie_api_key']}&language=en-US&query=#{params[:title]}&page=1&include_adult=false")
 
       end
 
       data = JSON.parse(response.body, symbolize_names: true)
 
-      @movies = data[:results]
+      if data[:success] == false
+        flash[:errors] = data[:errors]
+        redirect_to user_discover_index_path(@user)
+      else
+        @movies = data[:results]
+      end
     end
 
     private
--- spec/features/users/discover/index_spec.rb
+++ spec/features/users/discover/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Discover Index' do
 
   it 'has a link to view top rated movies' do
     json_response = File.read('spec/fixtures/top_rated_movies.json')
-    stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=2f8f6c343a2a2acbd770dfbfbb00e38a&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1000").
+    stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['movie_api_key']}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1000").
       to_return(status: 200, body: json_response)
 
     visit user_discover_index_path(user)
@@ -18,7 +18,7 @@ RSpec.describe 'Discover Index' do
 
   it 'has a field to search for movies by title' do
     json_response = File.read('spec/fixtures/movies_with_green.json')
-    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=2f8f6c343a2a2acbd770dfbfbb00e38a&language=en-US&query=Green&page=1&include_adult=false").
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_api_key']}&language=en-US&query=Green&page=1&include_adult=false").
       to_return(status: 200, body: json_response)
 
     visit user_discover_index_path(user)
@@ -31,7 +31,7 @@ RSpec.describe 'Discover Index' do
 
   it 'accepts multiple words in search field' do
     json_response = File.read('spec/fixtures/movies_with_spider_man.json')
-    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=2f8f6c343a2a2acbd770dfbfbb00e38a&language=en-US&query=Spider%20Man&page=1&include_adult=false").
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_api_key']}&language=en-US&query=Spider%20Man&page=1&include_adult=false").
       to_return(status: 200, body: json_response)
 
     visit user_discover_index_path(user)
