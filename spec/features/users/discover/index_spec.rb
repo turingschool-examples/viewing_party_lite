d5f409cf2f817d7ec4393f6016725c5ed6d0c28b
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-02-01 10:27:19 -0800

    Add routes and tests for discover index
    Co-authored-by: Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>

---
+++ spec/features/users/discover/index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover Index Page', type: :feature do
+  let!(:user1) { User.create!(name: "Anthony", email: "anthony@gmail.com") }
+
+  describe "the default basics" do
+    it 'has a title' do
+      visit user_discover_index_path(user1)
+
+      expect(page).to have_content("#{user1.name}'s Discover Movies Page")
+    end
+  end
+
+  describe 'Top Rated Movies Search Button' do
+    it 'has a button that will display the top rated movies' do
+      visit user_discover_index_path(user1)
+
+      expect(page).to have_button("Top Rated Movies")
+
+    end
+  end
+
+  describe 'Movie Search by Title Field and Button' do
+    it 'has a field to type movies into and a button to search for those movies' do
+      visit user_discover_index_path(user1)
+
+      expect(page).to have_field(:title)
+      expect(page).to have_button("Search")
+    end
+
+    it 'will take me to the movies results page (index) when I fill in the field and click submit' do
+      visit user_discover_index_path(user1)
+
+      fill_in(:title, with: "Fight Club")
+      click_button "Search"
+
+      expect(current_path).to eq(user_movies_path(user1))
+    end
+  end
+end
