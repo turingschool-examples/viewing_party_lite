bd6e129b18ed872d46d4aa416c53158c708625d0
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-01 14:35:40 -0700

    Add Discover Page button to movie results index page
    Co-authored-by: Anthony Tallent <anthonytallent567@gmail.com>

---
+++ spec/features/users/movies/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'The Movie Results Index', type: :feature do
+  let!(:user1) { User.create!(name: "Anthony", email: "anthony@gmail.com") }
+  let!(:user2) { User.create!(name: "Thomas", email: "thomas@gmail.com") }
+  let!(:user3) { User.create!(name: "Jessica", email: "jessica@gmail.com") }
+  
+  describe 'the discover page button' do
+    it 'takes a user back to their discover page' do
+      visit user_movies_path(user1)
+      
+      expect(page).to have_button("Discover Page")
+
+      within("#discover-page-button") do
+        click_button("Discover Page")
+      end
+
+      expect(current_path).to eq(user_discover_index_path(user1))
+    end
+  end
+end
