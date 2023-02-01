8d7dcb289b56cf85e63456ec63547d6c467a76db
J Seymour <JustJakeSeymour@gmail.com>
2023-01-31 19:30:43 -0700

    set up tests for discover page spec

---
+++ spec/features/users/discover_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Discover Movies Page" do
+  before :each do
+    @user = create(:user)
+
+    visit user_discover_path(@user)
+  end
+
+  describe "displays content" do
+    it "button to discover top rated movies" do
+      expect(page).to have_button("Find Top Rated Movies")
+
+      click_button("Find Top Rated Movies")
+
+      expect(current_path).to eq("/users/#{@user.id}/movies")
+    end
+    
+    it "search by movie title text field and button" do
+      within "#keyword-search" do
+        expect(page).to have_field(:search)
+        expect(page).to have_button("Find Movies")
+        
+        fill_in(:search, with: "Test")
+
+        click_button("Find Movies")
+        
+        expect(current_path).to eq("/users/#{@user.id}/movies")
+      end
+    end
+  end
+  
+end
