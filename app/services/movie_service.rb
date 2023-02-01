de1017c6a0acf573f9a6e87d073682e10f4e9cc6
Anhnhi Tran <anhtran811@gmail.com>
2023-01-31 22:03:28 -0700

    Passes test for movie service

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,13 +1,14 @@
 class MovieService
-  def self.get_movies(movie)
-    JSON.parse(connection(movie).get.body)
+  def self.get_movie(id)
+    response = connection.get("movie/#{id}")
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   private
   
   def self.connection
     Faraday.new( 
-      url: "https://api.themoviedb.org/3/movie/#{movie}",
+      url: "https://api.themoviedb.org/3/",
       params: { api_key: ENV['MOVIE_DB_KEY'] }
     )
   end
