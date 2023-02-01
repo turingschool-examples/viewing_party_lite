b08ae71507f27b578cb8c9e3d30a83be413fa9a2
Joe King <j.king92@gmail.com>
2023-02-01 13:06:55 -0700

    feat: Add movies controller with call to TMDB API so the movie results page will populate

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,19 @@
 class MoviesController < ApplicationController
   def index
+    if params[:q] == 'top rated'
+      conn = Faraday.new(url: "https://api.themoviedb.org", params: {api_key: ENV['api_key']})
     
+      response = conn.get("/3/movie/top_rated")
+      data = JSON.parse(response.body, symbolize_names: true)
+      @movies = data[:results]
+    else
+      keyword = params[:q]
+      conn = Faraday.new(url: "https://api.themoviedb.org", params: {api_key: ENV['api_key']})
+      
+      response = conn.get("/3/search/movie", { query: keyword, include_adult: false } )
+      data = JSON.parse(response.body, symbolize_names: true)
+      @movies = data[:results]
+    end
   end
 
   private
