24bfdc99ad98be4a188373a90bbc1bea1e443c25
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-01 09:11:20 -0800

    test: Set up testing for movie results

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Movie Results Page' do
+
   let!(:charlie) { User.create!(name: 'Charlie', email: 'charlie_boy@gmail.com') }
 
-  describe 'when the user visits the discover movies page' do
-    xit 'has a button to direct the user to the movie results page' do
-    end
-  end
+  # xit 'redirects to movies results page (movies index)' do 
+  #   visit discover_user_path(charlie) 
+
+  #   click_button "Top Movies"
+
+  #   expect(current_path).to eq user_movies_path(charlie)
+  # end
+
+  # xit 'redirects to movies results page (movies index)' do 
+  #   visit discover_user_path(charlie) 
+
+  #   click_button "Search"
+
+  #   expect(current_path).to eq user_movies_path(charlie)
+  # end
 
   describe 'when the user visits the movie results page' do
     xit 'lists a maximum of 20 movie results' do
