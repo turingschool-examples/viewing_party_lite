e1ecd87eea68ac6c8116affd23ad8e1998abc3f4
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-02-01 10:31:26 -0800

    Add search bar to view and test

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -4,15 +4,24 @@ RSpec.describe 'Discover Movies Page' do
   describe 'as a user when i visit /users/:id/discover' do
     before :each do
       @user = User.create!(name: 'Drew', email: 'drew@example.com')
-
     end
-      it 'has a link to discover top rated movies' do
-        visit user_discover_index_path(@user)
 
-        expect(page).to have_button 'Find Top Rated Movies'
-        click_button 'Find Top Rated Movies'
+    it 'has a link to discover top rated movies' do
+      visit user_discover_index_path(@user)
 
-        expect(page).to have_current_path "/users/#{@user.id}/movies?q=top+rated"
-      end
+      expect(page).to have_button 'Find Top Rated Movies'
+      click_button 'Find Top Rated Movies'
+
+      expect(page).to have_current_path "/users/#{@user.id}/movies?q=top+rated"
+    end
+    it 'Has a search field for movie names' do
+      visit user_discover_index_path(@user)
+
+      expect(page).to have_field :keyword
+      fill_in :keyword, with: 'Fast'
+      click_on 'Find Movies'
+
+      expect(page).to have_current_path "/users/#{@user.id}/movies?utf8=%E2%9C%93&keyword=Fast&commit=Find+Movies"
+    end
   end
 end
