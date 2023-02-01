3d3f1f4a40253240a149b55ca754dea12aae7742
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-01 09:12:08 -0800

    test: Test for top movies button

---
+++ spec/features/users/discover_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'User Discover Movies Page' do 
+
+  let!(:charlie) { User.create!(name: 'Charlie', email: 'charlie_boy@gmail.com') }
+
+  it 'redirects to movies results page (movies index)' do 
+    visit discover_user_path(charlie) 
+
+    click_button "Top Movies"
+
+    expect(current_path).to eq user_movies_path(charlie)
+  end
+
+  xit 'redirects to movies results page (movies index)' do 
+    visit discover_user_path(charlie) 
+
+    click_button "Search"
+
+    expect(current_path).to eq user_movies_path(charlie)
+  end
+end
