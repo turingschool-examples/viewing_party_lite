3ef3d0c9236494992a0b447ef99e8113a8d11d7a
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-31 17:47:44 -0700

    feat: Adds API call services and User show dashboard info

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,12 +1,10 @@
 class MovieService
   def initialize
-    @conn = Faraday.new(url: "https://api.themoviedb.org/3") do |faraday|
-      faraday.headers['api_key'] = ENV['movie_api_key']
-    end
+    @conn = Faraday.new(url: "https://api.themoviedb.org/3")
   end
 
   def movie(id)
-    response = @conn.get("/movie/#{id}")
+    response = @conn.get("/3/movie/#{id}?api_key=05c2cc0439c2ee12465d6fb2652cc6c4")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 RSpec.describe 'user show page' do
   before(:each) do
     @user1 = create(:user)
+    @viewing_party1 = create(:viewing_party, movie_id: 550)
+    @viewing_party2 = create(:viewing_party, movie_id: 540)
+    @viewing_party3 = create(:viewing_party, movie_id: 530)
+    @viewing_party4 = create(:viewing_party, movie_id: 570)
+
+    @user_viewing_party1 = UserViewingParty.create(user: @user1, viewing_party: @viewing_party1, host: true)
+    @user_viewing_party2 = UserViewingParty.create(user: @user1, viewing_party: @viewing_party2, host: true)
+    @user_viewing_party3 = UserViewingParty.create(user: @user1, viewing_party: @viewing_party3, host: true)
 
     visit user_path(@user1)
   end
@@ -16,6 +24,17 @@ RSpec.describe 'user show page' do
       click_on('Discover')
 
       expect(current_path).to eq(user_discover_index_path(@user1))
+    end
+
+    it 'shows "<user\'s name>\'s Dashboard"' do
+      expect(page).to have_content("#{@user1.name}'s Dashboard")
+    end
+
+    it 'lists viewing parties' do
+      expect(page).to have_content(@viewing_party1.get_movie)
+      expect(page).to have_content(@viewing_party2.get_movie)
+      expect(page).to have_content(@viewing_party3.get_movie)
+      expect(page).to_not have_content(@viewing_party4.get_movie)
     end
   end
 end
