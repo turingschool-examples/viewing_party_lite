3a51b65ea23da3430aecc889d690dede8192efad
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-01 09:46:54 -0700

    feat: create user discover movies page with tests

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -15,15 +15,19 @@ RSpec.describe 'user dashboard' do
     expect(page).to have_content("John Doe's Dashboard")
   end
 
-  it 'has a button to discover movies' do
+  it 'has a button to discover movies and when I click that button I am redirected to user discover page' do
     visit user_path(@user1)
 
     expect(page).to have_button("Discover Movies")
+    click_button "Discover Movies"
+    save_and_open_page
+    expect(current_path).to eq user_discover_index_path(@user1)
+
   end
 
   it 'has a section that lists viewing parties' do
     visit user_path(@user1)
-    save_and_open_page
+   
     within "#viewing_party#{@vp1.id}" do
       expect(page).to have_content('January 30, 2023')
       expect(page).to have_content('7:00 pm')
@@ -34,7 +38,6 @@ RSpec.describe 'user dashboard' do
       expect(page).to have_content('8:00 pm')
       expect(page).to have_content('Invited')
 
-      save_and_open_page
     end
   end
 end
