e80f674b0e5a209755dd938a116178424045e38d
Alex Pitzel <pitzelalex@gmail.com>
2023-01-31 23:09:28 -0500

    feat: Create dynamic api ping based on which discover link

--- Gemfile
+++ Gemfile
@@ -59,6 +59,7 @@ group :test do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
   gem 'simplecov'
+  gem 'webmock'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,12 +1,29 @@
 class Users::MoviesController < ApplicationController
   def index
-    conn = Faraday.new(
-      url: 'https://api.themoviedb.org',
-      params: {
-        api_key: 'c4d6d86f378d33bacc04481f7e6380b2',
-        language: 'en-US'
-      }
-    )
-    # require 'pry'; binding.pry
+    conn = Faraday.new(url: 'https://api.themoviedb.org')
+    require 'pry'; binding.pry
+    response = conn.get(type_url[:path]) do |req|
+      req.params = query_params
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @movies = json[:results]
+  end
+end
+
+private
+
+def type_url
+  params.permit(:path)
+end
+
+def query_params
+  if params[:type] == 'top'
+    { api_key: 'c4d6d86f378d33bacc04481f7e6380b2',
+      language: 'en' }
+  elsif params[:type] == 'search'
+    { api_key: 'c4d6d86f378d33bacc04481f7e6380b2',
+      language: 'en',
+      query: params[:title] }
   end
 end
