bd619804c0cd279409a6bab6b254a7a61be5ef2b
Anhnhi Tran <anhtran811@gmail.com>
2023-02-01 15:21:47 -0700

    Completes poros spec with passing tests, fixed formatting to movie facade

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,9 +1,4 @@
 class MovieFacade
-  attr_reader :movie_service
-
-  def initialize
-  end
-
   def get_movie(id)
     movie_info = MovieService.get_movie(id)  
     movie_detail = MovieDetail.new(movie_info)
