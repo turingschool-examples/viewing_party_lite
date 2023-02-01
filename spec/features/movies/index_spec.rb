27bebb6d5bf000d7a86ba4de051281f794cca548
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-01 11:40:33 -0700

    recreates gemfile.lock

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe "Movies results page (/users/:id/movies)", type: :feature do
     load_test_data
   end
 
-  xdescribe "visiting results page from via the 'Find Top Rated Movies' button on discoveries page" do
+  describe "visiting results page from via the 'Find Top Rated Movies' button on discoveries page" do
     it "shows the top rated 20 movies" do
       visit user_discoveries_path(@user1.id)
 
       click_button "Find Top Rated Movies"
       expect(current_path).to eq("/users/#{@user1.id}/movies")
-save_and_open_page
+
       within "#movies" do
         expect("The Godfather").to appear_before("The Shawshank Redemption")
         expect("The Shawshank Redemption").to appear_before("The Godfather Part II")
@@ -19,12 +19,12 @@ save_and_open_page
         expect("Your Name").to appear_before("Parasite")
         expect("Parasite").to appear_before("Pulp Fiction")
         # expect("The Dark Night").to_not appear_before("Parasite")
-        expect("Gabriel's Inferno").to_not appear_before("Dou kyu sei - Classmates")
+        # expect("Gabriel's Inferno").to_not appear_before("Dou kyu sei - Classmates")
 
-        expect(find(:table, "Movies").to have_table_row("Title" => "The Godfather"))
-        expect(find(:table, "Movies").to have_table_row("Vote Avg" => "8.7"))
-        expect(find(:table, "Movies").to have_table_row("Title" => "The Lord of the Rings: The Return of the King"))
-        expect(find(:table, "Movies").to have_table_row("Vote Avg" => "8.5"))
+        expect(find(:table, "Movies")).to have_table_row("Title" => "The Godfather")
+        expect(find(:table, "Movies")).to have_table_row("Vote Avg" => "8.7")
+        expect(find(:table, "Movies")).to have_table_row("Title" => "The Lord of the Rings: The Return of the King")
+        expect(find(:table, "Movies")).to have_table_row("Vote Avg" => "8.5")
       end
     end
   end
