4e8826b376ab11dd4e3d45d66944dfa40fa7e301
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-01 11:46:00 -0800

    feat: Update search by movie title

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,29 @@
 class MoviesController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |f| 
-      f.headers["api_key"] = ENV["movie_api_key"]
-    end  
+    if params[:top_rated]
+      conn = Faraday.new(url: 'https://api.themoviedb.org') do |f|
+        f.headers['api_key'] = ENV['movie_api_key']
+      end
+      response = conn.get('/3/discover/movie?')
 
-    response = conn.get("/3/movie/top_rated?")
+      data = JSON.parse(response.body, symbolize_names: true)
+      
+      results = data[:results].find_all do |movie|
+        movie[:title].downcase.include?(params[:query].downcase)
+      end
 
-    data = JSON.parse(response.body, symbolize_names: true)
+      @top_20 = results.first(20)
 
-    @top_20 = data[:results].first(20)
+    else 
+      conn = Faraday.new(url: 'https://api.themoviedb.org') do |f|
+        f.headers['api_key'] = ENV['movie_api_key']
+      end
+
+      response = conn.get('/3/movie/top_rated?')
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      @top_20 = data[:results].first(20)
+    end
   end
 end
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,21 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Movie Results Page' do
+  before :each do 
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'Api-Key'=>'bcd246575bc3c3494a7259d3f55f76c9',
+      'User-Agent'=>'Faraday v2.5.2'
+      }).
+    to_return(status: 200, body: File.read("./spec/fixtures/top_rated_movies_response.json"), headers: {})  
+
+    stub_request(:get, "https://api.themoviedb.org/3/discover/movie").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'Api-Key'=>'bcd246575bc3c3494a7259d3f55f76c9',
+      'User-Agent'=>'Faraday v2.5.2'
+      }).
+    to_return(status: 200, body: File.read("./spec/fixtures/discover_movies_response.json"), headers: {})  
+  end
 
   let!(:charlie) { User.create!(name: 'Charlie', email: 'charlie_boy@gmail.com') }
 
   describe 'movie db api' do
     it 'displays the top rated movies' do 
-      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
-      with(
-        headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Api-Key'=>'bcd246575bc3c3494a7259d3f55f76c9',
-        'User-Agent'=>'Faraday v2.5.2'
-        }).
-      to_return(status: 200, body: File.read("./spec/fixtures/top_rated_movies_response.json"), headers: {})  
-
       visit discover_user_path(charlie)
 
       click_button "Top Movies"
@@ -25,23 +36,13 @@ RSpec.describe 'Movie Results Page' do
     end
 
     it 'searches for movie by title' do 
-      stub_request(:get, "https://api.themoviedb.org/3/discover/movie?").
-      with(
-        headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Api-Key'=>'bcd246575bc3c3494a7259d3f55f76c9',
-        'User-Agent'=>'Faraday v2.5.2'
-        }).
-      to_return(status: 200, body: File.read("./spec/fixtures/discover_movies_response.json"), headers: {})  
-
       visit discover_user_path(charlie)
 
-      fill_in "Movie Search", with: "the"
+      fill_in "Search Movie Title:", with: "the"
       click_button "Search"
 
       expect(current_path).to eq "/users/#{charlie.id}/movies"
-      expect(page).to have_content("The Godfather")
+      expect(page).to have_content("The Enforcer")
       expect(page).to have_content("Avatar: The Way of Water")
       expect(page).to_not have_content("Devotion")
     end
