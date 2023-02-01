d9edf62884394751867691be67a4e4cfda864799
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-01 09:12:34 -0800

    feat: create button for top movies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def discover
+    @user = User.find(params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show new create] do
     get 'discover', on: :member
+    get 'movies'
   end
 end
