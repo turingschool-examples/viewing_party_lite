923389c3ae1fc57eb09993b5bd9855b28c5dd0de
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-31 16:02:50 -0800

    feat: added a service for the top rated movies

---
+++ app/services/top_rated_movie_service.rb
@@ -0,0 +1,16 @@
+class TopRatedMovieService
+  def movies
+    JSON.parse(conn.body)
+  end
+
+  private
+
+  def conn
+    Faraday.new(
+      url: "https://api.themoviedb.org/3/movie/top_rated",
+      params: {api_key: ENV['MOVIE_DB_KEY'] }
+    )
+
+  end
+  
+end
