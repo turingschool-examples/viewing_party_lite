cdf7be1b34c7cfcbe190eae122a7c579d53ef0a9
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-01 09:41:19 -0800

    reformat

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show new create] do
     get 'discover', on: :member
-    # get 'movies', controller: 'movies'
   end
 end
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -4,22 +4,6 @@ RSpec.describe 'Movie Results Page' do
 
   let!(:charlie) { User.create!(name: 'Charlie', email: 'charlie_boy@gmail.com') }
 
-  # xit 'redirects to movies results page (movies index)' do 
-  #   visit discover_user_path(charlie) 
-
-  #   click_button "Top Movies"
-
-  #   expect(current_path).to eq user_movies_path(charlie)
-  # end
-
-  # xit 'redirects to movies results page (movies index)' do 
-  #   visit discover_user_path(charlie) 
-
-  #   click_button "Search"
-
-  #   expect(current_path).to eq user_movies_path(charlie)
-  # end
-
   describe 'when the user visits the movie results page' do
     xit 'lists a maximum of 20 movie results' do
       visit user_movies_path(charlie)
