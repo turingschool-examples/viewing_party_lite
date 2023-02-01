758a3e5e2734c14af079d6dac216becd9dde9331
Thomas Casady <tscasady@gmail.com>
2023-01-31 22:54:53 -0600

    Fix bad source for has_many relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :hosted_parties, :class_name => "ViewingParty", foreign_key: "host_id" 
   has_many :viewing_party_users
-  has_many :invited_parties, through: :viewing_party_users, :source => :user
+  has_many :invited_parties, through: :viewing_party_users, :source => :viewing_party
 
   validates_presence_of :name
   validates :email, uniqueness: true, presence: true
