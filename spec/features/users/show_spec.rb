0989da81657182a8e6a7132da097aa1f75a32979
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-31 18:10:15 -0600

    add user dashboard spec test for discover button
    Co-authored-by: J Seymour <110737252+JustJakeSeymour@users.noreply.github.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -9,5 +9,13 @@
       it 'displays username at top of page' do
         expect(page).to have_content("#{@user.name}'s Dashboard")
       end
+
+      it 'displays button to Discover Movies Page' do
+        expect(page).to have_button('Discover Movies')
+
+        click_button('Discover Movies')
+
+        expect(current_path).to eq("/users/#{@user.id}/discover")
+      end
     end
   end
