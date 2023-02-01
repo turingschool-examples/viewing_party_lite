5596fa1d284879276d58706df97e5eeb7f86343f
Thomas Casady <tscasady@gmail.com>
2023-02-01 16:19:03 -0600

    Start tests for movies/index

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,11 +1,11 @@
-class Movie 
-  attr_reader :id, 
-              :title, 
+class Movie
+  attr_reader :id,
+              :title,
               :vote_average
-              
+
   def initialize(movie_params)
-    @id = movie_params["id"]
-    @title = movie_params["original_title"]
-    @vote_average = movie_params["vote_average"]
+    @id = movie_params['id']
+    @title = movie_params['original_title']
+    @vote_average = movie_params['vote_average']
   end
 end
---
+++ spec/features/users/movies/index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Movie Results Page', type: :feature do
+  describe 'movie results ' do
+
+    let!(:user) { create(:user) }
+
+    before :each do
+    end
+
+    it 'displays only up to 20 movies' do
+      expect(movies.length).to be <= 20
+    end
+
+    it 'has a button to return to the discover page' do
+      expect(page).to have_button "Discover Page"
+
+      click_button "Discover Page"
+
+      expect(current_path).to eq discover_user_path
+    end
+
+    describe 'it can display movies from a top movies request' do
+      it 'displays each movies title and vote avg' do
+        movies.each do |movie|
+          within "#movie_#{movie_id}" do
+            expect(page).to have_content(movie.title)
+            expect(page).to have_content(movie.vote_average) 
+          end
+        end
+      end
+    end
+  end
+end
