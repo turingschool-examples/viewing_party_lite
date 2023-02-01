b65bb0c96004476ecc357e549c3fe3fff6836a5e
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-31 17:41:56 -0800

    test: Set up it blocks for movie results spec

---
+++ spec/features/movie_results_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Movie Results Page' do 
+
+  let!(:charlie) { User.create!(name: "Charlie", email: "charlie_boy@gmail.com") }
+
+  describe 'when the user visits the discover movies page' do 
+    xit 'has a button to direct the user to the movie results page' do 
+
+    end
+  end 
+
+  describe 'when the user visits the movie results page' do 
+    it 'lists a maximum of 20 movie results' do 
+      visit user_movies_path(charlie)
+    end
+
+    xit 'has a title for each movie as a link to the movie details page' do 
+
+    end
+
+    xit 'displays the vote average for each movie' do 
+
+    end
+  end
+end
