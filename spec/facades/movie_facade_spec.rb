7665a52646c2b536a4ebd735ab532e2b9b7d96bf
Joe King <j.king92@gmail.com>
2023-02-02 15:23:44 -0700

    test: Add tests for all MovieFacade methods

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -14,10 +14,42 @@ RSpec.describe MovieFacade do
       .to_return(status: 200, body: File.read('spec/fixtures/search_for_the_matrix.json'))
   end
 
+  it 'returns a cast (array of actors)' do
+    cast = MovieFacade.actors(238)
+
+    cast.each do |actor|
+      expect(actor).to be_a Actor
+    end
+  end
+
   it 'returns a movie' do
     movie = MovieFacade.movie_details(238)
 
     expect(movie).to be_a Movie
+  end
+
+  it 'returns an array of reviews' do
+    reviews = MovieFacade.review_details(238)
+
+    reviews.each do |review|
+      expect(review).to be_a Review
+    end
+  end
+
+  it 'returns an array of top rated movies' do
+    movies = MovieFacade.discover_movie('top rated')
+
+    movies.each do |movie|
+      expect(movie).to be_a Movie
+    end
+  end
+
+  it 'returns an array of queried movies' do
+    movies = MovieFacade.discover_movie('The Matrix')
+
+    movies.each do |movie|
+      expect(movie).to be_a Movie
+    end
   end
 end
   
