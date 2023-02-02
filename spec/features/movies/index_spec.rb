2d04dba8227cf1c1646067a074b11a9883ead982
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-01 17:49:58 -0800

    Debug webmocks and add dotenv gem

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -2,26 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Movie Results Page' do
   before :each do
-    stub_request(:get, 'https://api.themoviedb.org/3/movie/top_rated')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Api-Key' => 'bcd246575bc3c3494a7259d3f55f76c9',
-          'User-Agent' => 'Faraday v2.5.2'
-        }
-      )
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_DB_KEY']}")
       .to_return(status: 200, body: File.read('./spec/fixtures/top_rated_movies_response.json'), headers: {})
 
-    stub_request(:get, 'https://api.themoviedb.org/3/discover/movie')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Api-Key' => 'bcd246575bc3c3494a7259d3f55f76c9',
-          'User-Agent' => 'Faraday v2.5.2'
-        }
-      )
+    stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_DB_KEY']}")
       .to_return(status: 200, body: File.read('./spec/fixtures/discover_movies_response.json'), headers: {})
   end
 
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -2,26 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Movie details page' do
   before :each do
-    stub_request(:get, 'https://api.themoviedb.org/3/movie/top_rated')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Api-Key' => 'bcd246575bc3c3494a7259d3f55f76c9',
-          'User-Agent' => 'Faraday v2.5.2'
-        }
-      )
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_DB_KEY']}")
       .to_return(status: 200, body: File.read('./spec/fixtures/top_rated_movies_response.json'), headers: {})
 
-    stub_request(:get, 'https://api.themoviedb.org/3/discover/movie')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Api-Key' => 'bcd246575bc3c3494a7259d3f55f76c9',
-          'User-Agent' => 'Faraday v2.5.2'
-        }
-      )
+    stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_DB_KEY']}")
       .to_return(status: 200, body: File.read('./spec/fixtures/discover_movies_response.json'), headers: {})
   end
 
@@ -30,16 +14,16 @@ RSpec.describe 'Movie details page' do
   describe 'When I visit the movie details page' do
     it 'has button to create viewing party' do
       visit "/users/#{charlie.id}/movies/315162"
-      
-      click_button "Create a Viewing Party"
+
+      click_button 'Create a Viewing Party'
 
       expect(current_path).to eq "/users/#{charlie.id}/movies/315162/viewing-party/new"
     end
 
-    it 'has button to return to discover page' do 
+    it 'has button to return to discover page' do
       visit "/users/#{charlie.id}/movies/315162"
-     
-      click_button "Discover Page"
+
+      click_button 'Discover Page'
 
       expect(current_path).to eq discover_user_path(charlie)
     end
