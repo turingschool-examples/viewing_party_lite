cb3f79c0baebe8642027c99ada2de5fc3ba839c6
Lacey Weaver <jlweave89@gmail.com>
2023-02-02 14:14:24 -0700

    Add return to discover movies button

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -14,7 +14,12 @@ RSpec.describe "Movies details page (/users/:id/movies/:id)", type: :feature do
      expect(page).to have_button("Create Viewing Party for The Godfather")
     end
 
-    it "there is a button to return to the discover page"
+    it "there is a button to return to the discover page" do
+      click_button "Return to Discover Movies"
+
+      expect(current_path).to eq(user_discoveries_path(@user1.id))
+
+    end
 
     it "page displays movie title, vote avg of the movie, run time in hours and minutes, genres, summary discription"
 
