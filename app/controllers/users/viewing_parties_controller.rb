6b7fc951eb1d2990f1de2c49e3f1755a007107cc
Kelsie Porter <krporter22@gmail.com>
2023-02-02 16:29:20 -0700

    Add strong params for viewing_party

--- app/controllers/users/viewing_parties_controller.rb
+++ app/controllers/users/viewing_parties_controller.rb
@@ -1,5 +1,18 @@
 class Users::ViewingPartiesController < ApplicationController
   def new
+    @host = User.find(params[:user_id])
+    @movie = MovieFacade.new(params[:movie_id]).movie
+    @viewing_party = ViewingParty.new
+  end
 
+  private
+
+  def viewing_party_params
+    params.require(:viewing_party)
+          .permit(
+            :duration, 
+            :date, 
+            :start_time
+          )
   end
 end
