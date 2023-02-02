140ac740df9704be1b2af0cbabd96f27be267a7f
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-01 21:19:53 -0600

    call MovieFacade top_rated_movies method in movies controller

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,5 +1,8 @@
 class Users::MoviesController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    if params[:q] == "top_rated"
+      @movies = MovieFacade.top_rated_movies
   end
 
   def show
