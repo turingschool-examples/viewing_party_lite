e7f9dfcf2132a623c1f27bee677aff1a81504561
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-01 21:16:23 -0600

    add MovieService wtih top_rated, conn, and parse_body methods

---
+++ app/services/movie_service.rb
@@ -0,0 +1,17 @@
+class MovieService
+  def self.top_rated_movies
+    response = conn.get("movie/top_rated")
+    parse_body(response)
+  end
+
+  def self.conn
+    Faraday.new(
+      url: 'https://api.themoviedb.org/3',
+      headers: { 'Authorization' => "Bearer #{ENV['v4_key']}" }
+    )
+  end
+
+  def self.parse_body(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
