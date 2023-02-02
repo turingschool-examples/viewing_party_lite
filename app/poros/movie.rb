185c61cf75d681c0361feef518d8ab03d3ddf8b3
Joe King <j.king92@gmail.com>
2023-02-01 18:16:20 -0700

    test: Add tests for movie class

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -3,15 +3,18 @@ class Movie
               :title,
               :vote_avg,
               :runtime,
-              :genres,
               :summary
 
   def initialize(movie_data)
-    @id = movie_data[:id]
-    @title = movie_data[:title]
-    @vote_avg = movie_data[:vote_average]
-    @runtime = movie_data[:runtime]
-    @genres = movie_data[:genres]
-    @summary = movie_data[:overview]
+    @id         = movie_data[:id]
+    @title      = movie_data[:title]
+    @vote_avg   = movie_data[:vote_average]
+    @runtime    = movie_data[:runtime]
+    @genre_data = movie_data[:genres]
+    @summary    = movie_data[:overview]
+  end
+
+  def genres
+    @genre_data.map { |genre| genre[:name] }
   end
 end
---
+++ spec/poros/movie_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Movie do
+  it 'exists' do
+    movie_data = {
+      id: '1',
+      title: 'When Harry Met Sally',
+      vote_average: '7.6',
+      runtime: '115',
+      genres: [ { "id": 18, "name": "Drama" } , { "id": 10, "name": "Rom-Com"}],
+      overview: 'classic rom com with classic rom com actors'
+    }
+
+    movie = Movie.new(movie_data)
+
+    expect(movie).to be_a Movie
+    expect(movie.id).to eq('1')
+    expect(movie.title).to eq('When Harry Met Sally')
+    expect(movie.vote_avg).to eq('7.6')
+    expect(movie.runtime).to eq('115')
+    expect(movie.summary).to eq('classic rom com with classic rom com actors')
+  end
+
+  describe '#genres' do
+    it 'turns genre data into array of genre strings' do
+      movie_data = {
+        id: '1',
+        title: 'When Harry Met Sally',
+        vote_average: '7.6',
+        runtime: '115',
+        genres: [ { "id": 18, "name": "Drama" } , { "id": 10, "name": "Rom-Com"}],
+        overview: 'classic rom com with classic rom com actors'
+      }
+  
+      movie = Movie.new(movie_data)
+
+      expect(movie.genres).to eq(['Drama', 'Rom-Com'])
+    end
+  end
+end
