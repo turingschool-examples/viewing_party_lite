cbc7aaefd2df8eed4f8fa6d1714a6237c1fe7765
Joe King <j.king92@gmail.com>
2023-02-02 16:59:55 -0700

    feat: Add option to invite users to new viewing party form

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -5,9 +5,12 @@ RSpec.describe 'new viewing party page' do
     stub_request(:get, "https://api.themoviedb.org/3/movie/238?api_key=#{ENV['api_key']}")
       .to_return(status: 200, body: File.read('spec/fixtures/movie_details_godfather238.json'), headers: {})
 
-    @user1 = User.create!(name: 'John Doe', email: 'johndoe@ymail.com')
-    @movie = MovieFacade.movie_details(238)
-    # require 'pry'; binding.pry
+      @user1 = User.create!(name: 'John Doe', email: 'johndoe@ymail.com')
+      @user2 = User.create!(name: 'John Smith', email: 'johndoe@gmail.com')
+      @user3 = User.create!(name: 'John Williams', email: 'johndoe@email.com')
+      @user4 = User.create!(name: 'John Kennedy', email: 'johndoe@webmail.com')
+      @user5 = User.create!(name: 'John Deere', email: 'johndoe@mail.com')
+      @movie = MovieFacade.movie_details(238)
   end
 
   it 'displays the movie title for the new viewing party' do
@@ -24,13 +27,19 @@ RSpec.describe 'new viewing party page' do
 
   it 'has a form to enter details for user party and duration defaults to movies runtime' do
     visit new_user_movie_viewing_party_path(@user1, @movie.id)
-save_and_open_page
+    users = [@user2, @user3, @user4, @user5]
+    
     within '#party-details' do
       expect(page).to have_content("The Godfather")
       expect(page).to have_field('Duration of Party', with: @movie.runtime)
       expect(page).to have_field('Day')
       expect(page).to have_field('Start Time')
       expect(page).to have_button('Create Party')
+      users.each do |user|
+        expect(page).to have_content(user.name)
+        expect(page).to have_content(user.email)
+      end
+      expect(page).to_not have_content(@user1.name)
     end
   end
 
