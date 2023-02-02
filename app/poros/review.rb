f1c549f9819988fecabee59786a6b9a2124f91c4
Max MacGillivray <mary.macgillivray@gmail.com>
2023-02-02 13:44:53 -0700

    Changed the attributes in the review poro

--- app/poros/review.rb
+++ app/poros/review.rb
@@ -1,10 +1,10 @@
 class Review
   attr_reader :id,
-              :page,
-              :results
+              :author,
+              :content
   def initialize(response)
-    @id = response[:id]
-    @page = response[:page]
-    @results = response[:results]
+    @id = response[:id] #in base hash
+    @author = response[:author] #in a hash in an array in base hash
+    @content = response[:content] #in a hash in an array 
   end
 end
