5d55a4b6be477880421843f2d1c871d45bbf652d
Drew Layton <dlayton66@gmail.com>
2023-02-01 18:51:03 -0700

    feat/test: User Story 9: Add ability to search for movies

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show, :create] do
     resources :discover, only: [:index]
-    resources :movies, only: [:index]
+    resources :movies, only: [:index, :show]
   end
   get '/register', to: 'users#new'
 end
