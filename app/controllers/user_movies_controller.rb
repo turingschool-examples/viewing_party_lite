7cd16e345464d4890d929dcc86c33e7a27d1ce2e
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-01 19:10:54 -0800

    refactor: pulled the service calls from the movies controller into the facade

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,10 +1,16 @@
+# require './app/facades/movie_facade'
+
 class UserMoviesController < ApplicationController
   def details
     @user = User.find(params[:id])
     if params[:q] == "top rated"
-      @top_rated = TopRatedMovieService.movies["results"]
+      @top_rated = MovieFacade.top_rated
+    
     elsif params[:search]
-      @search_results = MovieSearchService.search(params[:search])["results"]
+      @search_results = MovieFacade.movie_search(params[:search])
+  
     end
   end
+
+  
 end
--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,11 +1,15 @@
 class MovieFacade
-  attr_reader :movie_service
-
-  def initialize
-  end
 
   def get_movie(id)
     movie_info = MovieService.get_movie(id)  
     movie_detail = MovieDetail.new(movie_info)
+  end
+
+  def self.top_rated
+    TopRatedMovieService.movies["results"]
+  end
+
+  def self.movie_search(query)
+    MovieSearchService.search(query)["results"]
   end
 end
