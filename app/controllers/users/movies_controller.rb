4110282b88c62ac12c3dd4f2beb6c06664461e25
Kelsie Porter <krporter22@gmail.com>
2023-02-01 19:50:07 -0700

    Add conditional logic to view and users movies controller

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,7 +1,13 @@
 class Users::MoviesController < ApplicationController
   
   def index
-    @top_movies = movie_service.get_top_movies
+    @search_term = params[:title_search]
+
+    if @search_term
+      @movies = movie_service.search_movies(@search_term)
+    else
+      @movies = movie_service.get_top_movies
+    end
     @user = User.find(params[:user_id])
   end
 
