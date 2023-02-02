026160c811e344b369543a5a91bc7a03c5323c51
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-02 12:42:53 -0800

    test: wrote test for initial display of page and site title on new viewing party page

---
+++ spec/features/view_parties/new_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'new view party page' do
+  before(:each) do
+    User.delete_all
+
+    @user = create(:user)
+
+    json_response = File.read('spec/fixtures/movie.json')
+    stub_request(:get, "https://api.themoviedb.org/3/movie/238?api_key=#{ENV['MOVIE_DB_KEY']}")
+      .to_return(status: 200, body: json_response, headers: {})
+
+    @movie_detail = MovieDetail.new(JSON.parse(json_response, symbolize_names: true))
+
+    visit new_user_movie_viewing_party_path(@user.id, @movie_detail.id)
+  end
+
+  it 'displays the site title and page title at the top' do
+    expect(page).to have_content("Viewing Party")
+    expect(page).to have_content("Create a Movie Party for #{@movie_detail.title}")
+
+
+  end
+
+
+end
