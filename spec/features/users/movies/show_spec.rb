11dd97c452fe5f30fc86c8641774e8f1ec7e5307
Thomas Casady <tscasady@gmail.com>
2023-02-01 21:38:15 -0600

    Implement movie factory to show page test.

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Movie Details', type: :feature do
   
   let!(:user) { create(:user) }
-  let!(:movie_id) { 13 }
+  let!(:movie) { FactoryBot.build(:movie)}
 
   before :each do
     #TODO: Can I remove the api interpolation?
-    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['moviedb_key']}&language=en-US" 
+    url = "https://api.themoviedb.org/3/movie/#{movie.id}?api_key=#{ENV['moviedb_key']}&language=en-US" 
     response = File.read("spec/fixtures/forrest.json")
     stub_request(:get, url).to_return(status: 200, body: response)
 
-    visit user_movie_path(user, movie_id)
+    visit user_movie_path(user, movie.id)
   end
 
   describe 'the movie show page' do
@@ -32,6 +32,7 @@ RSpec.describe 'Movie Details', type: :feature do
     describe 'movie data' do
       it 'displays the movies title, vote_avg, runtime, genre, summary, and cast' do
         #TODO: this test is getting bloated, break up.
+
         within "#movie" do
           expect(page).to have_content "Movie Title: #{movie.title}"
           expect(page).to have_content "Runtime: #{movie.runtime}"
