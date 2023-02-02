d120bc5ac5b62e700d93d642a0cc7fba8e4a61bc
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-02 14:59:29 -0800

    test: add testing for movie services

---
+++ spec/services/movies_service_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe MoviesService do
+  before :each do
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_DB_KEY']}")
+      .to_return(status: 200, body: File.read('./spec/fixtures/top_rated_movies_response.json'), headers: {})
+
+    stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_DB_KEY']}")
+      .to_return(status: 200, body: File.read('./spec/fixtures/discover_movies_response.json'), headers: {})
+  end
+
+  it 'can return the searched movie data' do 
+    response = MoviesService.movie_search 
+
+    expect(response).to be_a Hash
+    expect(response[:results]).to be_a Array
+    response[:results].each do |movie_result_data|
+      expect(movie_result_data).to have_key(:id)
+      expect(movie_result_data).to have_key(:title)
+      expect(movie_result_data).to have_key(:overview)
+      expect(movie_result_data).to have_key(:vote_average)
+      expect(movie_result_data).to have_key(:vote_count)
+    end
+  end
+
+  it 'can return the top rated data' do 
+    response = MoviesService.top_rated 
+    
+    expect(response).to be_a Hash
+    expect(response[:results]).to be_a Array
+    response[:results].each do |movie_result_data|
+      expect(movie_result_data).to have_key(:id)
+      expect(movie_result_data).to have_key(:title)
+      expect(movie_result_data).to have_key(:overview)
+      expect(movie_result_data).to have_key(:vote_average)
+      expect(movie_result_data).to have_key(:vote_count)
+    end 
+  end
+end
