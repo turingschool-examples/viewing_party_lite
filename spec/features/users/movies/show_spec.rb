b75c486f7ed77d2ef0b429a8f80ab6be2b5a4f0f
J Seymour <JustJakeSeymour@gmail.com>
2023-02-02 16:22:22 -0700

    write spec file for movie detail (show) page
    Co-authored-by: Nigel AM <nigel.aungmyint@gmail.com>

---
+++ spec/features/users/movies/show_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "Movie Detail (Show) page" do
+  before :each do
+    @user = create(:user)
+
+    visit user_movie_path(@user, id: '238')
+  end
+
+  describe "buttons to other pages" do
+    it "button to discover page" do
+      expect(page).to have_button("Discover Page")
+      
+      click_button("Discover Page")
+      
+      expect(current_path).to eq user_discover_index_path(@user)
+    end
+
+    it "button to create a viewing party" do
+      expect(page).to have_button("Create Viewing Party for The Godfather")
+      
+      click_button("Create Viewing Party for The Godfather")
+
+      expect(current_path).to eq("users/@user.id/movies/238/viewing_party/new")
+      # expect(current_path).to eq(new_user_movie_viewing_party_path)
+    end
+  end
+
+  describe "displays movie information" do
+    it "movie information" do
+      within "#movie-title" do
+        expect(page).to have_content("The Godfather")
+      end
+
+      within "#movie-details" do
+        expect(page).to have_content("Vote: 8.7")
+        expect(page).to have_content("Runtime: 2hr 55min")
+        expect(page).to have_content("Genre: Drama, Crime")
+        expect(page).to have_content("Summary: Spanning the years 1945 to 1955," + 
+          "a chronicle of the fictional Italian-American Corleone crime family.")
+        expect(page).to have_content("Cast: Marlon Brando, Al Pachino")
+        expect(page).to have_content("2 Reviews: ")
+        expect(page).to have_content("The Godfather Review by Al Carlson")
+        expect(page).to have_content("futuretv")
+      end
+    end
+  end
+end
