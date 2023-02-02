36bd43b7ac295fe55cdf6de7998b52bee60139d1
Thomas Casady <tscasady@gmail.com>
2023-02-01 20:56:24 -0600

    Add additional attributes to movie poro.

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,11 +1,17 @@
 class Movie
   attr_reader :id,
               :title,
-              :vote_average
+              :vote_average,
+              :runtime,
+              :genre,
+              :summary
 
   def initialize(movie_params)
     @id = movie_params['id']
     @title = movie_params['original_title']
     @vote_average = movie_params['vote_average']
+    @runtime = movie_params['runtime']
+    @genre = movie_params['genre']
+    @summary = movie_params['summary']
   end
 end
