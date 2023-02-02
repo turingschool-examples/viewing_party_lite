83a8095656c4b12f0d7a7616d080c0d1507e1139
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-02 03:12:18 -0600

    add movie results spec tests for keyword search

--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe 'Movies Results Page' do
   end
   it 'can show 20 top rated movies' do
     visit user_discover_index_path(@user)
-
+    
     click_button("Find Top Rated Movies")
+    
+    expect(page).to have_content("Vote Average:", count: 20)
 
     within '#movie-1' do
       expect(page).to have_link('The Godfather')
@@ -21,27 +23,18 @@ RSpec.describe 'Movies Results Page' do
       expect(page).to have_link('The Godfather Part II')
       expect(page).to have_content('Vote Average: 8.6') 
     end
-    within '#movie-4' do
-      expect(page).to have_link('Puss in Boots: The Last Wish')
-      expect(page).to have_content('Vote Average: 8.6') 
-    end
-    within '#movie-5' do
-      expect(page).to have_link('Dilwale Dulhania Le Jayenge')
-      expect(page).to have_content('Vote Average: 8.6') 
-    end
-    within '#movie-6' do
-      expect(page).to have_link("Schindler's List")
-      expect(page).to have_content('Vote Average: 8.6') 
-    end
-
-    expect(page).to have_content("Vote Average:", count: 20)
   end
 
   it 'each movie title link leads to movie details page' do
-    visit "/users/#{@user.id}/movies?q=top_rated"
+    visit user_movies_path(@user, q: 'top_rated')
     
     click_link 'The Godfather'
-    expect(current_path).to eq "/users/#{@user.id}/movies/238"
+    expect(current_path).to eq user_movie_path(@user, id: '238')
+    
+    visit user_movies_path(@user, q: 'Lego')
+    
+    click_link "The Lego Movie"
+    expect(current_path).to eq user_movie_path(@user, id: '137106')
   end
 
   it 'has a button to return to Discover Page' do
@@ -49,13 +42,28 @@ RSpec.describe 'Movies Results Page' do
     
     click_button("Discover Page")
 
-    expect(current_path).to eq user_discover_index_path(@user)
+    expect(current_path).to eq user_discover_index_path(@user)    
   end
 
   it 'shows 20 movies from a keyword search' do
     visit user_discover_index_path(@user)
 
-    fill_in('keyword', with: 'Lego')
+    fill_in(:q, with: 'Lego')
     click_button('Find Movies')
+
+    expect(page).to have_content("Vote Average:", count: 20)
+
+    within '#movie-1' do
+      expect(page).to have_link('The Lego Movie')
+      expect(page).to have_content('Vote Average: 7.4') 
+    end
+    within '#movie-2' do
+      expect(page).to have_link('The Lego Batman Movie')
+      expect(page).to have_content('Vote Average: 7.2') 
+    end
+    within '#movie-3' do
+      expect(page).to have_link('The Lego Ninjago Movie')
+      expect(page).to have_content('Vote Average: 6.5') 
+    end
   end
 end
