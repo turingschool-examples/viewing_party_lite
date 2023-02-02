fe4c1848015f41ac633a4abe2e27ff7bbc6a2161
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-02 03:14:43 -0600

    adjust controller conditional for sad path

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -3,8 +3,11 @@ class Users::MoviesController < ApplicationController
     @user = User.find(params[:user_id])
     if params[:q] == "top_rated"
       @movies = MovieFacade.top_rated_movies
-    elsif params[:q] == "keyword"
-      @movies = MovieFacade.keyword_search(params[:keyword])
+    elsif params[:q] == ""
+      flash[:alert] = "Error: Search must be specified"
+      redirect_to user_discover_index_path(@user)
+    else
+      @movies = MovieFacade.keyword_search(params[:q])
     end
   end
 
