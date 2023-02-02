7b20261580d20d0ffd25db6ab0eebc46312ec6cf
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-02 15:37:35 -0800

    create conn method for movie services

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -1,27 +1,23 @@
 class MoviesService
   def self.movie_search
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |f|
-      f.params['api_key'] = ENV['movie_api_key']
-    end
     parse_data(conn.get('/3/discover/movie?'))
+  end
+
+  def self.top_rated
+    parse_data(conn.get('/3/movie/top_rated?'))
+  end
+
+  def self.find_movie_details(movie)
+    parse_data(conn.get("/3/movie/#{movie}?"))
   end
 
   def self.parse_data(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.top_rated
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |f|
+  def self.conn 
+    Faraday.new(url: 'https://api.themoviedb.org') do |f|
       f.params['api_key'] = ENV['movie_api_key']
     end
-
-    parse_data(conn.get('/3/movie/top_rated?'))
-  end
-
-  def self.find_movie_details(movie)
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |f|
-      f.params['api_key'] = ENV['movie_api_key']
-    end
-    parse_data(conn.get("/3/movie/#{movie}?"))
   end
 end
