25b3f3b69e45a64b7b18f21e87c8173a2e55a48f
Anhnhi Tran <anhtran811@gmail.com>
2023-02-02 12:08:51 -0700

    Adds hosting boolean to migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_31_021356) do
+ActiveRecord::Schema.define(version: 2023_02_02_190750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_01_31_021356) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.boolean "hosting", default: false
   end
 
   create_table "viewing_parties", force: :cascade do |t|
