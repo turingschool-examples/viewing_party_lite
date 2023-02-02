571980c354044e9f94940b70c7b3fbec4b56f72b
Anhnhi Tran <anhtran811@gmail.com>
2023-02-02 13:17:59 -0700

    Delete hosting boolean column, add host id to viewing parties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_02_190750) do
+ActiveRecord::Schema.define(version: 2023_02_02_201627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,13 +25,13 @@ ActiveRecord::Schema.define(version: 2023_02_02_190750) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.boolean "hosting", default: false
   end
 
   create_table "viewing_parties", force: :cascade do |t|
     t.string "duration"
     t.datetime "party_date"
     t.bigint "movie_id"
+    t.integer "host_id"
     t.index ["movie_id"], name: "index_viewing_parties_on_movie_id"
   end
 
