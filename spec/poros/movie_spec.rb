eb91d7ced4c7ae3c18891e00cf62f01cdcfc4932
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-03 08:50:02 -0700

    refactor/test: create poros test for runtime_calc

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -37,4 +37,20 @@ RSpec.describe Movie do
       expect(movie.genres).to eq(['Drama', 'Rom-Com'])
     end
   end
+
+  describe '#runtime_calc' do
+    it 'formats run time into Xhr Xmin' do
+      movie_data = {
+        id: '1',
+        title: 'When Harry Met Sally',
+        vote_average: '7.6',
+        runtime: '115',
+        genres: [{ "id": 18, "name": 'Drama' }, { "id": 10, "name": 'Rom-Com' }],
+        overview: 'classic rom com with classic rom com actors'
+      }
+      movie = Movie.new(movie_data) 
+      
+      expect(movie.runtime_calc).to eq('1hr 55min')
+    end
+  end
 end
