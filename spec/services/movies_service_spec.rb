02207e4ce11e7c169d0b1417989dfd6f93a7bcaa
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-02 17:26:06 -0800

    test: Add test for movie credits services

--- spec/services/movies_service_spec.rb
+++ spec/services/movies_service_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe MoviesService do
   
     stub_request(:get, "https://api.themoviedb.org/3/movie/497?api_key=#{ENV['MOVIE_DB_KEY']}")
       .to_return(status: 200, body: File.read('./spec/fixtures/green_mile/details_response.json'), headers: {})
+  
+    stub_request(:get, "https://api.themoviedb.org/3/movie/497/credits?api_key=#{ENV['MOVIE_DB_KEY']}")
+      .to_return(status: 200, body: File.read('./spec/fixtures/green_mile/credits_response.json'), headers: {})
   end
 
   it 'can return the searched movie data' do 
@@ -49,5 +52,13 @@ RSpec.describe MoviesService do
     expect(response).to have_key(:overview)
     expect(response).to have_key(:vote_average)
     expect(response).to have_key(:vote_count)
+  end
+
+  it 'can return the movie credits data' do 
+    response = MoviesService.find_movie_credits(497)
+
+    expect(response).to be_a Hash 
+    expect(response).to have_key(:id)
+    expect(response).to have_key(:cast)
   end
 end
