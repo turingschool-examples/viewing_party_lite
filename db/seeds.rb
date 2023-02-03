4760f3345552bea42285cf0103b7b697352dda46
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-03 10:27:32 -0700

    feat: Populate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@user1 = User.create!(name: 'John Doe', email: 'johndoe@ymail.com')
+@user2 = User.create!(name: 'Exene Cervenka', email: 'exene@hotmail.com')
+@user3 = User.create!(name: 'Billy Zoom', email: 'zoomies@aol.com')
+@user4 = User.create!(name: 'DJ Bonebreak', email: 'djb@yahoo.com')
+@user5 = User.create!(name: 'Bob Sagat', email: 'bobbys@gmail.com')
+@user6 = User.create!(name: 'Don Letts', email: 'lettsgymail.com')
+@user7 = User.create!(name: 'Siouxsie Sioux', email: 'siouxsie@thebanshees.com')
+@user8 = User.create!(name: 'Genesis P. Orridge', email: 'psychictv@.throbbinggristlecom')
+@user9 = User.create!(name: 'Simon Gallup', email: 'simon@thecure.com')
+@user10 = User.create!(name: 'Jere Lehmus', email: 'jerelehmus.gmail.com')
+@user11 = User.create!(name: 'Danny Elfman', email: 'elfman@hotmail.com')
+@user12 = User.create!(name: 'Lemmy Kilmister', email: 'thekingofkings@motorhead.com')
+
+@vp1 = ViewingParty.create!(movie_id: 90652, date: 'Mon, 30 Jan 2023', party_duration: 118, start_time: Time.parse('19:00:00 UTC'))
+@vp2 = ViewingParty.create!(movie_id: 21137, date: 'Tue, 31 Jan 2023', party_duration: 95, start_time: Time.parse('20:00:00 UTC'))
+@vp3 = ViewingParty.create!(movie_id: 48677, date: 'Fri, 10 Feb 2023', party_duration: 300, start_time: Time.parse('17:30:00 UTC'))
+@vp4 = ViewingParty.create!(movie_id: 14577, date: 'Tue, 28 Feb 2023', party_duration: 154, start_time: Time.parse('20:00:00 UTC'))
+
+@vpu1 = @user1.viewing_party_users.create!(viewing_party_id: @vp1.id, hosting: true)
+@vpu2 = @user2.viewing_party_users.create!(viewing_party_id: @vp2.id, hosting: true)
+@vpu3 = @user3.viewing_party_users.create!(viewing_party_id: @vp3.id, hosting: true)
+@vpu4 = @user4.viewing_party_users.create!(viewing_party_id: @vp4.id, hosting: true)
+@vpu5 = @user1.viewing_party_users.create!(viewing_party_id: @vp4.id, hosting: false)
+@vpu6 = @user2.viewing_party_users.create!(viewing_party_id: @vp1.id, hosting: false)
+@vpu7 = @user3.viewing_party_users.create!(viewing_party_id: @vp1.id, hosting: false)
+@vpu8 = @user4.viewing_party_users.create!(viewing_party_id: @vp2.id, hosting: false)
+@vpu9 = @user1.viewing_party_users.create!(viewing_party_id: @vp2.id, hosting: false)
+@vpu10 = @user5.viewing_party_users.create!(viewing_party_id: @vp2.id, hosting: false)
+@vpu11 = @user12.viewing_party_users.create!(viewing_party_id: @vp2.id, hosting: false)
+@vpu12 = @user6.viewing_party_users.create!(viewing_party_id: @vp2.id, hosting: false)
+@vpu13 = @user10.viewing_party_users.create!(viewing_party_id: @vp3.id, hosting: false)
+@vpu14 = @user2.viewing_party_users.create!(viewing_party_id: @vp3.id, hosting: false)
+@vpu15 = @user9.viewing_party_users.create!(viewing_party_id: @vp1.id, hosting: false)
+@vpu16 = @user7.viewing_party_users.create!(viewing_party_id: @vp4.id, hosting: false)
+@vpu17 = @user8.viewing_party_users.create!(viewing_party_id: @vp4.id, hosting: false)
+@vpu18 = @user2.viewing_party_users.create!(viewing_party_id: @vp4.id, hosting: false)
