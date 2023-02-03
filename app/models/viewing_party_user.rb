b7b10f66d7f54d55b9ecf147f085fd4f7aea3283
J Seymour <JustJakeSeymour@gmail.com>
2023-02-03 10:19:03 -0700

    viewing party user associations and validations
    Co-authored-by: Nigel AM <nigel.aungmyint@gmail.com>

--- app/models/viewing_party_user.rb
+++ app/models/viewing_party_user.rb
@@ -1,4 +1,7 @@
 class ViewingPartyUser < ApplicationRecord
-  belongs_to :viewing_pary
   belongs_to :user
+  belongs_to :viewing_party
+
+  validates_presence_of :user_id
+  validates_presence_of :viewing_party_id
 end
---
+++ spec/models/viewing_party_user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe ViewingPartyUser, type: :model do
+  describe 'associations' do
+    it { should belong_to :user }
+    it { should belong_to :viewing_party }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :viewing_party_id }
+  end
+end
