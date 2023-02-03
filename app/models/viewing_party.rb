05b4c5e09e143c433e87edf2f81eeb9e1d0788be
Kelsie Porter <krporter22@gmail.com>
2023-02-02 19:29:32 -0700

    Add validations to ViewingParty and tests

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -3,8 +3,15 @@ class ViewingParty < ApplicationRecord
   has_many :viewing_party_users
   has_many :users, through: :viewing_party_users
 
+  validates_presence_of :duration,
+                       :date,
+                       :start_time,
+                       :movie_id
+
+  #placeholder movie method
   def movie
     movie ||= MovieFacade.new(movie_id).movie
   end
+
 end
 
