0b5b9e0c3a7e45f6a5e8d41fc1d3bcd44c0aed24
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-02 22:11:42 -0600

    add title and discover page button to new viewing party page

--- app/controllers/users/movies/viewing_parties_controller.rb
+++ app/controllers/users/movies/viewing_parties_controller.rb
@@ -1,4 +1,7 @@
 class Users::Movies::ViewingPartiesController < ApplicationController
   def new
+    # require 'pry'; binding.pry
+    @user = User.find(params[:user_id])
+    @movie = MovieFacade.movie_details(params[:movie_id])
   end
 end
