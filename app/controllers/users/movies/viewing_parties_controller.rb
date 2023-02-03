da7e7ced081079f490ebb2293c62a14e7ab26b4c
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-03 10:17:55 -0600

    adjust viewing party controller instance variables

--- app/controllers/users/movies/viewing_parties_controller.rb
+++ app/controllers/users/movies/viewing_parties_controller.rb
@@ -2,6 +2,11 @@ class Users::Movies::ViewingPartiesController < ApplicationController
   def new
     # require 'pry'; binding.pry
     @user = User.find(params[:user_id])
+    @guests = User.all_except(@user)
     @movie = MovieFacade.movie_details(params[:movie_id])
+  end
+
+  def create
+    # require 'pry'; binding.pry
   end
 end
