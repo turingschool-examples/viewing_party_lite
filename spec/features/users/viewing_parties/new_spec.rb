a1849a3687ff5caf3bb5e5b9335c1a46a0f639e4
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-02-02 20:46:26 -0600

    Add test t make sure new viewing party displays the correct information on the user dashboard after clcking Create Viewing Party

--- spec/features/users/viewing_parties/new_spec.rb
+++ spec/features/users/viewing_parties/new_spec.rb
@@ -36,6 +36,33 @@ RSpec.describe 'Viewing Party New' do
     expect(user_viewing_party_2.hosting).to eq(false)
   end
 
+  it 'displays the information on the users page' do
+    visit new_user_movie_viewing_party_path(user, 238)
+    start_time = Time.now
+
+    expect(page).to have_field('viewing_party[duration]', with: 175 )
+
+    fill_in('viewing_party[date]', with: Date.today)
+    fill_in('viewing_party[start_time]', with: start_time)
+
+    page.check("viewing_party[#{users.second.id}]")
+    page.check("viewing_party[#{users.fourth.id}]")
+
+    click_button('Create Viewing Party')
+
+    viewing_party = ViewingParty.first
+
+    within '#hosted_parties' do
+      expect(page).to have_link("The Godfather")
+      expect(page.find('img')[:src]).to eq("https://image.tmdb.org/t/p/w500/#{viewing_party.movie.image}")
+      expect(page).to have_content(viewing_party.date)
+      expect(page).to have_content(viewing_party.start_time.utc.strftime('%l:%M %p'))
+      expect(page).to have_content("You are the Host")
+      expect(page).to have_content(viewing_party.users[1].name)
+      expect(page).to have_content(viewing_party.users[2].name)
+    end
+  end
+
   it 'will not create if runtime is longer than duration' do
     visit new_user_movie_viewing_party_path(user, 238)
     start_time = Time.now
