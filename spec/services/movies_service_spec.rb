a01b68e7e4c67bb4af2554248985fc24352a7d70
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-02 16:01:40 -0800

    Add test for find movie details

--- spec/services/movies_service_spec.rb
+++ spec/services/movies_service_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe MoviesService do
 
     stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_DB_KEY']}")
       .to_return(status: 200, body: File.read('./spec/fixtures/discover_movies_response.json'), headers: {})
+  
+    stub_request(:get, "https://api.themoviedb.org/3/movie/497?api_key=#{ENV['MOVIE_DB_KEY']}")
+      .to_return(status: 200, body: File.read('./spec/fixtures/green_mile/details_response.json'), headers: {})
   end
 
   it 'can return the searched movie data' do 
@@ -35,5 +38,16 @@ RSpec.describe MoviesService do
       expect(movie_result_data).to have_key(:vote_average)
       expect(movie_result_data).to have_key(:vote_count)
     end 
+  end
+
+  it 'can return the specific movie' do 
+    response = MoviesService.find_movie_details(497)
+
+    expect(response).to be_a Hash 
+    expect(response).to have_key(:id)
+    expect(response).to have_key(:title)
+    expect(response).to have_key(:overview)
+    expect(response).to have_key(:vote_average)
+    expect(response).to have_key(:vote_count)
   end
 end
