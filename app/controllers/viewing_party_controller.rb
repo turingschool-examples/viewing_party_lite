7a41ad3b0b99a4aeea0fec2abeb6656178cb35bd
Joe King <j.king92@gmail.com>
2023-02-03 08:05:27 -0700

    refactor: Added additional tests and moved creation of vp-users to happen alongside creation of viewing party

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -11,14 +11,13 @@ class ViewingPartyController < ApplicationController
     vp = ViewingParty.new(viewing_party_params)
     if vp.save
       ViewingPartyUser.create!(viewing_party: vp, user: user, hosting: true)
+      viewing_party_users_params.each do |user_id|
+        ViewingPartyUser.create!(viewing_party_id: vp.id, user_id: user_id, hosting: false) if User.exists?(user_id)
+      end
       redirect_to user_path(user)
     else
       redirect_to new_user_movie_viewing_party_path(user, params[:movie_id])
       flash[:alert] = vp.errors.full_messages.to_sentence
-    end
-
-    viewing_party_users_params.each do |user_id|
-      ViewingPartyUser.create!(viewing_party_id: vp.id, user_id: user_id, hosting: false) if User.exists?(user_id)
     end
   end
 
