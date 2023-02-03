e906743050cb959f649e4338779077d9233f3ae9
J Seymour <JustJakeSeymour@gmail.com>
2023-02-03 13:36:57 -0700

    one more pass to remove comments / pry
    Co-authored-by: Nigel AM <nigel.aungmyint@gmail.com>

--- app/controllers/users/movies/viewing_parties_controller.rb
+++ app/controllers/users/movies/viewing_parties_controller.rb
@@ -1,6 +1,5 @@
 class Users::Movies::ViewingPartiesController < ApplicationController
   def new
-    # require 'pry'; binding.pry
     @user = User.find(params[:user_id])
     @guests = User.all_except(@user)
     @movie = MovieFacade.movie_details(params[:movie_id])
