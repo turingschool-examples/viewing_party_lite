482b4c1f67f7215df78e6a5c7a70421c24259837
Samuel Cox <samc1253@gmail.com>
2023-02-03 14:00:50 -0700

    Finish final tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,11 @@ class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: false, message: 'User already exists with given email' }
   validates :email, email: true
 
-  before_validation :downcase_email
-
   def find_user_viewing_party(viewing_party)
     self.user_viewing_parties.find_by(viewing_party_id: viewing_party.id)
   end
 
   def all_but_self
     User.where.not(id: self.id)
-  end
-
-  private
-
-  def downcase_email
-    self.email = email.downcase if self.email
   end
 end
--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -14,15 +14,33 @@ RSpec.describe ViewingParty, type: :model do
   end
 
   describe 'Instance Methods' do
-    it 'returns the movie for a given viewing party' do
+    let!(:users) { create_list(:user, 9) }
+    let!(:user) { create(:user) }
+    before :each do
       json_response = File.read('spec/fixtures/the_godfather.json')
       stub_request(:get, "https://api.themoviedb.org/3/movie/238?api_key=#{ENV['movie_api_key']}&language=en-US").
         to_return(status: 200, body: json_response)
-
-      viewing_party = ViewingParty.new({duration: 175, movie_id: 238, start_time: Time.now, date: Date.today})
+    end
+    it 'returns the movie for a given viewing party' do
+      viewing_party = ViewingParty.create!({duration: 175, movie_id: 238, start_time: Time.now, date: Date.today})
 
       expect(viewing_party.movie).to be_a(Movie)
       expect(viewing_party.movie.title).to eq('The Godfather')
+    end
+
+    it 'returns the host for a given viewing party' do
+      viewing_party = ViewingParty.create!({duration: 175, movie_id: 238, start_time: Time.now, date: Date.today})
+      user_viewing_party = viewing_party.user_viewing_parties.create!({user_id: user.id, hosting: true})
+      expect(viewing_party.host).to eq(user.name)
+    end
+
+    it 'returns the invitees for a given viewing party' do
+      viewing_party = ViewingParty.create!({duration: 175, movie_id: 238, start_time: Time.now, date: Date.today})
+      user_viewing_party = viewing_party.user_viewing_parties.create!({user_id: user.id, hosting: true})
+      users.each_with_index do |invitee, i|
+        viewing_party.user_viewing_parties.create!({user_id: invitee.id}) unless i == 4
+      end
+      expect(viewing_party.invitees).to eq(users.map(&:name) - [users[4].name])
     end
   end
 end
