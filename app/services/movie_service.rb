aaa31326d20f23d7fe6aec7fc17c42569b7cb853
Thomas Casady <tscasady@gmail.com>
2023-02-03 11:30:38 -0600

    Refactor parse and movie to helpers, move movie building to facade.

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -6,22 +6,18 @@ class MovieService
 
   def get_top_movies
     response = service.get('/3/movie/top_rated')
-    
-    build_movie_objects(response)
+    parse(response)    
   end
 
   def search_movies(search_query)
     response = service.get('/3/search/movie') do |request|
       request.params["query"] = search_query
     end
-
-    build_movie_objects(response)
+    parse(response)
   end
 
   def movie
-   movie_params = JSON.parse(service.get("/3/movie/#{@movie_id}").body, symbolize_names: true)
-   movie_params = movie_params.merge(JSON.parse(service.get("/3/movie/#{@movie_id}/credits").body, symbolize_names: true))
-   movie_params = movie_params.merge(JSON.parse(service.get("/3/movie/#{@movie_id}/reviews").body, symbolize_names: true))
+    get_basic_movie_info.merge(get_cast).merge(get_reviews)
   end
 
   private
@@ -41,9 +37,19 @@ class MovieService
     }
   end
 
-  def build_movie_objects(response)
-    JSON.parse(response.body, symbolize_names: true)[:results].map do |movie|
-      Movie.new(movie)
-    end
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_basic_movie_info
+    parse(service.get("/3/movie/#{@movie_id}"))
+  end
+
+  def get_cast
+    parse(service.get("/3/movie/#{@movie_id}/credits"))
+  end
+
+  def get_reviews
+    parse(service.get("/3/movie/#{@movie_id}/reviews"))
   end
 end
