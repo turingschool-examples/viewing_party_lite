8cdb00f31c17d189dcd5d88e9ed41b94f15817aa
J Seymour <JustJakeSeymour@gmail.com>
2023-02-02 20:44:46 -0700

    initial webmock & vcr set up with test

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe MovieFacade do
   describe 'class methods' do
     describe '.top_rated_movies' do
       it 'returns an array of top rated movie objects' do
-        expect(MovieFacade.top_rated_movies).to all be_a(Movie)
-        expect(MovieFacade.top_rated_movies.length).to eq(20)
+        VCR.use_cassette "top_rated" do
+          expect(MovieFacade.top_rated_movies).to all be_a(Movie)
+          expect(MovieFacade.top_rated_movies.length).to eq(20)
+        end
       end
     end
 
