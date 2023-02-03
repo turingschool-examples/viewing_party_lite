402d53b20d0d72fbed54439d4c8566894bc30a2d
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-03 08:10:37 -0800

    feat: cleaned up edge cases for search results

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -5,6 +5,10 @@ class UserMoviesController < ApplicationController
       @top_rated = MovieFacade.top_rated
     elsif params[:search] && params[:search] != (nil || "")
       @search_results = MovieFacade.movie_search(params[:search])
+      if @search_results == [] 
+        redirect_to "/users/#{@user.id}/discover"
+        flash.alert = "No Results Found"
+      end
     else
       redirect_to "/users/#{@user.id}/discover"
       flash.alert = "No Results Found"
