a5b0c2485c10d1949c66eef9832da1bc23cbee2e
Lacey Weaver <jlweave89@gmail.com>
2023-02-02 21:55:13 -0700

    Add start of last story

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -26,4 +26,41 @@ RSpec.describe "User Show Page", type: :feature do
     end
   end
 
+  describe "when I visit the users dashboard" do
+    before (:each) do
+      visit user_path(@user1.id)
+      click_button("Discover Movies")
+      click_button("Find Top Rated Movies")
+      click_link("The Godfather")
+      click_button("Create Viewing Party for The Godfather")
+      fill_in :duration, with: 186
+      select 2023, from: :_date_1i
+      select "March", from: :_date_2i
+      select "20", from: :_date_3i
+      select 12, from: :_time_4i
+      select 24, from: :_time_5i
+      check @user2.name
+      click_button "Create Party"
+    end
+
+    xit "should show a movie image" do
+      within"#viewing_parties" do
+        expect(page).to have_css(HTML)
+      end
+    end
+
+    it "should show movie title which links to the movie show page" do
+      within"#The Godfather" do
+        expect(page).to have_content("The Godfather")
+      end
+    end
+
+    it "shows the date and time of the event"
+
+    it "shows the user is the host for the party"
+
+    it "shows list of friends invited to viewing party"
+
+  end
+
 end
