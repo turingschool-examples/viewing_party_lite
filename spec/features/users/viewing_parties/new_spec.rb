20345a15d70196fc81c8a5dff9e85c3b921181f1
Kelsie Porter <krporter22@gmail.com>
2023-02-02 20:40:10 -0700

    Add sad path tests

--- spec/features/users/viewing_parties/new_spec.rb
+++ spec/features/users/viewing_parties/new_spec.rb
@@ -43,14 +43,51 @@ RSpec.describe 'New Viewing Party Page' do
         page.check(@user2.display_name)
         page.check(@user3.display_name)
         click_button "Create Party"
-  
-        expect(current_path).to eq(user_path(@user.id))
       end
+
+      expect(current_path).to eq(user_path(@user.id))
     end
   end
 
   describe 'New viewing party form - sad path' do 
+    it 'should not create a new viewing party if the form is not completely filled out' do 
+      user1 = User.create(name: "River", email: "river@example.com")
+      user2 = User.create(name: "Bodi", email: "bodi@example.com")
+      user3 = User.create(name: "Dean", email: "dean@example.com")
 
+      within(".new_viewing_party_form") do 
+        expect(page).to have_field("Movie Title", with: "American Beauty", disabled: true)
+
+        fill_in("Duration of Party", with: 10)
+        select("2025", from: "[date(1i)]")
+        select("March", from: "[date(2i)]")
+        select("5", from: "[date(3i)]")
+        select("20", from: "[start_time(4i)]")
+        select("00", from: "[start_time(5i)]")
+        page.check(@user2.display_name)
+        page.check(@user3.display_name)
+        click_button "Create Party"
+      end
+
+      expect(current_path).to eq(new_user_movie_viewing_party_path(@user.id, @movie_id))
+      expect(page).to have_content("Viewing Party duration must be greater than or equal to movie runtime which is 122")
+
+      within(".new_viewing_party_form") do 
+        expect(page).to have_field("Movie Title", with: "American Beauty", disabled: true)
+
+        fill_in("Duration of Party", with: "")
+        select("2025", from: "[date(1i)]")
+        select("March", from: "[date(2i)]")
+        select("5", from: "[date(3i)]")
+        select("20", from: "[start_time(4i)]")
+        select("00", from: "[start_time(5i)]")
+        page.check(@user2.display_name)
+        page.check(@user3.display_name)
+        click_button "Create Party"
+      end
+
+      expect(current_path).to eq(new_user_movie_viewing_party_path(@user.id, @movie_id))
+      expect(page).to have_content("Viewing Party duration must be greater than or equal to movie runtime which is 122")
+    end
   end
-
 end
