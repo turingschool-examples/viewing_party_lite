4ba8ee27d6791f1144bbe709cf90f0eea754f0c9
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-02 17:28:02 -0800

    test: Add test for movie credits facade

--- spec/facade/movies_facade_spec.rb
+++ spec/facade/movies_facade_spec.rb
@@ -5,11 +5,14 @@ RSpec.describe MoviesFacade do
     stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_DB_KEY']}")
     .to_return(status: 200, body: File.read('./spec/fixtures/top_rated_movies_response.json'), headers: {})
 
-  stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_DB_KEY']}")
-    .to_return(status: 200, body: File.read('./spec/fixtures/discover_movies_response.json'), headers: {})
+    stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIE_DB_KEY']}")
+      .to_return(status: 200, body: File.read('./spec/fixtures/discover_movies_response.json'), headers: {})
   
-  stub_request(:get, "https://api.themoviedb.org/3/movie/497?api_key=#{ENV['MOVIE_DB_KEY']}")
-    .to_return(status: 200, body: File.read('./spec/fixtures/green_mile/details_response.json'), headers: {})
+    stub_request(:get, "https://api.themoviedb.org/3/movie/497?api_key=#{ENV['MOVIE_DB_KEY']}")
+      .to_return(status: 200, body: File.read('./spec/fixtures/green_mile/details_response.json'), headers: {})
+
+    stub_request(:get, "https://api.themoviedb.org/3/movie/497/credits?api_key=#{ENV['MOVIE_DB_KEY']}")
+      .to_return(status: 200, body: File.read('./spec/fixtures/green_mile/credits_response.json'), headers: {})
   end
 
   it 'can return the collection of movie result objects' do 
@@ -34,5 +37,11 @@ RSpec.describe MoviesFacade do
     movie = MoviesFacade.movie_details(497)
 
     expect(movie).to be_an_instance_of(MovieDetail)
+  end
+
+  it 'can return the movie credit object' do 
+    movie = MoviesFacade.movie_credits(497)
+
+    expect(movie).to be_an_instance_of(MovieCredit)
   end
 end
