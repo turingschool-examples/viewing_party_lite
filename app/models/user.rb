dda9e5c3b8338f1748ef01463b55fe74b7138f2f
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-03 10:18:18 -0600

    test and add user model all_except method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   validates_presence_of :name
   validates :email, presence: true, uniqueness: true
 
+  def self.all_except(user)
+    where.not(id: user)
+  end
+
+  def name_and_email
+    "#{name} (#{email})"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
   end
+
+  describe 'class methods' do
+    describe '.all_except' do
+      it 'returns all users except the one specified' do
+        user1,user2,user3 = create_list(:user, 3)
+
+        expect(User.all_except(user1)).to eq([user2,user3])
+      end
+    end
+  end
 end
