e6783c1f97994ad366675aad4d81219152321c89
J Seymour <JustJakeSeymour@gmail.com>
2023-02-03 10:15:36 -0700

    viewing party associations and validations spec
    Co-authored-by: Nigel AM <nigel.aungmyint@gmail.com>

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,3 +1,11 @@
 class ViewingParty < ApplicationRecord
   belongs_to :user
+  has_many :viewing_party_users
+  has_many :users, through: :viewing_party_users
+
+  validates_presence_of :user_id
+  validates :movie_id, presence: true, numericality: true 
+  validates_presence_of :day 
+  validates_presence_of :start_time 
+  validates :duration, presence: true, numericality: true
 end
---
+++ spec/models/viewing_party_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe ViewingParty, type: :model do
+  describe 'associations' do
+    it { should belong_to :user }
+    it { should have_many :viewing_party_users }
+    it { should have_many(:users).through(:viewing_party_users) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :movie_id }
+    it { should validate_numericality_of :movie_id }
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :day }
+    it { should validate_presence_of :start_time }
+    it { should validate_presence_of :duration }
+    it { should validate_numericality_of :duration }
+
+    # how to ensure duration is > movie.runtime
+  end
+end
