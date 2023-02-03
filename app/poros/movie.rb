f84681051651712d0e1181f9408424861d4fad8d
J Seymour <JustJakeSeymour@gmail.com>
2023-02-02 17:45:44 -0700

    build movie poros to detailed spec
    Co-authored-by: Nigel AM <nigel.aungmyint@gmail.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,11 +1,38 @@
 class Movie
   attr_reader :id,
               :title,
-              :vote_average
+              :vote_average,
+              :runtime,
+              :overview
 
   def initialize(data)
     @id = data[:id]
     @title = data[:title]
     @vote_average = data[:vote_average]
+    @runtime = data[:runtime]
+    @genres = data[:genres]
+    @credits = data[:credits]
+    @overview = data[:overview]
+    @reviews = data[:reviews]
+  end
+
+  def genres
+    @genres.map do |genre|
+      genre[:name]
+    end
+  end
+
+  def cast
+    return unless @credits
+      @credits[:cast][0..9].map do |cast|
+        { name: cast[:name], character: cast[:character] }
+      end
+  end
+
+  def reviews
+    return unless @reviews
+      @reviews[:results].map do |review|
+        { author: review[:author], content: review[:content] }
+      end
   end
 end
