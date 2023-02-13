92ae8792f3e6f1772f8504e52bf43980883c3dec
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-13 13:46:20 -0700

    fix: Sad path errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :email, :password, :password_confirmation)
+    params.permit(:name, :email.downcase, :password, :password_confirmation)
   end
 
   def validate_passwords
