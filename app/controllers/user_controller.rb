2ca607501f577e7eb0b32ceb80e21d2031c7a363
Brian Hayes <Brian.hayes1200@gmail.com>
2023-03-21 17:56:48 -0600

    Feat: add functionality to display a users viewing parties on their dashboard

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,5 +2,6 @@ class UserController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @viewing_parties = @user.viewing_parties
   end
 end
--- spec/features/user/dashboard_spec.rb
+++ spec/features/user/dashboard_spec.rb
@@ -3,11 +3,36 @@ require "rails_helper"
 RSpec.describe "User Dashboard" do 
   before :each do
     @user_1 = create(:user)
-
+    @party1 = create(:viewing_party)
+    @party2 = create(:viewing_party)
+    create(:user_party, user: @user_1, viewing_party: @party1, host: false)
+    create(:user_party, user: @user_1, viewing_party: @party2, host: true)
+    
     visit "users/#{@user_1.id}"
   end
 
   it "will have the user's name" do 
     expect(page).to have_content(@user_1.name)
   end 
+
+  # it 'will have a button to Discover Movies' do 
+  #   expect(page).to have_button("Discover Movies")
+
+  #   click_on "Discover Movies"
+
+  #   expect(current_path).to eq("users/:id/discover")
+  # end
+
+  it 'will have a section that lists viewing parties' do 
+    within "#party#{@party1.id}" do 
+      expect(page).to have_content("Invited")
+      expect(page).to have_content(Date.today)
+      expect(page).to have_content(Time.now.strftime("%I:%M:%S"))
+    end 
+    
+    within "#party#{@party2.id}" do 
+      expect(page).to have_content("Hosting")
+      expect(page).to have_content(Time.now.strftime("%I:%M:%S"))
+    end
+  end
 end
